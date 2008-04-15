ce0282f5eeec342d9e754cbb20a344ea93a3d197
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-04-15 11:19:57 +0000

    title in mashup.py.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@244 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonDataSource/src/mashup.py
+++ JythonDataSource/src/mashup.py
@@ -1,5 +1,6 @@
 ds1= getDataSet( 'http://cdaweb.gsfc.nasa.gov/istp_public/data/ace/swe/2007/ac_k0_swe_20070105_v01.cdf?He_ratio' )
 ds2= getDataSet( 'http://cdaweb.gsfc.nasa.gov/istp_public/data/ace/swe/2007/ac_k0_swe_20070105_v01.cdf?Np' )
 He= ds2 * ds1  
+He.putProperty( 'TITLE', 'Ace SWE Helium Density' )  # This doesn't show up because there is no metadata model.
 
 
