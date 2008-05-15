e2ce84e35831837dc6a8a0e1ff86260060ebb995
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-05-15 14:15:59 +0000

    move jython scripts to "scripts"
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@322 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/jumbojar.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+rm -r temp
+mkdir temp; cd temp
+
+for i in  ../dist/lib/*.jar ; do
+   echo $i
+   name=${i}
+   name=`basename $name`
+   mkdir $name; unzip -q -d $name $i
+done
+echo "done unzip libs"
+
+rm -r ../jumbojar
+mkdir ../jumbojar
+
+
+# add all the classes, assuming no conflicts
+echo "copy classes"
+for i in *; do
+   echo $i
+   cp -r $i/* ../jumbojar
+done
+
+
+# special handling of the META-INF stuff.
+
+file=org.virbo.datasource.DataSourceFactory
+rm ../jumbojar/META-INF/$file
+touch ../jumbojar/META-INF/$file
+for i in `find . -name $file | xargs` ; do
+   cat $i >> ../jumbojar/META-INF/$file
+done
+
+file=org.virbo.datasource.DataSourceFactory.extensions
+rm ../jumbojar/META-INF/$file
+touch ../jumbojar/META-INF/$file
+for i in `find . -name $file | xargs` ; do
+   cat $i >> ../jumbojar/META-INF/$file
+done
+
+file=org.virbo.datasource.DataSourceFactory.mimeTypes
+rm ../jumbojar/META-INF/$file
+touch ../jumbojar/META-INF/$file
+for i in `find . -name $file | xargs` ; do
+   cat $i >> ../jumbojar/META-INF/$file
+done
+
+# VirboAutoplot is the application, copy it last so it's META-INF stuff is used.
+unzip -o -d ../jumbojar ../dist/*.jar
+
+cd ../jumbojar
+zip -r ../jumbojar.jar *
+
