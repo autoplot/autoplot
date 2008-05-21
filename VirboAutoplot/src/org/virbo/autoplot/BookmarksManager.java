56a713be5cc32f48acf14cb2f9511c3c1030a9c8
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-05-21 15:48:01 +0000

    demos.xml bookmarks file moved.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@335 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/BookmarksManager.java
+++ VirboAutoplot/src/org/virbo/autoplot/BookmarksManager.java
@@ -318,7 +318,7 @@ public class BookmarksManager extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
     
     private void resetToDefaultMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resetToDefaultMenuItemActionPerformed
-        String surl= System.getProperty("autoplot.default.bookmarks","http://www.cottagesystems.com/virbo/apps/autoplot/bookmarks.xml");
+        String surl= System.getProperty("autoplot.default.bookmarks","http://www.autoplot.org/data/demos.xml");
         int r= JOptionPane.showConfirmDialog( this, "Reset your bookmarks to "+surl+"?" );
         if ( r==JOptionPane.OK_OPTION ) {
             try {
