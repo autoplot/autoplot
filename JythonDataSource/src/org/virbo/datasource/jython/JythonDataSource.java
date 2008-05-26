a4b08481c80a7e39c132929944ea544aad6ae03f
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-05-26 12:43:30 +0000

    improve error feedback.  bugfix when expr like func(smooth=1) used, equal is interpreted as params string =.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@357 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonDataSource/src/org/virbo/datasource/jython/JythonDataSource.java
+++ JythonDataSource/src/org/virbo/datasource/jython/JythonDataSource.java
@@ -2,19 +2,21 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.virbo.datasource.jython;
 
+import org.virbo.datasource.jython.PyQDataSetAdapter;
 import java.io.FileInputStream;
 import java.net.URL;
 import org.das2.util.monitor.NullProgressMonitor;
 import org.das2.util.monitor.ProgressMonitor;
 import org.python.core.Py;
+import org.python.core.PyException;
 import org.python.core.PyList;
 import org.python.core.PyObject;
 import org.python.util.PythonInterpreter;
 import org.virbo.dataset.QDataSet;
 import org.virbo.datasource.AbstractDataSource;
+import org.virbo.datasource.jython.JythonOps;
 
 /**
  *
@@ -22,33 +24,48 @@ import org.virbo.datasource.AbstractDataSource;
  */
 public class JythonDataSource extends AbstractDataSource {
 
-    public JythonDataSource( URL url ) {
+    public JythonDataSource(URL url) {
         super(url);
     }
-    
+
     @Override
     public QDataSet getDataSet(ProgressMonitor mon) throws Exception {
-        PythonInterpreter interp =  new PythonInterpreter();
-        Py.getAdapter().addPostClass( new PyQDataSetAdapter() );
-        
-        interp.set( "monitor", mon );
-        interp.execfile( JythonDataSource.class.getResource("imports.py").openStream(), "imports.py" );
+        PythonInterpreter interp = new PythonInterpreter();
+        Py.getAdapter().addPostClass(new PyQDataSetAdapter());
 
-        interp.execfile( new FileInputStream( super.getFile( new NullProgressMonitor() ) ) );
-        
-        String expr= super.params.get("arg_0");
-        if ( expr==null ) expr="result";
-        
-        PyObject result= interp.eval( expr );
-        
-        QDataSet res;
-        if ( result instanceof PyList ) {
-            res=   PyQDataSetAdapter.adaptList((PyList)result);
-        } else {
-            res= (QDataSet) result.__tojava__( QDataSet.class );
+        interp.set("monitor", mon);
+        interp.execfile(JythonDataSource.class.getResource("imports.py").openStream(), "imports.py");
+
+        PyException causedBy=null;
+        try {
             
-        }
-        return res;
-    }
+            try {
+                interp.execfile(new FileInputStream(super.getFile(new NullProgressMonitor())));
+            } catch ( PyException ex ) {
+                causedBy= ex;
+            }
+            String expr= super.getURL();
+            int i= expr.indexOf("?");
+            if ( i>-1 ) {
+                expr= expr.substring(i+1);
+            } else {
+                expr= "result";
+            }
+            PyObject result = interp.eval(expr);
 
+            QDataSet res;
+            if (result instanceof PyList) {
+                res = JythonOps.coerce((PyList) result);
+            } else {
+                res = (QDataSet) result.__tojava__(QDataSet.class);
+            }
+
+            return res;
+            
+        } catch (PyException ex) {
+            
+            throw new RuntimeException("PyException: " + ex + "\ncaused by:\n"+causedBy );
+        }
+
+    }
 }
