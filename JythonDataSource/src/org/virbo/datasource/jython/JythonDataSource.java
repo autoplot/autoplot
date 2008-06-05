f84fbbf8b03e8278d3e1c1439d74852b383c7d14
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-06-05 15:28:07 +0000

    caching. better handling of exceptions.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@375 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonDataSource/src/org/virbo/datasource/jython/JythonDataSource.java
+++ JythonDataSource/src/org/virbo/datasource/jython/JythonDataSource.java
@@ -4,9 +4,14 @@
  */
 package org.virbo.datasource.jython;
 
+import java.io.File;
 import org.virbo.jythonsupport.PyQDataSetAdapter;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Date;
+import java.util.Map;
 import org.das2.util.monitor.NullProgressMonitor;
 import org.das2.util.monitor.ProgressMonitor;
 import org.python.core.Py;
@@ -16,41 +21,65 @@ import org.python.core.PyObject;
 import org.python.util.PythonInterpreter;
 import org.virbo.dataset.QDataSet;
 import org.virbo.datasource.AbstractDataSource;
+import org.virbo.datasource.DataSetURL;
+import org.virbo.datasource.capability.Caching;
 import org.virbo.jythonsupport.JythonOps;
+import org.virbo.jythonsupport.Ops;
 
 /**
  *
  * @author jbf
  */
-public class JythonDataSource extends AbstractDataSource {
+public class JythonDataSource extends AbstractDataSource implements Caching {
 
     public JythonDataSource(URL url) {
         super(url);
+        addCability(Caching.class, this);
     }
 
     @Override
-    public QDataSet getDataSet(ProgressMonitor mon) throws Exception {
-        PythonInterpreter interp = new PythonInterpreter();
-        Py.getAdapter().addPostClass(new PyQDataSetAdapter());
-
-        interp.set("monitor", mon);
-        interp.execfile(JythonDataSource.class.getResource("imports.py").openStream(), "imports.py");
-
-        PyException causedBy=null;
+    public synchronized QDataSet getDataSet(ProgressMonitor mon) throws Exception {
+        PyException causedBy = null;
         try {
-            
-            try {
-                interp.execfile(new FileInputStream(super.getFile(new NullProgressMonitor())));
-            } catch ( PyException ex ) {
-                causedBy= ex;
+            if (interp == null) {
+                interp = new PythonInterpreter();
+                Py.getAdapter().addPostClass(new PyQDataSetAdapter());
+
+                interp.set("monitor", mon);
+                interp.execfile(Ops.class.getResource("imports.py").openStream(), "imports.py");
+
+                // interp.exec("def getParam( x, def ):\n  return params.has_key(x) ? params[x] : def\n\n");
+
+                interp.exec("params=dict()");
+                for (String s : params.keySet()) {
+                    if (!s.equals("arg_0")) {
+                        interp.exec("params['" + s + "']=" + params.get(s));
+                    }
+                }
+
+                try {
+                    interp.execfile(new FileInputStream(super.getFile(new NullProgressMonitor())));
+                } catch (PyException ex) {
+                    causedBy = ex;
+                    ex.printStackTrace();
+                }
+
+                if (mon.isCancelled()) {
+                    throw new IllegalArgumentException("monitor cancel not supported");
+                }
+                if (causedBy == null) {
+                    cacheDate = resourceDate(this.url);
+                    cacheUrl = cacheUrl(this.url);
+                }
             }
-            String expr= super.getURL();
-            int i= expr.indexOf("?");
-            if ( i>-1 ) {
-                expr= expr.substring(i+1);
-            } else {
-                expr= "result";
+
+            String expr = params.get("arg_0");
+
+            if (expr == null) {
+                expr = "result";
             }
+
+
             PyObject result = interp.eval(expr);
 
             QDataSet res;
@@ -60,12 +89,72 @@ public class JythonDataSource extends AbstractDataSource {
                 res = (QDataSet) result.__tojava__(QDataSet.class);
             }
 
+            if (causedBy != null) {
+                interp = null;
+                cacheUrl = null;
+                cacheDate = null;
+            }
+
             return res;
-            
+
         } catch (PyException ex) {
-            
-            throw new RuntimeException("PyException: " + ex + "\ncaused by:\n"+causedBy );
+            String msg = "PyException: " + ex;
+            if (causedBy != null) {
+                msg += "\ncaused by:\n" + causedBy;
+            }
+            interp = null;
+            cacheUrl = null;
+            cacheDate = null;
+
+            throw new RuntimeException(msg);
         }
+    }
+    PythonInterpreter interp = null;
+
+    private String cacheUrl(URL url) {
+        DataSetURL.URLSplit split = DataSetURL.parse(url.toString());
+        Map<String, String> params = DataSetURL.parseParams(split.params);
+        params.remove("arg_0");
+        split.params = DataSetURL.formatParams(params);
+        return DataSetURL.format(split);
+    }
+
+    private Date resourceDate(URL url) throws IOException {
+        File src = DataSetURL.getFile(url, new NullProgressMonitor());
+        return new Date(src.lastModified());
+    }
+    Date cacheDate = null;
+    String cacheUrl = null;
+
+    private boolean useCache(URL url) {
+        try {
+            if ((cacheDate != null && !resourceDate(url).after(cacheDate)) && (cacheUrl != null && cacheUrl.equals(cacheUrl(url)))) {
+                return true;
+            }
+            return false;
+        } catch (IOException ex) {
+            return false;
+        }
+    }
+
+    public boolean satisfies(String surl) {
+        try {
+            return useCache(new URL(surl));
+        } catch (IOException ex) {
+            return false;
+        }
+    }
+
+    public void resetURL(String surl) {
+        try {
+            this.url = new URL(surl);
+            DataSetURL.URLSplit split = DataSetURL.parse(url.toString());
+            params = DataSetURL.parseParams(split.params);
+            resourceURL = new URL(split.file);
+        } catch (MalformedURLException ex) {
+            throw new RuntimeException(ex);
+        }
+
 
     }
 }
