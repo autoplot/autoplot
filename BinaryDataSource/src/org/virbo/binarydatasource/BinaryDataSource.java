0ec4bd0a322e79eb9dac7f612c8bf212b19d1d54
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-06-08 16:20:01 +0000

    various initial coding changes. byte is default type.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@412 0c594bb0-8543-0410-a25e-d8ec440261db

--- BinaryDataSource/src/org/virbo/binarydatasource/BinaryDataSource.java
+++ BinaryDataSource/src/org/virbo/binarydatasource/BinaryDataSource.java
@@ -18,6 +18,7 @@ import java.nio.ShortBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
 import org.das2.util.monitor.ProgressMonitor;
+import org.virbo.dataset.IndexGenDataSet;
 import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QDataSet;
 import org.virbo.datasource.AbstractDataSource;
@@ -29,114 +30,128 @@ import org.virbo.datasource.DataSetURL;
  */
 public class BinaryDataSource extends AbstractDataSource {
 
-    URL url;
+    public final static String DOUBLE = "double";
+    public final static String FLOAT = "float";
+    public final static String LONG = "long";
+    public final static String INT = "int";
+    public final static String SHORT = "short";
+    public final static String BYTE = "byte";
 
-    public final static String DOUBLE= "double";
-    public final static String FLOAT= "float";
-    public final static String LONG= "long";
-    public final static String INT= "int";
-    public final static String SHORT= "short";
-    public final static String BYTE= "byte";
-    
     public BinaryDataSource(URL url) {
         super(url);
-        this.url = url;
     }
 
-    private int getIntParameter( String name, int deflt ) {
-        String sval = params.get( name );
+    private int getIntParameter(String name, int deflt) {
+        String sval = params.get(name);
         int result = sval == null ? deflt : Integer.parseInt(sval);
         return result;
     }
 
-    private String getParameter( String name, String deflt ) {
-        String sval = params.get( name );
+    private String getParameter(String name, String deflt) {
+        String sval = params.get(name);
         String result = sval == null ? deflt : sval;
         return result;
     }
-        
-    private int byteCount( String type ) {
-if ( type.equals(DOUBLE) ) {
+
+    private int byteCount(String type) {
+        if (type.equals(DOUBLE)) {
             return 8;
-        } else if ( type.equals(FLOAT) ) {
+        } else if (type.equals(FLOAT)) {
             return 4;
-        } else if ( type.equals(LONG) ) {
+        } else if (type.equals(LONG)) {
             return 8;
-        } else if ( type.equals(INT) ) {
+        } else if (type.equals(INT)) {
             return 4;
-        } else if ( type.equals(SHORT) ) {
+        } else if (type.equals(SHORT)) {
             return 2;
-        } else if ( type.equals(BYTE) ) {
+        } else if (type.equals(BYTE)) {
             return 1;
         } else {
-            throw new IllegalArgumentException("bad type: "+type);
-        }        
+            throw new IllegalArgumentException("bad type: " + type);
+        }
     }
-    
+
     public QDataSet getDataSet(ProgressMonitor mon) throws Exception {
-        
+
         File f = DataSetURL.getFile(url, mon);
-        
+
         FileChannel fc = new FileInputStream(f).getChannel();
 
-        int offset = getIntParameter( "byteoffset", 0 );
+        final int offset = getIntParameter("byteoffset", 0);
 
-        int length = getIntParameter( "bytelength", (((int) f.length()) - offset) );
+        int length = getIntParameter("bytelength", (((int) f.length()) - offset));
 
-        int fieldCount = getIntParameter( "fieldCount", params.get("dep0") == null ? 1 : 2);
-                
+        int fieldCount = getIntParameter("fieldCount", params.get("dep0") == null ? 1 : 2);
+
         ByteBuffer buf = (MappedByteBuffer) fc.map(MapMode.READ_ONLY, offset, length);
 
-        int dep0= getIntParameter( "dep0", -1 );
+        int dep0 = getIntParameter("dep0", -1);
 
         int defltcol;
-        if( dep0==-1 ) {
-            defltcol= 0;
+        if (dep0 == -1) {
+            defltcol = 0;
         } else {
-            if ( dep0>0 ) defltcol= 0; else defltcol=1;
+            if (dep0 > 0) {
+                defltcol = 0;
+            } else {
+                defltcol = 1;
+            }
+        }
+
+        int col = getIntParameter("column", defltcol);
+
+        String columnType = getParameter("type", BYTE );
+
+        String encoding = getParameter("byteOrder", "little");
+        if (encoding.equals("big")) {
+            buf.order(ByteOrder.BIG_ENDIAN);
+        } else {
+            buf.order(ByteOrder.LITTLE_ENDIAN);
         }
         
-        int col = getIntParameter( "column", defltcol );
+        final int recSizeBytes=  byteCount(columnType) * fieldCount;
+        final int recCount= length / recSizeBytes;
+        MutablePropertyDataSet ds = makeDataSet(1, recCount, fieldCount, col, 1, 1, 0, buf, columnType);
 
-        String columnType= getParameter( "type", DOUBLE );
+        if (dep0 > -1) {
+            String dep0Type = getParameter("dep0Type", columnType);
 
-        String encoding= getParameter( "byteOrder", "little" );
-        if ( encoding.equals("big") ) buf.order( ByteOrder.BIG_ENDIAN ); else buf.order( ByteOrder.LITTLE_ENDIAN );
-        
-        MutablePropertyDataSet ds = makeDataSet( 1, length / byteCount(columnType) / fieldCount, fieldCount, col, 1, 1, 0, buf, columnType );
-
-        
-        if (dep0 >-1) {
-            String dep0Type = getParameter( "dep0Type", columnType );
-            
-            QDataSet dep0ds = makeDataSet( 1, length / byteCount(columnType) / fieldCount, fieldCount, dep0, 1, 1, 0, buf, dep0Type );
+            QDataSet dep0ds = makeDataSet(1, recCount, fieldCount, dep0, 1, 1, 0, buf, dep0Type);
+            ds.putProperty(QDataSet.DEPEND_0, dep0ds);
+        } else {
+            IndexGenDataSet dep0ds= new IndexGenDataSet(recCount) {
+                @Override
+                public double value(int i) {
+                    return offset + i * recSizeBytes;
+                }
+            };
+            dep0ds.putProperty( QDataSet.CADENCE, recSizeBytes );
             ds.putProperty(QDataSet.DEPEND_0, dep0ds);
         }
 
         return ds;
     }
 
-    private MutablePropertyDataSet makeDataSet( int rank, int len0, int reclen0, int recoffs0, int len1, int reclen1, int recoffs1, ByteBuffer buf, String type ) {
-        if ( type.equals(DOUBLE) ) {
+    private MutablePropertyDataSet makeDataSet(int rank, int len0, int reclen0, int recoffs0, int len1, int reclen1, int recoffs1, ByteBuffer buf, String type) {
+        if (type.equals(DOUBLE)) {
             DoubleBuffer dbuf = buf.asDoubleBuffer();
-            return new Double( rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, dbuf  );
-        } else if ( type.equals(FLOAT) ) {
-            FloatBuffer fbuf= buf.asFloatBuffer();
-            return new Float( rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, fbuf );
-        } else if ( type.equals(LONG) ) {
-            LongBuffer fbuf= buf.asLongBuffer();
-            return new Long( rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, fbuf );
-        } else if ( type.equals(INT) ) {
-            IntBuffer fbuf= buf.asIntBuffer();
-            return new Int( rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, fbuf );
-        } else if ( type.equals(SHORT) ) {
-            ShortBuffer fbuf= buf.asShortBuffer();
-            return new Short( rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, fbuf );
-        } else if ( type.equals(BYTE) ) {
-            return new Byte( rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, buf );
+            return new Double(rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, dbuf);
+        } else if (type.equals(FLOAT)) {
+            FloatBuffer fbuf = buf.asFloatBuffer();
+            return new Float(rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, fbuf);
+        } else if (type.equals(LONG)) {
+            LongBuffer fbuf = buf.asLongBuffer();
+            return new Long(rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, fbuf);
+        } else if (type.equals(INT)) {
+            IntBuffer fbuf = buf.asIntBuffer();
+            return new Int(rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, fbuf);
+        } else if (type.equals(SHORT)) {
+            ShortBuffer fbuf = buf.asShortBuffer();
+            return new Short(rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, fbuf);
+        } else if (type.equals(BYTE)) {
+            return new Byte(rank, len0, reclen0, recoffs0, len1, reclen1, recoffs1, buf);
         } else {
-            throw new IllegalArgumentException("bad type: "+type);
+            throw new IllegalArgumentException("bad type: " + type);
         }
     }
-
 }
