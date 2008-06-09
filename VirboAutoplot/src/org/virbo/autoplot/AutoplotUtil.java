3a55fc61cd8f42ab78b653049009807dc7d9a2c1
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-06-09 01:14:46 +0000

    improve efficiency of autoranging by rearranging code. look in dataSet returned by DataSource for metadata.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@424 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/AutoplotUtil.java
+++ VirboAutoplot/src/org/virbo/autoplot/AutoplotUtil.java
@@ -8,6 +8,7 @@
  */
 package org.virbo.autoplot;
 
+import edu.uiowa.physics.pw.das.datum.Datum;
 import edu.uiowa.physics.pw.das.datum.DatumRange;
 import edu.uiowa.physics.pw.das.datum.DatumRangeUtil;
 import edu.uiowa.physics.pw.das.datum.Units;
@@ -19,6 +20,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.lang.reflect.Method;
+import java.util.Map;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.xml.parsers.DocumentBuilder;
@@ -37,10 +39,11 @@ import org.xml.sax.SAXException;
  * @author jbf
  */
 public class AutoplotUtil {
-    
+
     private final static Logger log = Logger.getLogger("virbo.autoplot.AutoRangeDescriptor.autoRange");
-    
+
     public static class AutoRangeDescriptor {
+
         DatumRange range;
         boolean log;
         double min;
@@ -48,9 +51,12 @@ public class AutoplotUtil {
         double robustMin;
         double robustMax;
         double median;
-        public String toString() { return ""+range+" "+(log?"log":""); }
+
+        public String toString() {
+            return "" + range + " " + (log ? "log" : "");
+        }
     }
-    
+
     /**
      * return indeces that order the data.  This is a bubble sort currently.
      * @param result an array of indeces indexing data.
@@ -68,7 +74,7 @@ public class AutoplotUtil {
         }
         return result;
     }
-    
+
     /**
      * return the median of three numbers
      */
@@ -86,45 +92,45 @@ public class AutoplotUtil {
         }
         return median;
     }
-    
+
     /**
      * return the min and max of 3-point medians
      * @param ds a rank N dataset.
      * @throws IllegalArgumentException if fewer than three points are found.
      */
     private static double[] median3Range(QDataSet ds) throws IllegalArgumentException {
-        
+
         double[] d = new double[3]; // ordered points, rotating buffer
         int id; // index of the first point.
         int dc = 0; // number of valid points in d.
-        
+
         Units u = (Units) ds.property(QDataSet.UNITS);
-        
+
         int i;
-        
+
         //TODO: use dataset iterator
         DataSetIterator iter = DataSetIterator.create(ds);
-        
-        while ( iter.hasNext() && dc<3 ) {
-            double dd= iter.next();
-            if (!Double.isNaN(dd) && (u == null || !u.isFill(dd) ) ) {
+
+        while (iter.hasNext() && dc < 3) {
+            double dd = iter.next();
+            if (!Double.isNaN(dd) && (u == null || !u.isFill(dd))) {
                 d[dc++] = dd;
             }
         }
-        
+
         // TODO: find a compile-time exception for this error.
         if (dc < 3) {
             throw new IllegalArgumentException("need 3 valid points");
         }
-        
+
         id = 0;
-        
+
         double min = medianOfThree(d);
         double max = medianOfThree(d);
-        while ( iter.hasNext() ) {
+        while (iter.hasNext()) {
             double d1 = iter.next();
-            
-            if ( !Double.isNaN(d1) && (u == null || !u.isFill(d1))) {
+
+            if (!Double.isNaN(d1) && (u == null || !u.isFill(d1))) {
                 d[id++] = d1;
                 if (id == d.length) {
                     id = 0;
@@ -138,25 +144,25 @@ public class AutoplotUtil {
                 }
             }
         }
-        
+
         return new double[]{min, max};
     }
-    
-    public static AutoRangeDescriptor autoRange(QDataSet ds) {
-        
+
+    public static AutoRangeDescriptor autoRange(QDataSet ds, Map properties) {
+
         log.fine("enter autoRange");
-        
+
         Units u = (Units) ds.property(QDataSet.UNITS);
         if (u == null) {
             u = Units.dimensionless;
         }
-        
+
         AutoRangeDescriptor result = new AutoRangeDescriptor();
-        
+
         double[] dd;
-        
+
         boolean mono = Boolean.TRUE.equals(ds.property(QDataSet.MONOTONIC));
-        
+
         if (mono) {
             double cadence = DataSetUtil.guessCadence(ds);
             if (ds.length() > 1) {
@@ -171,9 +177,9 @@ public class AutoplotUtil {
         } else {
             // find min and max of three-point medians
             try {
-                MomentDescriptor moment= moment( ds );
-                dd= robustRange( ds, moment );
-               // dd = median3Range(ds);
+                MomentDescriptor moment = moment(ds);
+                dd = robustRange(ds, moment);
+            // dd = median3Range(ds);
             } catch (IllegalArgumentException ex) {
                 if (UnitsUtil.isTimeLocation(u)) {
                     dd = new double[]{0, Units.days.createDatum(1).doubleValue(u.getOffsetUnits())};
@@ -182,7 +188,7 @@ public class AutoplotUtil {
                 }
             }
         }
-        
+
         double median;
         int total;
         if (dd[0] == dd[1]) {
@@ -198,7 +204,7 @@ public class AutoplotUtil {
             total = ds.length(); // only non-zero is checked.
         } else {
             // find the median by looking at the histogram.  If the dataset should be log, then the data will bunch up in the lowest bins.
-            QDataSet hist = DataSetOps.histogram(ds, dd[0], dd[1]+(dd[1]-dd[0])*0.01, (dd[1] - dd[0]) / 100);
+            QDataSet hist = DataSetOps.histogram(ds, dd[0], dd[1] + (dd[1] - dd[0]) * 0.01, (dd[1] - dd[0]) / 100);
             total = 0;
             for (int i = 0; i < hist.length(); i++) {
                 total += hist.value(i);
@@ -213,19 +219,19 @@ public class AutoplotUtil {
                 }
             }
         }
-        
-        if (total < 3 ) {
+
+        if (total < 3) {
             result.min = dd[0];
             result.max = dd[1];
             result.range = DatumRange.newDatumRange(dd[0], dd[1], u);
-            
+
         } else {
             result.min = dd[0];
             result.max = dd[1];
             result.median = median;
             result.robustMin = dd[0];
             result.robustMax = dd[1];
-            
+
             // lin/log logic: in which space is ( median - min5 ) more equal to ( max5 - median )?  Also, max5 / min5 > 1e3
             double clin = (result.robustMax - result.median) / (result.median - result.robustMin);
             if (clin > 1.0) {
@@ -235,12 +241,12 @@ public class AutoplotUtil {
             if (clog > 1.0) {
                 clog = 1 / clog;
             }
-            
+
             if (clog > clin && result.robustMax / result.robustMin > 1e2) {
                 result.log = true;
             }
-            
-            if ( UnitsUtil.isRatioMeasurement(u) || UnitsUtil.isIntervalMeasurement(u) ) {
+
+            if (UnitsUtil.isRatioMeasurement(u) || UnitsUtil.isIntervalMeasurement(u)) {
                 if (result.log) {
                     result.range = DatumRange.newDatumRange(DasMath.exp10(Math.floor(DasMath.log10(result.robustMin))),
                             DasMath.exp10(Math.ceil(DasMath.log10(result.robustMax))), u);
@@ -250,15 +256,40 @@ public class AutoplotUtil {
             } else {
                 result.range = DatumRange.newDatumRange(result.robustMin, result.robustMax, u);
             }
-            
+
         }
-        
+
+        if (properties != null) {
+            String log = (String) properties.get(QDataSet.SCALE_TYPE);
+            if (log != null) {
+                result.log = log.equals("log");
+            }
+            DatumRange range = (DatumRange) properties.get(QDataSet.TYPICAL_RANGE);
+
+            // see if the typical range is consistent with range seen.  If the
+            // typical range won't hide the data's structure, then use it.
+            if (range != null) {
+                double d1, d2;
+                if (result.log) {
+                    Datum dd1= result.range.min().ge( range.min() ) ? result.range.min() : range.min();
+                    Datum dd2= result.range.max().ge( range.min() ) ? result.range.max() : range.min();
+                    d1 = DatumRangeUtil.normalizeLog(range, dd1 );
+                    d2 = DatumRangeUtil.normalizeLog(range, dd2 );
+                } else {
+                    d1 = DatumRangeUtil.normalize(range, result.range.min());
+                    d2 = DatumRangeUtil.normalize(range, result.range.max());
+                }
+                if ( d2-d1 > 0.1 ) {
+                    result.range = range;
+                }
+            }
+        }
+
         log.fine("exit autoRange");
-        
-        
+
         return result;
     }
-    
+
     /**
      * open the URL in a browser.   Borrowed from http://www.centerkey.com/java/browser/.
      */
@@ -290,72 +321,72 @@ public class AutoplotUtil {
             JOptionPane.showMessageDialog(null, errMsg + ":\n" + e.getLocalizedMessage());
         }
     }
-    
+
     public static class MomentDescriptor {
-        
+
         double[] moment;
         Units units;
         int rank;
         int invalidCount;
         int validCount;
     }
-    
+
     /**
      * performs the moment (mean,variance,etc) on the dataset.
      * @returns MomentDescriptor
      */
     public static MomentDescriptor moment(QDataSet ds) {
-        
+
         MomentDescriptor result = new MomentDescriptor();
-        
+
         result.rank = ds.rank();
         result.moment = new double[2];
-        
+
         Units u = (Units) ds.property(QDataSet.UNITS);
         if (u == null) {
             u = Units.dimensionless;
         }
         int validCount = 0;
         int invalidCount = 0;
-        
+
         double approxMean = 0.;
-        
-        DataSetIterator iter= DataSetIterator.create( ds );
-        while ( iter.hasNext() ) {
+
+        DataSetIterator iter = DataSetIterator.create(ds);
+        while (iter.hasNext()) {
             double d = iter.next();
-            if ( ! u.isValid(d) ) {
+            if (!u.isValid(d)) {
                 invalidCount++;
             } else {
                 validCount++;
                 approxMean += d;
             }
         }
-        
+
         if (validCount > 0) {
             approxMean /= validCount; // approximate--suseptible to number error.
         }
-        
+
         result.invalidCount = invalidCount;
         result.validCount = validCount;
-        
+
         double mean = 0;
         double stddev = 0;
-        
+
         if (validCount > 0) {
-            iter= DataSetIterator.create( ds );
-            while ( iter.hasNext() ) {
+            iter = DataSetIterator.create(ds);
+            while (iter.hasNext()) {
                 double d = iter.next();
-                if ( u.isValid(d) ) {
+                if (u.isValid(d)) {
                     mean += (d - approxMean);
                     stddev += Math.pow(d - approxMean, 2);
                 }
             }
-            
+
             mean /= validCount;
             mean += approxMean;
-            
+
             result.moment[0] = mean;
-            
+
             if (validCount > 1) {
                 stddev /= (validCount - 1); // this will be very close to result, even though correction should be made since approxMean != mean.
                 stddev = Math.sqrt(stddev);
@@ -363,20 +394,20 @@ public class AutoplotUtil {
             } else {
                 result.moment[1] = u.getFillDouble();
             }
-            
+
         } else {
             result.moment[0] = u.getFillDouble();
         }
-        
+
         return result;
     }
-    
+
     public static Document readDoc(InputStream is) throws SAXException, IOException, ParserConfigurationException {
-         DocumentBuilder builder;
-         builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-         InputSource source = new InputSource(new InputStreamReader(is));
-         Document document = builder.parse(source);
-        
+        DocumentBuilder builder;
+        builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        InputSource source = new InputSource(new InputStreamReader(is));
+        Document document = builder.parse(source);
+
 //        DOMParser parser = new org.apache.xerces.parsers.DOMParser();
 //        
 //        Reader in = new BufferedReader(new InputStreamReader(is));
@@ -388,33 +419,32 @@ public class AutoplotUtil {
         return document;
     }
 
-    
     private static double[] robustRange(QDataSet ds, MomentDescriptor moment) throws IllegalArgumentException {
-        double min= Double.POSITIVE_INFINITY, max= Double.NEGATIVE_INFINITY;
-        
+        double min = Double.POSITIVE_INFINITY, max = Double.NEGATIVE_INFINITY;
+
         Units u = (Units) ds.property(QDataSet.UNITS);
         if (u == null) {
             u = Units.dimensionless;
         }
-        
-        DataSetIterator iter= DataSetIterator.create(ds);
-        
+
+        DataSetIterator iter = DataSetIterator.create(ds);
+
         // four std dev's from the mean.
-        double stdmin= moment.moment[0] - moment.moment[1]* 4;
-        double stdmax= moment.moment[0] + moment.moment[1]* 4;
-        
-        while ( iter.hasNext() ) {
-            double d= iter.next();
-            if ( u.isValid(d) && d >= stdmin && d <= stdmax ) {
-                min= min > d ? d : min;
-                max= max < d ? d : max;
+        double stdmin = moment.moment[0] - moment.moment[1] * 4;
+        double stdmax = moment.moment[0] + moment.moment[1] * 4;
+
+        while (iter.hasNext()) {
+            double d = iter.next();
+            if (u.isValid(d) && d >= stdmin && d <= stdmax) {
+                min = min > d ? d : min;
+                max = max < d ? d : max;
             }
         }
-        
-        if ( min== Double.POSITIVE_INFINITY ) {
+
+        if (min == Double.POSITIVE_INFINITY) {
             throw new IllegalArgumentException("no valid data found");
         } else {
-            return new double[] { min, max };
+            return new double[]{min, max};
         }
     }
 }
