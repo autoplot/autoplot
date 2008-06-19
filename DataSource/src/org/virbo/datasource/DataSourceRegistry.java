b82b61480aa83cc6c893ff326025517379399356
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-06-19 11:32:15 +0000

    register DataSourceFormats.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@497 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/DataSourceRegistry.java
+++ DataSource/src/org/virbo/datasource/DataSourceRegistry.java
@@ -10,7 +10,10 @@ package org.virbo.datasource;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import org.virbo.datasource.datasource.DataSourceFormat;
 
 /**
  *
@@ -21,11 +24,15 @@ public class DataSourceRegistry {
     private static DataSourceRegistry instance;
     HashMap dataSourcesByExt;
     HashMap dataSourcesByMime;
+    HashMap dataSourceFormatByExt;
+    HashMap extToDescription;
 
     /** Creates a new instance of DataSourceRegistry */
     private DataSourceRegistry() {
         dataSourcesByExt = new HashMap();
         dataSourcesByMime = new HashMap();
+        dataSourceFormatByExt= new HashMap();
+        extToDescription= new HashMap();
     }
 
     public static DataSourceRegistry getInstance() {
@@ -33,6 +40,14 @@ public class DataSourceRegistry {
             instance = new DataSourceRegistry();
         }
         return instance;
+    }
+
+    public List<String> getFormatterExtensions() {
+        List<String> result= new ArrayList<String>();
+        for ( Object k: dataSourceFormatByExt.keySet() ) {
+            result.add( (String)k );
+        }
+        return result;
     }
 
     /**
@@ -57,9 +72,20 @@ public class DataSourceRegistry {
      * factory class is given, so that the class is not accessed until first
      * use.
      */
-    public void register(String className, String extension) {
+    public void registerExtension(String className, String extension, String description ) {
         if (extension.indexOf('.') != 0) extension= "."+extension;
         dataSourcesByExt.put(extension, className);
+        if ( description!=null ) extToDescription.put( extension, description );
+    }
+
+    /**
+     * register the data source factory by extension.  The name of the
+     * factory class is given, so that the class is not accessed until first
+     * use.
+     */
+    public void registerFormatter(String className, String extension) {
+        if (extension.indexOf('.') != 0) extension= "."+extension;
+        dataSourceFormatByExt.put(extension, className);
     }
 
     public void registerMimeType(String className, String mimeType) {
@@ -103,6 +129,37 @@ public class DataSourceRegistry {
             result = (DataSourceFactory) o;
         }
         return result;
+    }
+
+    public DataSourceFormat getFormatByExt( String extension ) {
+        if (extension.indexOf('.') != 0) extension= "."+extension;
+        Object o = dataSourceFormatByExt.get(extension);
+        if (o == null) {
+            return null;
+        }
+
+        DataSourceFormat result;
+        if (o instanceof String) {
+            try {
+                Class clas = Class.forName((String) o);
+                Constructor constructor = clas.getDeclaredConstructor(new Class[]{});
+                result = (DataSourceFormat) constructor.newInstance(new Object[]{});
+            } catch (ClassNotFoundException ex) {
+                throw new RuntimeException(ex);
+            } catch (NoSuchMethodException ex) {
+                throw new RuntimeException(ex);
+            } catch (InstantiationException ex) {
+                throw new RuntimeException(ex);
+            } catch (IllegalAccessException ex) {
+                throw new RuntimeException(ex);
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        } else {
+            result = (DataSourceFormat) o;
+        }
+        return result;
+        
     }
 
     public DataSourceFactory getSourceByMime(String mime) {
