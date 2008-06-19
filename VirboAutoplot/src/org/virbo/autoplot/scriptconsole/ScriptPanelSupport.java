819e2d17a0f64db1c2de53bfc06e7013f6b50948
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-06-19 12:13:34 +0000

    model.update() now takes parameter to allow autoranging.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@511 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/scriptconsole/ScriptPanelSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/scriptconsole/ScriptPanelSupport.java
@@ -148,7 +148,7 @@ public class ScriptPanelSupport {
                 if (updateSurl) {
                     model.setDataSourceURL(file.toString());
                 } else {
-                    model.update();
+                    model.update(true);
                 }
             }
 
