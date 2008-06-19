5d49b2789b77331a40f0d6a79d1c5258706a59cc
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-06-19 11:51:05 +0000

    open local chooser has separate file filters for each extension.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@504 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/DataSetSelectorSupport.java
+++ DataSource/src/org/virbo/datasource/DataSetSelectorSupport.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.virbo.datasource;
 
 import java.awt.event.ActionEvent;
@@ -30,67 +29,95 @@ public class DataSetSelectorSupport {
     DataSetSelector ui;
     public final String PREF_LAST_OPEN_FOLDER = "last_open_folder";
     public final String PREF_RECENTLY_OPENED_FILES = "recently_opened_files";
+    private JMenu recentMenu = null;
 
-    private JMenu recentMenu=null;
-    
-    DataSetSelectorSupport( DataSetSelector ui ) {
-        this.ui= ui;
+    DataSetSelectorSupport(DataSetSelector ui) {
+        this.ui = ui;
     }
-    
+
     private static File userHome() {
-        return new File( System.getProperty("user.home") );
+        return new File(System.getProperty("user.home"));
     }
-    
+
     /**
      * return the extension for the filename or url string.
      * @param surl URL string or filename.  
      * @return the extension, with the period. E.g. ".cdf",
      *    or null if an extension is not found.
      */
-    private static String getExt( String surl ) throws IllegalArgumentException {
-        int i= surl.lastIndexOf("?");
-        if ( i==-1 ) i= surl.length();
-        i= surl.lastIndexOf(".",i);
-        if ( i==-1 ) return null; else return surl.substring(i);
+    private static String getExt(String surl) throws IllegalArgumentException {
+        int i = surl.lastIndexOf("?");
+        if (i == -1) {
+            i = surl.length();
+        }
+        i = surl.lastIndexOf(".", i);
+        if (i == -1) {
+            return null;
+        } else {
+            return surl.substring(i);
+        }
     }
-    
+
     public Action openLocalAction() {
         return new AbstractAction("Open File...") {
+
             public void actionPerformed(ActionEvent e) {
-                Preferences prefs= Preferences.userNodeForPackage(DataSetSelectorSupport.class);
-                
-                String currentDirectory= prefs.get(PREF_LAST_OPEN_FOLDER, userHome().toString()  );
-                final HashMap exts= DataSourceRegistry.getInstance().dataSourcesByExt;
-                
-                JFileChooser chooser= new JFileChooser(currentDirectory);
-                
-                String tt= "vap";
-                
-                for ( Object ext: exts.keySet() ) {
-                    tt+= ", "+ ext;
-                }
-                
-                final String extStr= tt;
-                
-                FileFilter ff= new FileFilter() {
+                Preferences prefs = Preferences.userNodeForPackage(DataSetSelectorSupport.class);
+
+                String currentDirectory = prefs.get(PREF_LAST_OPEN_FOLDER, userHome().toString());
+                final HashMap exts = DataSourceRegistry.getInstance().dataSourcesByExt;
+
+                JFileChooser chooser = new JFileChooser(currentDirectory);
+
+                FileFilter ff;
+                ff= new FileFilter() {
+
                     @Override
                     public boolean accept(File f) {
-                        if ( f.isDirectory() ) return true;
-                        String t= f.toString();
-                        String ext= getExt(t);
-                        return t.endsWith(".vap") || ( ext!=null && exts.containsKey(ext) ) ;
+                        if (f.isDirectory()) {
+                            return true;
+                        }
+                        String t = f.toString();
+                        String ext = getExt(t);
+                        return t.endsWith(".vap") || (ext != null && exts.containsKey(ext));
                     }
 
                     @Override
                     public String getDescription() {
-                        return extStr;
+                        return "supported formats and .vap files";
                     }
                 };
-                      
-                chooser.setFileFilter( ff );
+
                 
-                int result= chooser.showOpenDialog(ui);
-                if ( result== JFileChooser.APPROVE_OPTION ) {
+                chooser.addChoosableFileFilter(ff);
+                FileFilter select= ff;
+                
+                for (Object ext1 : exts.keySet()) {
+                    final String extf = (String) ext1;
+                    ff = new FileFilter() {
+
+                        @Override
+                        public boolean accept(File f) {
+                            if (f.isDirectory()) {
+                                return true;
+                            }
+                            String t = f.toString();
+                            String ext = getExt(t);
+                            return (ext != null && extf.equals(ext));
+                        }
+
+                        @Override
+                        public String getDescription() {
+                            return "*" + extf;
+                        }
+                    };
+                    chooser.addChoosableFileFilter(ff);
+                }
+                        
+                chooser.setFileFilter(select);
+                
+                int result = chooser.showOpenDialog(ui);
+                if (result == JFileChooser.APPROVE_OPTION) {
                     try {
                         prefs.put(PREF_LAST_OPEN_FOLDER, chooser.getSelectedFile().getParent().toString());
                         ui.setValue(chooser.getSelectedFile().toURI().toURL().toString());
@@ -99,37 +126,38 @@ public class DataSetSelectorSupport {
                         Logger.getLogger(DataSetSelectorSupport.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 }
-                
+
             }
         };
     }
 
-    
     protected void refreshRecentFilesMenu() {
-        if ( recentMenu!=null ) {
+        if (recentMenu != null) {
             recentMenu.removeAll();
-            if ( ui.getRecent()==null ) return;
-            ArrayList<String> recent= new ArrayList<String>( ui.getRecent() );
+            if (ui.getRecent() == null) {
+                return;
+            }
+            ArrayList<String> recent = new ArrayList<String>(ui.getRecent());
             Collections.reverse(recent);
-            for ( String s : recent ) {
-                final String f= s;
-                Action a= new AbstractAction( String.valueOf(f) ) {
-                    public void actionPerformed( ActionEvent e ) {
+            for (String s : recent) {
+                final String f = s;
+                Action a = new AbstractAction(String.valueOf(f)) {
+
+                    public void actionPerformed(ActionEvent e) {
                         ui.setValue(f);
                         ui.maybePlot();
                     }
                 };
-                recentMenu.add( a );
+                recentMenu.add(a);
             }
-        }        
+        }
     }
-    
+
     JMenu recentMenu() {
-        if ( recentMenu==null ) {
-            recentMenu= new JMenu("Open Recent");
-            refreshRecentFilesMenu();            
+        if (recentMenu == null) {
+            recentMenu = new JMenu("Open Recent");
+            refreshRecentFilesMenu();
         }
         return recentMenu;
     }
-    
 }
