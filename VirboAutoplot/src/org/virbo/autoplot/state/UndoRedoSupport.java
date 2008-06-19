314061ec6b4529773f5553e8d2d8d864109f4da2
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-06-19 12:12:42 +0000

    undo/redo factored out into callable methods.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@510 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/state/UndoRedoSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/state/UndoRedoSupport.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package org.virbo.autoplot.state;
 
 import java.awt.event.ActionEvent;
@@ -22,93 +21,109 @@ import org.virbo.autoplot.ApplicationModel;
  * @author jbf
  */
 public class UndoRedoSupport {
-    
+
     ApplicationModel applicationModel;
-    
+
     /** Creates a new instance of UndoRedoSupport */
-    public UndoRedoSupport( ApplicationModel applicationModel ) {
-        this.applicationModel= applicationModel;
-        applicationModel.addPropertyChangeListener( new PropertyChangeListener() {
-            public void propertyChange( PropertyChangeEvent ev ) {
-                if ( ev.getPropertyName().equals( ApplicationModel.PROPERTY_FILE ) ) {
+    public UndoRedoSupport(ApplicationModel applicationModel) {
+        this.applicationModel = applicationModel;
+        applicationModel.addPropertyChangeListener(new PropertyChangeListener() {
+
+            public void propertyChange(PropertyChangeEvent ev) {
+                if (ev.getPropertyName().equals(ApplicationModel.PROPERTY_FILE)) {
                     resetHistory();
                 }
             }
-        } );
+        });
     }
-    
+
     class StateStackElement {
+
         ApplicationState state;
         String deltaDesc;
-        public StateStackElement( ApplicationState state, String deltaDesc ) {
-            this.state= state;
-            this.deltaDesc= deltaDesc;
+
+        public StateStackElement(ApplicationState state, String deltaDesc) {
+            this.state = state;
+            this.deltaDesc = deltaDesc;
         }
+
         public String toString() {
             return deltaDesc;
         }
     }
-    
-    LinkedList<StateStackElement> stateStack= new LinkedList<StateStackElement>();
-    
+    LinkedList<StateStackElement> stateStack = new LinkedList<StateStackElement>();
     /**
      * points at the last saved state index + 1;
      */
-    int stateStackPos=0;
-    
+    int stateStackPos = 0;
+
     public Action getUndoAction() {
         return new AbstractAction("Undo") {
-            public void actionPerformed( ActionEvent e ) {
-                stateStackPos--;
-                if ( stateStackPos<0 ) stateStackPos=0;
-                if ( stateStackPos>0 ) {
-                    StateStackElement elephant= stateStack.get( stateStackPos-1 );
-                    ignoringUpdates= true;
-                    applicationModel.restoreState(elephant.state, false, true);
-                    ignoringUpdates= false;
-                }
+
+            public void actionPerformed(ActionEvent e) {
+                undo();
             }
-            
         };
     }
-    
+
+    public void undo() {
+        stateStackPos--;
+        if (stateStackPos < 0) {
+            stateStackPos = 0;
+        }
+        if (stateStackPos > 0) {
+            StateStackElement elephant = stateStack.get(stateStackPos - 1);
+            ignoringUpdates = true;
+            applicationModel.restoreState(elephant.state, false, true);
+            ignoringUpdates = false;
+        }
+    }
+
     public Action getRedoAction() {
         return new AbstractAction("redo") {
-            public void actionPerformed( ActionEvent e ) {
-                if ( stateStackPos>=stateStack.size() ) stateStackPos=stateStack.size()-1;
-                if ( stateStackPos<stateStack.size() ) {
-                    StateStackElement elephant= stateStack.get( stateStackPos );
-                    ignoringUpdates= true;
-                    applicationModel.restoreState(elephant.state, false, true);
-                    ignoringUpdates= false;
-                    stateStackPos++;
-                }
+            public void actionPerformed(ActionEvent e) {
+                redo();
             }
         };
     }
-    
-    public void pushState( PropertyChangeEvent ev ) {
-        if ( ignoringUpdates ) return;
-        
-        ApplicationState state= applicationModel.createState(false);
+
+    public void redo() {
+        if (stateStackPos >= stateStack.size()) {
+            stateStackPos = stateStack.size() - 1;
+        }
+        if (stateStackPos < stateStack.size()) {
+            StateStackElement elephant = stateStack.get(stateStackPos);
+            ignoringUpdates = true;
+            applicationModel.restoreState(elephant.state, false, true);
+            ignoringUpdates = false;
+            stateStackPos++;
+        }
+    }
+
+    public void pushState(PropertyChangeEvent ev) {
+        if (ignoringUpdates) {
+            return;
+        }
+        ApplicationState state = applicationModel.createState(false);
         StateStackElement elephant;
-        
-        if ( stateStackPos>0 ) {
-            elephant= stateStack.get(stateStackPos-1);
+
+        if (stateStackPos > 0) {
+            elephant = stateStack.get(stateStackPos - 1);
         } else {
-            elephant= null;
+            elephant = null;
         }
-        
-        if ( elephant!=null && state.equals(elephant.state) ) return;
-        
-        String labelStr= "initial";
-        if ( elephant!=null ) {
-            labelStr= state.diffs(elephant.state);
+
+        if (elephant != null && state.equals(elephant.state)) {
+            return;
         }
-        
-        stateStack.add(stateStackPos,new StateStackElement(state,labelStr) );
-        
-        while( stateStack.size() > (1+stateStackPos) ) {
+        String labelStr = "initial";
+        if (elephant != null) {
+            labelStr = state.diffs(elephant.state);
+        }
+
+        stateStack.add(stateStackPos, new StateStackElement(state, labelStr));
+
+        while (stateStack.size() > (1 + stateStackPos)) {
             stateStack.removeLast();
         }
         stateStackPos++;
@@ -119,30 +134,29 @@ public class UndoRedoSupport {
      * doesn't exist.
      */
     public String getUndoLabel() {
-        if ( stateStackPos>1 ) {
-            return "Undo "+stateStack.get(stateStackPos-1).deltaDesc;
+        if (stateStackPos > 1) {
+            return "Undo " + stateStack.get(stateStackPos - 1).deltaDesc;
         } else {
             return null;
         }
     }
-    
-        /**
+
+    /**
      * returns a label describing the redo operation, or null if the operation
      * doesn't exist.
      */
     public String getRedoLabel() {
-        if ( stateStackPos<stateStack.size() ) {
-            return "Redo "+stateStack.get(stateStackPos).deltaDesc;
+        if (stateStackPos < stateStack.size()) {
+            return "Redo " + stateStack.get(stateStackPos).deltaDesc;
         } else {
             return null;
         }
     }
 
     public void resetHistory() {
-        stateStack= new LinkedList<StateStackElement>();
-        stateStackPos= 0;
+        stateStack = new LinkedList<StateStackElement>();
+        stateStackPos = 0;
     }
-    
     /**
      * Holds value of property ignoringUpdates.
      */
@@ -163,5 +177,4 @@ public class UndoRedoSupport {
     public void setIgnoringUpdates(boolean ignoringUpdates) {
         this.ignoringUpdates = ignoringUpdates;
     }
-    
 }
