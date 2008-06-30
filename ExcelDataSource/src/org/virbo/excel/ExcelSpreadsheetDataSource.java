b02228b44671b63fdf9bb5e36de06fda9fb65946
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-06-30 12:19:54 +0000

    support for multiple-sheet workbooks.  firstRow parameter makes control more like ascii and binary data sources, and allows completion to query first row for plottable columns.  column name completion shows plottable columns.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@606 0c594bb0-8543-0410-a25e-d8ec440261db

--- ExcelDataSource/src/org/virbo/excel/ExcelSpreadsheetDataSource.java
+++ ExcelDataSource/src/org/virbo/excel/ExcelSpreadsheetDataSource.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package org.virbo.excel;
 
 import org.das2.util.monitor.ProgressMonitor;
@@ -15,9 +14,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,22 +26,18 @@ import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.virbo.dataset.QDataSet;
 import org.virbo.dataset.DataSetUtil;
 import org.virbo.datasource.AbstractDataSource;
-import org.virbo.datasource.CompletionContext;
 import org.virbo.datasource.DataSetURL;
-import org.virbo.datasource.DataSource;
-import org.virbo.datasource.DataSourceFactory;
-import org.virbo.datasource.MetadataModel;
 
 /**
  *
  * @author jbf
  */
 public class ExcelSpreadsheetDataSource extends AbstractDataSource {
-    
+
     POIFSFileSystem fs;
-    HSSFSheet sheet ;
+    HSSFSheet sheet;
     ExcelSpreadsheetDataSet data;
-    
+
     /** Creates a new instance of ExcelSpreadsheetDataSource.
      * file://c:/myfile.xls?column=N&depend0=G
      *   parameters:
@@ -52,139 +45,159 @@ public class ExcelSpreadsheetDataSource extends AbstractDataSource {
      *     depend0=id the column ID for the x tags
      * file://C:/iowaCitySales2004-2006.latlong.xls?column=M[2:]&depend0=B[2:]&plane0=C[2:]
      */
-    public ExcelSpreadsheetDataSource( URL url ) throws IOException {
-        super(url);
+    public ExcelSpreadsheetDataSource(URL url) throws IOException {
+	super(url);
     }
-    
+
     public QDataSet getDataSet(ProgressMonitor mon) throws IOException {
-        File file= DataSetURL.getFile( url, mon );
-        InputStream in= new FileInputStream( file );
-        fs=   new POIFSFileSystem( in );
-        HSSFWorkbook wb = new HSSFWorkbook(fs);
-        sheet = wb.getSheetAt(0);
-        String query= url.getQuery(); // the part after the ?
-        Map m= DataSetURL.parseParams(query);
-        String d= (String) m.get("column");
-        data= createDataSet( d );
-        
-        d= (String)m.get("depend0");
-        if ( d!=null ) {
-            ExcelSpreadsheetDataSet depend0=createDataSet(d);
-            data.putProperty( QDataSet.DEPEND_0, depend0 );
-        }
-        
-        d= (String)m.get("plane0");
-        if ( d!=null ) {
-            ExcelSpreadsheetDataSet depend0=createDataSet(d);
-            data.putProperty( QDataSet.PLANE_0, depend0 );
-        }
-        return data;
+	File file = DataSetURL.getFile(url, mon);
+	InputStream in = new FileInputStream(file);
+	fs = new POIFSFileSystem(in);
+	HSSFWorkbook wb = new HSSFWorkbook(fs);
+
+	String query = url.getQuery(); // the part after the ?
+	Map m = DataSetURL.parseParams(query);
+
+	String ssheet = (String) m.get("sheet");
+	if (ssheet == null) {
+	    sheet = wb.getSheetAt(0);
+	} else {
+	    sheet = wb.getSheet(ssheet);
+	}
+	if ( sheet==null ) {
+	    throw new IllegalArgumentException("Sheet not found: "+ssheet);
+	}
+
+	String firstRowString= (String) m.get("firstRow");
+	int firstRow= firstRowString==null ? -1 : Integer.parseInt(firstRowString)-1;
+	
+	String d = (String) m.get("column");
+	int[] spec= parseDataSetSpec(d,firstRow,-1);
+	data = new ExcelSpreadsheetDataSet( (short)spec[0], spec[1], spec[2] );
+
+	d = (String) m.get("depend0");
+	if (d != null) {
+	    spec= parseDataSetSpec(d,firstRow,-1);
+	    ExcelSpreadsheetDataSet depend0 = new ExcelSpreadsheetDataSet( (short)spec[0], spec[1], spec[2] );
+	    data.putProperty(QDataSet.DEPEND_0, depend0);
+	}
+
+	d = (String) m.get("plane0");
+	if (d != null) {
+	    spec= parseDataSetSpec(d,firstRow,-1);
+	    ExcelSpreadsheetDataSet p0 = new ExcelSpreadsheetDataSet( (short)spec[0], spec[1], spec[2] );
+	    data.putProperty(QDataSet.PLANE_0, p0);
+	}
+	return data;
     }
-    
-    private ExcelSpreadsheetDataSet createDataSet( String spec ) {
-        Pattern p= Pattern.compile("([A-Z]+)(\\[(\\d+):(\\d+)?\\])?");
-        Matcher m= p.matcher(spec);
-        
-        int firstRow, lastRow;
-        short columnNumber;
-        
-        if ( !m.matches() ) {
-            throw new IllegalArgumentException("bad spec!");
-        } else {
-            String col= m.group(1);
-            columnNumber= (short)(  col.charAt(0)-'A' );
-            if ( m.group(3)==null ) {
-                firstRow=0;
-                lastRow= sheet.getLastRowNum();
-            } else {
-                firstRow= Integer.parseInt( m.group(3) );
-                if ( m.group(4)==null ) {
-                    lastRow=  sheet.getLastRowNum();
-                } else {
-                    lastRow= Integer.parseInt( m.group(4) );
-                }
-            }
-            return new ExcelSpreadsheetDataSet( columnNumber, firstRow, lastRow );
-        }
-        
-        
+
+    /**
+     * Returns [ columnNumber, first, last ] 
+     * @param spec
+     * @return
+     */
+    private int[] parseDataSetSpec(String spec,int firstRow, int lastRow ) {
+	Pattern p = Pattern.compile("([A-Z]+)(\\[(\\d+):(\\d+)?\\])?");
+	Matcher m = p.matcher(spec);
+
+	short columnNumber;
+
+	if (!m.matches()) {
+	    throw new IllegalArgumentException("bad spec!");
+	} else {
+	    String col = m.group(1);
+	    columnNumber = (short) (col.charAt(0) - 'A');
+	    if (m.group(3) == null) {
+		if ( firstRow==-1 ) firstRow = 0;
+		if ( lastRow==-1 ) lastRow = sheet.getLastRowNum();
+	    } else {
+		firstRow = Integer.parseInt(m.group(3));
+		if (m.group(4) == null) {
+		    if ( lastRow==-1 ) lastRow = sheet.getLastRowNum();
+		} else {
+		    lastRow = Integer.parseInt(m.group(4));
+		}
+	    }
+	    return new int[] { columnNumber, firstRow, lastRow };
+	}
+
+
     }
-    
+
     class ExcelSpreadsheetDataSet implements QDataSet {
-        short columnNumber;
-        int firstRow;
-        int length;
-        Map properties= new HashMap();
-        
-        
-        /**
-         * @param firstRow is the first row to read.
-         * @param lastRow is the last row number, exclusive.
-         */
-        ExcelSpreadsheetDataSet( short columnNumber, int firstRow, int lastRow ) {
-            this.columnNumber= columnNumber;
-            this.firstRow= firstRow;
-            this.length= lastRow-firstRow;
-            HSSFCell cell= sheet.getRow(firstRow).getCell(columnNumber);
-            
-        }
-        
-        public int rank() {
-            return 1;
-        }
-        
-        public double value(int i) {
-            HSSFRow row=null;
-            HSSFCell cell=null;
-            try {
-                row= sheet.getRow(i+firstRow);
-                cell=row.getCell(columnNumber);
-                return cell.getNumericCellValue();
-            } catch ( RuntimeException e ) {
-                String cellID= String.valueOf( (char)(  'A'+columnNumber ) ) + ( firstRow + i );
-                return -1e31;
-            }
-        }
-        
-        public double value(int i0, int i1) {
-            throw new IllegalArgumentException("rank");
-        }
-        
-        public double value(int i0, int i1, int i2) {
-            throw new IllegalArgumentException("rank");
-        }
-        
-        public Object property(String name) {
-            return properties.get(name);
-        }
-        
-        public Object property(String name, int i) {
-            return properties.get(name);
-        }
-        
-        public Object property(String name, int i0, int i1) {
-            return properties.get(name);
-        }
-        
-        public void putProperty( String name, Object value ) {
-            properties.put( name, value );
-        }
-        
-        public int length() {
-            return length;
-        }
-        
-        public int length(int i) {
-            throw new IllegalArgumentException("ranklimit");
-        }
-        
-        public int length(int i, int j) {
-            throw new IllegalArgumentException("ranklimit");
-        }
-        
-        public String toString() {
-            return DataSetUtil.toString(this);
-        }
+
+	short columnNumber;
+	int firstRow;
+	int length;
+	Map properties = new HashMap();
+
+	/**
+	 * @param firstRow is the first row to read.
+	 * @param lastRow is the last row number, exclusive.
+	 */
+	ExcelSpreadsheetDataSet( short columnNumber, int firstRow, int lastRow ) {
+	    this.columnNumber = columnNumber;
+	    this.firstRow = firstRow;
+	    this.length = lastRow - firstRow;
+	    HSSFCell cell = sheet.getRow(firstRow).getCell(columnNumber);
+
+	}
+
+	public int rank() {
+	    return 1;
+	}
+
+	public double value(int i) {
+	    HSSFRow row = null;
+	    HSSFCell cell = null;
+	    try {
+		row = sheet.getRow(i + firstRow);
+		cell = row.getCell(columnNumber);
+		return cell.getNumericCellValue();
+	    } catch (RuntimeException e) {
+		String cellID = String.valueOf((char) ('A' + columnNumber)) + (firstRow + i);
+		return -1e31;
+	    }
+	}
+
+	public double value(int i0, int i1) {
+	    throw new IllegalArgumentException("rank");
+	}
+
+	public double value(int i0, int i1, int i2) {
+	    throw new IllegalArgumentException("rank");
+	}
+
+	public Object property(String name) {
+	    return properties.get(name);
+	}
+
+	public Object property(String name, int i) {
+	    return properties.get(name);
+	}
+
+	public Object property(String name, int i0, int i1) {
+	    return properties.get(name);
+	}
+
+	public void putProperty(String name, Object value) {
+	    properties.put(name, value);
+	}
+
+	public int length() {
+	    return length;
+	}
+
+	public int length(int i) {
+	    throw new IllegalArgumentException("ranklimit");
+	}
+
+	public int length(int i, int j) {
+	    throw new IllegalArgumentException("ranklimit");
+	}
+
+	public String toString() {
+	    return DataSetUtil.toString(this);
+	}
     }
-    
 }
--- ExcelDataSource/src/org/virbo/excel/ExcelSpreadsheetDataSourceFactory.java
+++ ExcelDataSource/src/org/virbo/excel/ExcelSpreadsheetDataSourceFactory.java
@@ -4,12 +4,22 @@
  */
 package org.virbo.excel;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.datasource.CompletionContext;
+import org.virbo.datasource.DataSetURL;
 import org.virbo.datasource.DataSource;
 import org.virbo.datasource.DataSourceFactory;
 import org.virbo.datasource.MetadataModel;
@@ -19,39 +29,98 @@ import org.virbo.datasource.MetadataModel;
  * @author jbf
  */
 public class ExcelSpreadsheetDataSourceFactory implements DataSourceFactory {
+    private static final String FIRST_ROW_DOC = "the row that contains the either the first record of data, or data column headings.  1 is the first row.";
 
     public DataSource getDataSource(URL url) throws IOException {
-        return new ExcelSpreadsheetDataSource(url);
+	return new ExcelSpreadsheetDataSource(url);
     }
 
-    public List<CompletionContext> getCompletions(CompletionContext cc,org.das2.util.monitor.ProgressMonitor mon) {
-        List<CompletionContext> result = new ArrayList<CompletionContext>();
-        if (cc.context == CompletionContext.CONTEXT_PARAMETER_NAME) {
-            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "column="));
-            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "depend0="));
-            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "plane0="));
-        } else if (cc.context == CompletionContext.CONTEXT_PARAMETER_VALUE) {
-            String param = CompletionContext.get(CompletionContext.CONTEXT_PARAMETER_NAME, cc);
-            if (param.equals("column")) {
-                result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "A[start:end]"));
-            } else if (param.equals("depend0")) {
-                result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "B[start:end]"));
-            } else if (param.equals("plane0")) {
-                result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "C[start:end]"));
-            }
-        }
-        return result;
+    public List<CompletionContext> getCompletions(CompletionContext cc, org.das2.util.monitor.ProgressMonitor mon) throws IOException {
+	List<CompletionContext> result = new ArrayList<CompletionContext>();
+	if (cc.context == CompletionContext.CONTEXT_PARAMETER_NAME) {
+	    result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "column="));
+	    result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "depend0="));
+	    result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "plane0="));
+	    result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "sheet="));
+	    result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "firstRow=",FIRST_ROW_DOC));
+	} else if (cc.context == CompletionContext.CONTEXT_PARAMETER_VALUE) {
+	    String param = CompletionContext.get(CompletionContext.CONTEXT_PARAMETER_NAME, cc);
+	    if (param.equals("column")) {
+		result.addAll( toCC( CompletionContext.CONTEXT_PARAMETER_VALUE, getColumns(cc, mon), null ));
+	    } else if (param.equals("depend0")) {
+		result.addAll( toCC( CompletionContext.CONTEXT_PARAMETER_VALUE, getColumns(cc, mon), null ));
+	    } else if (param.equals("plane0")) {
+		result.addAll( toCC( CompletionContext.CONTEXT_PARAMETER_VALUE, getColumns(cc, mon), null ));
+	    } else if (param.equals("sheet")) {
+		result.addAll( toCC( CompletionContext.CONTEXT_PARAMETER_VALUE, getSheets(cc, mon), "worksheet source" ) );
+	    } else if ( param.equals("firstRow") ) {
+		result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "<int>", FIRST_ROW_DOC));
+	    }
+	}
+	return result;
+    }
+    
+    List<CompletionContext> toCC( Object context, List<String> results, String doc ) {
+	List<CompletionContext> result= new ArrayList<CompletionContext>();
+	for ( String s: results ) {
+	    result.add( new CompletionContext( context, s, doc ) );
+	}
+	return result;
     }
 
+    private HSSFWorkbook getWorkbook( URL url, ProgressMonitor mon ) throws IOException {
+	File file = DataSetURL.getFile(url, mon);
+	InputStream in = new FileInputStream(file);
+	POIFSFileSystem fs = new POIFSFileSystem(in);
+	HSSFWorkbook wb = new HSSFWorkbook(fs);
+	return wb;
+    }
+
+    private List<String> getSheets(CompletionContext cc, ProgressMonitor mon) throws IOException {
+	HSSFWorkbook wb= getWorkbook(cc.resource, mon);
+	List<String> result= new ArrayList<String>();
+	for ( int i=0; i<wb.getNumberOfSheets(); i++ ) {
+	    String s= wb.getSheetName(i);
+	    result.add( s );
+	}
+	return result;
+    }
+	    
+    private List<String> getColumns( CompletionContext cc, ProgressMonitor mon) throws IOException {
+	HSSFWorkbook wb= getWorkbook(cc.resource, mon);
+	Map params= DataSetURL.parseParams(cc.params);
+	List<String> result= new ArrayList<String>();
+	HSSFSheet sheet;
+	String ssheet= (String) params.get("sheet");
+	if ( ssheet==null ) {
+	    sheet= wb.getSheetAt(0);
+	} else {
+	    sheet= wb.getSheet(ssheet);
+	}
+	
+	String firstRowString= (String) params.get("firstRow");
+	int firstRow= firstRowString==null ? 0 : Integer.parseInt(firstRowString)-1;
+	HSSFRow row= sheet.getRow(firstRow);
+	
+	row.getLastCellNum();
+	for ( int i=row.getFirstCellNum(); i<row.getLastCellNum(); i++ ) {
+	    HSSFCell cell= row.getCell((short)i);
+	    if ( cell!=null && cell.getCellType()==0 ) { // 1=String
+		result.add( ""+(char)(i+'A') );
+	    }
+	}
+	return result; 
+    }
+		
     public MetadataModel getMetadataModel(URL url) {
-        return MetadataModel.createNullModel();
+	return MetadataModel.createNullModel();
     }
 
-    public boolean reject(String surl,ProgressMonitor mon) {
-        return !surl.contains("column=");
+    public boolean reject(String surl, ProgressMonitor mon) {
+	return !surl.contains("column=");
     }
 
     public String urlForServer(String surl) {
-        return surl; // TODO
+	return surl; // TODO
     }
 }
