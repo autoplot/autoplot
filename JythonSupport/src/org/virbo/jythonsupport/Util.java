c82130e9a93ecc7e78d8762e904d84e17c55f9bc
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-07-23 13:16:12 +0000

    javadoc improvements. copy the loaded dataset to DDataSet because MutablePropertyDataSets have bug in jython delegation where properties disappear.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@703 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonSupport/src/org/virbo/jythonsupport/Util.java
+++ JythonSupport/src/org/virbo/jythonsupport/Util.java
@@ -5,8 +5,10 @@
 package org.virbo.jythonsupport;
 
 import java.net.URI;
+import java.util.logging.Logger;
 import org.das2.util.monitor.NullProgressMonitor;
 import org.das2.util.monitor.ProgressMonitor;
+import org.virbo.dataset.DDataSet;
 import org.virbo.dataset.QDataSet;
 import org.virbo.datasource.DataSetURL;
 import org.virbo.datasource.DataSource;
@@ -19,17 +21,20 @@ import org.virbo.datasource.DataSourceFactory;
 public class Util {
 
     /**
-     * 
-     * @param url
-     * @param mon
-     * @return
+     * load a dataset.
+     * @param surl dataSource identifier
+     * @param mon monitor for the loading, or null (None in jython).
+     * @return QDataSet that is the data from the DataSource.
      * @throws java.lang.Exception
      */
     public static QDataSet getDataSet(String surl, ProgressMonitor mon) throws Exception {
+        Logger.getLogger("virbo.jythonsupport").fine( "getDataSet("+surl+")");
         URI url = DataSetURL.getURI(surl);
         DataSourceFactory factory = DataSetURL.getDataSourceFactory(url, new NullProgressMonitor());
         DataSource result = factory.getDataSource( DataSetURL.getWebURL(url) );
-        return result.getDataSet(mon == null ? new NullProgressMonitor() : mon);
+        QDataSet rds= result.getDataSet(mon == null ? new NullProgressMonitor() : mon);
+        //Logger.getLogger("virbo.jythonsupport").fine( "created dataset #"+rds.getClass().gethashCode() );
+        return DDataSet.copy(rds); // fixes a bug where a MutablePropertiesDataSet and WritableDataSet copy in coerce
     }
 
     public static QDataSet getDataSet(String surl) throws Exception {
