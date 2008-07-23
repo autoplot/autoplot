ed6fd5c8294d033ae299375df4dc6b8e136d41ab
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-07-23 12:58:28 +0000

    lookup renamed to lookupUnits. getNode and toMetaTee introduced to support ImageDataSource.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@692 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/metatree/MetadataUtil.java
+++ DataSource/src/org/virbo/metatree/MetadataUtil.java
@@ -7,6 +7,9 @@ package org.virbo.metatree;
 
 import edu.uiowa.physics.pw.das.datum.NumberUnits;
 import edu.uiowa.physics.pw.das.datum.Units;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.w3c.dom.Node;
 
 /**
  *
@@ -14,11 +17,11 @@ import edu.uiowa.physics.pw.das.datum.Units;
  */
 public class MetadataUtil {
     /**
-     * lookup canonical units object, or allocate one.
+     * lookupUnits canonical units object, or allocate one.
      * @param units string identifier.
      * @return canonical units object.
      */
-    public static synchronized Units lookup(String units) {
+    public static synchronized Units lookupUnits(String units) {
         Units result;
         try {
             result= Units.getByName(units);
@@ -26,5 +29,44 @@ public class MetadataUtil {
             result= new NumberUnits( units );
         }
         return result;
+    }
+    
+    /**
+     * converts tree model node into canonical Map<String,Object>.  Branch nodes
+     * are HashMap<String,Object> as well.
+     * @param node
+     * @return
+     */
+    public static Map<String,Object> toMetaTree( Node node ) {
+        Map<String,Object> result= new LinkedHashMap<String,Object>();
+        Node child= node.getFirstChild();
+        while ( child!=null ) {
+            Object value;
+            if ( child.hasChildNodes() ) {
+                value= toMetaTree( child );
+            } else {
+                value= child.getNodeValue();
+            }
+            result.put( child.getNodeName(), value );
+            child= child.getNextSibling();
+        }
+        return result;
+    }
+    
+    public static Node getNode( Node node, String[] path ) {
+        int i=0;
+        Node child= node.getFirstChild();
+        while ( i<path.length ) {
+            if ( child==null ) throw new IllegalArgumentException("couldn't find node");
+            Object value;
+            if ( child.getNodeName().equals( path[i] ) ) {
+                node= child;
+                child= node.getFirstChild();
+                i++;
+            } else {
+                child= child.getNextSibling();
+            }
+        }
+        return node;
     }
 }
