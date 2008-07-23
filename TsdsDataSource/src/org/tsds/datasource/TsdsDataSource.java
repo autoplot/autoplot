3baafc499d1bdcbce07eac55b1864068b3c3c8d6
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-07-23 14:44:45 +0000

    introduce dataSource for Bob Weigel's TSDS server.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@725 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ TsdsDataSource/src/org/tsds/datasource/TsdsDataSource.java
@@ -0,0 +1,269 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tsds.datasource;
+
+import edu.uiowa.physics.pw.das.datum.Datum;
+import edu.uiowa.physics.pw.das.datum.DatumRange;
+import edu.uiowa.physics.pw.das.datum.DatumRangeUtil;
+import edu.uiowa.physics.pw.das.datum.TimeUtil;
+import edu.uiowa.physics.pw.das.datum.Units;
+import edu.uiowa.physics.pw.das.datum.UnitsUtil;
+import edu.uiowa.physics.pw.das.datum.format.DatumFormatter;
+import edu.uiowa.physics.pw.das.datum.format.TimeDatumFormatter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.DoubleBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.das2.util.monitor.ProgressMonitor;
+import org.virbo.binarydatasource.BufferDataSet;
+import org.virbo.dataset.QDataSet;
+import org.virbo.datasource.AbstractDataSource;
+import org.virbo.datasource.DataSetURL;
+import org.virbo.datasource.capability.TimeSeriesBrowse;
+import org.virbo.dsops.Ops;
+import org.virbo.metatree.MetadataUtil;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author jbf
+ */
+class TsdsDataSource extends AbstractDataSource {
+
+    public TsdsDataSource(URL url) {
+        super(url);
+        addCability( TimeSeriesBrowse.class, getTimeSeriesBrowse() );
+    }
+    DatumRange timeRange;
+    Datum resolution;
+    private static final int SIZE_DOUBLE = 8;
+
+    @Override
+    public QDataSet getDataSet(ProgressMonitor mon) throws Exception {
+        //http://www-pw.physics.uiowa.edu/das/das2Server
+        //?dataset=das2_1/voyager1/pws/sa-4s-pf.new
+        //&start_time=2004-01-01&end_time=2004-01-06&server=dataset&ascii=1
+
+        // datasource url:
+        //     tsds.http://timeseries.org/cgi-bin/get.cgi?StartDate=20010101&EndDate=20010101&ext=bin&ppd=24&param1=SourceAcronym_Subset-1-v0
+        //     tsds.http://timeseries.org/cgi-bin/get.cgi?StartDate=20010101&EndDate=20010101&ext=bin&out=tsml&ppd=24&param1=SourceAcronym_Subset-1-v0
+        // translates to:
+        //          http://timeseries.org/cgi-bin/get.cgi?StartDate=20010101&EndDate=20010101&ext=bin&ppd=24&param1=SourceAcronym_Subset-1-v0
+        Map<String, String> params2 = new LinkedHashMap<String, String>(params);
+
+        DatumFormatter df = new TimeDatumFormatter("%Y%m%d");
+
+        int ppd;
+
+        if (timeRange != null) {
+            params2.put("StartDate", "" + df.format(timeRange.min()));
+            // inclusive end times are a pain.
+            params2.put("EndDate", "" + df.format(TimeUtil.prevMidnight(timeRange.max())));
+        } else {
+            DatumRange dr0 = DatumRangeUtil.parseTimeRangeValid(params2.get("StartDate"));
+            DatumRange dr1 = DatumRangeUtil.parseTimeRangeValid(params2.get("EndDate"));
+            timeRange = new DatumRange(dr0.min(), dr1.max());
+        }
+
+        if (resolution != null) {
+            double resdays = resolution.doubleValue(Units.days);
+            ppd = (int) Math.ceil(1 / resdays);
+            if (ppd == 0)
+                ppd = 1;
+
+            params2.put("ppd", "" + ppd);
+        } else {
+            String sppd = params2.get("ppd");
+            if (sppd == null)
+                throw new IllegalArgumentException("need ppd");
+            ppd = Integer.parseInt(sppd);
+        }
+
+        URL url2 = new URL("" + this.resourceURL + "?" + DataSetURL.formatParams(params2));
+
+        Logger.getLogger("virbo.tsds.datasource").info("" + url2);
+
+        int points = (int) Math.ceil(timeRange.width().doubleValue(Units.days)) * ppd;
+        int size = points * SIZE_DOUBLE;
+
+        mon.setTaskSize(-1);
+        mon.started();
+        
+        HttpURLConnection connect = (HttpURLConnection) url2.openConnection();
+        connect.connect();
+        String type = connect.getContentType();
+
+        BufferDataSet result;
+        if (type.startsWith("text/xml")) {
+            result= tsml(connect.getInputStream(), mon );
+        } else {
+            result= dataUrl(connect.getInputStream(), size, points, mon);
+        }
+        
+        mon.finished();
+        return result;
+
+    }
+
+    public TimeSeriesBrowse getTimeSeriesBrowse() {
+        return new TimeSeriesBrowse() {
+
+            public void setTimeRange(DatumRange dr) {
+                timeRange = dr;
+            }
+
+            public void setTimeResolution(Datum d) {
+                resolution = d;
+            }
+
+            public URL getURL() {
+                String sparams = "dataset=" + params.get("dataset") + "&StartDate=" + timeRange.min() + "&EndDate=" + timeRange.max();
+                try {
+                    return new URL(resourceURL, "?" + sparams);
+                } catch (MalformedURLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        };
+    }
+
+    /**
+     * Read in the TSDS binary stream into wrap it to make a QDataSet.
+     * @param in
+     * @param size number of bytes to read.  The stream is consumed up to this point, or to the end of the stream.
+     * @param points number of data points.
+     * @param ProgressMonitor in started state.  finished should not be called here.
+     * @return
+     * @throws java.io.IOException
+     */
+    private BufferDataSet dataUrl( InputStream in, int size, int points, ProgressMonitor mon ) throws IOException {
+
+        ReadableByteChannel bin = Channels.newChannel(in);
+
+        ByteBuffer bbuf = ByteBuffer.allocate(size);
+        int totalBytesRead= 0;
+        int bytesRead= bin.read(bbuf);
+        
+        mon.setTaskSize(size);
+        
+        while ( bytesRead >= 0 && (bytesRead+totalBytesRead)<size ) {
+            totalBytesRead+=bytesRead;
+            bytesRead= bin.read(bbuf);
+            mon.setTaskProgress(totalBytesRead);
+            if ( mon.isCancelled() ) break;
+        }
+        
+        bbuf.flip();
+        bbuf.order(ByteOrder.LITTLE_ENDIAN);
+
+        DoubleBuffer dbuf = bbuf.asDoubleBuffer();
+
+        points = dbuf.limit();
+        return new org.virbo.binarydatasource.Double(1, points, 1, 0, 1, 1, 0, dbuf);
+    }
+
+    private QDataSet ttags( String sStartTime, int ppd, int points, String sTimePos ) {
+        Datum cadence= Units.days.createDatum(1).divide( ppd );
+        Datum startTime= TimeUtil.createValid(sStartTime);
+        Datum t0= startTime;
+        if ( sTimePos.equals("center") ) t0= t0.add( cadence.divide(2) );
+        
+        try {
+            return Ops.timegen(String.valueOf(t0), String.valueOf(cadence), points);
+        } catch (ParseException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+    
+    /**
+     * The resource is a TSML file, which is an xml description of a binary stream.  The
+     * URL of the stream is found within the description, and this is loaded.  TSML
+     * syntax description is used to parse the stream.
+     * 
+     * @param connect HTTPURLConnection 
+     * @param mon ProgressMonitor in started state.  finished should not be called here.
+     * @return 
+     * @throws javax.xml.parsers.ParserConfigurationException
+     * @throws java.io.IOException
+     * @throws org.xml.sax.SAXException
+     */
+    private BufferDataSet tsml( InputStream in, ProgressMonitor mon ) throws ParserConfigurationException, IOException, SAXException {
+        try {
+
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            InputSource source = new InputSource(in);
+            Document document = builder.parse(source);
+
+            XPathFactory factory = XPathFactory.newInstance();
+            XPath xpath = factory.newXPath();
+
+            String surl = xpath.evaluate("//TSML/DataURL/text()", document);
+
+            URL dataUrl = new URL(surl);
+            HttpURLConnection dconnect = (HttpURLConnection) dataUrl.openConnection();
+            
+            String sunits= xpath.evaluate("//TSML/Unit/text()", document);
+            
+            String sStartTime= xpath.evaluate("//TSML/StartDate/text()", document );
+            String sEndTime= xpath.evaluate("//TSML/EndDate/text()", document );
+            
+            String sppd= xpath.evaluate("//TSML/IntervalsPerDay/text()", document );
+            int ppd= Integer.parseInt(sppd);
+            
+            
+            DatumRange dr0 = DatumRangeUtil.parseTimeRangeValid(sStartTime);
+            DatumRange dr1 = DatumRangeUtil.parseTimeRangeValid(sEndTime);
+            
+            timeRange = new DatumRange(dr0.min(), dr1.max());
+            int points = (int) Math.ceil(timeRange.width().doubleValue(Units.days)) * ppd;
+            int size= points * SIZE_DOUBLE;
+                    
+            QDataSet ttags;
+            
+            String sTimePos= xpath.evaluate("//TSML/TimeStampPosition/text()", document );
+            if ( !sTimePos.equals("") ) {
+                ttags= ttags( sStartTime, ppd, points, sTimePos );
+            } else {
+                ttags= null;
+            }
+
+            String title= xpath.evaluate("//TSML/Name/text()", document );
+            String name= xpath.evaluate("//TSML/NameShort/text()", document );
+            
+            BufferDataSet data= dataUrl( dconnect.getInputStream(), size, points, mon );
+            data.putProperty( QDataSet.UNITS, MetadataUtil.lookupUnits(sunits) );
+            data.putProperty( QDataSet.DEPEND_0, ttags );
+            data.putProperty( QDataSet.NAME, name );
+            data.putProperty( QDataSet.TITLE, title );
+            
+            return data;
+            
+        } catch (XPathExpressionException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
---
+++ TsdsDataSource/src/org/tsds/datasource/TsdsDataSourceFactory.java
@@ -0,0 +1,105 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tsds.datasource;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.das2.util.monitor.ProgressMonitor;
+import org.virbo.datasource.CompletionContext;
+import org.virbo.datasource.DataSetURL;
+import org.virbo.datasource.DataSource;
+import org.virbo.datasource.DataSourceFactory;
+
+/**
+ *
+ * @author jbf
+ */
+public class TsdsDataSourceFactory implements DataSourceFactory {
+
+    public DataSource getDataSource(URL url) throws Exception {
+        return new TsdsDataSource(url);
+    }
+    
+    Map<String, List<String>> datasetsList = null;
+
+    public List<CompletionContext> getCompletions(CompletionContext cc, ProgressMonitor mon) throws Exception {
+        //http://www-pw.physics.uiowa.edu/das/das2Server
+        //?dataset=das2_1/voyager1/pws/sa-4s-pf.new
+        //&start_time=2004-01-01&end_time=2004-01-06&server=dataset&ascii=1
+
+        List<CompletionContext> result = new ArrayList<CompletionContext>();
+
+        if (cc.context == CompletionContext.CONTEXT_PARAMETER_NAME) {
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "param1=", "dataset identifier"));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "StartDate=", "YYYYMMDD start time"));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "EndTime=", "YYYYMMDD end time"));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "ppd=", "number of points per day"));
+        } else if (cc.context == CompletionContext.CONTEXT_PARAMETER_VALUE) {
+            String paramName = CompletionContext.get(CompletionContext.CONTEXT_PARAMETER_NAME, cc);
+            if (paramName.equals("dataset")) {
+                URL url= DataSetURL.getURL( CompletionContext.get(CompletionContext.CONTEXT_FILE, cc)  );
+                List<String> dss= getDatasetsList( url.toString() );
+                for ( String ds: dss ) {
+                    if ( ds.startsWith(cc.completable) ) {
+                        result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, ds ) );
+                    }
+                }
+            }
+        }
+
+        return result;
+
+    }
+
+    synchronized List<String> getDatasetsList(String surl) {
+        if (datasetsList == null) {
+            datasetsList = new HashMap<String, List<String>>();
+        }
+        List<String> result = datasetsList.get(surl);
+        
+        if (result == null) {
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(surl + "?server=list");
+                reader = new BufferedReader(new InputStreamReader(url.openStream()));
+                String s = reader.readLine();
+                ArrayList<String> list = new ArrayList<String>();
+                while (s != null) {
+                    list.add(s);
+                    s = reader.readLine();
+                }
+                datasetsList.put( surl, list );
+            } catch (IOException ex) {
+                Logger.getLogger(TsdsDataSourceFactory.class.getName()).log(Level.SEVERE, null, ex);
+                return null;
+            } finally {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(TsdsDataSourceFactory.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+        
+        return datasetsList.get(surl);
+        
+    }
+
+    public boolean reject(String surl, ProgressMonitor mon) {
+        DataSetURL.URLSplit split= DataSetURL.parse( surl );
+        Map params= DataSetURL.parseParams(split.params);
+        return !( params.containsKey("StartDate") && params.containsKey("EndDate") && params.containsKey("param1") );
+    }
+}
