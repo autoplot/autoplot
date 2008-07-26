eeb7e53c06f1845705528b5b2dcfc6647b802f22
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-07-26 20:07:00 +0000

    introduce JythonUtil and JythonUtil.createInterpreter so the interpreter comes from one place and is always configured the same.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@741 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ JythonSupport/src/org/virbo/jythonsupport/JythonUtil.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.jythonsupport;
+
+import java.io.IOException;
+import org.python.core.Py;
+import org.python.util.PythonInterpreter;
+
+/**
+ *
+ * @author jbf
+ */
+public class JythonUtil {
+    
+    /**
+     * create an interpretter object configured for Autoplot contexts:
+     *   * QDataSets are wrapped so that operators are overloaded.
+     *   * a standard set of names are imported.
+     *   
+     * @param sandbox limit symbols to safe symbols for server.
+     * @return PythonInterpreter ready for commands.
+     * @throws java.io.IOException
+     */
+    public static PythonInterpreter createInterpreter( boolean sandbox ) throws IOException {
+	PythonInterpreter interp = new PythonInterpreter();
+	Py.getAdapter().addPostClass(new PyQDataSetAdapter());
+
+	interp.execfile(JythonOps.class.getResource("imports.py").openStream(), "imports.py");
+	return interp;
+
+    }
+
+}
