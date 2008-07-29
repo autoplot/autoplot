936271e1963d1a51cc2b5c57b3da863723880ebb
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-07-29 03:11:29 +0000

    make Double backed dataset writable.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@757 0c594bb0-8543-0410-a25e-d8ec440261db

--- BinaryDataSource/src/org/virbo/binarydatasource/Double.java
+++ BinaryDataSource/src/org/virbo/binarydatasource/Double.java
@@ -1,8 +1,9 @@
 package org.virbo.binarydatasource;
 
 import java.nio.DoubleBuffer;
+import org.virbo.dataset.WritableDataSet;
 
-public class Double extends BufferDataSet {
+public class Double extends BufferDataSet implements WritableDataSet {
 
     DoubleBuffer back;
 
@@ -26,4 +27,18 @@ public class Double extends BufferDataSet {
         //if (RANGE_CHECK) rangeCheck( i0, i1, i2 );
         return back.get( offset(i0, i1, i2));
     }
+
+    public void putValue(int i0, double d) {
+        back.put( offset(i0, 0, 0), d );
+    }
+
+    public void putValue(int i0, int i1, double d) {
+        back.put( offset(i0, i1, 0), d );
+    }
+
+    public void putValue(int i0, int i1, int i2, double d) {
+        back.put( offset(i0, i1, i2), d );
+    }
+    
+    
 }
