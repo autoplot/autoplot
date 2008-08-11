b73e801f0cf82d7e5bb052d73030177d0f815f83
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-08-11 16:26:57 +0000

    experimental jython script servlet.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@788 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ AutoplotServlet/src/java/org/virbo/autoplot/ScriptServlet.java
@@ -0,0 +1,98 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.autoplot;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.python.util.PythonInterpreter;
+import org.virbo.autoplot.scriptconsole.LoggingOutputStream;
+
+/**
+ *
+ * @author jbf
+ */
+public class ScriptServlet extends HttpServlet {
+   
+    /** 
+    * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        try {
+            String script= request.getParameter("script");
+            
+            if ( script==null ) {
+                script= "setCanvasSize( 400, 400 )\n"+
+                    "setDataSourceURL( 'http://www.sarahandjeremy.net/jeremy/1wire/data/2008/0B000800408DD710.20080117.d2s' )\n"+
+                    "setTitle( 'Garage 20080117' )\n" +
+                    "response.setContentType('image/png')\n" + 
+                    "out = response.getOutputStream()\n" + 
+                    "writeToPng( out )\n";
+            }
+            
+            // do minimal taint checking.
+            String[] ss= script.split("\n");
+             
+            for ( int i=0; i<ss.length; i++ ) {
+                if ( ss[i].contains("import") ) throw new IllegalArgumentException("imports not allowed for security");
+                if ( ss[i].contains("eval") ) throw new IllegalArgumentException("eval not allowed for security");
+            }
+                    
+            PythonInterpreter interp = new PythonInterpreter();
+            interp.setOut( new LoggingOutputStream( Logger.getLogger("virbo.scriptservlet"), Level.INFO ) );
+            
+            interp.execfile(AutoPlotUI.class.getResource("imports.py").openStream(), "imports.py");
+            
+            interp.set( "response", response );
+            
+            ScriptContext._setOutputStream( new LoggingOutputStream( Logger.getLogger("virbo.scriptservlet"), Level.INFO ) ); 
+            
+            interp.exec(script);            
+            
+        } finally { 
+            
+            
+        }
+    } 
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+    * Handles the HTTP <code>GET</code> method.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+    * Handles the HTTP <code>POST</code> method.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /** 
+    * Returns a short description of the servlet.
+    */
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ AutoplotServlet/web/script.jsp
@@ -0,0 +1,43 @@
+<%-- 
+    Document   : script
+    Created on : Jul 24, 2008, 9:46:49 AM
+    Author     : jbf
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Autoplot Script Servlet</title>
+    </head>
+    <body>
+
+    <h1>Autoplot Script Servlet</h1>
+    
+    This demonstrates how scripting might be used with Autoplot to provide
+    precise specification of an image.
+    
+    Caveat: this allows arbitrary code to be executed on the server, so this 
+    server should not be left on and should not be advertised.  
+    
+    <form action="ScriptServlet" method="POST">
+        Enter Script:
+        <textarea rows="10" cols="80" name="script" >
+setCanvasSize( 400, 400 )
+setDataSourceURL( 'http://www.sarahandjeremy.net/jeremy/1wire/data/2008/0B000800408DD710.20080117.d2s' )
+setTitle( 'Garage 20080117' )
+
+response.setContentType("image/png");
+out = response.getOutputStream()
+writeToPng( out )
+out.close()
+        </textarea>
+        <input type="submit" value="Plot" />
+    </form>
+
+    
+    </body>
+</html>
