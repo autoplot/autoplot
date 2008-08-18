7eae0f5eda479893b9d58cc79cc701c36b0cac34
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-08-18 14:35:07 +0000

    support for rank 3.  Interpret units.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@828 0c594bb0-8543-0410-a25e-d8ec440261db

--- CdfDataSource/src/org/virbo/cdfdatasource/CdfFileDataSource.java
+++ CdfDataSource/src/org/virbo/cdfdatasource/CdfFileDataSource.java
@@ -6,9 +6,9 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package org.virbo.cdfdatasource;
 
+import edu.uiowa.physics.pw.das.datum.Units;
 import org.virbo.metatree.IstpMetadataModel;
 import org.das2.util.monitor.ProgressMonitor;
 import gsfc.nssdc.cdf.Attribute;
@@ -28,125 +28,147 @@ import org.virbo.dataset.QDataSet;
 import org.virbo.dataset.WritableDataSet;
 import org.virbo.datasource.AbstractDataSource;
 import org.virbo.datasource.MetadataModel;
+import org.virbo.metatree.MetadataUtil;
 
 /**
  *
  * @author jbf
  */
 public class CdfFileDataSource extends AbstractDataSource {
-    
+
     HashMap properties;
-    HashMap<String,Object> attributes;
-    
+    HashMap<String, Object> attributes;
+
     /** Creates a new instance of CdfFileDataSource */
-    public CdfFileDataSource( URL url ) {
-        super( url );
+    public CdfFileDataSource(URL url) {
+        super(url);
     }
-    
-    
+
     /* read all the variable attributes into a HashMap */
-    private HashMap<String,Object> readAttributes( CDF cdf, Variable var, int depth ) {
-        HashMap<String,Object> properties= new HashMap<String,Object>();
-        Pattern p= Pattern.compile("DEPEND_[0-9]");
-	
-        Vector v= cdf.getAttributes();
-        for ( int i=0; i<v.size(); i++ ) {
-            Attribute attr= (Attribute)v.get(i);
-            Entry entry= null;
+    private HashMap<String, Object> readAttributes(CDF cdf, Variable var, int depth) {
+        HashMap<String, Object> properties = new HashMap<String, Object>();
+        Pattern p = Pattern.compile("DEPEND_[0-9]");
+
+        Vector v = cdf.getAttributes();
+        for (int i = 0; i < v.size(); i++) {
+            Attribute attr = (Attribute) v.get(i);
+            Entry entry = null;
             try {
-                entry= attr.getEntry(var);
-		
-		
-		if ( p.matcher(attr.getName()).matches() & depth==0 ) {
-                    Object val= entry.getData();
-		    String name= (String)val;
-		    Map<String,Object> newVal= readAttributes( cdf, cdf.getVariable(name), depth+1 );
-                    newVal.put( "NAME", name ); // tuck it away, we'll need it later.
-		    properties.put( attr.getName(), newVal );
-                    
-		} else {
-                    properties.put( attr.getName(), entry.getData() );
+                entry = attr.getEntry(var);
+
+
+                if (p.matcher(attr.getName()).matches() & depth == 0) {
+                    Object val = entry.getData();
+                    String name = (String) val;
+                    Map<String, Object> newVal = readAttributes(cdf, cdf.getVariable(name), depth + 1);
+                    newVal.put("NAME", name); // tuck it away, we'll need it later.
+                    properties.put(attr.getName(), newVal);
+
+                } else {
+                    properties.put(attr.getName(), entry.getData());
                 }
-            } catch ( CDFException e ) {
+            } catch (CDFException e) {
             }
         }
-        
+
         return properties;
     }
-    
-    
-    public org.virbo.dataset.QDataSet getDataSet( ProgressMonitor mon ) throws IOException, CDFException {
+
+    public org.virbo.dataset.QDataSet getDataSet(ProgressMonitor mon) throws IOException, CDFException {
         File cdfFile;
-        cdfFile= getFile( mon );
-        
-        String fileName= cdfFile.toString();
-        if ( System.getProperty("os.name").startsWith("Windows") ) fileName= CdfUtil.win95Name( cdfFile );
-        
-        Map map= getParams();
-        
-        CDF cdf= CDF.open( fileName, CDF.READONLYon );
-        String svariable= (String) map.get("id");
-        if ( svariable==null ) svariable= (String) map.get("arg_0");
-        
-        Variable variable= cdf.getVariable( svariable );
-        attributes= readAttributes( cdf, variable, 0 );
-        
-        WritableDataSet result= wrapDataSet(cdf, svariable, false);
+        cdfFile = getFile(mon);
+
+        String fileName = cdfFile.toString();
+        if (System.getProperty("os.name").startsWith("Windows"))
+            fileName = CdfUtil.win95Name(cdfFile);
+
+        Map map = getParams();
+
+        CDF cdf = CDF.open(fileName, CDF.READONLYon);
+        String svariable = (String) map.get("id");
+        if (svariable == null)
+            svariable = (String) map.get("arg_0");
+
+        Variable variable = cdf.getVariable(svariable);
+        attributes = readAttributes(cdf, variable, 0);
+
+        WritableDataSet result = wrapDataSet(cdf, svariable, false);
         cdf.close();
-        
+
         return result;
-        
+
     }
-    
+
     /**
      * @param reform for depend_1, we read the one and only rec, and the rank is decreased by 1.
      */
-    private WritableDataSet wrapDataSet( final CDF cdf, final String svariable, boolean reform ) throws CDFException {
-        Variable variable= cdf.getVariable( svariable );
-        
-        long varType= variable.getDataType();
-        long numRec= variable.getNumWrittenRecords();
-        
-        if ( numRec==0 ) throw new IllegalArgumentException("variable "+svariable+" contains no records!");
-        
+    private WritableDataSet wrapDataSet(final CDF cdf, final String svariable, boolean reform) throws CDFException {
+        Variable variable = cdf.getVariable(svariable);
+
+        long varType = variable.getDataType();
+        long numRec = variable.getNumWrittenRecords();
+
+        if (numRec == 0)
+            throw new IllegalArgumentException("variable " + svariable + " contains no records!");
+
         WritableDataSet result;
-        if ( reform ) {
-            result= CdfUtil.wrapCdfHyperData( variable, 0, -1 );
+        if (reform) {
+            result = CdfUtil.wrapCdfHyperData(variable, 0, -1);
         } else {
-            result= CdfUtil.wrapCdfHyperData( variable, 0, numRec );
+            result = CdfUtil.wrapCdfHyperData(variable, 0, numRec);
         }
-        result.putProperty( QDataSet.NAME, svariable );
-        HashMap thisAttributes= readAttributes( cdf, variable, 0 );
-        
-        Map dep0m= (Map) thisAttributes.get( "DEPEND_0" );
-        if ( dep0m!=null ) {
-            try {
-                WritableDataSet depDs= wrapDataSet( cdf, (String)dep0m.get("NAME") , false);
-                if ( DataSetUtil.isMonotonic(depDs) ) {
-                    depDs.putProperty( QDataSet.MONOTONIC, Boolean.TRUE );
-                }
-                result.putProperty( QDataSet.DEPEND_0, depDs );
-            } catch ( Exception e ) {
-                e.printStackTrace();
+        result.putProperty(QDataSet.NAME, svariable);
+        HashMap thisAttributes = readAttributes(cdf, variable, 0);
+
+        final boolean doUnits= true;
+        if ( doUnits && thisAttributes.containsKey("UNITS" ) ) {
+            Units mu= MetadataUtil.lookupUnits( (String)thisAttributes.get("UNITS") );
+            Units u= (Units) result.property(QDataSet.UNITS);
+            if ( u==null ) {
+                result.putProperty(QDataSet.UNITS, mu);
             }
         }
         
-        Map dep1m= (Map) thisAttributes.get( "DEPEND_1" );
-        if ( dep1m!=null ) {
-            try {
-                WritableDataSet depDs= wrapDataSet( cdf, (String)dep1m.get("NAME"), true );
-                if ( DataSetUtil.isMonotonic(depDs) ) {
-                    depDs.putProperty( QDataSet.MONOTONIC, Boolean.TRUE );
+        for (int idep = 0; idep < 3; idep++) {
+            Map dep = (Map) thisAttributes.get("DEPEND_" + idep);
+            if (dep != null) {
+                try {
+                    WritableDataSet depDs = wrapDataSet( cdf, (String) dep.get("NAME"), idep>0 );
+                    if (DataSetUtil.isMonotonic(depDs)) {
+                        depDs.putProperty(QDataSet.MONOTONIC, Boolean.TRUE);
+                    }
+                    result.putProperty("DEPEND_" + idep, depDs);
+                } catch (Exception e) {
+                    e.printStackTrace(); // to support lanl.
                 }
-                result.putProperty( QDataSet.DEPEND_1, depDs );
-            } catch ( Exception e ) {
-                e.printStackTrace(); // to support lanl.
+            } else {
+                String s = (String) thisAttributes.get("LABL_PTR_" + idep);
+                if (s != null) {
+                    try {
+                        WritableDataSet depDs = wrapDataSet( cdf, s, idep>0 );
+                        result.putProperty("DEPEND_" + idep, depDs);
+                    } catch (Exception e) {
+                        e.printStackTrace(); // to support lanl.
+                    }
+                }
             }
+        }
+
+        if ( result.rank()==3 ) { // need to swap for rank 3.
+            QDataSet dep1= (QDataSet) result.property(QDataSet.DEPEND_1);
+            QDataSet dep2= (QDataSet) result.property(QDataSet.DEPEND_2);
+            result.putProperty(QDataSet.DEPEND_2, dep1 );
+            result.putProperty(QDataSet.DEPEND_1, dep2 );
+            
+            Object att1= attributes.get( QDataSet.DEPEND_1 );
+            Object att2= attributes.get( QDataSet.DEPEND_2 );
+            attributes.put( QDataSet.DEPEND_1, att2 );
+            attributes.put( QDataSet.DEPEND_2, att1 );
         }
         
         return result;
     }
-    
+
     @Override
     public boolean asynchronousLoad() {
         return true;
@@ -156,13 +178,12 @@ public class CdfFileDataSource extends AbstractDataSource {
     public MetadataModel getMetadataModel() {
         return new IstpMetadataModel();
     }
-        
-    
+
     @Override
-    public Map<String,Object> getMetaData( ProgressMonitor mon ) {
-        if ( attributes==null ) return null; // transient state
-        
+    public Map<String, Object> getMetaData(ProgressMonitor mon) {
+        if (attributes == null)
+            return null; // transient state
+
         return attributes;
     }
-    
 }
