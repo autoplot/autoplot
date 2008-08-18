cbeb6db15d2e700703f83e02a94c6c46f5d51523
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-08-18 13:26:26 +0000

    layout listener ignores changes in components that are not visible.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@816 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/LayoutListener.java
+++ VirboAutoplot/src/org/virbo/autoplot/LayoutListener.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.virbo.autoplot;
 
 import edu.uiowa.physics.pw.das.graph.DasAxis;
 import edu.uiowa.physics.pw.das.graph.DasColorBar;
 import edu.uiowa.physics.pw.das.graph.DasPlot;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -25,39 +25,41 @@ public class LayoutListener implements PropertyChangeListener {
 
     ApplicationModel model;
     Timer t;
-    static Logger logger= Logger.getLogger("virbo.autoplot.autolayout");
-    
-    LayoutListener( ApplicationModel model ) {
-        this.model= model;
-        model.plot.addPropertyChangeListener( DasPlot.PROP_TITLE, this );
-        model.plot.getXAxis().addPropertyChangeListener( DasAxis.PROP_BOUNDS, this );
-        
+    static Logger logger = Logger.getLogger("virbo.autoplot.autolayout");
+
+    LayoutListener(ApplicationModel model) {
+        this.model = model;
+        model.plot.addPropertyChangeListener(DasPlot.PROP_TITLE, this);
+        model.plot.getXAxis().addPropertyChangeListener(DasAxis.PROP_BOUNDS, this);
+
         //model.plot.getYAxis().addPropertyChangeListener( DasAxis.PROP_LABEL, this );
-        model.plot.getYAxis().addPropertyChangeListener( DasAxis.PROP_BOUNDS, this );
-        
-        model.colorbar.addPropertyChangeListener( "visible", this );
-        model.colorbar.addPropertyChangeListener( DasAxis.PROP_BOUNDS, this );
+        model.plot.getYAxis().addPropertyChangeListener(DasAxis.PROP_BOUNDS, this);
+
+        model.colorbar.addPropertyChangeListener("visible", this);
+        model.colorbar.addPropertyChangeListener(DasAxis.PROP_BOUNDS, this);
 
     }
-    
+
     public void propertyChange(PropertyChangeEvent evt) {
-        if ( model.autolayout ) {
-            logger.fine("property change: "+evt.getPropertyName() );
-            if ( t==null ) { 
-                logger.fine("create timer " );
-                t= new Timer( 100, new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        logger.fine("do autolayout");
-                        model.canvas.performingChange( this, "autolayout" );
-                        LayoutUtil.autolayout( model.canvas, model.plot.getRow(), model.plot.getColumn() );
-                        model.canvas.changePerformed( this, "autolayout" );
-                    }
-                } );
-                t.setRepeats(false);
+        if (model.autolayout) {
+            logger.fine("property change: " + evt.getPropertyName());
+            if (evt.getSource() instanceof Component &&
+                    ((Component) evt.getSource()).isVisible()) {
+                if (t == null) {
+                    logger.fine("create timer ");
+                    t = new Timer(100, new ActionListener() {
+                        public void actionPerformed(ActionEvent e) {
+                            logger.fine("do autolayout");
+                            model.canvas.performingChange(this, "autolayout");
+                            LayoutUtil.autolayout(model.canvas, model.plot.getRow(), model.plot.getColumn());
+                            model.canvas.changePerformed(this, "autolayout");
+                        }
+                    });
+                    t.setRepeats(false);
+                }
+                model.canvas.registerPendingChange(this, "autolayout");
+                t.restart();
             }
-            model.canvas.registerPendingChange( this, "autolayout" );
-            t.restart();
         }
     }
-
 }
