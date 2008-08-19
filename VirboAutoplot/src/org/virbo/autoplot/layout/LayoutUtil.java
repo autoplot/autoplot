410eaacb1e038b341e77a197ed8ec1b8f41c52c6
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-08-19 15:38:50 +0000

    bugfix: ignore tweaks to position that are less then a 0.1 em.  This fixes a bug where some plots would jiggle by one pixel, appearently due to roundoff errors.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@851 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/layout/LayoutUtil.java
+++ VirboAutoplot/src/org/virbo/autoplot/layout/LayoutUtil.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.virbo.autoplot.layout;
 
 import edu.uiowa.physics.pw.das.graph.DasCanvas;
@@ -19,6 +18,25 @@ import java.util.List;
  * @author jbf
  */
 public class LayoutUtil {
+
+    private static boolean ALLOW_EXCESS_SPACE= true;
+    
+    private static void maybeSetMaximum( DasDevicePosition c, double need, double norm, double em, int pt ) {
+       if ( ALLOW_EXCESS_SPACE && em < 0) return;
+       if ( Math.abs(c.getEmMaximum()-em )<0.1 ) return;
+        c.setMaximum(norm);
+        c.setEmMaximum(em);
+        c.setPtMaximum(pt);
+    }
+    
+    private static void maybeSetMinimum( DasDevicePosition c, double need, double norm, double em, int pt ) {
+        if ( ALLOW_EXCESS_SPACE && em < 0 ) return;
+        if ( Math.abs(c.getEmMinimum()-em)<0.1 ) return;
+        c.setMinimum(norm);
+        c.setEmMinimum(em);
+        c.setPtMinimum(pt);
+    }
+    
     /**
      * resets the layout on the canvas so that labels are not clipped (somewhat).
      * Child row and columns are inspected as well, and it's assumed that adjusting
@@ -32,112 +50,99 @@ public class LayoutUtil {
      * @param c
      * @param r 
      */
-    public static void autolayout( DasCanvas canvas, DasRow r, DasColumn c ) {
+    public static void autolayout(DasCanvas canvas, DasRow r, DasColumn c) {
         // horizontal
-        double em= c.getEmSize();
-        
-        int xmin= 90000;
-        int xmax= -90000;
-        int ymin= 90000;
-        int ymax= -90000;
-        
+        double em = c.getEmSize();
+
+        int xmin = 90000;
+        int xmax = -90000;
+        int ymin = 90000;
+        int ymax = -90000;
+
         Rectangle bounds;
-        for ( DasCanvasComponent cc : canvas.getCanvasComponents() ) {
-            if ( cc.getColumn()==c && cc.isVisible() ) {
-                bounds= cc.getBounds();
-                xmin= Math.min( xmin, bounds.x );
-                xmax= Math.max( xmax, bounds.x + bounds.width );
+        for (DasCanvasComponent cc : canvas.getCanvasComponents()) {
+            if (cc.getColumn() == c && cc.isVisible()) {
+                bounds = cc.getBounds();
+                xmin = Math.min(xmin, bounds.x);
+                xmax = Math.max(xmax, bounds.x + bounds.width);
             }
-            if ( cc.getRow()==r && cc.isVisible() ) {
-                bounds= cc.getBounds();
-                ymin= Math.min( ymin, bounds.y );
-                ymax= Math.max( ymax, bounds.y + bounds.height );
+            if (cc.getRow() == r && cc.isVisible()) {
+                bounds = cc.getBounds();
+                ymin = Math.min(ymin, bounds.y);
+                ymax = Math.max(ymax, bounds.y + bounds.height);
             }
         }
-     
-        bounds= getChildBounds( c );
-        if ( bounds!=null ) {
-            xmin= Math.min( xmin, bounds.x );
-            xmax= Math.max( xmax, bounds.x + bounds.width );
-            ymin= Math.min( ymin, bounds.y );
-            ymax= Math.max( ymax, bounds.y + bounds.height );
+
+        bounds = getChildBounds(c);
+        if (bounds != null) {
+            xmin = Math.min(xmin, bounds.x);
+            xmax = Math.max(xmax, bounds.x + bounds.width);
+            ymin = Math.min(ymin, bounds.y);
+            ymax = Math.max(ymax, bounds.y + bounds.height);
         }
-         
-        bounds= getChildBounds( r );
-        if ( bounds!=null ) {
-            xmin= Math.min( xmin, bounds.x );
-            xmax= Math.max( xmax, bounds.x + bounds.width );
-            ymin= Math.min( ymin, bounds.y );
-            ymax= Math.max( ymax, bounds.y + bounds.height );
+
+        bounds = getChildBounds(r);
+        if (bounds != null) {
+            xmin = Math.min(xmin, bounds.x);
+            xmax = Math.max(xmax, bounds.x + bounds.width);
+            ymin = Math.min(ymin, bounds.y);
+            ymax = Math.max(ymax, bounds.y + bounds.height);
         }
-        
-        double MARGIN_LEFT_RIGHT_EM= 1;
-        
-        boolean ALLOW_EXCESS_SPACE= true;
-        
+
+        double MARGIN_LEFT_RIGHT_EM = 1;
+
         int old, need;
-        old= c.getDMinimum();
-        need= old - xmin;
-        if ( ALLOW_EXCESS_SPACE && need < 0 ) need= 0;
-        c.setMinimum(0);
-        c.setEmMinimum( need / em + MARGIN_LEFT_RIGHT_EM );
-        c.setPtMinimum(0);
+        old = c.getDMinimum();
+        need = old - xmin;
+        maybeSetMinimum( c, need, 0, need / em + MARGIN_LEFT_RIGHT_EM, 0 );
+
+        old = c.getDMaximum();
+        need = xmax - old;
+        maybeSetMaximum(c,need, 1.0, -need / em - MARGIN_LEFT_RIGHT_EM,0);
+
+        old = r.getDMinimum();
+        need = old - ymin;
+        maybeSetMinimum(r, need, 0, need / em, 0);
+
+        old = r.getDMaximum();
+        need = ymax - old;
+        maybeSetMaximum(r, need, 1.0, -need/em,0);
         
-        old= c.getDMaximum();
-        need= xmax - old;
-        if ( ALLOW_EXCESS_SPACE && need < 0 ) need= 0;
-        c.setMaximum(1.0);
-        c.setEmMaximum( -need / em - MARGIN_LEFT_RIGHT_EM );
-        c.setPtMaximum(0);
-        
-        old= r.getDMinimum();
-        need= old - ymin;
-        if ( ALLOW_EXCESS_SPACE && need < 0 ) need= 0;
-        r.setMinimum(0);
-        r.setEmMinimum( need / em );
-        r.setPtMinimum(0);
-        
-        old= r.getDMaximum();
-        need= ymax - old;
-        if ( ALLOW_EXCESS_SPACE && need < 0 ) need= 0;        
-        r.setMaximum(1.0);
-        r.setEmMaximum( -need / em );
-        r.setPtMaximum(0);
     }
-    
+
     /**
      * 
      * @param col
      * @return
      */
-    public static List<DasDevicePosition> getChildColumns( DasDevicePosition col, boolean doCol ) {
-        DasCanvas canvas= col.getParent();
-        List<DasDevicePosition> result= new ArrayList<DasDevicePosition>();
-        for ( DasCanvasComponent cc : canvas.getCanvasComponents() ) {
-            DasDevicePosition ccol= doCol ? cc.getColumn() : cc.getRow();
-            if ( cc.getColumn().getParentDevicePosition()==col ) {
-                result.add( cc.getColumn() );
+    public static List<DasDevicePosition> getChildColumns(DasDevicePosition col, boolean doCol) {
+        DasCanvas canvas = col.getParent();
+        List<DasDevicePosition> result = new ArrayList<DasDevicePosition>();
+        for (DasCanvasComponent cc : canvas.getCanvasComponents()) {
+            DasDevicePosition ccol = doCol ? cc.getColumn() : cc.getRow();
+            if (cc.getColumn().getParentDevicePosition() == col) {
+                result.add(cc.getColumn());
             }
         }
         return result;
     }
-    
+
     /**
      * look for attached columns, get the bounds of all attachments.
      * @param col
      * @return the bounds of all children, or null.
      */
-    public static Rectangle getChildBounds( DasColumn col ) {
-        DasCanvas canvas= col.getParent();
-        Rectangle rect= null;
-        List<DasDevicePosition> result= new ArrayList<DasDevicePosition>();
-        for ( DasCanvasComponent cc : canvas.getCanvasComponents() ) {
-            DasDevicePosition ccol= cc.getColumn();
-            if ( cc.getColumn().getParentDevicePosition()==col && cc.isVisible() ) {
-                if ( rect==null ) {
-                    rect= cc.getBounds() ;
+    public static Rectangle getChildBounds(DasColumn col) {
+        DasCanvas canvas = col.getParent();
+        Rectangle rect = null;
+        List<DasDevicePosition> result = new ArrayList<DasDevicePosition>();
+        for (DasCanvasComponent cc : canvas.getCanvasComponents()) {
+            DasDevicePosition ccol = cc.getColumn();
+            if (cc.getColumn().getParentDevicePosition() == col && cc.isVisible()) {
+                if (rect == null) {
+                    rect = cc.getBounds();
                 } else {
-                    rect.add( cc.getBounds() );
+                    rect.add(cc.getBounds());
                 }
             }
         }
@@ -149,21 +154,20 @@ public class LayoutUtil {
      * @param row
      * @return the bounds of all children, or null.
      */
-    public static Rectangle getChildBounds( DasRow row ) {
-        DasCanvas canvas= row.getParent();
-        Rectangle rect= null;
-        List<DasDevicePosition> result= new ArrayList<DasDevicePosition>();
-        for ( DasCanvasComponent cc : canvas.getCanvasComponents() ) {
-            DasDevicePosition ccol= cc.getColumn();
-            if ( cc.getRow().getParentDevicePosition()==row && cc.isVisible() ) {
-                if ( rect==null ) {
-                    rect= cc.getBounds() ;
+    public static Rectangle getChildBounds(DasRow row) {
+        DasCanvas canvas = row.getParent();
+        Rectangle rect = null;
+        List<DasDevicePosition> result = new ArrayList<DasDevicePosition>();
+        for (DasCanvasComponent cc : canvas.getCanvasComponents()) {
+            DasDevicePosition ccol = cc.getColumn();
+            if (cc.getRow().getParentDevicePosition() == row && cc.isVisible()) {
+                if (rect == null) {
+                    rect = cc.getBounds();
                 } else {
-                    rect.add( cc.getBounds() );
+                    rect.add(cc.getBounds());
                 }
             }
         }
         return rect;
     }
-    
 }
