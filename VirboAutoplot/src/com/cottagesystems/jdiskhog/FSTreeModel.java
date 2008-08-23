37885b45ef96751b22757e7d489769f576478739
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-08-23 15:42:53 +0000

    in the FileSystemTree for the cache tool, combine folders with single folder or file into one node.  This is to remove the tedium of having to drill down through all the single folders, and to improve readability.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@874 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/com/cottagesystems/jdiskhog/FSTreeModel.java
+++ VirboAutoplot/src/com/cottagesystems/jdiskhog/FSTreeModel.java
@@ -25,25 +25,39 @@ public class FSTreeModel implements TreeModel {
 
     class TreeNode {
 
+        File parentFile;
         File f;
 
         TreeNode(File f) {
-            this.f = f;
+            this( f, null );
+        }
+        
+        TreeNode( File f, File parentFile ) {
+            this.f= f;
+            this.parentFile= parentFile;
         }
 
         public File getFile() {
             return f;
         }
 
+        private String getName() {
+            if ( parentFile==null ) {
+                return f.getName();
+            } else {
+                return f.toString().substring( parentFile.toString().length()+1 );
+            }
+        }
+        
         public String toString() {
             if (f.isFile()) {
-                return f.getName() + " " + nf.format(f.length() / 1000) + " KB";
+                return getName() + " " + nf.format(f.length() / 1000) + " KB";
             } else {
                 Long usages = model.usage(f);
                 if (usages == null) {
-                    return f.getName() + " ??? KB";
+                    return getName() + " ??? KB";
                 } else {
-                    return f.getName() + " " + nf.format(model.usage(f)) + " KB";
+                    return getName() + " " + nf.format(model.usage(f)) + " KB";
                 }
             }
         }
@@ -62,8 +76,23 @@ public class FSTreeModel implements TreeModel {
     }
 
     public Object getChild(Object parent, int index) {
-        File[] ff = listings.get(((TreeNode) parent).getFile());
-        return new TreeNode(ff[index]);
+        File fparent= ((TreeNode) parent).getFile();
+        File[] ff = listings.get(fparent);
+
+        File reportParent= null;
+        
+        File ff1 = ff[index];
+        while ( ff1.isDirectory() ) {
+            File[] ffs = ff1.listFiles();
+            if (ffs.length == 1) {
+                ff1 = ffs[0];
+            } else {
+                break;
+            }
+            reportParent= fparent;
+        }
+
+        return new TreeNode(ff1,fparent);
     }
 
     public synchronized int getChildCount(Object parent) {
@@ -71,52 +100,53 @@ public class FSTreeModel implements TreeModel {
         File[] ff = listings.get(f);
         if (ff == null) {
             ff = f.listFiles();
-            Arrays.sort( ff, new Comparator() {
+            Arrays.sort(ff, new Comparator() {
+
                 public int compare(Object o1, Object o2) {
-                    long s1,s2;
-                    File f1=(File)o1, f2=(File)o2;
-                    
-                    if ( f1==null || f2==null ) {
-                        System.err.println( ""+ f + " lists null!" );
+                    long s1, s2;
+                    File f1 = (File) o1, f2 = (File) o2;
+
+                    if (f1 == null || f2 == null) {
+                        System.err.println("" + f + " lists null!");
                         return -1;
                     }
-                    
-                    
-                    if ( f1.isFile() ) {
-                        s1= f1.length() / 1000;
+
+
+                    if (f1.isFile()) {
+                        s1 = f1.length() / 1000;
                     } else {
-                        Long l= model.usage(f1);
-                        if ( l==null ) {
-                            s1=0;
+                        Long l = model.usage(f1);
+                        if (l == null) {
+                            s1 = 0;
                         } else {
-                            s1= l;
+                            s1 = l;
                         }
                     }
-                    
-                    if ( f2.isFile() ) {
-                        s2= f2.length() / 1000;
+
+                    if (f2.isFile()) {
+                        s2 = f2.length() / 1000;
                     } else {
-                        Long l= model.usage(f2);
-                        if ( l==null ) {
-                            s2=0;
+                        Long l = model.usage(f2);
+                        if (l == null) {
+                            s2 = 0;
                         } else {
-                            s2= l;
+                            s2 = l;
                         }
                     }
-                    
-                    if ( f1.isFile()&&f2.isFile() ) {
-                        String key1= ((File)o1).getName();
-                        String key2= ((File)o2).getName();
+
+                    if (f1.isFile() && f2.isFile()) {
+                        String key1 = ((File) o1).getName();
+                        String key2 = ((File) o2).getName();
                         return key1.compareTo(key2);
-            
-                    } else if ( f1.isDirectory()&&f2.isDirectory() ) {
-                        return s1<s2 ? 1 : -1;
+
+                    } else if (f1.isDirectory() && f2.isDirectory()) {
+                        return s1 < s2 ? 1 : -1;
                     } else {
                         return f1.isDirectory() ? -1 : 1;
                     }
-                    
+
                 }
-            } );
+            });
             listings.put(f, ff);
         }
         return ff.length;
@@ -131,12 +161,11 @@ public class FSTreeModel implements TreeModel {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-
     public File getFile(TreePath context) {
-        Object node= context.getPathComponent(context.getPathCount()-1);
-        return ((TreeNode)node).getFile();
+        Object node = context.getPathComponent(context.getPathCount() - 1);
+        return ((TreeNode) node).getFile();
     }
-    
+
     public int getIndexOfChild(Object parent, Object child) {
         File f = ((TreeNode) parent).getFile();
         File fchild = ((TreeNode) child).getFile();
