a638a8e481443ba003e0505aab9fa590736f4457
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-08-23 16:31:14 +0000

    javadoc improvements
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@889 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonSupport/src/org/das2/jythoncompletion/Utilities.java
+++ JythonSupport/src/org/das2/jythoncompletion/Utilities.java
@@ -16,18 +16,38 @@ import javax.swing.text.JTextComponent;
  */
 public class Utilities {
 
-    static int getRowStart(JTextComponent editor, int pos) {
+    /**
+     * return the offset of the first character of the line.
+     * See javax.swing.text.Utilities, it's the same thing.
+     * @param editor
+     * @param pos
+     * @return
+     */
+    public static int getRowStart(JTextComponent editor, int pos) {
         Element root = editor.getDocument().getDefaultRootElement();
         int iele= root.getElementIndex(pos);
         return root.getElement(iele).getStartOffset();
     }
 
-    static int getRowEnd(JTextComponent editor, int pos) {
+    /**
+     * return the offset of the last character of the line.
+     * @param editor
+     * @param pos
+     * @return
+     */
+    public static int getRowEnd(JTextComponent editor, int pos) {
         Element root = editor.getDocument().getDefaultRootElement();
         int iele= root.getElementIndex(pos);
         return root.getElement(iele).getEndOffset();
     }
 
+    /**
+     * return the line number that contains the offset.
+     * @param a
+     * @param offset
+     * @return the line number, zero is the first line.
+     * @throws javax.swing.text.BadLocationException
+     */
     public static int getLineNumberForOffset(JTextArea a, int offset) throws BadLocationException {
         int line = 0;
         while (line < a.getRows()) {
