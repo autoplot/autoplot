24a612377c586d9cccf6b5a03ba5eee3eca01d52
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-08-26 21:52:15 +0000

    clean up imports
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@915 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/com/cottagesystems/jdiskhog/DiskUsageModel.java
+++ VirboAutoplot/src/com/cottagesystems/jdiskhog/DiskUsageModel.java
@@ -6,12 +6,8 @@ package com.cottagesystems.jdiskhog;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
