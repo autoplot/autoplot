616b288e514e766916198f5ef1b6ffd2bf10ec5b
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-09-05 01:41:34 +0000

    improve support for cdf.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@954 0c594bb0-8543-0410-a25e-d8ec440261db

--- CdfDataSource/src/org/virbo/cdfdatasource/CdfDataSourceFormat.java
+++ CdfDataSource/src/org/virbo/cdfdatasource/CdfDataSourceFormat.java
@@ -9,11 +9,10 @@ import edu.uiowa.physics.pw.das.datum.UnitsConverter;
 import edu.uiowa.physics.pw.das.datum.UnitsUtil;
 import gsfc.nssdc.cdf.Attribute;
 import gsfc.nssdc.cdf.CDF;
-import gsfc.nssdc.cdf.CDFConstants;
 import gsfc.nssdc.cdf.CDFException;
+import gsfc.nssdc.cdf.Entry;
 import gsfc.nssdc.cdf.Variable;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,6 +20,7 @@ import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.dataset.QDataSet;
 import org.virbo.dataset.QubeDataSetIterator;
 import org.virbo.datasource.datasource.DataSourceFormat;
+import static gsfc.nssdc.cdf.CDFConstants.*;
 
 /**
  * Format the QDataSet into CDF tables.  
@@ -65,31 +65,34 @@ public class CdfDataSourceFormat implements DataSourceFormat {
         if (dep0 != null) {
             String name= nameFor(dep0);
             addVariableRank1(dep0, name);
-            depend_0 = Attribute.create(cdf, "DEPEND_0", CDFConstants.VARIABLE_SCOPE);
+            depend_0 = Attribute.create(cdf, "DEPEND_0", VARIABLE_SCOPE);
         }
         
         QDataSet dep1 = (QDataSet) data.property(QDataSet.DEPEND_1);
 
         if (dep1 != null) {
             String name= nameFor(dep1);
-            addVariableRank1(dep1, name);
-            depend_1 = Attribute.create(cdf, "DEPEND_1", CDFConstants.VARIABLE_SCOPE);
+            addVariableRank1NoVary(dep1, name);
+            depend_1 = Attribute.create(cdf, "DEPEND_1", VARIABLE_SCOPE);
         }
 
-        addVariableRank1(data, nameFor(data) );
-
+        Variable var= addVariableRank1(data, nameFor(data) );
+        
+        if ( depend_0!=null ) Entry.create( depend_0, var.getID(), CDF_CHAR, nameFor(dep0) );
+        if ( depend_1!=null ) Entry.create( depend_1, var.getID(), CDF_CHAR, nameFor(dep1) );
+        
         cdf.close();
 
     }
 
-    private Variable addVariableRank1(QDataSet ds, String name) throws CDFException {
+    private Variable addVariableRank1NoVary( QDataSet ds, String name ) throws CDFException {
         Units units = (Units) ds.property(QDataSet.UNITS);
-        long type = CDFConstants.CDF_DOUBLE;
+        long type = CDF_DOUBLE;
 
         UnitsConverter uc = UnitsConverter.IDENTITY;
 
         if (units != null && UnitsUtil.isTimeLocation(units)) {
-            type = CDFConstants.CDF_EPOCH;
+            type = CDF_EPOCH;
             uc = units.getConverter(Units.cdfEpoch);
         }
 
@@ -97,7 +100,42 @@ public class CdfDataSourceFormat implements DataSourceFormat {
         
         if ( ds.rank()==1 ) {
             var = Variable.create(cdf, name, type,
-                1L, 0L, new long[]{1}, CDFConstants.VARY, new long[]{CDFConstants.NOVARY});
+                1L, 1L, new long[]{ds.length()}, NOVARY, new long[]{VARY});
+
+            double[][] dexport = new double[1][];
+
+            dexport[0]= new double[ds.length()];
+            for ( int j=0; j<ds.length(); j++ ) {
+                dexport[0][j]= uc.convert(ds.value(j));
+            }
+
+            var.putHyperData( 0, 1, 1L, 
+                    new long[] { 0 }, 
+                    new long[] { ds.length() },
+                    new long[] { 1 }, 
+                    dexport );
+        } else {
+            throw new IllegalArgumentException("not supported!");
+        }
+        return var;
+    }
+    
+    private Variable addVariableRank1(QDataSet ds, String name) throws CDFException {
+        Units units = (Units) ds.property(QDataSet.UNITS);
+        long type = CDF_DOUBLE;
+
+        UnitsConverter uc = UnitsConverter.IDENTITY;
+
+        if (units != null && UnitsUtil.isTimeLocation(units)) {
+            type = CDF_EPOCH;
+            uc = units.getConverter(Units.cdfEpoch);
+        }
+
+        Variable var;
+        
+        if ( ds.rank()==1 ) {
+            var = Variable.create(cdf, name, type,
+                1L, 0L, new long[]{1}, VARY, new long[]{NOVARY});
 
             double[] dexport = new double[ds.length()];
 
@@ -114,7 +152,7 @@ public class CdfDataSourceFormat implements DataSourceFormat {
                     dexport );
         } else if ( ds.rank()==2 ) {
             var = Variable.create(cdf, name, type,
-                1L, 1L, new long[]{ds.length(0)}, CDFConstants.VARY, new long[]{CDFConstants.NOVARY});
+                1L, 1L, new long[]{ds.length(0)}, VARY, new long[]{VARY});
 
             double[][] dexport = new double[ds.length()][];
 
@@ -125,14 +163,6 @@ public class CdfDataSourceFormat implements DataSourceFormat {
                 }
             }
 
-            //double[] dexport2 = new double[ds.length()*ds.length(0)];
-            //QubeDataSetIterator iter = new QubeDataSetIterator(ds);
-            //int i = 0;
-            //while (iter.hasNext()) {
-            //    iter.next();
-            //    dexport2[i++] = uc.convert(iter.getValue(ds));
-            //}
-            
             var.putHyperData( 0, ds.length(), 1L, 
                     new long[] { 0 }, 
                     new long[] { ds.length(0) },
@@ -142,21 +172,6 @@ public class CdfDataSourceFormat implements DataSourceFormat {
         } else {
             throw new IllegalArgumentException("rank limit");
         }
-
-       /* QDataSet dep0 = (QDataSet) ds.property(QDataSet.DEPEND_0);
-        if ( dep0!=null ) {
-            String dep0name= nameFor(dep0);
-        
-            Entry entry= Entry.create( depend_0, var.getID(), CDFConstants.CDF_CHAR, dep0name );
-        }
-        
-        
-        QDataSet dep1=  (QDataSet) ds.property(QDataSet.DEPEND_1);
-        if ( dep1!=null ) {
-            String dep1name= nameFor(dep1);
-        
-            Entry entry= Entry.create( depend_1, var.getID(), CDFConstants.CDF_CHAR, dep1name );
-        }*/
 
         return var;
     }
