f5187cb8906421ebd46123b2eb4dd8d20dd5821a
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-09-24 13:37:36 +0000

    if the extension is .qds, then serialize to QStream instead of legacy das2stream.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1040 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/virbo/das2Stream/Das2StreamDataSourceFormat.java
+++ DataSourcePack/src/org/virbo/das2Stream/Das2StreamDataSourceFormat.java
@@ -14,6 +14,7 @@ import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.dataset.QDataSet;
 import org.virbo.dataset.TableDataSetAdapter;
 import org.virbo.dataset.VectorDataSetAdapter;
+import org.virbo.datasource.DataSetURL;
 import org.virbo.datasource.datasource.DataSourceFormat;
 
 /**
@@ -22,17 +23,22 @@ import org.virbo.datasource.datasource.DataSourceFormat;
  */
 public class Das2StreamDataSourceFormat implements DataSourceFormat {
 
-    public void formatData(File url, java.util.Map<String,String> params, QDataSet data, ProgressMonitor mon) throws Exception {
-        if ( data.rank() == 2 ) {
-            TableDataSet tds = TableDataSetAdapter.create( data );
-            FileOutputStream fo = new FileOutputStream( url );
-            TableUtil.dumpToAsciiStream(tds, fo);
-            fo.close();
-        } else if ( data.rank() == 1) {
-            VectorDataSet vds = VectorDataSetAdapter.create(data);
-            FileOutputStream fo = new FileOutputStream( url );
-            VectorUtil.dumpToAsciiStream(vds, fo);
-            fo.close();
+    public void formatData(File url, java.util.Map<String, String> params, QDataSet data, ProgressMonitor mon) throws Exception {
+        DataSetURL.URLSplit split = DataSetURL.parse(url.toURI().toString());
+        if (split.ext.equals(".qds")) {
+            new org.virbo.qstream.SimpleStreamFormatter().format( data, new FileOutputStream(url), true );
+        } else {
+            if (data.rank() == 2) {
+                TableDataSet tds = TableDataSetAdapter.create(data);
+                FileOutputStream fo = new FileOutputStream(url);
+                TableUtil.dumpToAsciiStream(tds, fo);
+                fo.close();
+            } else if (data.rank() == 1) {
+                VectorDataSet vds = VectorDataSetAdapter.create(data);
+                FileOutputStream fo = new FileOutputStream(url);
+                VectorUtil.dumpToAsciiStream(vds, fo);
+                fo.close();
+            }
         }
     }
 }
