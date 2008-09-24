f25441a5b0fa408724674fb82e2e875445bc4047
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-09-24 12:26:21 +0000

    TimeSeriesBrowse must provide getResolution and getTimeRange before data is loaded.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1024 0c594bb0-8543-0410-a25e-d8ec440261db

--- TsdsDataSource/src/org/tsds/datasource/TsdsDataSource.java
+++ TsdsDataSource/src/org/tsds/datasource/TsdsDataSource.java
@@ -23,6 +23,7 @@ import java.nio.DoubleBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.text.ParseException;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.logging.Level;
@@ -33,6 +34,8 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
+import org.das2.util.TimeParser;
+import org.das2.util.monitor.NullProgressMonitor;
 import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.binarydatasource.BufferDataSet;
 import org.virbo.dataset.DDataSet;
@@ -55,21 +58,54 @@ class TsdsDataSource extends AbstractDataSource {
 
     public TsdsDataSource(URL url) {
         super(url);
-        addCability(TimeSeriesBrowse.class, getTimeSeriesBrowse());
-    }
-    DatumRange timeRange;
-    Datum resolution;
-    int currentPpd= -1;
-    
-    private static final int SIZE_DOUBLE = 8;
+        try {
+            addCability(TimeSeriesBrowse.class, getTimeSeriesBrowse());
 
-    private static final Logger logger= Logger.getLogger("virbo.tsds.datasource");
+            setTSBParameters();  // we don't yet know the parameter resolution, but the time range is set.
+            
+            ProgressMonitor mon = new NullProgressMonitor();
+
+            mon.setProgressMessage("loading parameter metadata");
+            LinkedHashMap<String, String> params3 = new LinkedHashMap<String, String>(params);
+            params3.put("out", "tsml");
+            params3.remove("ppd");
+
+            URL url3 = new URL("" + this.resourceURL + "?" + DataSetURL.formatParams(params3));
+            initialTsml(url3.openStream());
+
+            haveInitialTsml = true;
+
+            setTSBParameters();
+            // limit the resolution accessible by this data source to that specified by the URL.
+            parameterPpd = currentPpd;
+
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(TsdsDataSource.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(TsdsDataSource.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(TsdsDataSource.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    /**
+     * current timeRange, which will be quantized to granule boundaries.
+     */
+    DatumRange timeRange;
+    /**
+     * current timeRange, which will be quantized to granule boundaries.
+     */
+    Datum resolution;
+    /**
+     * current points per day, should be short-circuit to timeRange.
+     */
+    int currentPpd = -1;
+    private static final int SIZE_DOUBLE = 8;
+    private static final Logger logger = Logger.getLogger("virbo.tsds.datasource");
     Document document;
-    
-    DatumRange parameterRange= null; // extent of the parameter.
-    int parameterPpd= -1 ; // max resolution of the parameter.
-    
-    
+    DatumRange parameterRange = null; // extent of the parameter.
+    int parameterPpd = -1; // max resolution of the parameter.
+    boolean haveInitialTsml = false;
+
     private DatumRange quantize(DatumRange timeRange) {
         timeRange = new DatumRange(TimeUtil.prevMidnight(timeRange.min()), TimeUtil.nextMidnight(timeRange.max()));
         return timeRange;
@@ -77,11 +113,11 @@ class TsdsDataSource extends AbstractDataSource {
 
     private int quantizePpd(Datum resolution) {
         int[] ppds = new int[]{1, 12, 144, 1440, 17280, 86400, 864000};
-        if ( resolution==null ) return 1;
+        if (resolution == null) return 1;
         double resdays = resolution.doubleValue(Units.days);
         double dppd = 1 / resdays;
         int ppd = ppds[ppds.length - 1];
-        for (int i = 0; i < ppds.length && ppds[i]<=parameterPpd; i++) {
+        for (int i = 0; i < ppds.length && ppds[i] <= parameterPpd; i++) {
             if (ppds[i] > dppd) {
                 ppd = ppds[i];
                 return ppd;
@@ -90,15 +126,41 @@ class TsdsDataSource extends AbstractDataSource {
         return parameterPpd;
     }
 
-    boolean inRequest= false;
-    
-    @Override
-    public  QDataSet getDataSet(ProgressMonitor mon) throws Exception {
-        if ( inRequest ) {
-            System.err.println("came back again");
-            
+    private void setTSBParameters() {
+        Map<String, String> params2 = new LinkedHashMap<String, String>(params);
+
+        DatumRange dr0 = DatumRangeUtil.parseTimeRangeValid(params2.get("StartDate"));
+        DatumRange dr1 = DatumRangeUtil.parseTimeRangeValid(params2.get("EndDate"));
+        timeRange = quantize(new DatumRange(dr0.min(), dr1.max()));
+
+        int ppd;
+        String sppd = params2.get("ppd");
+        if (sppd != null) {
+            ppd = Integer.parseInt(sppd);
+            if (ppd > parameterPpd) {
+                currentPpd = parameterPpd;
+            } else {
+                currentPpd = ppd;
+            }
+            resolution = Units.days.createDatum(1.0).divide(currentPpd);
         } else {
-            inRequest= true;
+            ppd = -1;
+            currentPpd = -1;
+            resolution = null;
+        }
+
+    }
+    boolean inRequest = false;
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public QDataSet getDataSet(ProgressMonitor mon) throws Exception {
+
+        if (inRequest) {
+            System.err.println("came back again");
+
+        } else {
+            inRequest = true;
         }
         //http://www-pw.physics.uiowa.edu/das/das2Server
         //?dataset=das2_1/voyager1/pws/sa-4s-pf.new
@@ -115,58 +177,49 @@ class TsdsDataSource extends AbstractDataSource {
 
         int ppd;
         // quantum levels
-       
+
         if (timeRange != null) {
             System.err.println(timeRange.min().toString());
             System.err.println(timeRange.max().toString());
             System.err.println(timeRange.toString());
-            timeRange= quantize(timeRange);
+            timeRange = quantize(timeRange);
             params2.put("StartDate", "" + df.format(timeRange.min()));
             params2.put("EndDate", "" + df.format(TimeUtil.prev(TimeUtil.DAY, timeRange.max())));
         } else {
-            DatumRange dr0 = DatumRangeUtil.parseTimeRangeValid(params2.get("StartDate"));
-            DatumRange dr1 = DatumRangeUtil.parseTimeRangeValid(params2.get("EndDate"));
-            timeRange = quantize(new DatumRange(dr0.min(), dr1.max()));
+            setTSBParameters();
         }
 
-        if (resolution != null) {
-            
-            ppd= quantizePpd(resolution);
-            currentPpd= ppd;
-            
-            params2.put("ppd", "" + ppd);
-            resolution= Units.days.createDatum(1.0).divide(ppd);
-
+        if (currentPpd == -1) {
+            params2.put("ppd", "1");
         } else {
-            String sppd = params2.get("ppd");
-            if (sppd != null) {
-                ppd = Integer.parseInt(sppd);
-            } else {
-                ppd= -1;
-            }
+            params2.put("ppd", "" + currentPpd);
         }
+
 
         mon.setTaskSize(-1);
         mon.started();
-        
-        if ( parameterPpd==-1 ) {
+
+        if (!haveInitialTsml) {
             mon.setProgressMessage("loading parameter metadata");
-            LinkedHashMap params3= new LinkedHashMap(params2);
+            LinkedHashMap params3 = new LinkedHashMap(params2);
             params3.remove("ppd");
             params3.put("out", "tsml");
             URL url3 = new URL("" + this.resourceURL + "?" + DataSetURL.formatParams(params3));
             initialTsml(url3.openStream());
+            haveInitialTsml = true;
         }
-        
-        if ( ppd==-1 ) {
-            ppd= 1;
-            params2.put("ppd", ""+ppd );
+
+        if (currentPpd == -1) {
+            ppd = 1;
+            params2.put("ppd", "" + ppd);
+        } else {
+            ppd = currentPpd;
         }
-        
-        
+
+
         URL url2 = new URL("" + this.resourceURL + "?" + DataSetURL.formatParams(params2));
 
-        
+
         int points = (int) Math.ceil(timeRange.width().doubleValue(Units.days)) * ppd;
         int size = points * SIZE_DOUBLE;
 
@@ -183,8 +236,8 @@ class TsdsDataSource extends AbstractDataSource {
         }
 
         mon.finished();
-        inRequest= false;
-        
+        inRequest = false;
+
         return result;
 
     }
@@ -201,17 +254,29 @@ class TsdsDataSource extends AbstractDataSource {
 
             public void setTimeResolution(Datum d) {
                 resolution = d;
-                currentPpd= quantizePpd(resolution);
+                if (resolution == null) {
+                    currentPpd = -1;
+                } else {
+                    currentPpd = quantizePpd(resolution);
+                    resolution = Units.days.createDatum(1.0).divide(currentPpd);
+                }
             }
 
             public URL getURL() {
-                String sparams = "dataset=" + params.get("dataset") + "&StartDate=" + timeRange.min() + "&EndDate=" + timeRange.max() +
-                        "&ppd="+currentPpd;
                 try {
-                    return new URL(resourceURL, "?" + sparams);
+                    return new URL(TsdsDataSource.this.getURL());
                 } catch (MalformedURLException ex) {
+                    Logger.getLogger(TsdsDataSource.class.getName()).log(Level.SEVERE, null, ex);
                     throw new RuntimeException(ex);
                 }
+            }
+
+            public DatumRange getTimeRange() {
+                return timeRange;
+            }
+
+            public Datum getTimeResolution() {
+                return resolution;
             }
         };
     }
@@ -243,7 +308,7 @@ class TsdsDataSource extends AbstractDataSource {
         }
 
         in.close();
-        
+
         bbuf.flip();
         bbuf.order(ByteOrder.LITTLE_ENDIAN);
 
@@ -256,15 +321,15 @@ class TsdsDataSource extends AbstractDataSource {
     private QDataSet ttags(String sStartTime, int ppd, int points, String sTimePos) {
         Datum cadence = Units.days.createDatum(1).divide(ppd);
         Datum startTime = TimeUtil.createValid(sStartTime);
-        Datum endTime= startTime.add( Units.days.createDatum( (1.*points) / ppd ) );
+        Datum endTime = startTime.add(Units.days.createDatum((1. * points) / ppd));
         Datum t0 = startTime;
         if (sTimePos.equals("center"))
             t0 = t0.add(cadence.divide(2));
 
         try {
-            DDataSet result= DDataSet.copy( Ops.timegen(String.valueOf(t0), String.valueOf(cadence), points) );
-            DatumRange timeRange= new DatumRange( startTime, endTime );
-            result.putProperty( QDataSet.CACHE_TAG, new CacheTag( timeRange, cadence ) );
+            DDataSet result = DDataSet.copy(Ops.timegen(String.valueOf(t0), String.valueOf(cadence), points));
+            DatumRange timeRange = new DatumRange(startTime, endTime);
+            result.putProperty(QDataSet.CACHE_TAG, new CacheTag(timeRange, cadence));
             return result;
         } catch (ParseException ex) {
             throw new RuntimeException(ex);
@@ -278,7 +343,7 @@ class TsdsDataSource extends AbstractDataSource {
      * @throws java.io.IOException
      * @throws org.xml.sax.SAXException
      */
-    private void initialTsml( InputStream in ) throws ParserConfigurationException, IOException, SAXException {
+    private void initialTsml(InputStream in) throws ParserConfigurationException, IOException, SAXException {
         try {
             DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
             InputSource source = new InputSource(in);
@@ -294,21 +359,19 @@ class TsdsDataSource extends AbstractDataSource {
             String sppd = xpath.evaluate("//TSML/IntervalsPerDay/text()", document);
 
             int ppd = Integer.parseInt(sppd);
-            if (parameterPpd == -1) {
-                parameterPpd = ppd;
-            }
+            parameterPpd = ppd;
 
             DatumRange dr0 = DatumRangeUtil.parseTimeRangeValid(sStartTime);
             DatumRange dr1 = DatumRangeUtil.parseTimeRangeValid(sEndTime);
-            
-            parameterRange= new DatumRange( dr0.min(), dr1.max() );
-            
+
+            parameterRange = new DatumRange(dr0.min(), dr1.max());
+
         } catch (XPathExpressionException ex) {
             Logger.getLogger(TsdsDataSource.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
     /**
      * The resource is a TSML file, which is an xml description of a binary stream.  The
      * URL of the stream is found within the description, and this is loaded.  TSML
@@ -328,7 +391,7 @@ class TsdsDataSource extends AbstractDataSource {
             InputSource source = new InputSource(in);
             document = builder.parse(source);
             in.close();
-            
+
             XPathFactory factory = XPathFactory.newInstance();
             XPath xpath = factory.newXPath();
 
@@ -340,11 +403,11 @@ class TsdsDataSource extends AbstractDataSource {
             String sEndTime = xpath.evaluate("//TSML/EndDate/text()", document);
 
             String sppd = xpath.evaluate("//TSML/IntervalsPerDay/text()", document);
-            
+
             int ppd = Integer.parseInt(sppd);
-            if ( parameterPpd==-1 ) {
-                parameterPpd= ppd;
-                
+            if (parameterPpd == -1) {
+                parameterPpd = ppd;
+
             }
 
             DatumRange dr0 = DatumRangeUtil.parseTimeRangeValid(sStartTime);
@@ -370,14 +433,14 @@ class TsdsDataSource extends AbstractDataSource {
             URL dataUrl = new URL(surl);
 
             mon.setProgressMessage("loading mean");
-            logger.fine( "loading "+dataUrl );
+            logger.fine("loading " + dataUrl);
             BufferDataSet data = dataUrl(dataUrl.openStream(), size, points, mon);
 
             boolean minMax = true;
             if (minMax && surl.contains("-filter_0-")) {
                 String sDataMax = surl.replace("-filter_0-", "-filter_2-");
                 //System.err.println(sDataMax);
-                logger.fine( "loading "+sDataMax );
+                logger.fine("loading " + sDataMax);
                 mon.setProgressMessage("loading max");
                 BufferDataSet dataMax = dataUrl(new URL(sDataMax).openStream(), size, points, mon);
                 String sDataMin = surl.replace("-filter_0-", "-filter_3-");
@@ -401,10 +464,20 @@ class TsdsDataSource extends AbstractDataSource {
 
     @Override
     public Map<String, Object> getMetaData(ProgressMonitor mon) throws Exception {
-        Node n= document.getFirstChild();
+        Node n = document.getFirstChild();
         return MetadataUtil.toMetaTree(n);
     }
-    
-    
-    
+
+    @Override
+    public String getURL() {
+        TimeParser tp = TimeParser.create("%Y%m%d");
+
+        String sparams = "param1=" + params.get("param1") 
+                + "&StartDate=" + tp.format(timeRange.min(), null) 
+                + "&EndDate=" + tp.format(timeRange.max(), null) +
+                "&ppd=" + currentPpd;
+        
+        return this.resourceURL.toString() + "?" + sparams;
+       
+    }
 }
