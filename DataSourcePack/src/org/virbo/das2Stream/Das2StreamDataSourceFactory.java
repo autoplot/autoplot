92987edb772c31b613942d8aa10b4a2a658b0b69
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-09-24 17:43:53 +0000

    add completions for QStreams.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1061 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/virbo/das2Stream/Das2StreamDataSourceFactory.java
+++ DataSourcePack/src/org/virbo/das2Stream/Das2StreamDataSourceFactory.java
@@ -4,15 +4,24 @@
  */
 package org.virbo.das2Stream;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.channels.Channels;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.datasource.CompletionContext;
+import org.virbo.datasource.DataSetURL;
 import org.virbo.datasource.DataSource;
 import org.virbo.datasource.DataSourceFactory;
 import org.virbo.datasource.MetadataModel;
+import org.virbo.dsutil.AsciiParser;
+import org.virbo.qstream.QDataSetStreamHandler;
+import org.virbo.qstream.StreamException;
 
 /**
  *
@@ -24,8 +33,17 @@ public class Das2StreamDataSourceFactory implements DataSourceFactory {
         return new Das2StreamDataSource(url);
     }
 
-    public List<CompletionContext> getCompletions(CompletionContext cc,org.das2.util.monitor.ProgressMonitor mon) {
-        return Collections.emptyList();
+    public List<CompletionContext> getCompletions(CompletionContext cc,org.das2.util.monitor.ProgressMonitor mon) throws IOException, StreamException {
+        List<CompletionContext> result= new ArrayList<CompletionContext>();
+        if ( cc.context==cc.CONTEXT_PARAMETER_NAME ) {
+            if ( cc.resource.toString().endsWith(".qds") ) {
+                List<String> params= getNames( cc, mon );
+                for ( String s: params ) {
+                    result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, s ) );
+                }
+            }
+        }
+        return result;
     }
 
     public MetadataModel getMetadataModel(URL url) {
@@ -35,8 +53,21 @@ public class Das2StreamDataSourceFactory implements DataSourceFactory {
     public boolean reject(String surl, ProgressMonitor mon) {
         return false;
     }
-
-    public String urlForServer(String surl) {
-        return surl; // TODO
+    
+    
+    private List<String> getNames( CompletionContext cc, ProgressMonitor mon ) throws IOException, StreamException {
+        
+        Map params= DataSetURL.parseParams( cc.params );
+        Object o;
+        File file= DataSetURL.getFile( cc.resource, mon  );
+        
+        QDataSetStreamHandler h= new QDataSetStreamHandler();
+        h.setReadPackets(false); // don't read any records, just scan for datasets.
+        org.virbo.qstream.StreamTool.readStream( Channels.newChannel(new FileInputStream(file) ), h );
+            
+        return h.getDataSetNames();
+        
     }
+
+
 }
