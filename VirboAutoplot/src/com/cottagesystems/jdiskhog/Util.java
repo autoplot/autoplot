009cede387b91a89bba73a7a0c4f0a3fbc72058e
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-09-24 12:24:08 +0000

    tweak: when fileCopy gets two folders, the source folder is copied into the dest folder.  Before just the files were copied.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1021 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/com/cottagesystems/jdiskhog/Util.java
+++ VirboAutoplot/src/com/cottagesystems/jdiskhog/Util.java
@@ -2,17 +2,21 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.cottagesystems.jdiskhog;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.channels.FileChannel;
 
 /**
  *
  * @author jbf
  */
 public class Util {
+
     /**
      * deletes all files and folders below root, and root, just as "rm -r" would.
      * TODO: check links
@@ -41,10 +45,36 @@ public class Util {
         }
         return success;
     }
-    
-    public static void main( String[] args ) throws IOException {
+
+    public static void fileCopy( File src, File dst ) throws FileNotFoundException, IOException {
+        if ( src.isDirectory() && dst.isDirectory() ) {
+            File dst1= new File( dst, src.getName() );
+            dst1.mkdir();
+            dst= dst1;
+            File[] files= src.listFiles();
+            for ( File f:files ) {
+                if ( f.isDirectory() ) {
+                    dst1= new File( dst, f.getName() );
+                    dst1.mkdir();
+                } else {
+                    dst1= dst;
+                }
+                fileCopy( f, dst1 );
+            }
+            return;
+        } else if ( dst.isDirectory() ) {
+            dst= new File( dst, src.getName() );
+        }
+        FileChannel ic = new FileInputStream(src).getChannel();
+        FileChannel oc = new FileOutputStream(dst).getChannel();
+        ic.transferTo(0, ic.size(), oc);
+        ic.close();
+        oc.close();
+    }
+
+    public static void main(String[] args) throws IOException {
         // verify handling of links
-        File f= new File( "/home/jbf/.wine/dosdevices/c:/windows/profiles/jbf/My Documents");
+        File f = new File("/home/jbf/.wine/dosdevices/c:/windows/profiles/jbf/My Documents");
         System.err.println(f.exists());
         System.err.println(f);
         System.err.println(f.getCanonicalFile());
