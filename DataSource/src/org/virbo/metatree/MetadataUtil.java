f1fd675cb9c32d87b6302134f81c92185d8fe382
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-11-13 18:14:23 +0000

    support creating timeLocationUnits from strings like "seconds since 1989-01-01T00:00"
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1316 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/metatree/MetadataUtil.java
+++ DataSource/src/org/virbo/metatree/MetadataUtil.java
@@ -5,10 +5,16 @@
 
 package org.virbo.metatree;
 
+import java.text.ParseException;
 import org.das2.datum.NumberUnits;
 import org.das2.datum.Units;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import org.das2.datum.Datum;
+import org.das2.datum.TimeLocationUnits;
+import org.das2.datum.TimeUtil;
+import org.das2.datum.UnitsConverter.ScaleOffset;
+import org.das2.datum.UnitsUtil;
 import org.w3c.dom.Node;
 
 /**
@@ -29,6 +35,58 @@ public class MetadataUtil {
             result= new NumberUnits( units );
         }
         return result;
+    }
+    
+    /**
+     * return canonical das2 unit for colloquial time.
+     * @param string
+     * @return
+     */
+    public static Units lookupTimeLengthUnit(String s) throws ParseException {
+        s= s.toLowerCase().trim();
+        if ( s.startsWith("sec") ) {
+            return Units.seconds;
+        } else if ( s.startsWith("ms") || s.startsWith("millisec") ) {
+            return Units.milliseconds;
+        } else if ( s.equals("hr") || s.startsWith("hour") ) {
+            return Units.hours;
+        } else if ( s.equals("mn") || s.startsWith("min") ) {
+            return Units.minutes;
+        } else if ( s.startsWith("us") || s.startsWith("\u00B5s" ) || s.startsWith("micros")) {
+            return Units.microseconds;
+        } else if ( s.startsWith("ns") || s.startsWith("nanos" ) ) {
+            return Units.nanoseconds;
+        } else if ( s.startsWith("d") ) {
+            return Units.days;
+        } else {
+            throw new ParseException("failed to identify unit: "+s,0);
+        }
+    }
+    
+    /**
+     * lookupUnits canonical units object, or allocate one.  If one is
+     * allocated, then parse for "<unit> since <datum>"
+     * @param timeUnits
+     * @return
+     */
+    public static synchronized Units lookupTimeUnits( String units ) throws ParseException {
+        Units result;
+        try {
+            result= Units.getByName(units);
+            return result;
+        } catch ( IllegalArgumentException ex ) {
+            String[] ss= units.split("since");
+            String soffsetUnits= ss[0];
+            Units offsetUnits= lookupTimeLengthUnit(ss[0]);
+            Datum datum= TimeUtil.create(ss[1]);
+            String canonicalName = "" + offsetUnits + " since "+ datum;
+            result= new TimeLocationUnits(canonicalName, canonicalName, offsetUnits);
+            result.registerConverter( Units.us2000, 
+                    new ScaleOffset( 
+                    offsetUnits.convertDoubleTo(Units.microseconds, 1.0), 
+                    datum.doubleValue(Units.us2000) ) );
+            return result;
+        }
     }
     
     /**
@@ -69,4 +127,26 @@ public class MetadataUtil {
         }
         return node;
     }
+    
+    /**
+     * returns the object at the path, or null if the path position doesn't exist.
+     * @param tree
+     * @param path
+     * @return Object at the node, if it exists.
+     */
+    public static Object getNode( Map<String,Object> tree, String[] path ) {
+        int i=0;
+        Object child= tree.get( path[0] );
+        i++;
+        while ( i<path.length ) {
+            if ( child==null ) return null;
+            if ( !(child instanceof Map) ) return null;
+            Object value;
+            
+            child= ((Map)child).get( path[i] );
+            i++;
+        }
+        return child;
+    }
+    
 }
