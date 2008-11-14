a7b8cecbd89c660a9dc2640b9a819e1dadcccfde
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-11-14 03:58:50 +0000

    completion of field name includes label with the first record values.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1321 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/virbo/ascii/AsciiTableDataSourceFactory.java
+++ DataSourcePack/src/org/virbo/ascii/AsciiTableDataSourceFactory.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package org.virbo.ascii;
 
 import java.io.File;
@@ -25,19 +24,20 @@ import org.virbo.datasource.DataSourceFactory;
 import org.virbo.datasource.MetadataModel;
 import org.virbo.datasource.URLSplit;
 import org.virbo.dsutil.AsciiParser;
+import org.virbo.dsutil.AsciiParser.DelimParser;
 
 /**
  *
  * @author jbf
  */
 public class AsciiTableDataSourceFactory implements DataSourceFactory {
-    
+
     /** Creates a new instance of AsciiTableDataSourceFactory */
     public AsciiTableDataSourceFactory() {
     }
-    
+
     public DataSource getDataSource(URL url) throws FileNotFoundException, IOException {
-        return new AsciiTableDataSource( url );
+        return new AsciiTableDataSource(url);
     }
 
     public String editPanel(String surl) throws Exception {
@@ -48,76 +48,64 @@ public class AsciiTableDataSourceFactory implements DataSourceFactory {
         return MetadataModel.createNullModel();
     }
 
-    public List<CompletionContext> getCompletions(CompletionContext cc,org.das2.util.monitor.ProgressMonitor mon) throws Exception {
-        if ( cc.context==CompletionContext.CONTEXT_PARAMETER_NAME ) {
-            List<CompletionContext> result= new ArrayList<CompletionContext>();
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "skip=" ) );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "recCount=" ) );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "column=" ) );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "fixedColumns=" ) );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "rank2=" ) );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "time=") );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "timeFormat=",
-                    "template for parsing time digits, default is ISO8601.") );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "depend0=") );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "fill=" ) );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "comment=", 
-                    "comment line prefix, default is hash (#)" ) );
-	    result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "validMin=",
-		    "values less than this value are treated as fill.") );
-	    result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "validMax=",
-		    "values greater than this value are treated as fill." ) );	    
-	    result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "delim=",
-		    "parse records by splitting on delimiter." ) );
-	    
+    public List<CompletionContext> getCompletions(CompletionContext cc, org.das2.util.monitor.ProgressMonitor mon) throws Exception {
+        if (cc.context == CompletionContext.CONTEXT_PARAMETER_NAME) {
+            List<CompletionContext> result = new ArrayList<CompletionContext>();
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "skip="));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "recCount="));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "column="));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "fixedColumns="));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "rank2="));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "time="));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "timeFormat=",
+                    "template for parsing time digits, default is ISO8601."));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "depend0="));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "fill="));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "comment=",
+                    "comment line prefix, default is hash (#)"));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "validMin=",
+                    "values less than this value are treated as fill."));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "validMax=",
+                    "values greater than this value are treated as fill."));
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "delim=",
+                    "parse records by splitting on delimiter."));
+
             return result;
-        } else if ( cc.context==CompletionContext.CONTEXT_PARAMETER_VALUE ) {
-            String paramName= CompletionContext.get( CompletionContext.CONTEXT_PARAMETER_NAME, cc );
-            if ( paramName.equals("skip") ) {
-                return Collections.singletonList( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "<int>",  "the number of lines to skip before attempting to parse."  )  );
-            } else if ( paramName.equals("recCount") ) {
-                return Collections.singletonList( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "<int>",  "limit number of records to parse."  )  );
-            } else if ( paramName.equals("rank2" ) ) {
-                List<CompletionContext> result= new ArrayList<CompletionContext>();
-                result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "<int>", "number of columns to expect" ) );
-                result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "1:", "all but first column" ) );
-                result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "1:5", "second through 5th columns" ) );
-		result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "-5:", "last five columns" ) );                
+        } else if (cc.context == CompletionContext.CONTEXT_PARAMETER_VALUE) {
+            String paramName = CompletionContext.get(CompletionContext.CONTEXT_PARAMETER_NAME, cc);
+            if (paramName.equals("skip")) {
+                return Collections.singletonList(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "<int>", "the number of lines to skip before attempting to parse."));
+            } else if (paramName.equals("recCount")) {
+                return Collections.singletonList(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "<int>", "limit number of records to parse."));
+            } else if (paramName.equals("rank2")) {
+                List<CompletionContext> result = new ArrayList<CompletionContext>();
+                result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "<int>", "number of columns to expect"));
+                result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "1:", "all but first column"));
+                result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "1:5", "second through 5th columns"));
+                result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "-5:", "last five columns"));
                 return result;
-            } else if ( paramName.equals("column") ) {
-                String[] columns= getFieldNames( cc, mon );
-                List<CompletionContext> result= new ArrayList<CompletionContext>();
-                for ( String s: columns ) {
-                    result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, s ) );
-                }
+            } else if (paramName.equals("column")) {
+                List<CompletionContext> result = getFieldNames(cc, mon);
                 return result;
-            } else if ( paramName.equals("fixedColumns") ) {
-                return Collections.singletonList( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "<int>", "Hint at the number of columns to expect, then use fast parser that assumes fixed columns." )  );
-            } else if ( paramName.equals("time") ) {
-                String[] columns= getFieldNames( cc, mon );
-                List<CompletionContext> result= new ArrayList<CompletionContext>();
-                for ( String s: columns ) {
-                    result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, s ) );
-                }
+            } else if (paramName.equals("fixedColumns")) {
+                return Collections.singletonList(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "<int>", "Hint at the number of columns to expect, then use fast parser that assumes fixed columns."));
+            } else if (paramName.equals("time")) {
+                List<CompletionContext> result = getFieldNames(cc, mon);
                 return result;
-            } else if ( paramName.equals("depend0") ) {
-                String[] columns= getFieldNames( cc, mon );
-                List<CompletionContext> result= new ArrayList<CompletionContext>();
-                for ( String s: columns ) {
-                    result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, s ) );
-                }
+            } else if (paramName.equals("depend0")) {
+                List<CompletionContext> result = getFieldNames(cc, mon);
                 return result;
-            } else if ( paramName.equals("timeFormat") ) {
-                List<CompletionContext> result= new ArrayList<CompletionContext>();
-                result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "%Y %j %H" ) );
-                result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "ISO8601" ) );
+            } else if (paramName.equals("timeFormat")) {
+                List<CompletionContext> result = new ArrayList<CompletionContext>();
+                result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "%Y %j %H"));
+                result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "ISO8601"));
                 return result;
-            } else if ( paramName.equals("fill") ) {
-                return Collections.singletonList( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "<double>" ) );
-            } else if ( paramName.equals("validMin") ) {
-                return Collections.singletonList( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "<double>" ) );
-            } else if ( paramName.equals("validMax") ) {
-                return Collections.singletonList( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "<double>" ) );		
+            } else if (paramName.equals("fill")) {
+                return Collections.singletonList(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "<double>"));
+            } else if (paramName.equals("validMin")) {
+                return Collections.singletonList(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "<double>"));
+            } else if (paramName.equals("validMax")) {
+                return Collections.singletonList(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_VALUE, "<double>"));
             } else {
                 return Collections.emptyList();
             }
@@ -125,8 +113,8 @@ public class AsciiTableDataSourceFactory implements DataSourceFactory {
             return Collections.emptyList();
         }
     }
-    
-    public boolean reject( String surl ,ProgressMonitor mon ) {
+
+    public boolean reject(String surl, ProgressMonitor mon) {
         return false;
     }
 
@@ -134,17 +122,34 @@ public class AsciiTableDataSourceFactory implements DataSourceFactory {
         return surl;
     }
 
-    private String[] getFieldNames(CompletionContext cc, ProgressMonitor mon ) throws IOException {
-        
-        Map params= URLSplit.parseParams( cc.params );
-        Object o;
-        File file= DataSetURL.getFile( cc.resource, mon  );
-        
-        AsciiParser parser=  AsciiParser.newParser(5);
-        if ( params.containsKey("skip") ) parser.setSkipLines( Integer.parseInt((String) params.get("skip")) );
-        
-        parser.guessDelimParser( parser.readFirstRecord(file.toString()) );
-        return parser.getFieldNames();
-    }
+    private List<CompletionContext> getFieldNames(CompletionContext cc, ProgressMonitor mon) throws IOException {
 
+        Map params = URLSplit.parseParams(cc.params);
+        Object o;
+        File file = DataSetURL.getFile(cc.resource, mon);
+
+        AsciiParser parser = AsciiParser.newParser(5);
+        if (params.containsKey("skip")) {
+            parser.setSkipLines(Integer.parseInt((String) params.get("skip")));
+        }
+        
+        String line= parser.readFirstRecord(file.toString());
+        DelimParser dp= parser.guessDelimParser(line);
+        String[] fields= dp.fields(line);
+
+        String[] columns = parser.getFieldNames();
+        List<CompletionContext> result = new ArrayList<CompletionContext>();
+        
+        for ( int i=0; i<columns.length; i++ ) {
+            String s= columns[i];
+            String label= s;
+            if ( ! label.equals(fields[i]) && label.startsWith("field") ) label += " ("+fields[i]+")";
+            
+            result.add(new CompletionContext(
+                    CompletionContext.CONTEXT_PARAMETER_VALUE, 
+                    s,
+                    label, null ) ) ;
+        }
+        return result;
+    }
 }
