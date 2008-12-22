8a03ec792f62f3e469489d4d66b21d15d4a221fa
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-12-22 19:18:58 +0000

    trivial: speedTest introduced.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1536 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ TsdsDataSource/src/org/tsds/datasource/SpeedTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tsds.datasource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+//import sun.net.www.http.HttpClient;
+
+/**
+ *
+ * @author jbf
+ */
+public class SpeedTest {
+
+    private static int empty( InputStream in ) throws IOException {
+        byte[] buf= new byte[2048];
+        int total= 0;
+        int bytes= in.read(buf);
+        while ( bytes!=-1 ) {
+            total+=bytes;
+            bytes= in.read(buf);
+        }
+        return total;
+    }
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        String[] urls = new String[]{
+            "http://timeseries.org/get.cgi?StartDate=20030301&EndDate=20030331&ppd=1&ext=bin&out=tsml&param1=OMNI_OMNIHR-26-v0",
+            "http://timeseries.org/OMNI_OMNIHR-26-v0-to_20030301-tf_20030331-ppd_1-filter_0-ext_bin.bin",
+            "http://timeseries.org/OMNI_OMNIHR-26-v0-to_20030301-tf_20030331-ppd_1-filter_2-ext_bin.bin",
+            "http://timeseries.org/OMNI_OMNIHR-26-v0-to_20030301-tf_20030331-ppd_1-filter_3-ext_bin.bin",
+        };
+        for (int i = 0; i < urls.length; i++) {
+            URL url = new URL(urls[i]);
+            System.err.println(url);
+            HttpURLConnection cc= (HttpURLConnection) url.openConnection();
+            cc.connect();
+            System.err.println("  "+empty(cc.getInputStream()) );
+        }
+        
+        for (int i = 0; i < urls.length; i++) {
+            URL url = new URL(urls[i]);
+            System.err.println("2: " + url);
+            //HttpClient client= HttpClient.New(url);
+            //client.afterConnect();
+            //System.err.println("  "+empty(client.getInputStream()) );
+        }
+    }
+}
