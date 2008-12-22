d920f3e3497dd1866f8dc10bc97e19b2e6d63790
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2008-12-22 17:42:29 +0000

    properly handle window closing as cancel.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1525 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/DataSourceEditorDialog.java
+++ DataSource/src/org/virbo/datasource/DataSourceEditorDialog.java
@@ -25,7 +25,7 @@ public class DataSourceEditorDialog extends javax.swing.JDialog {
         this.validate();
     }
 
-    protected boolean cancelled = false;
+    protected boolean cancelled = true;
     public static final String PROP_CANCELLED = "cancelled";
 
     public boolean isCancelled() {
@@ -123,6 +123,7 @@ private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
 }//GEN-LAST:event_jButton2ActionPerformed
 
 private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    this.cancelled= false;
     this.setModifiers(evt.getModifiers());
     this.setVisible(false);
 }//GEN-LAST:event_jButton1ActionPerformed
