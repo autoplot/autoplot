d09a2a2058edc92e499e316eac66a70cf4b19b57
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-02-02 16:38:15 +0000

    add type=binary option, which formats using binary encodings rather than ascii.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1605 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/virbo/das2Stream/Das2StreamDataSourceFormat.java
+++ DataSourcePack/src/org/virbo/das2Stream/Das2StreamDataSourceFormat.java
@@ -14,7 +14,6 @@ import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.dataset.QDataSet;
 import org.virbo.dataset.TableDataSetAdapter;
 import org.virbo.dataset.VectorDataSetAdapter;
-import org.virbo.datasource.DataSetURL;
 import org.virbo.datasource.URLSplit;
 import org.virbo.datasource.datasource.DataSourceFormat;
 
@@ -26,18 +25,31 @@ public class Das2StreamDataSourceFormat implements DataSourceFormat {
 
     public void formatData(File url, java.util.Map<String, String> params, QDataSet data, ProgressMonitor mon) throws Exception {
         URLSplit split = URLSplit.parse(url.toURI().toString());
+        boolean binary= "binary".equals( params.get( "type" ) );
         if (split.ext.equals(".qds")) {
-            new org.virbo.qstream.SimpleStreamFormatter().format( data, new FileOutputStream(url), true );
+            if ( binary ) {
+                new org.virbo.qstream.SimpleStreamFormatter().format( data, new FileOutputStream(url), false );
+            } else {
+                new org.virbo.qstream.SimpleStreamFormatter().format( data, new FileOutputStream(url), true );
+            }
         } else {
             if (data.rank() == 2) {
                 TableDataSet tds = TableDataSetAdapter.create(data);
                 FileOutputStream fo = new FileOutputStream(url);
-                TableUtil.dumpToAsciiStream(tds, fo);
+                if ( binary ) {
+                    TableUtil.dumpToBinaryStream(tds, fo);
+                } else {
+                    TableUtil.dumpToAsciiStream(tds, fo);
+                }
                 fo.close();
             } else if (data.rank() == 1) {
                 VectorDataSet vds = VectorDataSetAdapter.create(data);
                 FileOutputStream fo = new FileOutputStream(url);
-                VectorUtil.dumpToAsciiStream(vds, fo);
+                if ( binary ) {
+                    VectorUtil.dumpToBinaryStream(vds, fo);
+                } else {
+                    VectorUtil.dumpToAsciiStream(vds, fo);
+                }
                 fo.close();
             }
         }
