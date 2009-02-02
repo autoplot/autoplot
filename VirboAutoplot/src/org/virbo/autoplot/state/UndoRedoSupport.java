78322cadfd7f81327f41f9f679de23f548e2d3f3
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-02-02 18:20:14 +0000

    trivial fix imports.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1609 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/state/UndoRedoSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/state/UndoRedoSupport.java
@@ -14,7 +14,6 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
