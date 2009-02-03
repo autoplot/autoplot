e8687b3f0ec64ab34b3da67d827d1e9f5088b781
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-02-03 18:04:02 +0000

    put in sloppy equals test to account for rounding errors.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1612 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/state/DatumRangePersistenceDelegate.java
+++ VirboAutoplot/src/org/virbo/autoplot/state/DatumRangePersistenceDelegate.java
@@ -15,6 +15,7 @@ import java.beans.DefaultPersistenceDelegate;
 import java.beans.Encoder;
 import java.beans.Expression;
 import java.beans.PersistenceDelegate;
+import org.das2.datum.Datum;
 import org.das2.datum.DatumRangeUtil;
 import org.das2.datum.UnitsUtil;
 
@@ -27,10 +28,33 @@ public class DatumRangePersistenceDelegate extends PersistenceDelegate {
     public DatumRangePersistenceDelegate()  {
     }
 
+    private Datum abs( Datum w ) {
+        return w.getUnits().createDatum( Math.abs(w.doubleValue(w.getUnits() )) );
+    }
+
+    private boolean sloppyEquals( Object o1, Object o2 ) {
+        if ( o1==o2 ) return true;
+        if ( !( o1 instanceof DatumRange ) || !(o2 instanceof DatumRange ) ) {
+            return false;
+        }
+        DatumRange dr1= (DatumRange) o1;
+        DatumRange dr2= (DatumRange) o2;
+
+        if ( !dr1.getUnits().isConvertableTo(dr2.getUnits()) ) return false;
+        Datum w= dr1.width();
+        if ( w.doubleValue( w.getUnits() )==0 ) {
+            return dr1.equals(dr2) && dr2.width()==w;
+        } else {
+            Datum err= w.divide(1000);
+            return abs( dr1.min().subtract(dr2.min()) ).lt( err ) && abs( dr1.min().subtract(dr2.min()) ).lt(err) ;
+        }
+
+    }
+
     @Override
     protected boolean mutatesTo(Object oldInstance, Object newInstance) {
         // super checks for non-null and same class type.
-        return super.mutatesTo(oldInstance, newInstance) && oldInstance.equals(newInstance);
+        return super.mutatesTo(oldInstance, newInstance) && sloppyEquals( oldInstance,newInstance);
     }
 
 
