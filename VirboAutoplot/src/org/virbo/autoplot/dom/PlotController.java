4185117524c051a6ca98671ce637ecb3341b4bce
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-02-18 12:12:37 +0000

    refactor rename field "domplot" to "domPlot". Improve support for isotropic. bugfix: String == check on axis property UNITS meant DateTimeDatumFormatter wasn't used for time axes.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1660 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/PlotController.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/PlotController.java
@@ -24,20 +24,26 @@ import org.virbo.autoplot.util.DateTimeDatumFormatter;
 public class PlotController {
 
     Application dom;
-    Plot domplot;
+    Plot domPlot;
     private DasPlot dasPlot;
     private DasColorBar dasColorBar;
 
     public PlotController(Application dom, Plot plot, DasPlot dasPlot, DasColorBar colorbar) {
         this.dom = dom;
-        this.domplot = plot;
+        this.domPlot = plot;
         this.dasPlot = dasPlot;
         this.dasColorBar = colorbar;
         dasPlot.addPropertyChangeListener(listener);
         dasPlot.getXAxis().addPropertyChangeListener(listener);
         dasPlot.getYAxis().addPropertyChangeListener(listener);
+        domPlot.addPropertyChangeListener( Plot.PROP_ISOTROPIC, new PropertyChangeListener() {
+            public void propertyChange( PropertyChangeEvent e ) {
+                if ( domPlot.isIsotropic() ) checkIsotropic(null);
+            }
+        });
         plot.controller = this;
     }
+
     private PropertyChangeListener listener = new PropertyChangeListener() {
         public String toString() {
             return ""+PlotController.this;
@@ -49,11 +55,11 @@ public class PlotController {
                 if (((DasAxis) e.getSource()).valueIsAdjusting()) {
                     return;
                 }
-                if (domplot.isIsotropic()) {
+                if (domPlot.isIsotropic()) {
                     checkIsotropic(axis);
                 }
 
-                if ( e.getPropertyName()==DasAxis.PROP_UNITS ) {
+                if ( e.getPropertyName().equals(DasAxis.PROP_UNITS) ) {
                     if (UnitsUtil.isTimeLocation(axis.getUnits())) {
                         axis.setUserDatumFormatter(new DateTimeDatumFormatter());
                     } else {
@@ -75,7 +81,7 @@ public class PlotController {
     }
 
     public void resetZoom() {
-        List<Panel> panels = dom.getController().getPanelsFor(domplot);
+        List<Panel> panels = dom.getController().getPanelsFor(domPlot);
         Plot newSettings = null;
         for (Panel p : panels) {
             Plot plot1 = p.getPlotDefaults();
@@ -90,19 +96,32 @@ public class PlotController {
                 newSettings.zaxis.log = newSettings.zaxis.log & plot1.zaxis.log;
             }
         }
-        domplot.syncTo(newSettings);
+        domPlot.syncTo(newSettings);
     }
 
     private void checkIsotropic(DasAxis axis) {
-        if ((axis == dasPlot.getXAxis() || axis == dasPlot.getYAxis()) && dasPlot.getXAxis().getUnits().isConvertableTo(dasPlot.getYAxis().getUnits()) && !dasPlot.getXAxis().isLog() && !dasPlot.getYAxis().isLog()) {
+        Datum scalex = dasPlot.getXAxis().getDatumRange().width().divide(dasPlot.getXAxis().getDLength());
+        Datum scaley = dasPlot.getYAxis().getDatumRange().width().divide(dasPlot.getYAxis().getDLength());
+
+        if ( ! scalex.getUnits().isConvertableTo(scaley.getUnits())
+                || dasPlot.getXAxis().isLog()
+                || dasPlot.getYAxis().isLog() ) {
+            return;
+        }
+
+        if ( axis==null ) {
+            axis= scalex.gt(scaley) ? axis=  dasPlot.getXAxis()  : dasPlot.getYAxis() ;
+        }
+
+        if ( (axis == dasPlot.getXAxis() || axis == dasPlot.getYAxis()) ) {
             DasAxis otherAxis = dasPlot.getYAxis();
             if (axis == dasPlot.getYAxis()) {
                 otherAxis = dasPlot.getXAxis();
             }
-            Datum ratio = axis.getDatumRange().width().divide(axis.getDLength());
+            Datum scale = axis.getDatumRange().width().divide(axis.getDLength());
             DatumRange otherRange = otherAxis.getDatumRange();
-            Datum otherRatio = otherRange.width().divide(otherAxis.getDLength());
-            double expand = (ratio.divide(otherRatio).doubleValue(Units.dimensionless) - 1) / 2;
+            Datum otherScale = otherRange.width().divide(otherAxis.getDLength());
+            double expand = (scale.divide(otherScale).doubleValue(Units.dimensionless) - 1) / 2;
             if (Math.abs(expand) > 0.0001) {
                 DatumRange newOtherRange = DatumRangeUtil.rescale(otherRange, 0 - expand, 1 + expand);
                 otherAxis.setDatumRange(newOtherRange);
@@ -112,20 +131,20 @@ public class PlotController {
 
     public synchronized void bindTo(DasPlot p) {
         ApplicationController ac= dom.getController();
-        ac.bind( this.domplot, "title", p, "title" );
-        this.domplot.getXaxis().getController().bindTo(p.getXAxis());
-        this.domplot.getYaxis().getController().bindTo(p.getYAxis());
+        ac.bind( this.domPlot, "title", p, "title" );
+        this.domPlot.getXaxis().getController().bindTo(p.getXAxis());
+        this.domPlot.getYaxis().getController().bindTo(p.getYAxis());
     }
     
     public synchronized void bindTo(DasColorBar colorbar) {
         ApplicationController ac= dom.getController();
-        ac.bind( this.domplot, "zaxis.range", colorbar, "datumRange");
-        ac.bind( this.domplot, "zaxis.log", colorbar, "log");
-        ac.bind( this.domplot, "zaxis.label", colorbar, "label" );
+        ac.bind( this.domPlot, "zaxis.range", colorbar, "datumRange");
+        ac.bind( this.domPlot, "zaxis.log", colorbar, "log");
+        ac.bind( this.domPlot, "zaxis.label", colorbar, "label" );
     }
 
     public BindingModel[] getBindings() {
-        return dom.getController().getBindingsFor(domplot);
+        return dom.getController().getBindingsFor(domPlot);
     }
 
     public BindingModel getBindings(int index) {
@@ -134,13 +153,13 @@ public class PlotController {
 
     
     public boolean valueIsAdjusting() {
-        return domplot.getXaxis().getController().valueIsAdjusting() || domplot.getYaxis().getController().valueIsAdjusting() || domplot.getZaxis().getController().valueIsAdjusting();
+        return domPlot.getXaxis().getController().valueIsAdjusting() || domPlot.getYaxis().getController().valueIsAdjusting() || domPlot.getZaxis().getController().valueIsAdjusting();
     }
 
     public Application getApplication() {
         return dom;
     }
     public String toString() {
-        return this.domplot + " controller";
+        return this.domPlot + " controller";
     }
 }
