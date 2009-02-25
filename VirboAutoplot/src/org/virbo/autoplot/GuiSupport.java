5521154f933919ac98663acd7d2fd1b8f881c8db
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-02-25 16:37:51 +0000

    export recent
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1701 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/GuiSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/GuiSupport.java
@@ -23,6 +23,7 @@ import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.prefs.Preferences;
@@ -38,6 +39,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.filechooser.FileFilter;
+import org.virbo.autoplot.bookmarks.Bookmark;
 import org.virbo.autoplot.dom.ApplicationController;
 import org.virbo.autoplot.dom.DataSourceController;
 import org.virbo.autoplot.dom.Panel;
@@ -118,21 +120,22 @@ public class GuiSupport {
         return new AbstractAction("Export Data...") {
 
             public void actionPerformed(ActionEvent e) {
-                DataSourceController dsc= parent.applicationModel.getDataSourceFilterController();
-                
-                if ( dsc.getFillDataSet()==null ) {
-                    JOptionPane.showMessageDialog( parent, "No Data to Export.");
+                DataSourceController dsc = parent.applicationModel.getDataSourceFilterController();
+
+                if (dsc.getFillDataSet() == null) {
+                    JOptionPane.showMessageDialog(parent, "No Data to Export.");
                     return;
                 }
-                
+
                 JFileChooser chooser = new JFileChooser();
-                                
+
                 List<String> exts = DataSourceRegistry.getInstance().getFormatterExtensions();
-                FileFilter deflt= null;
-                for ( String ext : exts ) {
-                    final String ex= ext;
-                    final String desc= "";
-                    FileFilter ff= new FileFilter() {
+                FileFilter deflt = null;
+                for (String ext : exts) {
+                    final String ex = ext;
+                    final String desc = "";
+                    FileFilter ff = new FileFilter() {
+
                         @Override
                         public boolean accept(File f) {
                             return f.toString().endsWith(ex) || f.isDirectory();
@@ -140,64 +143,68 @@ public class GuiSupport {
 
                         @Override
                         public String getDescription() {
-                            return "*"+ex; // DANGER: this is parsed below
+                            return "*" + ex; // DANGER: this is parsed below
                         }
                     };
-                    if ( ext.equals(".qds") ) {
-                        deflt= ff;
+                    if (ext.equals(".qds")) {
+                        deflt = ff;
                     }
                     chooser.addChoosableFileFilter(ff);
                 }
-                
+
                 chooser.setFileFilter(deflt);
-                
+
                 Preferences prefs = Preferences.userNodeForPackage(AutoPlotUI.class);
                 String currentFileString = prefs.get("DumpDataCurrentFile", "");
-                
-                if ( dsc.getFillDataSet()!=null ) {
-                    String name= (String) dsc.getFillDataSet().property( QDataSet.NAME );
-                    if ( name!=null ) chooser.setSelectedFile(new File(name.toLowerCase())); 
+
+                if (dsc.getFillDataSet() != null) {
+                    String name = (String) dsc.getFillDataSet().property(QDataSet.NAME);
+                    if (name != null) {
+                        chooser.setSelectedFile(new File(name.toLowerCase()));
+                    }
                 }
 
-                if (!currentFileString.equals("") && new File( currentFileString ).exists() ) {
-                    File folder= new File( currentFileString ).getParentFile();
+                if (!currentFileString.equals("") && new File(currentFileString).exists()) {
+                    File folder = new File(currentFileString).getParentFile();
                     chooser.setCurrentDirectory(folder);
-                    //chooser.setSelectedFile(new File(currentFileString));
+                //chooser.setSelectedFile(new File(currentFileString));
                 }
-                
+
                 int r = chooser.showSaveDialog(parent);
                 if (r == JFileChooser.APPROVE_OPTION) {
                     try {
                         prefs.put("DumpDataCurrentFile", chooser.getSelectedFile().toString());
-                        
-                        String s=  chooser.getSelectedFile().toString();
-                        
-                        String ext= DataSetURL.getExt(s);
-                        if ( ext==null ) ext= "";
-                        
-                        DataSourceFormat format= DataSourceRegistry.getInstance().getFormatByExt(ext);
-                        if ( format==null ) {
-                            if ( chooser.getFileFilter().getDescription().startsWith("*.") ) { 
-                                ext= chooser.getFileFilter().getDescription().substring(1);
-                                format= DataSourceRegistry.getInstance().getFormatByExt(ext);
-                                if ( format==null ) {
-                                    JOptionPane.showMessageDialog( parent, "No formatter for extension: "+ext );
+
+                        String s = chooser.getSelectedFile().toString();
+
+                        String ext = DataSetURL.getExt(s);
+                        if (ext == null) {
+                            ext = "";
+                        }
+
+                        DataSourceFormat format = DataSourceRegistry.getInstance().getFormatByExt(ext);
+                        if (format == null) {
+                            if (chooser.getFileFilter().getDescription().startsWith("*.")) {
+                                ext = chooser.getFileFilter().getDescription().substring(1);
+                                format = DataSourceRegistry.getInstance().getFormatByExt(ext);
+                                if (format == null) {
+                                    JOptionPane.showMessageDialog(parent, "No formatter for extension: " + ext);
                                     return;
                                 } else {
-                                    s= s+ext;
+                                    s = s + ext;
                                 }
                             } else {
-                                JOptionPane.showMessageDialog( parent, "No formatter for extension: "+ext );
+                                JOptionPane.showMessageDialog(parent, "No formatter for extension: " + ext);
                                 return;
                             }
                         }
-                        format.formatData( new File(s),new java.util.HashMap<String, String>(), 
-                                dsc.getFillDataSet(), new DasProgressPanel("formatting data")  );
-                        parent.setStatus("created file "+s);
+                        format.formatData(new File(s), new java.util.HashMap<String, String>(),
+                                dsc.getFillDataSet(), new DasProgressPanel("formatting data"));
+                        parent.setStatus("created file " + s);
 
                     } catch (IOException ex) {
                         parent.applicationModel.application.getExceptionHandler().handle(ex);
-                    } catch ( Exception ex ) {
+                    } catch (Exception ex) {
                         parent.applicationModel.application.getExceptionHandler().handle(ex);
                     }
                 }
@@ -205,83 +212,91 @@ public class GuiSupport {
         };
     }
 
-    public static JMenu createEZAccessMenu( final Plot plot ) {
+    public static JMenu createEZAccessMenu(final Plot plot) {
 
         JMenu result = new JMenu("plot style");
         result.add(new JMenuItem(new AbstractAction("scatter") {
 
             public void actionPerformed(ActionEvent e) {
-                Panel panel= plot.getController().getApplication().getController().getPanel();
-                panel.setRenderType( ApplicationModel.RenderType.scatter );
+                Panel panel = plot.getController().getApplication().getController().getPanel();
+                panel.setRenderType(ApplicationModel.RenderType.scatter);
             }
         }));
 
         result.add(new JMenuItem(new AbstractAction("colorScatter") {
-            Panel panel= plot.getController().getApplication().getController().getPanel();
+
+            Panel panel = plot.getController().getApplication().getController().getPanel();
+
             public void actionPerformed(ActionEvent e) {
-                panel.setRenderType( ApplicationModel.RenderType.colorScatter );
+                panel.setRenderType(ApplicationModel.RenderType.colorScatter);
             }
         }));
-        
+
         result.add(new JMenuItem(new AbstractAction("series") {
 
             public void actionPerformed(ActionEvent e) {
-                Panel panel= plot.getController().getApplication().getController().getPanel();
-                panel.setRenderType( ApplicationModel.RenderType.series );
+                Panel panel = plot.getController().getApplication().getController().getPanel();
+                panel.setRenderType(ApplicationModel.RenderType.series);
             }
         }));
 
         result.add(new JMenuItem(new AbstractAction("histogram") {
 
             public void actionPerformed(ActionEvent e) {
-                Panel panel= plot.getController().getApplication().getController().getPanel();
-                panel.setRenderType( ApplicationModel.RenderType.histogram );
+                Panel panel = plot.getController().getApplication().getController().getPanel();
+                panel.setRenderType(ApplicationModel.RenderType.histogram);
             }
         }));
 
         result.add(new JMenuItem(new AbstractAction("fill to zero") {
+
             public void actionPerformed(ActionEvent e) {
-                Panel panel= plot.getController().getApplication().getController().getPanel();
-                panel.setRenderType( ApplicationModel.RenderType.fill_to_zero );
+                Panel panel = plot.getController().getApplication().getController().getPanel();
+                panel.setRenderType(ApplicationModel.RenderType.fill_to_zero);
             }
         }));
 
         result.add(new JMenuItem(new AbstractAction("huge scatter") {
+
             public void actionPerformed(ActionEvent e) {
-                Panel panel= plot.getController().getApplication().getController().getPanel();
-                panel.setRenderType( ApplicationModel.RenderType.hugeScatter );
+                Panel panel = plot.getController().getApplication().getController().getPanel();
+                panel.setRenderType(ApplicationModel.RenderType.hugeScatter);
             }
         }));
 
         result.add(new JMenuItem(new AbstractAction("spectrogram") {
+
             public void actionPerformed(ActionEvent e) {
-                Panel panel= plot.getController().getApplication().getController().getPanel();
-                panel.setRenderType( ApplicationModel.RenderType.spectrogram );
+                Panel panel = plot.getController().getApplication().getController().getPanel();
+                panel.setRenderType(ApplicationModel.RenderType.spectrogram);
             }
         }));
 
         return result;
     }
-    
+
     protected void addKeyBindings(JPanel thisPanel) {
-        thisPanel.getActionMap().put("UNDO", parent.undoRedoSupport.getUndoAction() );
-        thisPanel.getActionMap().put("REDO", parent.undoRedoSupport.getRedoAction() );
+        thisPanel.getActionMap().put("UNDO", parent.undoRedoSupport.getUndoAction());
+        thisPanel.getActionMap().put("REDO", parent.undoRedoSupport.getRedoAction());
         thisPanel.getActionMap().put("RESET_ZOOM", new AbstractAction() {
+
             public void actionPerformed(ActionEvent e) {
                 parent.applicationModel.resetZoom();
             }
-        } );
+        });
         thisPanel.getActionMap().put("INCREASE_FONT_SIZE", new AbstractAction() {
+
             public void actionPerformed(ActionEvent e) {
                 parent.applicationModel.increaseFontSize();
-            }                
-        } );
+            }
+        });
         thisPanel.getActionMap().put("DECREASE_FONT_SIZE", new AbstractAction() {
+
             public void actionPerformed(ActionEvent e) {
                 parent.applicationModel.decreaseFontSize();
-            }                
-        } );
-        
+            }
+        });
+
         InputMap map = new ComponentInputMap(thisPanel);
         map.put(KeyStroke.getKeyStroke(KeyEvent.VK_Z, KeyEvent.CTRL_DOWN_MASK), "UNDO");
         map.put(KeyStroke.getKeyStroke(KeyEvent.VK_Y, KeyEvent.CTRL_DOWN_MASK), "REDO");
@@ -291,7 +306,33 @@ public class GuiSupport {
         map.put(KeyStroke.getKeyStroke(KeyEvent.VK_EQUALS, KeyEvent.CTRL_DOWN_MASK), "INCREASE_FONT_SIZE");
         map.put(KeyStroke.getKeyStroke(KeyEvent.VK_EQUALS, KeyEvent.SHIFT_DOWN_MASK | KeyEvent.CTRL_DOWN_MASK), "INCREASE_FONT_SIZE");  // american keyboard
         thisPanel.setInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW, map);
-        
+
     }
-    
+
+    protected void exportRecent( Component c ) {
+        JFileChooser chooser = new JFileChooser();
+        chooser.setFileFilter(new javax.swing.filechooser.FileFilter() {
+
+            public boolean accept(File f) {
+                return f.isDirectory() || f.getName().endsWith(".xml");
+            }
+
+            public String getDescription() {
+                return "bookmarks files (*.xml)";
+            }
+        });
+        int r = chooser.showSaveDialog(c);
+        if (r == JFileChooser.APPROVE_OPTION) {
+            try {
+                File f= chooser.getSelectedFile();
+                if ( !f.toString().endsWith(".xml") ) f= new File( f.toString()+".xml" );
+                String format = Bookmark.formatBooks( parent.applicationModel.getRecent() );
+                FileOutputStream out = new FileOutputStream(f);
+                out.write(format.getBytes());
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
