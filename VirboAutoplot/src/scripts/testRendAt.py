28a68fefc38f0b72504a97486514008b90758ce0
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-03-13 11:53:23 +0000

    script testing das2's dasPlot rendAt(pixelCol,pixelRow) function.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1766 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/scripts/testRendAt.py
@@ -0,0 +1,19 @@
+dom= getDocumentModel()
+
+canvas= dom.controller.canvas
+w= canvas.size.width
+h= canvas.size.height
+
+if ( canvas.fitted==False ): print "canvas must be fitted"
+
+dasPlot= dom.controller.plot.controller.dasPlot
+
+rendAt= zeros( w, h )
+
+for i in range(w):
+   print i, w
+   for j in range(h):
+      rendAt[i,h-j-1]= dasPlot.findRendererAt(i,j)
+
+formatDataSet( rendAt, 'file:///tmp/rendAt.qds?binary=1' )
+
