68a02c50c74e6df5e0c0b597fdaaa5e39694f99f
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-03-13 11:47:54 +0000

    introduce "layout" panel, which provides a list of all panels and a cartoon view of the canvas.  Eventually this should indicate bindings and connectors as well.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1765 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/virbo/autoplot/LayoutUI.java
@@ -0,0 +1,299 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*
+ * LayoutUI.java
+ *
+ * Created on Mar 7, 2009, 6:24:23 AM
+ */
+package org.virbo.autoplot;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.AbstractAction;
+import javax.swing.AbstractListModel;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import org.das2.components.propertyeditor.PropertyEditor;
+import org.das2.graph.DasPlot;
+import org.virbo.autoplot.dom.Application;
+import org.virbo.autoplot.dom.ApplicationController;
+import org.virbo.autoplot.dom.Options;
+import org.virbo.autoplot.dom.Panel;
+import org.virbo.autoplot.dom.Plot;
+import org.virbo.autoplot.util.CanvasLayoutPanel;
+
+/**
+ *
+ * @author jbf
+ */
+public class LayoutUI extends javax.swing.JPanel {
+
+    /** Creates new form LayoutUI */
+    public LayoutUI() {
+        initComponents();
+        updateList();
+        canvasLayoutPanel1.addPropertyChangeListener(CanvasLayoutPanel.PROP_COMPONENT, new PropertyChangeListener() {
+
+            public void propertyChange(PropertyChangeEvent evt) {
+                Plot plot = app.getController().getPlotFor((Component) canvasLayoutPanel1.getComponent());
+                if (plot != null) {
+                    app.getController().setPlot(plot);
+                }
+            }
+        });
+        jList1.addListSelectionListener(panelSelectionListener);
+
+        createPopupMenus();
+
+        MouseListener popupTrigger = createPopupTrigger();
+        canvasLayoutPanel1.addMouseListener(popupTrigger);
+        jList1.addMouseListener(popupTrigger);
+    }
+
+    private MouseListener createPopupTrigger() {
+        return new MouseAdapter() {
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.isPopupTrigger()) {
+                    JPopupMenu menu = contextMenus.get(e.getComponent());
+                    if (menu != null) {
+                        menu.show(e.getComponent(), e.getX(), e.getY());
+                    }
+                }
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (e.isPopupTrigger()) {
+                    JPopupMenu menu = contextMenus.get(e.getComponent());
+                    if (menu != null) {
+                        menu.show(e.getComponent(), e.getX(), e.getY());
+                    }
+                }
+            }
+        };
+    }
+    ;
+    Map<Component, JPopupMenu> contextMenus = null;
+
+    private synchronized void createPopupMenus() {
+        contextMenus = new HashMap<Component, JPopupMenu>();
+
+        JPopupMenu plotContextMenu = new JPopupMenu();
+
+        JMenuItem item;
+        item = new JMenuItem(new AbstractAction("Remove Bindings") {
+
+            public void actionPerformed(ActionEvent e) {
+                Plot domPlot = app.getController().getPlot();
+                List<Panel> panels = app.getController().getPanelsFor(domPlot);
+                for (Panel pan : panels) {
+                    app.getController().unbind(pan);
+                }
+                app.getController().unbind(domPlot);
+            }
+        });
+        item.setToolTipText("remove any plot and panel property bindings");
+        plotContextMenu.add(item);
+
+        item = new JMenuItem(new AbstractAction("Edit Plot Properties") {
+
+            public void actionPerformed(ActionEvent e) {
+                Plot domPlot = app.getController().getPlot();
+                PropertyEditor edit = new PropertyEditor(domPlot);
+                edit.showDialog(LayoutUI.this);
+            }
+        });
+        plotContextMenu.add(item);
+
+        item = new JMenuItem(new AbstractAction("Delete Plot") {
+
+            public void actionPerformed(ActionEvent e) {
+                if (app.getPlots().length > 1) {
+                    Plot domPlot = app.getController().getPlot();
+                    List<Panel> panels = app.getController().getPanelsFor(domPlot);
+                    for (Panel pan : panels) {
+                        if (app.getPanels().length > 1) {
+                            app.getController().deletePanel(pan);
+                        } else {
+                            app.getController().setStatus("warning: the last panel may not be deleted");
+                        }
+                    }
+                    app.getController().deletePlot(domPlot);
+                } else {
+                    app.getController().setStatus("warning: last plot may not be deleted");
+                }
+            }
+        });
+        plotContextMenu.add(item);
+
+        contextMenus.put(canvasLayoutPanel1, plotContextMenu);
+
+        JPopupMenu panelContextMenu = new JPopupMenu();
+
+        item = new JMenuItem(new AbstractAction("Edit Panel Properties") {
+
+            public void actionPerformed(ActionEvent e) {
+                Panel panel = app.getController().getPanel();
+                PropertyEditor edit = new PropertyEditor(panel);
+                edit.showDialog(LayoutUI.this);
+            }
+        });
+        panelContextMenu.add(item);
+
+        item = new JMenuItem(new AbstractAction("Delete Panel") {
+
+            public void actionPerformed(ActionEvent e) {
+                Object[] os= jList1.getSelectedValues();
+                for ( Object o : os ) {
+                    Panel panel = (Panel) o;
+                    app.getController().deletePanel(panel);
+                }
+                
+            }
+        });
+        panelContextMenu.add(item);
+
+        contextMenus.put(jList1, panelContextMenu);
+    }
+    ListSelectionListener panelSelectionListener = new ListSelectionListener() {
+
+        public void valueChanged(ListSelectionEvent e) {
+            if (jList1.getSelectedValues().length == 1) {
+                Panel p = (Panel) jList1.getSelectedValue();
+                Plot plot = app.getController().getPlotFor(p);
+                app.getController().setPlot(plot);
+                app.getController().setPanel(p);
+            }
+        }
+    };
+    Application app;
+    AbstractListModel panelList;
+    PropertyChangeListener panelsListener = new PropertyChangeListener() {
+
+        public void propertyChange(PropertyChangeEvent evt) {
+            updateList();
+        }
+    };
+    private PropertyChangeListener plotListener = new PropertyChangeListener() {
+
+        public void propertyChange(PropertyChangeEvent evt) {
+            List<Panel> p = app.getController().getPanelsFor(app.getController().getPlot());
+            List<Panel> allPanels = Arrays.asList(app.getPanels());
+            int[] indices = new int[p.size()];
+            for (int i = 0; i < p.size(); i++) {
+                indices[i] = allPanels.indexOf(p.get(i));
+            }
+            jList1.setSelectedIndices(indices);
+            DasPlot dasPlot = app.getController().getPlot().getController().getDasPlot();
+            canvasLayoutPanel1.setComponent(dasPlot);
+        }
+    };
+    private PropertyChangeListener panelListener = new PropertyChangeListener() {
+        public void propertyChange(PropertyChangeEvent evt) {
+            Panel p = app.getController().getPanel();
+            List<Panel> allPanels = Arrays.asList(app.getPanels());
+            jList1.setSelectedIndex(allPanels.indexOf(p));
+        }
+    };
+
+    public void setApplication(Application app) {
+        this.app = app;
+        canvasLayoutPanel1.setContainer(app.getController().getDasCanvas());
+        canvasLayoutPanel1.addComponentType(DasPlot.class, Color.BLUE);
+        app.getController().bind(app.getOptions(), Options.PROP_BACKGROUND, canvasLayoutPanel1, "background");
+        app.addPropertyChangeListener(Application.PROP_PANELS, panelsListener);
+        app.getController().addPropertyChangeListener(ApplicationController.PROP_PLOT, plotListener);
+        app.getController().addPropertyChangeListener(ApplicationController.PROP_PANEL, panelListener);
+    }
+
+    private void updateList() {
+        panelList = new AbstractListModel() {
+            public int getSize() {
+                return app.getPanels().length;
+            }
+
+            public Object getElementAt(int index) {
+                return app.getPanels(index);
+            }
+        };
+        jList1.setModel(panelList);
+    }
+
+    /** This method is called from within the constructor to
+     * initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is
+     * always regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jList1 = new javax.swing.JList();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        canvasLayoutPanel1 = new org.virbo.autoplot.util.CanvasLayoutPanel();
+
+        jList1.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        jScrollPane1.setViewportView(jList1);
+
+        jLabel1.setText("Panels:");
+
+        jLabel2.setText("Plots:");
+
+        canvasLayoutPanel1.setText("canvasLayoutPanel1");
+
+        org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                    .add(jLabel2)
+                    .add(canvasLayoutPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 189, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jLabel1)
+                    .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 209, Short.MAX_VALUE)))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(jLabel1)
+                    .add(jLabel2))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(canvasLayoutPanel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 277, Short.MAX_VALUE)
+                    .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 277, Short.MAX_VALUE)))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private org.virbo.autoplot.util.CanvasLayoutPanel canvasLayoutPanel1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JList jList1;
+    private javax.swing.JScrollPane jScrollPane1;
+    // End of variables declaration//GEN-END:variables
+}
