83d7925b5119602b6e50aab94a6c4a1eea5a5030
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-03-17 15:18:18 +0000

    remove setters since we use a persistence delegate.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1776 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/BindingModel.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/BindingModel.java
@@ -14,78 +14,52 @@ import java.beans.PropertyChangeSupport;
  */
 public class BindingModel {
 
+    public BindingModel( String context, String src, String srcProp, String dst, String dstProp ) {
+        this.bindingContextId= context;
+        this.srcId= src;
+        this.srcProperty= srcProp;
+        this.dstId= dst;
+        this.dstProperty= dstProp;
+    }
+
     protected String bindingContextId = null;
-    public static final String PROP_BINDINGCONTEXTID = "bindingContextId";
 
     public String getBindingContextId() {
         return bindingContextId;
     }
 
-    public void setBindingContextId(String bindingContextId) {
-        String oldBindingContextId = this.bindingContextId;
-        this.bindingContextId = bindingContextId;
-        propertyChangeSupport.firePropertyChange(PROP_BINDINGCONTEXTID, oldBindingContextId, bindingContextId);
-    }
 
     protected String srcId = null;
-    public static final String PROP_SOURCEID = "srcId";
 
     public String getSrcId() {
         return srcId;
     }
 
-    public void setSrcId(String sourceId) {
-        String oldSourceId = this.srcId;
-        this.srcId = sourceId;
-        propertyChangeSupport.firePropertyChange(PROP_SOURCEID, oldSourceId, sourceId);
-    }
-
-
-
     protected String dstId;
-    public static final String PROP_DESTID = "dstId";
 
     public String getDstId() {
         return dstId;
     }
 
-    public void setDstId(String destId) {
-        String oldDestId = this.dstId;
-        this.dstId = destId;
-        propertyChangeSupport.firePropertyChange(PROP_DESTID, oldDestId, destId);
-    }
 
 
     protected String srcProperty;
-    public static final String PROP_SRCPROPERTY = "srcProperty";
 
     public String getSrcProperty() {
         return srcProperty;
     }
 
-    public void setSrcProperty(String srcProperty) {
-        String oldSrcProperty = this.srcProperty;
-        this.srcProperty = srcProperty;
-        propertyChangeSupport.firePropertyChange(PROP_SRCPROPERTY, oldSrcProperty, srcProperty);
-    }
 
 
     protected String dstProperty;
-    public static final String PROP_DSTPROPERTY = "dstProperty";
 
     public String getDstProperty() {
         return dstProperty;
     }
 
-    public void setDstProperty(String dstProperty) {
-        String oldDstProperty = this.dstProperty;
-        this.dstProperty = dstProperty;
-        propertyChangeSupport.firePropertyChange(PROP_DSTPROPERTY, oldDstProperty, dstProperty);
-    }
-
     @Override
     public String toString() {
-        return ""+ srcId + "."+ srcProperty + " <--> " + dstId + "." + dstProperty + "  ("+bindingContextId +")";
+        return ""+ srcId + "."+ srcProperty + " to " + dstId + "." + dstProperty + "  ("+bindingContextId +")";
     }
 
     @Override
