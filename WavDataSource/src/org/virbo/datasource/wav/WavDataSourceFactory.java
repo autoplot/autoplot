157244e79057e23ed2af36af4ccd718a57b87740
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-03-19 16:57:47 +0000

    trivial fix imports
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1814 0c594bb0-8543-0410-a25e-d8ec440261db

--- WavDataSource/src/org/virbo/datasource/wav/WavDataSourceFactory.java
+++ WavDataSource/src/org/virbo/datasource/wav/WavDataSourceFactory.java
@@ -9,7 +9,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
