aaffbe87baf4e757ebef498bbcab9374910dbc42
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-04-02 23:55:58 +0000

    to avoid deadlocks, tighten synchronized blocks and remove unnecessary propertyChangeSupport synchronization.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1951 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/TimeSeriesBrowseController.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/TimeSeriesBrowseController.java
@@ -171,19 +171,19 @@ public class TimeSeriesBrowseController {
     
     private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
-    public synchronized void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+    public void removePropertyChangeListener(String propertyName, PropertyChangeListener listener) {
         propertyChangeSupport.removePropertyChangeListener(propertyName, listener);
     }
 
-    public synchronized void removePropertyChangeListener(PropertyChangeListener listener) {
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
         propertyChangeSupport.removePropertyChangeListener(listener);
     }
 
-    public synchronized void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
+    public void addPropertyChangeListener(String propertyName, PropertyChangeListener listener) {
         propertyChangeSupport.addPropertyChangeListener(propertyName, listener);
     }
 
-    public synchronized void addPropertyChangeListener(PropertyChangeListener listener) {
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
         propertyChangeSupport.addPropertyChangeListener(listener);
     }
 
