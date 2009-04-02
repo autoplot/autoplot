7d605688ee71f45cb11f1022d33530b3280a4e73
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-04-02 16:10:14 +0000

    add documentation to big/little to help people (me) remember which is which.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1942 0c594bb0-8543-0410-a25e-d8ec440261db

--- BinaryDataSource/src/org/virbo/binarydatasource/BinaryDataSourceFactory.java
+++ BinaryDataSource/src/org/virbo/binarydatasource/BinaryDataSourceFactory.java
@@ -92,8 +92,8 @@ public class BinaryDataSourceFactory extends AbstractDataSourceFactory {
                 return result;
             } else if ( paramName.equals("byteOrder") ) {
                 List<CompletionContext> result= new ArrayList<CompletionContext>();
-                result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "little") ); 
-                result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "big") ); 
+                result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "little", "first byte has little significance") );
+                result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "big", "first byte has big significance") );
                 return result;
             } else {
                 return Collections.emptyList();
