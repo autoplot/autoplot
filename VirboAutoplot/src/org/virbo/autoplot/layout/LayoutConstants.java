ab15000c5c9326281b158a108ab3017b404823b3
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-04-03 01:54:44 +0000

    introduce LayoutConstants.ABOVE and .BELOW, used in addPlot.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@1967 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/virbo/autoplot/layout/LayoutConstants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.autoplot.layout;
+
+/**
+ * Constants for specifying layout
+ * @author jbf
+ */
+public class LayoutConstants {
+    public static final Object ABOVE= new String("above");
+    public static final Object BELOW= new String("below");
+
+}
