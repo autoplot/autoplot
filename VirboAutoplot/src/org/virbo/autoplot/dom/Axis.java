579751ce0ecc81aa2f93878d08022b29834e5908
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-04-09 17:00:38 +0000

    bugfix: vap files could have the log property set but not the range, causing an inconsistent state that would result in marching axes.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2057 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/Axis.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/Axis.java
@@ -2,11 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.virbo.autoplot.dom;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.das2.datum.Datum;
 import org.das2.datum.DatumRange;
 import org.das2.datum.Units;
 
@@ -15,6 +15,7 @@ import org.das2.datum.Units;
  * @author jbf
  */
 public class Axis extends DomNode {
+
     protected DatumRange range = new DatumRange(0, 100, Units.dimensionless);
     public static final String PROP_RANGE = "range";
 
@@ -22,17 +23,21 @@ public class Axis extends DomNode {
         return range;
     }
 
+    /**
+     * set the range property.  Right now, if the axis is log, and the range
+     * contains negative values, then the axis will be in an invalid state.
+     * We cannot change the range setting automatically because the next setting
+     * may be the log property, then the order of property setter calls would
+     * matter.  TODO: consider mutatorLock...  TODO: consider making the axis
+     * linear...
+     * @param range
+     */
     public void setRange(DatumRange range) {
-        try {
-            DatumRange oldRange = this.range;
-            this.range = range;
-            propertyChangeSupport.firePropertyChange(PROP_RANGE, oldRange, range);
-        } catch ( Exception e ) {
-            System.err.println( propertyChangeSupport.toString() );
-            e.printStackTrace();
-        }
+        DatumRange oldRange = this.range;
+        this.range= range;
+        propertyChangeSupport.firePropertyChange(PROP_RANGE, oldRange, range);
     }
-
+    
     protected boolean log = false;
     public static final String PROP_LOG = "log";
 
@@ -40,14 +45,46 @@ public class Axis extends DomNode {
         return log;
     }
 
+    private DatumRange logCheckRange(DatumRange range, boolean log) {
+        Datum min = this.range.min();
+        Datum max = this.range.max();
+        Units u = range.getUnits();
+        boolean changed=false;
+        if (max.doubleValue(u) <= 0.) {
+            max = u.createDatum(100);
+            changed= true;
+        }
+        if (min.doubleValue(u) <= 0.) {
+            min = max.divide(1000);
+            changed= true;
+        }
+        if ( changed ) {
+            return new DatumRange(min, max);
+        } else {
+            return range;
+        }
+    }
+
+    /**
+     * set the log property.  If the value makes the range invalid (log and zero),
+     * then the range is adjusted to make it valid.  This works because the order
+     * of property setters doesn't matter.
+     * @param log
+     */
     public void setLog(boolean log) {
         boolean oldLog = this.log;
         this.log = log;
+        // ensure that log doesn't make axis invalid.
+        if (log) {
+            DatumRange oldRange = this.range;
+            this.range = logCheckRange(this.range,log);
+            if ( this.range.equals(oldRange) ) {
+                propertyChangeSupport.firePropertyChange(PROP_RANGE, oldRange, range);
+            }
+        }
         propertyChangeSupport.firePropertyChange(PROP_LOG, oldLog, log);
     }
-
     protected String label = "";
-    
     /**
      * consise label for the axis.
      */
@@ -62,7 +99,6 @@ public class Axis extends DomNode {
         this.label = label;
         propertyChangeSupport.firePropertyChange(PROP_LABEL, oldLabel, label);
     }
-    
     protected boolean drawTickLabels = true;
     public static final String PROP_DRAWTICKLABELS = "drawTickLabels";
 
@@ -75,27 +111,26 @@ public class Axis extends DomNode {
         this.drawTickLabels = drawTickLabels;
         propertyChangeSupport.firePropertyChange(PROP_DRAWTICKLABELS, oldDrawTickLabels, drawTickLabels);
     }
-
     AxisController controller;
-    
+
     public AxisController getController() {
         return controller;
     }
 
     public void syncTo(DomNode n) {
         super.syncTo(n);
-        Axis that= (Axis)n;
-        this.setLog( that.isLog() );
-        this.setRange( that.getRange() );
-        this.setLabel( that.getLabel() );
-        
+        Axis that = (Axis) n;
+        this.setLog(that.isLog());
+        this.setRange(that.getRange());
+        this.setLabel(that.getLabel());
+
     }
 
     public List<Diff> diffs(DomNode node) {
-        Axis that= (Axis)node;
-        List<Diff> result= new ArrayList<Diff>();
+        Axis that = (Axis) node;
+        List<Diff> result = new ArrayList<Diff>();
         boolean b;
-        
+
         b= that.log==this.log ;
         if ( !b ) result.add( new PropertyChangeDiff( "log" , that.log, this.log) );
         b=  that.range.equals(this.range) ;
@@ -104,8 +139,7 @@ public class Axis extends DomNode {
         if ( !b ) result.add(new PropertyChangeDiff("label", that.label , this.label ) );
         b=  that.drawTickLabels==this.drawTickLabels;
         if ( !b ) result.add(new PropertyChangeDiff("drawTickLabels", that.drawTickLabels, this.drawTickLabels ) );
-        
+
         return result;
     }
-
 }
