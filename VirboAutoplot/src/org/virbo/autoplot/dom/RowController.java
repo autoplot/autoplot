b8fa7c7caebf67d8ce6307efeb13d7939c2a8018
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-05-05 13:00:36 +0000

    introduce layout into Dom
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2203 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/virbo/autoplot/dom/RowController.java
@@ -0,0 +1,72 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.autoplot.dom;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.ParseException;
+import java.util.logging.Logger;
+import org.das2.graph.DasCanvas;
+import org.das2.graph.DasDevicePosition;
+import org.das2.graph.DasRow;
+
+/**
+ *
+ * @author jbf
+ */
+public class RowController {
+    Row row;
+    DasRow dasRow;
+    Canvas canvas;
+
+    RowController( Row row ) {
+        this.row= row;
+    }
+
+    protected void createDasPeer( Canvas canvas, DasRow parent ) {
+        DasCanvas c= canvas.getController().getDasCanvas();
+        dasRow= DasRow.create( c, parent, row.getTop(), row.getBottom() );
+        PropertyChangeListener list= new PropertyChangeListener() {
+            @Override
+            public void propertyChange(PropertyChangeEvent evt) {
+                if ( evt.getPropertyName().equals( DasDevicePosition.PROP_DMAXIMUM ) ) {
+                    row.setBottom( DasDevicePosition.formatLayoutStr(dasRow, false ) );
+                } else if ( evt.getPropertyName().equals( DasDevicePosition.PROP_DMINIMUM ) ) {
+                    row.setTop( DasDevicePosition.formatLayoutStr(dasRow, true) );
+                }
+            }
+        };
+        dasRow.addPropertyChangeListener(list);
+        list= new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent evt) {
+                try {
+                    double[] dd= DasDevicePosition.parseFormatStr((String)evt.getNewValue());
+                    if ( evt.getPropertyName().equals(Row.PROP_TOP) ) {
+                        dasRow.setMin( dd[0], dd[1], (int) dd[2] );
+                    } else if ( evt.getPropertyName().equals(Row.PROP_BOTTOM) ) {
+                        dasRow.setMax( dd[0], dd[1], (int) dd[2] );
+                    }
+                    DasDevicePosition.parseLayoutStr( dasRow, row.getTop() + "," + row.getBottom() );
+                } catch (ParseException ex) {
+                    Logger.getLogger(RowController.class.getName().toString()).warning("parse exception: "+ex);
+                    row.setTop( DasDevicePosition.formatLayoutStr(dasRow, true) );
+                    row.setBottom( DasDevicePosition.formatLayoutStr(dasRow, false ) );
+                }
+            }
+        };
+        row.addPropertyChangeListener(Row.PROP_BOTTOM,list);
+        row.addPropertyChangeListener(Row.PROP_TOP,list);
+        this.canvas= canvas;
+    }
+
+    public DasRow getDasRow() {
+        return dasRow;
+    }
+
+    public Canvas getCanvas() {
+        return canvas;
+    }
+}
