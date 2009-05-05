2644e05e647db9ed55b5f4f29b1dee3ce8c4d622
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-05-05 12:14:32 +0000

    introduce a bunch of BeanEditors shamelessly stolen from Netbeans.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2195 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/netbeans/beaninfo/editors/PointCustomEditor.java
@@ -0,0 +1,225 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ */
+package org.netbeans.beaninfo.editors;
+
+import java.awt.Point;
+import java.awt.Dimension;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+/** Custom property editor for Point and Dimension
+*
+* @author   Ian Formanek
+*/
+public class PointCustomEditor extends javax.swing.JPanel
+implements PropertyChangeListener {
+
+    static final long serialVersionUID =-4067033871196801978L;
+
+    private boolean dimensionMode = false;
+    
+    /** Initializes the Form */
+    public PointCustomEditor(PointEditor editor ) {
+        initComponents ();
+        this.editor = editor;
+        Point point = (Point)editor.getValue ();
+        if (point == null) point = new Point (0, 0);
+        xField.setText (Integer.toString(point.x)); // NOI18N
+        yField.setText (Integer.toString(point.y)); // NOI18N
+        
+        xField.getAccessibleContext().setAccessibleDescription(NbBundle.getMessage(PointCustomEditor.class, "ACSD_CTL_X"));
+        yField.getAccessibleContext().setAccessibleDescription(NbBundle.getMessage(PointCustomEditor.class, "ACSD_CTL_Y"));
+        
+        commonInit( NbBundle.getMessage(PointCustomEditor.class, "CTL_Point") );
+    }
+    
+    public PointCustomEditor(DimensionEditor editor ) {
+        dimensionMode = true;
+
+        initComponents();
+        this.editor = editor;
+        Dimension dimension = (Dimension)editor.getValue ();
+        if (dimension == null) dimension = new Dimension (0, 0);
+        xField.setText (Integer.toString(dimension.width));    // NOI18N
+        yField.setText (Integer.toString(dimension.height));  // NOI18N
+        
+        xLabel.setLabelFor(xField);
+        yLabel.setLabelFor(yField);
+
+        xField.getAccessibleContext().setAccessibleDescription(NbBundle.getMessage(PointCustomEditor.class, "ACSD_CTL_Width"));
+        yField.getAccessibleContext().setAccessibleDescription(NbBundle.getMessage(PointCustomEditor.class, "ACSD_CTL_Height"));
+        
+        commonInit( NbBundle.getMessage(PointCustomEditor.class, "CTL_Dimension") );
+    }
+    
+    private void commonInit( String panelTitle ) {
+        getAccessibleContext().setAccessibleDescription(NbBundle.getMessage(PointCustomEditor.class, "ACSD_PointCustomEditor"));
+        
+        setBorder (new javax.swing.border.EmptyBorder(12, 12, 0, 11));
+        insidePanel.setBorder (new javax.swing.border.CompoundBorder (
+                                   new javax.swing.border.TitledBorder (
+                                       new javax.swing.border.EtchedBorder (),
+                                       " " + panelTitle + " "
+                                   ),
+                                   new javax.swing.border.EmptyBorder (new java.awt.Insets(5, 5, 5, 5))));
+
+
+    }
+
+    public java.awt.Dimension getPreferredSize () {
+        return new java.awt.Dimension (280, 160);
+    }
+
+    private Object getPropertyValue () throws IllegalStateException {
+        try {
+            int x = Integer.parseInt (xField.getText ());
+            int y = Integer.parseInt (yField.getText ());
+            if ( dimensionMode ) {
+                if ((x < 0) || (y < 0)) {
+                    IllegalStateException ise = new IllegalStateException();
+                    throw ise;
+                }
+                return new Dimension (x, y);
+            } else {
+                return new Point (x, y);
+            }
+        } catch (NumberFormatException e) {
+            IllegalStateException ise = new IllegalStateException();
+            throw ise;
+        }
+    }
+
+
+    public void propertyChange(PropertyChangeEvent evt) {
+/*        if (
+            PropertyEnv.PROP_STATE.equals(evt.getPropertyName())
+            &&
+            PropertyEnv.STATE_VALID.equals(evt.getNewValue())
+        ) {
+            editor.setValue(getPropertyValue());
+        } */
+    }
+
+
+    /** This method is called from within the constructor to
+     * initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is
+     * always regenerated by the FormEditor.
+     */
+    // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
+    private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
+
+        insidePanel = new javax.swing.JPanel();
+        xLabel = new javax.swing.JLabel();
+        xField = new javax.swing.JTextField();
+        yLabel = new javax.swing.JLabel();
+        yField = new javax.swing.JTextField();
+
+        setLayout(new java.awt.BorderLayout());
+
+        insidePanel.setLayout(new java.awt.GridBagLayout());
+
+        xLabel.setLabelFor(xField);
+        java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("org/netbeans/beaninfo/editors/Bundle"); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        insidePanel.add(xLabel, gridBagConstraints);
+
+        xField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                updateInsets(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(4, 8, 4, 0);
+        insidePanel.add(xField, gridBagConstraints);
+
+        yLabel.setLabelFor(yField);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        insidePanel.add(yLabel, gridBagConstraints);
+
+        yField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                updateInsets(evt);
+            }
+        });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridwidth = java.awt.GridBagConstraints.REMAINDER;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(4, 8, 4, 0);
+        insidePanel.add(yField, gridBagConstraints);
+
+        add(insidePanel, java.awt.BorderLayout.CENTER);
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    private void updateInsets (java.awt.event.ActionEvent evt) {//GEN-FIRST:event_updateInsets
+        try {
+            int x = Integer.parseInt (xField.getText ());
+            int y = Integer.parseInt (yField.getText ());
+            if ( dimensionMode )
+                editor.setValue (new Dimension (x, y));
+            else
+                editor.setValue (new Point (x, y));
+        } catch (NumberFormatException e) {
+            // [PENDING beep]
+        }
+    }//GEN-LAST:event_updateInsets
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel insidePanel;
+    private javax.swing.JTextField xField;
+    private javax.swing.JLabel xLabel;
+    private javax.swing.JTextField yField;
+    private javax.swing.JLabel yLabel;
+    // End of variables declaration//GEN-END:variables
+
+    private ArrayOfIntSupport editor;
+
+}
+
---
+++ VirboAutoplot/src/org/netbeans/beaninfo/editors/RectangleCustomEditor.java
@@ -0,0 +1,291 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ */
+
+package org.netbeans.beaninfo.editors;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Rectangle;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+
+/**
+* @author   Ian Formanek
+*/
+public class RectangleCustomEditor extends javax.swing.JPanel
+implements KeyListener, PropertyChangeListener {
+    static final long serialVersionUID =-9015667991684634296L;
+   
+    private HashMap<JTextField, JLabel> labelMap = new HashMap<JTextField, JLabel>();
+
+    /** Initializes the Form */
+    public RectangleCustomEditor(RectangleEditor editor ) {
+
+        initComponents ();
+        this.editor = editor;
+        Rectangle rectangle = (Rectangle)editor.getValue ();
+        if (rectangle == null) rectangle = new Rectangle (0, 0, 0, 0);
+        xField.setText (Integer.toString(rectangle.x)); // NOI18N
+        yField.setText (Integer.toString(rectangle.y)); // NOI18N
+        widthField.setText (Integer.toString(rectangle.width)); // NOI18N
+        heightField.setText (Integer.toString(rectangle.height)); // NOI18N
+
+        setBorder (new javax.swing.border.EmptyBorder (new java.awt.Insets(5, 5, 5, 5)));
+        jPanel2.setBorder (new javax.swing.border.CompoundBorder (
+                               new javax.swing.border.TitledBorder (
+                                   new javax.swing.border.EtchedBorder (),
+                                   " " + "CTL_Rectangle" + " "),
+                               new javax.swing.border.EmptyBorder (new java.awt.Insets(5, 5, 5, 5))));
+
+        xLabel.setLabelFor(xField);
+        yLabel.setLabelFor(yField);
+        widthLabel.setLabelFor(widthField);
+        heightLabel.setLabelFor(heightField);
+
+        labelMap.put(widthField,widthLabel);
+        labelMap.put(xField,xLabel);
+        labelMap.put(yField,yLabel);
+        labelMap.put(heightField,heightLabel);
+//        HelpCtx.setHelpIDString (this, RectangleCustomEditor.class.getName ());
+
+    }
+
+    public java.awt.Dimension getPreferredSize () {
+        return new java.awt.Dimension (280, 160);
+    }
+
+    private Object getPropertyValue () throws IllegalStateException {
+        try {
+            int x = Integer.parseInt (xField.getText ());
+            int y = Integer.parseInt (yField.getText ());
+            int width = Integer.parseInt (widthField.getText ());
+            int height = Integer.parseInt (heightField.getText ());
+            if ((x < 0) || (y < 0) || (width < 0) || (height < 0)) {
+                IllegalStateException ise = new IllegalStateException();
+                throw ise;
+            }
+            return new Rectangle (x, y, width, height);
+        } catch (NumberFormatException e) {
+            IllegalStateException ise = new IllegalStateException();
+            throw ise;
+        }
+    }
+
+
+    private void initComponents () {
+        setLayout (new java.awt.BorderLayout ());
+
+        jPanel2 = new javax.swing.JPanel ();
+        jPanel2.setLayout (new java.awt.GridBagLayout ());
+        java.awt.GridBagConstraints gridBagConstraints1;
+
+        xLabel = new javax.swing.JLabel ();
+        xLabel.setText (null);
+
+        gridBagConstraints1 = new java.awt.GridBagConstraints ();
+        gridBagConstraints1.anchor = java.awt.GridBagConstraints.WEST;
+        jPanel2.add (xLabel, gridBagConstraints1);
+
+        xField = new javax.swing.JTextField ();
+        xField.addKeyListener (this);
+        
+        gridBagConstraints1 = new java.awt.GridBagConstraints ();
+        gridBagConstraints1.gridwidth = 0;
+        gridBagConstraints1.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints1.insets = new java.awt.Insets (4, 8, 4, 0);
+        gridBagConstraints1.weightx = 1.0;
+        jPanel2.add (xField, gridBagConstraints1);
+
+        yLabel = new javax.swing.JLabel ();
+        yLabel.setText (null);
+
+        gridBagConstraints1 = new java.awt.GridBagConstraints ();
+        gridBagConstraints1.anchor = java.awt.GridBagConstraints.WEST;
+        jPanel2.add (yLabel, gridBagConstraints1);
+
+        yField = new javax.swing.JTextField ();
+        yField.addKeyListener(this);
+
+        gridBagConstraints1 = new java.awt.GridBagConstraints ();
+        gridBagConstraints1.gridwidth = 0;
+        gridBagConstraints1.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints1.insets = new java.awt.Insets (4, 8, 4, 0);
+        gridBagConstraints1.weightx = 1.0;
+        jPanel2.add (yField, gridBagConstraints1);
+
+        widthLabel = new javax.swing.JLabel ();
+        widthLabel.setText (null);
+
+        gridBagConstraints1 = new java.awt.GridBagConstraints ();
+        gridBagConstraints1.anchor = java.awt.GridBagConstraints.WEST;
+        jPanel2.add (widthLabel, gridBagConstraints1);
+
+        widthField = new javax.swing.JTextField ();
+        widthField.addKeyListener(this);
+        
+        gridBagConstraints1 = new java.awt.GridBagConstraints ();
+        gridBagConstraints1.gridwidth = 0;
+        gridBagConstraints1.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints1.insets = new java.awt.Insets (4, 8, 4, 0);
+        gridBagConstraints1.weightx = 1.0;
+        jPanel2.add (widthField, gridBagConstraints1);
+
+        heightLabel = new javax.swing.JLabel ();
+        heightLabel.setText (null);
+
+        gridBagConstraints1 = new java.awt.GridBagConstraints ();
+        gridBagConstraints1.anchor = java.awt.GridBagConstraints.WEST;
+        jPanel2.add (heightLabel, gridBagConstraints1);
+
+        heightField = new javax.swing.JTextField ();
+        heightField.addKeyListener(this);
+
+        gridBagConstraints1 = new java.awt.GridBagConstraints ();
+        gridBagConstraints1.gridwidth = 0;
+        gridBagConstraints1.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints1.insets = new java.awt.Insets (4, 8, 4, 0);
+        gridBagConstraints1.weightx = 1.0;
+        jPanel2.add (heightField, gridBagConstraints1);
+
+
+        add (jPanel2, "Center"); // NOI18N
+
+    }
+
+
+    private void updateRectangle () {
+        try {
+            int x = Integer.parseInt (xField.getText ());
+            int y = Integer.parseInt (yField.getText ());
+            int width = Integer.parseInt (widthField.getText ());
+            int height = Integer.parseInt (heightField.getText ());
+            editor.setValue (new Rectangle (x, y, width, height));
+        } catch (NumberFormatException e) {
+            // [PENDING beep]
+        }
+    }
+
+    public void keyPressed(java.awt.event.KeyEvent e) {
+    }    
+
+    public void keyReleased(java.awt.event.KeyEvent e) {
+        if (checkValues()) {
+            updateRectangle();
+        }
+    }
+    
+    public void keyTyped(java.awt.event.KeyEvent e) {
+    }
+    
+    private boolean checkValues() {
+        Component[] c = jPanel2.getComponents();
+        boolean valid=true;
+        for (int i=0; i < c.length; i++) {
+            if (c[i] instanceof JTextField) {
+                valid &= validFor((JTextField) c[i]);
+            }
+        }
+        return valid;
+    }
+    
+    private boolean validFor(JTextField c) {
+        String s = c.getText().trim();
+        try {
+            Integer.parseInt(s);
+            handleValid(c);
+            return true;
+        } catch (NumberFormatException e) {
+            handleInvalid(c);
+            return false;
+        }
+    }
+    
+    private void handleInvalid(JTextField c) {
+        c.setForeground(getErrorColor());
+        findLabelFor(c).setForeground(getErrorColor());
+    }
+    
+    private void handleValid(JTextField c) {
+        c.setForeground(getForeground());
+        findLabelFor(c).setForeground(getForeground());
+    }
+    
+    private Color getErrorColor() {
+        Color c=UIManager.getColor("nb.errorForeground");
+        if (c == null) {
+            c = Color.RED;
+        }
+        return c;
+    }
+    
+    private JLabel findLabelFor(JTextField c) {
+        return labelMap.get(c);
+    }
+
+    public void propertyChange(PropertyChangeEvent evt) {
+/*        if (PropertyEnv.PROP_STATE.equals(evt.getPropertyName()) && evt.getNewValue() == PropertyEnv.STATE_VALID) {
+            editor.setValue(getPropertyValue());
+        }*/
+    }
+    
+    // Variables declaration - do not modify
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel xLabel;
+    private javax.swing.JTextField xField;
+    private javax.swing.JLabel yLabel;
+    private javax.swing.JTextField yField;
+    private javax.swing.JLabel widthLabel;
+    private javax.swing.JTextField widthField;
+    private javax.swing.JLabel heightLabel;
+    private javax.swing.JTextField heightField;
+    // End of variables declaration
+
+    private RectangleEditor editor;
+
+}
+
