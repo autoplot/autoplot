115424a5534f112fbc71b541b9585fd2611f79cb
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-05-09 18:13:33 +0000

    use DomUtil setPropertyValue and getPropertyValue to support child expressions like plots[0].title.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2237 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/PropertyChangeDiff.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/PropertyChangeDiff.java
@@ -2,12 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.virbo.autoplot.dom;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
@@ -18,57 +14,49 @@ import java.util.logging.Logger;
  * @author jbf
  */
 public class PropertyChangeDiff implements Diff {
+
     String propertyName;
     Object oldVal;
     Object newVal;
 
-    PropertyChangeDiff( String propertyName, Object oldVal, Object newVal ) {
-        this.propertyName= propertyName;
-        this.oldVal= oldVal;
-        this.newVal= newVal;
+    PropertyChangeDiff(String propertyName, Object oldVal, Object newVal) {
+        this.propertyName = propertyName;
+        this.oldVal = oldVal;
+        this.newVal = newVal;
     }
 
-
-    private static void doSet(DomNode node, String propertyName, Object oldVal1, Object newVal ) {
-        try { 
-            BeanInfo info = Introspector.getBeanInfo(node.getClass());
-            for (PropertyDescriptor pd : info.getPropertyDescriptors()) {
-                if ( pd.getName().equals(propertyName) ) {
-                    Object oldVal= pd.getReadMethod().invoke(node);
-                    if ( !( oldVal==oldVal1 || oldVal.equals(oldVal1) ) ) {
-                        throw new IllegalArgumentException("old value");
-                    }
-                    pd.getWriteMethod().invoke(node, newVal);
-                }
+    private static void doSet(DomNode node, String propertyName, Object oldVal1, Object newVal) {
+        try {
+            Object oldVal =  DomUtil.getPropertyValue(node, propertyName);
+            if (!(oldVal == oldVal1 || oldVal.equals(oldVal1))) {
+                throw new IllegalArgumentException("old value");
             }
+            DomUtil.setPropertyValue( node, propertyName, newVal );
         } catch (IllegalAccessException ex) {
             Logger.getLogger(PropertyChangeDiff.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IllegalArgumentException ex) {
             Logger.getLogger(PropertyChangeDiff.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InvocationTargetException ex) {
             Logger.getLogger(PropertyChangeDiff.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IntrospectionException ex) {
-            Logger.getLogger(PropertyChangeDiff.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-
-    public void doDiff( DomNode node ) {
-        doSet( node, propertyName, oldVal, newVal );
+    public void doDiff(DomNode node) {
+        doSet(node, propertyName, oldVal, newVal);
     }
 
     public void undoDiff(DomNode node) {
-        doSet( node, propertyName, newVal, oldVal );
+        doSet(node, propertyName, newVal, oldVal);
     }
 
     public String propertyName() {
         return propertyName;
     }
-    
+
     public String toString() {
-        String s= this.propertyName + " " + oldVal + " to " + newVal;
-        if ( s.length()>30 ) {
-            s= this.propertyName;
+        String s = this.propertyName + " " + oldVal + " to " + newVal;
+        if (s.length() > 30) {
+            s = this.propertyName;
         }
         return s;
     }
