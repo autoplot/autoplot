55ffd14a18f51e684d8d7b0a1cca99f8637c9022
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-05-09 18:23:12 +0000

    use tickletimer instead of timer.  I'm not sure why...
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2242 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/LayoutListener.java
+++ VirboAutoplot/src/org/virbo/autoplot/LayoutListener.java
@@ -7,14 +7,12 @@ package org.virbo.autoplot;
 import org.das2.graph.DasAxis;
 import org.das2.graph.DasPlot;
 import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.logging.Logger;
-import javax.swing.Timer;
 import org.virbo.autoplot.dom.ApplicationController;
 import org.virbo.autoplot.layout.LayoutUtil;
+import org.virbo.autoplot.util.TickleTimer;
 
 /**
  * Listens to changes in axis labels and colorbar visible to trigger relayout.
@@ -22,49 +20,47 @@ import org.virbo.autoplot.layout.LayoutUtil;
  */
 public class LayoutListener implements PropertyChangeListener {
 
-    ApplicationModel model;  
-    Timer t;
+    final ApplicationModel model;
+    TickleTimer t;
     static Logger logger = Logger.getLogger("virbo.autoplot.autolayout");
-    private static final String PENDING_CHANGE_AUTOLAYOUT= "autolayout";
+    private static final String PENDING_CHANGE_AUTOLAYOUT = "autolayout";
 
-    public LayoutListener(ApplicationModel model) {
+    public LayoutListener(final ApplicationModel model) {
         this.model = model;
+        t = new TickleTimer(100, new PropertyChangeListener() {
+
+            public void propertyChange(PropertyChangeEvent evt) {
+                logger.fine("do autolayout");
+                ApplicationController applicationController = model.getDocumentModel().getController();
+                model.dom.getController().getCanvas().getController().performingChange(LayoutListener.this, PENDING_CHANGE_AUTOLAYOUT);
+                model.canvas.performingChange(LayoutListener.this, PENDING_CHANGE_AUTOLAYOUT);
+                LayoutUtil.autolayout(applicationController.getDasCanvas(),
+                        applicationController.getRow(), applicationController.getColumn());
+                model.canvas.changePerformed(LayoutListener.this, PENDING_CHANGE_AUTOLAYOUT);
+                model.dom.getController().getCanvas().getController().changePerformed(LayoutListener.this, PENDING_CHANGE_AUTOLAYOUT);
+
+            }
+        });
     }
 
-    public void listenTo( DasPlot plot ) {
+    public void listenTo(DasPlot plot) {
         plot.addPropertyChangeListener(DasPlot.PROP_TITLE, this);
         plot.getXAxis().addPropertyChangeListener(DasAxis.PROP_BOUNDS, this);
         plot.getYAxis().addPropertyChangeListener(DasAxis.PROP_BOUNDS, this);
     }
-    
-    public void listenTo( DasAxis colorbar ) {
+
+    public void listenTo(DasAxis colorbar) {
         colorbar.addPropertyChangeListener("visible", this);
-        colorbar.addPropertyChangeListener(DasAxis.PROP_BOUNDS, this );
+        colorbar.addPropertyChangeListener(DasAxis.PROP_BOUNDS, this);
     }
 
     public void propertyChange(PropertyChangeEvent evt) {
-        if (model.dom.getOptions().isAutolayout() ) {
+        if (model.dom.getOptions().isAutolayout() && !model.dom.getController().isValueAdjusting()) {
             logger.fine("property change: " + evt.getPropertyName());
             if (evt.getSource() instanceof Component &&
                     ((Component) evt.getSource()).isVisible()) {
-                if (t == null) {
-                    logger.fine("create timer ");
-                    t = new Timer(100, new ActionListener() {
-                        public synchronized void actionPerformed(ActionEvent e) {
-                            logger.fine("do autolayout");
-                            ApplicationController applicationController= model.getDocumentModel().getController();
-                            model.dom.getController().getCanvas().getController().performingChange(LayoutListener.this,PENDING_CHANGE_AUTOLAYOUT);
-                            model.canvas.performingChange(LayoutListener.this, PENDING_CHANGE_AUTOLAYOUT);
-                            LayoutUtil.autolayout( applicationController.getDasCanvas(), 
-                                    applicationController.getRow(), applicationController.getColumn() );
-                            model.canvas.changePerformed(LayoutListener.this, PENDING_CHANGE_AUTOLAYOUT);
-                            model.dom.getController().getCanvas().getController().changePerformed(LayoutListener.this,PENDING_CHANGE_AUTOLAYOUT);
-                        }
-                    });
-                    t.setRepeats(false);
-                }
                 model.canvas.registerPendingChange(this, PENDING_CHANGE_AUTOLAYOUT);
-                t.restart();
+                t.tickle(evt.getPropertyName());
             }
         }
     }
