296775c79d87b9481e8abf6b1593367e4dda3088
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-05-12 11:00:43 +0000

    bugfix
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2281 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/autoplot/pngwalk/DemoPngWalk.java
+++ VirboAutoplot/src/org/autoplot/pngwalk/DemoPngWalk.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.autoplot.pngwalk;
 
 import java.awt.event.ActionEvent;
@@ -18,9 +17,10 @@ import org.virbo.autoplot.ScriptContext;
  * @author jbf
  */
 public class DemoPngWalk {
-    public static void main( String[] args ) {
-        final PngWalkTool tool= new PngWalkTool();
-        
+
+    public static void main(String[] args) {
+        final PngWalkTool tool = new PngWalkTool();
+
         final ArgumentList alm = new ArgumentList("AutoPlotUI");
         alm.addBooleanSwitchArgument("nativeLAF", "n", "nativeLAF", "use the system look and feel");
 
@@ -32,43 +32,46 @@ public class DemoPngWalk {
             }
         }
 
-        final String template=  "file:/tmp/pngwalk/product_$Y$m$d.png" ; // One Slash!!
+        String template = "file:/tmp/pngwalk/product_$Y$m$d.png"; // One Slash!!
         //final String template=  "file:/home/jbf/temp/product_$Y$m$d.png" ; // One Slash!!
         //final String template=  "file:/net/spot3/home/jbf/fun/pics/2001minnesota/.*JPG" ;
         //final String template= "file:/home/jbf/public_html/voyager/VGPW_0201/BROWSE/V1/.*.PNG";
         //final String template= "file:///net/spot3/home/jbf/fun/pics/20080315_tenerife_masca_hike/IMG_.*.JPG";
         //final String template= "http://www.swpc.noaa.gov/ftpdir/lists/hpi/plots/pmap_$Y_$m_$d_...._S_.*_.*_.*_.*.gif";
 
-        tool.setTemplate( template );
-        tool.addFileAction( "file:/home/jbf/temp/product_.*.png", "autoplot", new AbstractAction( "launch Autoplot" ) {
-            public void actionPerformed(ActionEvent e) {
-                    String s = tool.getSelectedFile();
-                    int i0 = template.indexOf("_$Y");
-                    int i1= s.indexOf(".png");
-                    String timeRange = s.substring(i0+1,  i1);
-                    String productFile = template.substring(0,i0)+".vap";
+        tool.setTemplate(template);
+        tool.addFileAction("file:/home/jbf/temp/product_.*.png", "autoplot", new AbstractAction("Launch Autoplot") {
 
-                    final String suri = productFile + "?timeRange=" + timeRange;
-                    Runnable run= new Runnable() {
-                        public void run() {
+            public void actionPerformed(ActionEvent e) {
+                String s = tool.getSelectedFile();
+                String template = tool.getTemplate();
+                int i0 = template.indexOf("_$Y");
+                int i1 = s.indexOf(".png");
+                String timeRange = s.substring(i0 + 1, i1);
+                String productFile = template.substring(0, i0) + ".vap";
+
+                final String suri = productFile + "?timeRange=" + timeRange;
+                Runnable run = new Runnable() {
+
+                    public void run() {
                         try {
                             ScriptContext.plot(suri);
                         } catch (InterruptedException ex) {
                             Logger.getLogger(DemoPngWalk.class.getName()).log(Level.SEVERE, null, ex);
                         }
-                        }
-                    };
-                    new Thread(run).start();
+                    }
+                };
+                new Thread(run).start();
             }
-        } );
+        });
 
-        JFrame frame= new JFrame("png walk demo");
+        JFrame frame = new JFrame("png walk demo");
         frame.getContentPane().add(tool);
 
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
 
         frame.setVisible(true);
-        
+
     }
 }
