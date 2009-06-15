f20446583fcd4af354d373eb19bbbabd532156cd
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-06-15 12:07:09 +0000

    bugfix 2806556:  	 can't enter data source browser for remote files w/o query
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2568 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/FileSystemUtil.java
+++ DataSource/src/org/virbo/datasource/FileSystemUtil.java
@@ -9,7 +9,12 @@
 
 package org.virbo.datasource;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import org.das2.util.filesystem.FileSystem;
+import org.das2.util.filesystem.FileSystem.FileSystemOfflineException;
+import org.das2.util.monitor.ProgressMonitor;
 
 /**
  *
@@ -21,4 +26,55 @@ public class FileSystemUtil {
         String s= fs.getRootURL().toString();
         if ( resource.startsWith(s) ) return resource.substring(s.length()); else return resource;
     }
+
+    /**
+     * checks to see if the context uri appears to represent an existing
+     * data source.  false indicates that the resource is known to not exist.
+     * true indicates that the resource does exist.
+     *
+     * @param context URI, such as http://server.org/data/asciitable.dat
+     * @return
+     */
+    public static boolean resourceExists( String context ) throws FileSystemOfflineException, MalformedURLException {
+        URLSplit split= URLSplit.parse(context);
+        FileSystem fs= FileSystem.create( new URL( split.path ) );
+        if ( fs.getFileObject(split.file.substring(split.path.length())).exists() ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * have the filesystem download the resource.
+     *
+     * @param context URI, such as http://server.org/data/asciitable.dat
+     * @return
+     */
+    public static void doDownload(String context,ProgressMonitor mon) throws FileSystemOfflineException, MalformedURLException, IOException  {
+        URLSplit split= URLSplit.parse(context);
+        FileSystem fs= FileSystem.create( new URL( split.path ) );
+        fs.getFileObject(split.file.substring(split.path.length())).getFile(mon);
+    }
+
+    /**
+     * returns true if the resource is already in a local cache.
+     * TODO: applet support, we want to avoid multiple downloads.
+     * @param context
+     * @return
+     * @throws org.das2.util.filesystem.FileSystem.FileSystemOfflineException
+     * @throws java.net.MalformedURLException
+     */
+    static boolean resourceIsLocal(String context) throws FileSystemOfflineException, MalformedURLException {
+        URLSplit split= URLSplit.parse(context);
+        FileSystem fs= FileSystem.create( new URL( split.path ) );
+        if ( fs.getFileObject(split.file.substring(split.path.length())).isLocal() ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    
 }
