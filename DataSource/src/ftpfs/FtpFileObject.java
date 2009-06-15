170a4969ddf7a793df46f8a0f39a1f5c92056c2a
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-06-15 11:48:40 +0000

    support exists() without a download by looking for .listing file.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2567 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/ftpfs/FtpFileObject.java
+++ DataSource/src/ftpfs/FtpFileObject.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.das2.util.filesystem.FileSystem;
 import org.das2.util.filesystem.FileSystem.DirectoryEntry;
 import org.das2.util.filesystem.WebFileObject;
 import org.das2.util.filesystem.WebFileSystem;
@@ -49,6 +50,37 @@ public class FtpFileObject extends WebFileObject {
             return -1;
         }
         
+    }
+
+    @Override
+    public boolean exists() {
+        if ( getLocalFile()!=null && getLocalFile().exists() ) {
+            return true;
+        }
+
+        File listing= new File( this.getLocalFile().getParent(), ".listing" );
+        if ( !listing.exists() ) {
+            ftpfs.listDirectory( getParent().getNameExt() );
+        }
+        if ( listing.exists() ) {
+            try {
+                DirectoryEntry[] list = ftpfs.parseLsl(null, listing);
+                int ii= this.getNameExt().lastIndexOf("/");
+                String lookFor= this.getNameExt().substring(ii+1);
+                for (int i = 0; i < list.length; i++) {
+                    if (list[i].name.equals(lookFor)) {
+                        return true;
+                    }
+                }
+                return false;
+            } catch (IOException ex) {
+                Logger.getLogger(FtpFileObject.class.getName()).log(Level.SEVERE, null, ex);
+                return false;
+            }
+        } else {
+            FileSystem.getExceptionHandler().handleUncaught( new IOException("local listing file not found") );
+            return false;
+        }
     }
 
 
