fc026b9310103e3ae09e373be326cf0ee7d23be2
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-07-22 17:30:39 +0000

    add java test while jython has problems.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2736 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/test/endtoend/Test001.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.endtoend;
+
+import java.io.IOException;
+import org.virbo.autoplot.ScriptContext;
+import org.virbo.dataset.QDataSet;
+import org.virbo.dsops.Ops;
+import org.virbo.jythonsupport.Util;
+
+/**
+ *
+ * @author jbf
+ */
+public class Test001 {
+    public static void main(String[] args) throws InterruptedException, IOException, Exception {
+        QDataSet ds= Util.getDataSet( "http://www.autoplot.org/data/fireworks.wav" );
+        ScriptContext.plot( 0, ds );
+        ScriptContext.plot( 1, Ops.fftWindow( ds, 512 ) );
+        ScriptContext.setCanvasSize( 800, 600 );
+        ScriptContext.writeToPng( "test001.png" );
+    }
+}
