8a0b9f41b1d4011bde1d5466461f8b08ba5a8c70
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-07-23 02:29:53 +0000

    use ScriptContext.load since plot methods require the Application view to be initialized.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2745 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test003.java
+++ VirboAutoplot/src/test/endtoend/Test003.java
@@ -15,7 +15,7 @@ import org.virbo.autoplot.ScriptContext;
 public class Test003 {
     public static void main(String[] args) throws InterruptedException, IOException, Exception {
         try {
-            ScriptContext.plot( "file:/home/jbf/ct/lanl/hudson/geo_pitch_stack_test1_local.vap" );
+            ScriptContext.load( "/home/jbf/ct/lanl/hudson/geo_pitch_stack_test1_local.vap" );
             ScriptContext.setCanvasSize( 800, 600 );
             ScriptContext.writeToPng( "test003.png" );
             System.exit(0);  // TODO: something is firing up the event thread
