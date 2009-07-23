6778a725422f9c92abd53d83f02d11bb38140695
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-07-23 12:15:28 +0000

    test002 does histograms of both waveform and spectrum.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2748 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test002.java
+++ VirboAutoplot/src/test/endtoend/Test002.java
@@ -19,9 +19,10 @@ public class Test002 {
     public static void main(String[] args) throws InterruptedException, IOException, Exception {
         try {
             QDataSet ds = Util.getDataSet("http://www.autoplot.org/data/fireworks.wav");
+            ScriptContext.setCanvasSize(400, 800);
             ScriptContext.plot(0, Ops.autoHistogram(ds));
-            ScriptContext.getDocumentModel().getPlots(0).setTitle("auto histogram of http://www.autoplot.org/data/fireworks.wav");
-            ScriptContext.setCanvasSize(400, 400);
+            ScriptContext.plot(1, Ops.autoHistogram( Ops.fftWindow(ds, 512) ) );
+            //ScriptContext.getDocumentModel().getPlots(0).setTitle("auto histogram of http://www.autoplot.org/data/fireworks.wav");
             ScriptContext.writeToPng("test002.png");
             System.exit(0);  // TODO: something is firing up the event thread
         } catch (RuntimeException ex) {
