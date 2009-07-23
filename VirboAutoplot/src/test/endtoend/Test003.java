9f32375009abe570298874997cbcf274443ecbaf
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-07-23 01:13:03 +0000

    whoops
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2741 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test003.java
+++ VirboAutoplot/src/test/endtoend/Test003.java
@@ -7,8 +7,6 @@ package test.endtoend;
 
 import java.io.IOException;
 import org.virbo.autoplot.ScriptContext;
-import org.virbo.dataset.QDataSet;
-import org.virbo.jythonsupport.Util;
 
 /**
  *
@@ -17,7 +15,7 @@ import org.virbo.jythonsupport.Util;
 public class Test003 {
     public static void main(String[] args) throws InterruptedException, IOException, Exception {
         try {
-            QDataSet ds= Util.getDataSet( "file:/home/jbf/ct/lanl/hudson/geo_pitch_stack_test1_local.vap" );
+            ScriptContext.plot( "file:/home/jbf/ct/lanl/hudson/geo_pitch_stack_test1_local.vap" );
             ScriptContext.setCanvasSize( 800, 600 );
             ScriptContext.writeToPng( "test003.png" );
             System.exit(0);  // TODO: something is firing up the event thread
