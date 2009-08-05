d0f6aa669cd0435d1129a89c1b584ae06c250d51
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-05 02:07:48 +0000

    improve plots by forcing log and domain divider, erc.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2772 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test002.java
+++ VirboAutoplot/src/test/endtoend/Test002.java
@@ -5,7 +5,9 @@
 package test.endtoend;
 
 import java.io.IOException;
+import org.virbo.autoplot.RenderType;
 import org.virbo.autoplot.ScriptContext;
+import org.virbo.autoplot.dom.Application;
 import org.virbo.dataset.QDataSet;
 import org.virbo.dsops.Ops;
 import org.virbo.jythonsupport.Util;
@@ -18,10 +20,22 @@ public class Test002 {
 
     public static void main(String[] args) throws InterruptedException, IOException, Exception {
         try {
+            //ScriptContext.createGui();
             QDataSet ds = Util.getDataSet("http://www.autoplot.org/data/fireworks.wav");
+            final Application dom = ScriptContext.getDocumentModel();
+            dom.getCanvases(0).setFitted(false);
             ScriptContext.setCanvasSize(400, 800);
             ScriptContext.plot(0, Ops.autoHistogram(ds));
-            ScriptContext.plot(1, Ops.autoHistogram( Ops.fftWindow(ds, 512) ) );
+            dom.getPlots(0).getXaxis().getController().getDasAxis().setUseDomainDivider(true);
+            dom.getPanels(0).setRenderType( RenderType.fillToZero );
+            dom.getPlots(0).getYaxis().setLabel("auto histogram of!chttp://www.autoplot.org/data/fireworks.wav");
+
+            QDataSet ds2= Ops.autoHistogram( Ops.log10( Ops.fftWindow(ds, 512) ) ) ;
+            ScriptContext.plot(1, ds2 );
+            dom.getPlots(1).getXaxis().getController().getDasAxis().setUseDomainDivider(true);
+            dom.getPanels(1).setRenderType( RenderType.fillToZero );
+            dom.getPlots(1).getYaxis().setLabel("auto histogram of!cpower spectrum");
+            dom.getPlots(1).getXaxis().setLabel("log(Ops.fftWindow(512)");
             //ScriptContext.getDocumentModel().getPlots(0).setTitle("auto histogram of http://www.autoplot.org/data/fireworks.wav");
             ScriptContext.writeToPng("test002.png");
             System.exit(0);  // TODO: something is firing up the event thread
