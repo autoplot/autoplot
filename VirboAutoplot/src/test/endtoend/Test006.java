56769471022cd30260882466f540a8c7e5ebc2f9
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-07 18:07:52 +0000

    also do histograms of depend_0, since the times often are the problem.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2785 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test006.java
+++ VirboAutoplot/src/test/endtoend/Test006.java
@@ -4,7 +4,9 @@
  */
 package test.endtoend;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import org.virbo.autoplot.ScriptContext;
 import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QDataSet;
@@ -28,6 +30,17 @@ public class Test006 {
         String label= String.format( "test006_%03d", id );
         hist.putProperty( QDataSet.LABEL, label );
         ScriptContext.formatDataSet( hist, label+".qds");
+
+        QDataSet dep0= (QDataSet) ds.property( QDataSet.DEPEND_0 );
+        if ( dep0!=null ) {
+            MutablePropertyDataSet hist2= (MutablePropertyDataSet) Ops.autoHistogram(dep0);
+            ScriptContext.formatDataSet( hist2, label+".dep0.qds");
+        } else {
+            PrintWriter pw= new PrintWriter( label+".dep0.qds" );
+            pw.println("no dep0");
+            pw.close();
+        }
+
         System.err.printf( "Read in %9.3f seconds (%s): %s\n", t, label, uri );
     }
     
@@ -36,7 +49,11 @@ public class Test006 {
 
             doTest( 0, "ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/omni2_$Y.dat?column=field17&timerange=1963&timeFormat=$Y+$j+$H&time=field0&validMax=999" );
             doTest( 1, "http://goes.ngdc.noaa.gov/data/avg/2004/A1050412.TXT?skip=23&timeFormat=$y$m$d+$H$M&column=E1&time=YYMMDD" );
-            doTest( 2, "vap:http://vho.nasa.gov/mission/soho/celias_pm_30sec/1998.txt?time=YY&column=GSE_X&timeFormat=$y+$b+$d+$(ignore):$H:$M:$S" );
+            doTest( 2, "file:///home/jbf/ct/hudson/data.backup/dat/V11979066.CSV?depend0=field0&column=field13" );
+            doTest( 3, "file:///home/jbf/ct/hudson/data.backup/dat/power.dat.txt" );
+
+            //doTest( 2, "vap:http://vho.nasa.gov/mission/soho/celias_pm_30sec/1998.txt?time=YY&column=GSE_X&timeFormat=$y+$b+$d+$(ignore):$H:$M:$S" );
+
 
             System.exit(0);  // TODO: something is firing up the event thread
         } catch (RuntimeException ex) {
