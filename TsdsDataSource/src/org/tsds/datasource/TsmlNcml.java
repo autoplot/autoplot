264dcb744bc701a884fe0135971fc4924fae8526
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-08 12:44:58 +0000

    reformat code.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2793 0c594bb0-8543-0410-a25e-d8ec440261db

--- TsdsDataSource/src/org/tsds/datasource/TsmlNcml.java
+++ TsdsDataSource/src/org/tsds/datasource/TsmlNcml.java
@@ -60,15 +60,15 @@ import org.xml.sax.SAXException;
  * @author jbf
  */
 public class TsmlNcml {
+
     private static final int RANK_LIMIT = 2;
 
     public static void main(String[] args) throws Exception {
         new TsmlNcml().doRead(
-                new URL( "http://timeseries.org/cgi-bin/get.cgi?StartDate=19890104&EndDate=19890104&ext=bin" +
-                "&out=ncml&ppd=8&filter=4&param1=SourceAcronym_Subset1-1-v0" ), null );
+                new URL("http://timeseries.org/cgi-bin/get.cgi?StartDate=19890104&EndDate=19890104&ext=bin" +
+                "&out=ncml&ppd=8&filter=4&param1=SourceAcronym_Subset1-1-v0"), null);
     }
-
-    URL codebase= null;
+    URL codebase = null;
 
     /**
      * read the ncml+tsml from the url, and follow references to read in the dataset.
@@ -79,13 +79,13 @@ public class TsmlNcml {
      * @throws javax.xml.parsers.ParserConfigurationException
      * @throws org.xml.sax.SAXException
      */
-    public QDataSet doRead( URL url, URLConnection connect ) throws IOException, ParserConfigurationException, SAXException {
+    public QDataSet doRead(URL url, URLConnection connect) throws IOException, ParserConfigurationException, SAXException {
         //URL url= TsmlNcml.class.getResource("/test/data.filter4.tsml.ncml.xml");
-        codebase= url;
+        codebase = url;
 
         InputStream in;
-        if ( connect!=null ) {
-            in= connect.getInputStream();
+        if (connect != null) {
+            in = connect.getInputStream();
         } else {
             in = url.openStream();
         }
@@ -113,25 +113,26 @@ public class TsmlNcml {
         NodeList kids = aggr.getChildNodes();
         MutablePropertyDataSet result = null;
 
-        LinkedHashMap<String,MutablePropertyDataSet> dss= new LinkedHashMap();
+        LinkedHashMap<String, MutablePropertyDataSet> dss = new LinkedHashMap();
 
-        MutablePropertyDataSet depend= null; // the dataset that has dependencies.
-        String lastKey= null;
+        MutablePropertyDataSet depend = null; // the dataset that has dependencies.
+        String lastKey = null;
         for (int i = 0; i < kids.getLength(); i++) {
             Node n = kids.item(i);
             if (n.getNodeName().equals("netcdf")) {
                 MutablePropertyDataSet ds = netcdf(n);
-                dss.put( (String)ds.property(QDataSet.NAME), ds );
-                lastKey= (String)ds.property(QDataSet.NAME);
-                if ( ! ds.property("shape").equals( ds.property(QDataSet.NAME) ) ) depend= ds;
+                dss.put((String) ds.property(QDataSet.NAME), ds);
+                lastKey = (String) ds.property(QDataSet.NAME);
+                if (!ds.property("shape").equals(ds.property(QDataSet.NAME)))
+                    depend = ds;
             }
         }
 
-        if ( depend!=null ) {
-            String shape= (String) depend.property("shape");
-            String[] shapes= shape.split("[, ]");
-            for ( int i=0; i<shapes.length; i++ ) {
-                Ops.dependsOn( depend, i, dss.get(shapes[i]) );
+        if (depend != null) {
+            String shape = (String) depend.property("shape");
+            String[] shapes = shape.split("[, ]");
+            for (int i = 0; i < shapes.length; i++) {
+                Ops.dependsOn(depend, i, dss.get(shapes[i]));
             }
             return depend;
         } else {
@@ -139,25 +140,25 @@ public class TsmlNcml {
         }
     }
 
-    private Units lookupUnits( String sunits ) {
-                    if ( sunits.contains("since") ) {
-                        try {
-                            return SemanticOps.lookupTimeUnits(sunits);
-                        } catch (ParseException ex) {
-                            throw new RuntimeException(ex);
-                        }
-                    } else {
-                       return SemanticOps.lookupUnits(sunits);
-                    }
+    private Units lookupUnits(String sunits) {
+        if (sunits.contains("since")) {
+            try {
+                return SemanticOps.lookupTimeUnits(sunits);
+            } catch (ParseException ex) {
+                throw new RuntimeException(ex);
+            }
+        } else {
+            return SemanticOps.lookupUnits(sunits);
+        }
     }
 
     protected MutablePropertyDataSet netcdf(Node node) throws MalformedURLException, IOException {
         Map<String, Object> props = new HashMap();
         NodeList nl = node.getChildNodes();
         MutablePropertyDataSet result = null;
-        Map<String,Node> dimensions = new LinkedHashMap();
+        Map<String, Node> dimensions = new LinkedHashMap();
         NamedNodeMap attrs = node.getAttributes();
-        String dataType= null;
+        String dataType = null;
         if (attrs.getNamedItem("location") != null) {
             result = location(node);
         } else {
@@ -166,25 +167,25 @@ public class TsmlNcml {
                 if (child.getNodeName().equals("aggregation")) {
                     result = aggregation(child);
                 } else if (child.getNodeName().equals("dimension")) {
-                    dimensions.put( maybeGetAttr(child,"name"), child );
+                    dimensions.put(maybeGetAttr(child, "name"), child);
                 } else if (child.getNodeName().equals("attribute")) {
                     if (((Attr) child.getAttributes().getNamedItem("name")).getValue().equals("units")) {
                         props.put(QDataSet.UNITS, lookupUnits(child.getAttributes().getNamedItem("value").getTextContent()));
                     } else if (((Attr) child.getAttributes().getNamedItem("name")).getValue().equals("DataType")) {
-                        dataType= child.getAttributes().getNamedItem("value").getTextContent();
+                        dataType = child.getAttributes().getNamedItem("value").getTextContent();
                     }
                 } else if (child.getNodeName().equals("variable")) {
-                    result = variable( child, dimensions, null );
+                    result = variable(child, dimensions, null);
                 }
             }
         }
-        if ( dataType!=null ) {
-            if ( dataType.equals("vector") ) {
-                String[] componentLabels= new String[result.length(0)];
-                for ( int i=0; i<componentLabels.length; i++ ) {
-                    componentLabels[i]= "c"+i; // TODO: ask Bob how the components are labelled.
+        if (dataType != null) {
+            if (dataType.equals("vector")) {
+                String[] componentLabels = new String[result.length(0)];
+                for (int i = 0; i < componentLabels.length; i++) {
+                    componentLabels[i] = "c" + i; // TODO: ask Bob how the components are labelled.
                 }
-                result.putProperty( QDataSet.DEPEND_1, Ops.labels(componentLabels) ); //TODO: QDataSet scheme change coming COMPONENT.
+                result.putProperty(QDataSet.DEPEND_1, Ops.labels(componentLabels)); //TODO: QDataSet scheme change coming COMPONENT.
             }
         }
         return result;
@@ -201,18 +202,19 @@ public class TsmlNcml {
      * @param name
      * @return
      */
-    private static String maybeGetAttr( Node node, String name ) {
-        Node niosp= node.getAttributes().getNamedItem(name);
-        if ( niosp==null ) return null; else return niosp.getNodeValue();
+    private static String maybeGetAttr(Node node, String name) {
+        Node niosp = node.getAttributes().getNamedItem(name);
+        if (niosp == null) return null;
+        else return niosp.getNodeValue();
     }
 
     protected MutablePropertyDataSet location(Node node) throws MalformedURLException, IOException {
-        MutablePropertyDataSet result=null;
-        
-        String iosp= maybeGetAttr( node, "iosp" );
+        MutablePropertyDataSet result = null;
 
-        if ( "org.timeseries.tsds".equals(iosp) ) {
-            result= tsdsLocation( node );
+        String iosp = maybeGetAttr(node, "iosp");
+
+        if ("org.timeseries.tsds".equals(iosp)) {
+            result = tsdsLocation(node);
         }
         return result;
     }
@@ -224,7 +226,7 @@ public class TsmlNcml {
      * @param values  if non-null, these are the values read in via iosp.
      * @return
      */
-    protected MutablePropertyDataSet variable( Node node, Map<String,Node> dimensions, MutablePropertyDataSet values ) {
+    protected MutablePropertyDataSet variable(Node node, Map<String, Node> dimensions, MutablePropertyDataSet values) {
         Map<String, Object> props = new HashMap();
         NodeList nl = node.getChildNodes();
         MutablePropertyDataSet result = values;
@@ -232,7 +234,7 @@ public class TsmlNcml {
             Node child = nl.item(i);
             if (child.getNodeName().equals("attribute")) {
                 if (((Attr) child.getAttributes().getNamedItem("name")).getValue().equals("units")) {
-                    String sunits= child.getAttributes().getNamedItem("value").getTextContent();
+                    String sunits = child.getAttributes().getNamedItem("value").getTextContent();
                     props.put(QDataSet.UNITS, lookupUnits(sunits));
                 }
             } else if (child.getNodeName().equals("values")) {
@@ -240,19 +242,18 @@ public class TsmlNcml {
                 Double scale = Double.parseDouble(increment.getTextContent());
                 Node start = child.getAttributes().getNamedItem("start");
                 Double offset = Double.parseDouble(start.getTextContent());
-                int n = dimensionLength( dimensions.get( maybeGetAttr(node,"shape") ) );
+                int n = dimensionLength(dimensions.get(maybeGetAttr(node, "shape")));
                 result = new TagGenDataSet(n, scale, offset);
             }
         }
         for (Entry<String, Object> e : props.entrySet()) {
             result.putProperty(e.getKey(), e.getValue());
         }
-        result.putProperty( QDataSet.NAME, maybeGetAttr( node, "name" ) );
-        result.putProperty( "shape", maybeGetAttr(node,"shape") ); // used for aggregation to identify independent parameter
-        
+        result.putProperty(QDataSet.NAME, maybeGetAttr(node, "name"));
+        result.putProperty("shape", maybeGetAttr(node, "shape")); // used for aggregation to identify independent parameter
+
         return result;
     }
-
 
     /**
      * read in values in a different location using the org.timeseries.tsds IOServiceProvider.
@@ -265,57 +266,57 @@ public class TsmlNcml {
      * @throws java.net.MalformedURLException
      * @throws java.io.IOException
      */
-    protected MutablePropertyDataSet tsdsLocation( Node node ) throws MalformedURLException, IOException {
-        Map<String,Node> dims= new LinkedHashMap();
-        Node variable= null;
-        NodeList kids= node.getChildNodes();
-        
-        for ( int i=0; i<kids.getLength(); i++ ) {
-            Node n= kids.item(i);
-            if ( n.getNodeName().equals("dimension") ) {
-                dims.put( maybeGetAttr( n, "name"), n );
-            } else if ( n.getNodeName().equals("variable") ) {
-                variable= n;
+    protected MutablePropertyDataSet tsdsLocation(Node node) throws MalformedURLException, IOException {
+        Map<String, Node> dims = new LinkedHashMap();
+        Node variable = null;
+        NodeList kids = node.getChildNodes();
+
+        for (int i = 0; i < kids.getLength(); i++) {
+            Node n = kids.item(i);
+            if (n.getNodeName().equals("dimension")) {
+                dims.put(maybeGetAttr(n, "name"), n);
+            } else if (n.getNodeName().equals("variable")) {
+                variable = n;
             }
         }
 
-        String shape= maybeGetAttr( variable, "shape" );
-        String[] shapes= shape.split("[, ]");
+        String shape = maybeGetAttr(variable, "shape");
+        String[] shapes = shape.split("[, ]");
 
-        int len1= -1;
-        if ( dims.size()>1 ) {
-            len1= dimensionLength( dims.get( shapes[1]) );
+        int len1 = -1;
+        if (dims.size() > 1) {
+            len1 = dimensionLength(dims.get(shapes[1]));
         }
-        Object type= BufferDataSet.DOUBLE;
+        Object type = BufferDataSet.DOUBLE;
 
-        int size= ( len1!=-1 ? len1 : 1 ) * dimensionLength( dims.get(shapes[0]) ) * BufferDataSet.byteCount(type);
+        int size = (len1 != -1 ? len1 : 1) * dimensionLength(dims.get(shapes[0])) * BufferDataSet.byteCount(type);
 
-        String surl=  maybeGetAttr(node,"location");
-        if ( surl.contains("%2F%2F") ) surl= URLDecoder.decode(surl, "US-ASCII" ); // location can be either URL-encoded or use &amp;
-        
-        String s= maybeGetAttr( node, "iospParam" );
-        List<String> iospParam= Collections.emptyList();
-        if ( s!=null ) {
-            iospParam= Arrays.asList( s.split(",") );
+        String surl = maybeGetAttr(node, "location");
+        if (surl.contains("%2F%2F")) surl = URLDecoder.decode(surl, "US-ASCII"); // location can be either URL-encoded or use &amp;
+
+        String s = maybeGetAttr(node, "iospParam");
+        List<String> iospParam = Collections.emptyList();
+        if (s != null) {
+            iospParam = Arrays.asList(s.split(","));
         }
 
         MutablePropertyDataSet values;
-        if ( iospParam.contains("filter4") ) {
-            int points= dimensionLength( dims.get(shapes[0]) ) / 3;
-            BufferDataSet data3= (BufferDataSet)tsds( new URL( codebase, surl ), size, len1, type, new NullProgressMonitor() );
+        if (iospParam.contains("filter4")) {
+            int points = dimensionLength(dims.get(shapes[0])) / 3;
+            BufferDataSet data3 = (BufferDataSet) tsds(new URL(codebase, surl), size, len1, type, new NullProgressMonitor());
 
-            MutablePropertyDataSet data= data3.trim( 0, points );
-            BufferDataSet dataMin= data3.trim( RANK_LIMIT*points, 3*points );
-            dataMin.putProperty( QDataSet.NAME, "binmin" );
-            BufferDataSet dataMax= data3.trim( 1*points, RANK_LIMIT*points );
-            dataMax.putProperty( QDataSet.NAME, "binmax" );
+            MutablePropertyDataSet data = data3.trim(0, points);
+            BufferDataSet dataMin = data3.trim(RANK_LIMIT * points, 3 * points);
+            dataMin.putProperty(QDataSet.NAME, "binmin");
+            BufferDataSet dataMax = data3.trim(1 * points, RANK_LIMIT * points);
+            dataMax.putProperty(QDataSet.NAME, "binmax");
             data.putProperty(QDataSet.DELTA_PLUS, Ops.subtract(dataMax, data));
             data.putProperty(QDataSet.DELTA_MINUS, Ops.subtract(data, dataMin));
-            values= data;
+            values = data;
         } else {
-            values= tsds( new URL( codebase, surl ), size, len1, type, new NullProgressMonitor() );
+            values = tsds(new URL(codebase, surl), size, len1, type, new NullProgressMonitor());
         }
-        return variable( variable, dims, values );
+        return variable(variable, dims, values);
     }
 
     /**
@@ -327,17 +328,17 @@ public class TsmlNcml {
      * @param mon
      * @return
      */
-    protected MutablePropertyDataSet tsds( URL url, int size, int len1, Object type, ProgressMonitor mon ) throws IOException {
+    protected MutablePropertyDataSet tsds(URL url, int size, int len1, Object type, ProgressMonitor mon) throws IOException {
 
-        URLConnection connection= url.openConnection();
+        URLConnection connection = url.openConnection();
 
         InputStream in = connection.getInputStream();
         String encoding = connection.getContentEncoding();
-        logger.finer("downloading "+connection.getURL());
-        if ( encoding!=null && encoding.equalsIgnoreCase("gzip")) {
+        logger.finer("downloading " + connection.getURL());
+        if (encoding != null && encoding.equalsIgnoreCase("gzip")) {
             logger.finer("got gzip encoding");
             in = new GZIPInputStream(in);
-        } else if ( encoding!=null && encoding.equalsIgnoreCase("deflate")) {
+        } else if (encoding != null && encoding.equalsIgnoreCase("deflate")) {
             logger.finer("got deflate encoding");
             in = new InflaterInputStream(in, new Inflater(true));
         }
@@ -364,13 +365,12 @@ public class TsmlNcml {
         bbuf.flip();
         bbuf.order(ByteOrder.LITTLE_ENDIAN);
 
-        if ( len1==-1 ) {
+        if (len1 == -1) {
             int points = bbuf.limit() / BufferDataSet.byteCount(type);
-            return org.virbo.binarydatasource.BufferDataSet.makeDataSet( 1, BufferDataSet.byteCount(type), 0, points, 1, 1, bbuf, type );
+            return org.virbo.binarydatasource.BufferDataSet.makeDataSet(1, BufferDataSet.byteCount(type), 0, points, 1, 1, bbuf, type);
         } else {
             int points = bbuf.limit() / len1 / BufferDataSet.byteCount(type);
-            return org.virbo.binarydatasource.BufferDataSet.makeDataSet( RANK_LIMIT, len1* BufferDataSet.byteCount(type), 0, points, len1, 1, bbuf, type );
+            return org.virbo.binarydatasource.BufferDataSet.makeDataSet(RANK_LIMIT, len1 * BufferDataSet.byteCount(type), 0, points, len1, 1, bbuf, type);
         }
     }
-
 }
