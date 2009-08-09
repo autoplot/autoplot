ef7e6fbc514c05905913ef84bfbe328ac14cee27
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-09 12:24:49 +0000

    loading vap turns on autolayout.  disable this using a modifier in the vap uri.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2803 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test003.java
+++ VirboAutoplot/src/test/endtoend/Test003.java
@@ -18,7 +18,7 @@ public class Test003 {
             ScriptContext.getDocumentModel().getOptions().setAutolayout(false);
             ScriptContext.getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
 
-            ScriptContext.load( "/home/jbf/ct/lanl/hudson/geo_pitch_stack_test1_local.vap" );
+            ScriptContext.load( "file:///home/jbf/ct/lanl/hudson/geo_pitch_stack_test1_local.vap?options.autolayout=false" );
             ScriptContext.setCanvasSize( 800, 600 );
             ScriptContext.writeToPng( "test003.png" );
             System.exit(0);  // TODO: something is firing up the event thread
