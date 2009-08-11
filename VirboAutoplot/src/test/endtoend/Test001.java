9eb4177c1b429eadc8b20155e49ffed58fc8453c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-11 16:16:04 +0000

    add more basic tests, for example log-log spectrogram.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2821 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test001.java
+++ VirboAutoplot/src/test/endtoend/Test001.java
@@ -6,9 +6,10 @@
 package test.endtoend;
 
 import java.io.IOException;
-import org.virbo.autoplot.ScriptContext;
+import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QDataSet;
-import org.virbo.dsops.Ops;
+import static org.virbo.dsops.Ops.*;
+import static org.virbo.autoplot.ScriptContext.*;
 import org.virbo.jythonsupport.Util;
 
 /**
@@ -18,13 +19,29 @@ import org.virbo.jythonsupport.Util;
 public class Test001 {
     public static void main(String[] args) throws InterruptedException, IOException, Exception {
         try {
-            ScriptContext.getDocumentModel().getOptions().setAutolayout(false);
-            ScriptContext.getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
+            getDocumentModel().getOptions().setAutolayout(false);
+            getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
             QDataSet ds= Util.getDataSet( "http://www.autoplot.org/data/fireworks.wav" );
-            ScriptContext.plot( 0, ds );
-            ScriptContext.plot( 1, Ops.fftWindow( ds, 512 ) );
-            ScriptContext.setCanvasSize( 800, 600 );
-            ScriptContext.writeToPng( "test001.png" );
+            plot( 0, ds );
+            plot( 1, fftWindow( ds, 512 ) );
+            setCanvasSize( 800, 600 );
+            writeToPng( "test001_001.png" );
+
+            ds= Util.getDataSet( "file:/home/jbf/ct/hudson/data.backup/xls/2008-lion and tiger summary.xls?sheet=Samantha+tiger+lp+lofreq&firstRow=53&column=Complex_Modulus&depend0=Frequency" );
+            plot( 0, "xaxis should be log", ds );
+
+            MutablePropertyDataSet mds= (MutablePropertyDataSet) findgen(50,50);
+            MutablePropertyDataSet xds= (MutablePropertyDataSet) exp10( linspace( 0, 2, 50 ) );
+            xds.putProperty( QDataSet.SCALE_TYPE, "log" );
+            MutablePropertyDataSet yds= (MutablePropertyDataSet) exp10( linspace( 0, 5, 50 ) );
+            yds.putProperty( QDataSet.SCALE_TYPE, "log" );
+            mds.putProperty( QDataSet.DEPEND_0, xds );
+            mds.putProperty( QDataSet.DEPEND_1, yds );
+            plot( 1, "log-log spectrogram", mds );
+
+            setCanvasSize( 800, 600 );
+            writeToPng( "test001_002.png" );
+
             System.exit(0);  // TODO: something is firing up the event thread
         } catch ( RuntimeException ex ) {
             ex.printStackTrace();
