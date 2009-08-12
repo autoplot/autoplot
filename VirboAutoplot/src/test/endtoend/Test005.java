bf0c25affcb855237a46133949d98bc56ec07dcf
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-12 12:02:07 +0000

    use static imports to make code easier to cut-n-paste into python.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2829 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test005.java
+++ VirboAutoplot/src/test/endtoend/Test005.java
@@ -11,7 +11,7 @@ import org.das2.datum.DatumRange;
 import org.das2.datum.DatumRangeUtil;
 import org.das2.datum.Units;
 import org.das2.util.monitor.NullProgressMonitor;
-import org.virbo.autoplot.ScriptContext;
+import static org.virbo.autoplot.ScriptContext.*;
 import org.virbo.autoplot.dom.Axis;
 import org.virbo.autoplot.dom.Canvas;
 import org.virbo.datasource.DataSetURL;
@@ -27,28 +27,28 @@ public class Test005 {
 
     public static void xxx() {
         System.err.println("-- timer: "+ ( System.currentTimeMillis()-t0) );
-        final Canvas c = ScriptContext.getDocumentModel().getCanvases(0);
+        final Canvas c = getDocumentModel().getCanvases(0);
     }
 
     public static void main(String[] args) throws InterruptedException, IOException, Exception {
         try {
-            ScriptContext.setCanvasSize(800, 600);
-            ScriptContext.getDocumentModel().getOptions().setAutolayout(false);
-            ScriptContext.getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
+            setCanvasSize(800, 600);
+            getDocumentModel().getOptions().setAutolayout(false);
+            getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
 
 
             xxx();
 
-            ScriptContext.plot("http://cdaweb.gsfc.nasa.gov/cgi-bin/opendap/nph-dods/istp_public/data/genesis/3dl2_gim/2003/genesis_3dl2_gim_20030501_v01.cdf.dds?Proton_Density");
-            ScriptContext.writeToPng("test005_demo1.png");
+            plot("http://cdaweb.gsfc.nasa.gov/cgi-bin/opendap/nph-dods/istp_public/data/genesis/3dl2_gim/2003/genesis_3dl2_gim_20030501_v01.cdf.dds?Proton_Density");
+            writeToPng("test005_demo1.png");
 
             xxx();
 
             {
-                ScriptContext.plot("http://cdaweb.gsfc.nasa.gov/istp_public/data/polar/hyd_h0/$Y/po_h0_hyd_$Y$m$d_v01.cdf?ELECTRON_DIFFERENTIAL_ENERGY_FLUX&timerange=20000109");
-                Axis axis = ScriptContext.getDocumentModel().getPlots(0).getXaxis();
+                plot("http://cdaweb.gsfc.nasa.gov/istp_public/data/polar/hyd_h0/$Y/po_h0_hyd_$Y$m$d_v01.cdf?ELECTRON_DIFFERENTIAL_ENERGY_FLUX&timerange=20000109");
+                Axis axis = getDocumentModel().getPlots(0).getXaxis();
                 axis.setRange(DatumRangeUtil.rescale(axis.getRange(), -1, 2));
-                ScriptContext.writeToPng("test005_demo2.png");
+                writeToPng("test005_demo2.png");
             }
 
             xxx();
@@ -64,33 +64,33 @@ public class Test005 {
             }
             xxx();
 
-            ScriptContext.plot("http://cdaweb.gsfc.nasa.gov/cgi-bin/opendap/nph-dods/istp_public/data/genesis/3dl2_gim/2003/genesis_3dl2_gim_20030501_v01.cdf.dds?Proton_Density");
-            ScriptContext.writeToPng("test005_demo1_r.png");
+            plot("http://cdaweb.gsfc.nasa.gov/cgi-bin/opendap/nph-dods/istp_public/data/genesis/3dl2_gim/2003/genesis_3dl2_gim_20030501_v01.cdf.dds?Proton_Density");
+            writeToPng("test005_demo1_r.png");
             xxx();
 
-            ScriptContext.plot("http://autoplot.org/data/autoplot.xls?column=A");
-            ScriptContext.writeToPng("test005_demo4.png");
+            plot("http://autoplot.org/data/autoplot.xls?column=A");
+            writeToPng("test005_demo4.png");
             xxx();
-            ScriptContext.plot("http://autoplot.org/data/autoplot.cdf?BGSM");
-            ScriptContext.writeToPng("test005_demo5.png");
+            plot("http://autoplot.org/data/autoplot.cdf?BGSM");
+            writeToPng("test005_demo5.png");
             xxx();
-            ScriptContext.plot("http://autoplot.org/data/autoplot.xls?column=A");   // must delete extra panels from BGSM
-            ScriptContext.writeToPng("test005_demo4_r.png");
+            plot("http://autoplot.org/data/autoplot.xls?column=A");   // must delete extra panels from BGSM
+            writeToPng("test005_demo4_r.png");
             xxx();
-            ScriptContext.plot("http://autoplot.org/data/autoplot.xml");
-            ScriptContext.writeToPng("test005_demo6.png");
+            plot("http://autoplot.org/data/autoplot.xml");
+            writeToPng("test005_demo6.png");
             xxx();
-            ScriptContext.plot("ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/omni2_1963.dat");
-            ScriptContext.writeToPng("test005_demo7.png");
+            plot("ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/omni2_1963.dat");
+            writeToPng("test005_demo7.png");
             xxx();
-            ScriptContext.plot("ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/omni2_$Y.dat?timerange=1963-1965");
-            ScriptContext.writeToPng("test005_demo8.png");
+            plot("ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/omni2_$Y.dat?timerange=1963-1965");
+            writeToPng("test005_demo8.png");
             xxx();
-            ScriptContext.plot("vap:ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/omni2_$Y.dat?column=field17&timeFormat=$Y+$j+$H&time=field0&validMax=999&timerange=1972");
-            ScriptContext.writeToPng("test005_demo9.png");
+            plot("vap:ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/omni2_$Y.dat?column=field17&timeFormat=$Y+$j+$H&time=field0&validMax=999&timerange=1972");
+            writeToPng("test005_demo9.png");
             xxx();
-            ScriptContext.plot("http://autoplot.org/data/autoplot.ncml");
-            ScriptContext.writeToPng("test005_demo10.png");
+            plot("http://autoplot.org/data/autoplot.ncml");
+            writeToPng("test005_demo10.png");
             xxx();
             {
                 String[] list = org.virbo.jythonsupport.Util.list("http://cdaweb.gsfc.nasa.gov/istp_public/data/");
@@ -102,20 +102,20 @@ public class Test005 {
             }
             xxx();
             {
-            ScriptContext.plot("http://autoplot.org/data/hsi_qlimg_5050601_001.fits");
-            ScriptContext.getDocumentModel().getDataSourceFilters(0).setSliceIndex(2);
-            ScriptContext.getDocumentModel().getPlots(0).getZaxis().setRange(DatumRange.newDatumRange(-20e4, 20e4, Units.dimensionless));
-            ScriptContext.writeToPng("test005_demo12.png");
+            plot("http://autoplot.org/data/hsi_qlimg_5050601_001.fits");
+            getDocumentModel().getDataSourceFilters(0).setSliceIndex(2);
+            getDocumentModel().getPlots(0).getZaxis().setRange(DatumRange.newDatumRange(-20e4, 20e4, Units.dimensionless));
+            writeToPng("test005_demo12.png");
             }
             xxx();
 
-            ScriptContext.plot("http://autoplot.org/data/hsi_fsimg_5050612_001.fits");
-            ScriptContext.writeToPng("test005_demo13.png");
+            plot("http://autoplot.org/data/hsi_fsimg_5050612_001.fits");
+            writeToPng("test005_demo13.png");
 
             xxx();
 
-            ScriptContext.plot("vap:http://goes.ngdc.noaa.gov/data/avg/$Y/A105$y$m.TXT?skip=23&timeFormat=$y$m$d+$H$M&column=E1&time=YYMMDD&validMax=32000&timerange=Dec+2004");
-            ScriptContext.writeToPng("test005_demo14.png");
+            plot("vap:http://goes.ngdc.noaa.gov/data/avg/$Y/A105$y$m.TXT?skip=23&timeFormat=$y$m$d+$H$M&column=E1&time=YYMMDD&validMax=32000&timerange=Dec+2004");
+            writeToPng("test005_demo14.png");
 
             System.exit(0);  // TODO: something is firing up the event thread
         } catch (RuntimeException ex) {
