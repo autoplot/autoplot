f43bd6d4fa19c10660c8c91c30582276454360a3
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-14 13:02:31 +0000

    many more cdf files
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2857 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test012.java
+++ VirboAutoplot/src/test/endtoend/Test012.java
@@ -52,7 +52,30 @@ public class Test012 {
     public static void main(String[] args) throws InterruptedException, IOException, Exception {
         try {
 
+            getDocumentModel().getOptions().setAutolayout(false);
             doTest( 0, "vap:http://cdaweb.gsfc.nasa.gov/istp_public/data/cluster/c4/cp/2003/c4_cp_fgm_spin_20030102_v01.cdf?B_vec_xyz_gse__C4_CP_FGM_SPIN" );
+            doTest( 1, "vap:file:///home/jbf/ct/hudson/data/cdf/tha_l1_efw_20080402_v01.cdf?tha_efw" );
+            doTest( 2, "vap:file:///home/jbf/ct/hudson/data/cdf/l1_h0_mpa_20020202_v02.cdf?dens_e" );
+            doTest( 3, "vap:file:///home/jbf/ct/hudson/data/cdf/l1_h0_mpa_20020202_v02.cdf?Theta_l" );
+            //rank4
+            doTest( 4, "vap:file:///home/jbf/ct/hudson/data/cdf/l1_h0_mpa_20020202_v02.cdf?Ecounts[1]" );
+
+            // cdf_epoch16, subsetting.
+            doTest( 5, "vap:file:///home/jbf/ct/hudson/data/cdf/c2_waveform_wbd_200704170840_u01.cdf?WBD_Elec[1000000:1100000]" );
+            // uint1, subsetting.
+            doTest( 6, "vap:file:///home/jbf/ct/hudson/data/cdf/c2_waveform_wbd_200704170840_u01.cdf?DATA_QUALITY[::1090]" );
+
+            doTest( 7, "vap:file:///home/jbf/ct/hudson/data/cdf/i8_15sec_mag_19731030_v02.cdf?F1_Average_B_15s" );
+            doTest( 8, "vap:file:///home/jbf/ct/hudson/data/cdf/i8_15sec_mag_19731030_v02.cdf?B_Vector_GSM" );
+
+            doTest( 9, "vap:file:///home/jbf/ct/hudson/data/cdf/wi_h0_mfi_19941123_v04.cdf?P1GSM" );
+            doTest( 10, "vap:file:///home/jbf/ct/hudson/data/cdf/wi_h0_mfi_19941123_v04.cdf?ORTH_I" );
+
+            doTest( 11, "vap:file:///home/jbf/ct/hudson/data/cdf/po_h0_hyd_20010117_v01.cdf?ION_DIFFERENTIAL_ENERGY_FLUX" );
+
+            // rank 3
+            doTest( 12, "vap:file:///home/jbf/ct/hudson/data/cdf/po_h0_tim_19960409_v03.cdf?Flux_H" );
+
 
             System.exit(0);  // TODO: something is firing up the event thread
         } catch (RuntimeException ex) {
