4bba51423b5eb32bcb289542fd255e347542143c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-14 11:32:24 +0000

    test012, cdf files.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2850 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/test/endtoend/Test012.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.endtoend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import org.virbo.autoplot.ScriptContext;
+import org.virbo.dataset.MutablePropertyDataSet;
+import org.virbo.dataset.QDataSet;
+import org.virbo.dsops.Ops;
+import org.virbo.jythonsupport.Util;
+
+/**
+ * tests of CDF files.
+ * @author jbf
+ */
+public class Test012 {
+
+
+    public static void doTest( int id, String uri ) throws Exception {
+        QDataSet ds;
+        long t0= System.currentTimeMillis();
+        ds= Util.getDataSet( uri );
+        double t= (System.currentTimeMillis()-t0)/1000.;
+        MutablePropertyDataSet hist= (MutablePropertyDataSet) Ops.autoHistogram(ds);
+        hist.putProperty( QDataSet.TITLE, uri );
+        String label= String.format( "test007_%03d", id );
+        hist.putProperty( QDataSet.LABEL, label );
+        ScriptContext.formatDataSet( hist, label+".qds");
+
+        QDataSet dep0= (QDataSet) ds.property( QDataSet.DEPEND_0 );
+        if ( dep0!=null ) {
+            MutablePropertyDataSet hist2= (MutablePropertyDataSet) Ops.autoHistogram(dep0);
+            ScriptContext.formatDataSet( hist2, label+".dep0.qds");
+        } else {
+            PrintWriter pw= new PrintWriter( label+".dep0.qds" );
+            pw.println("no dep0");
+            pw.close();
+        }
+
+        System.err.printf( "Read in %9.3f seconds (%s): %s\n", t, label, uri );
+    }
+    
+    public static void main(String[] args) throws InterruptedException, IOException, Exception {
+        try {
+
+            doTest( 0, "vap:http://cdaweb.gsfc.nasa.gov/istp_public/data/cluster/c4/cp/2003/c4_cp_fgm_spin_20030102_v01.cdf?B_vec_xyz_gse__C4_CP_FGM_SPIN" );
+
+            System.exit(0);  // TODO: something is firing up the event thread
+        } catch (RuntimeException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
