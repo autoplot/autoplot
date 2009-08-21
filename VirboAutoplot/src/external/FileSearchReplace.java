984df0cfd5e0985bb492e2898ad1f68cdc55ebdd
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-21 15:07:53 +0000

    add FileSearchReplace to support compile-application-two-jars.sh
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2909 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/external/FileSearchReplace.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package external;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Pattern;
+
+/**
+ * Introduced to support compile-application-two-jars script, when I couldn't
+ * figure out how to use sed to insert a URL into a file.
+ * @author jbf
+ */
+public class FileSearchReplace {
+    public static void main( String[] args ) throws IOException {
+        if ( args.length<3 ) {
+            System.err.println("java external.FileSearchReplace <file> <regex> <replace> ...");
+            System.exit(0);
+        }
+        for ( int i=1; i<args.length; i+=2 ) {
+            args[i]= Pattern.quote(args[i]);
+        }
+        String file= args[0];
+
+        File fin= new File(file);
+        File fout= new File(file+".1");
+        BufferedReader in= new BufferedReader( new FileReader(fin) );
+        PrintWriter out= new PrintWriter( new FileWriter( fout ) );
+        String s= in.readLine();
+        while ( s!=null ) {
+            for ( int i=1; i<args.length; i+=2 ) {
+                s= s.replaceAll( args[i], args[i+1] );
+            }
+            out.println(s);
+            s= in.readLine();
+        }
+        out.close();
+        in.close();
+        fout.renameTo( fin );
+    }
+}
