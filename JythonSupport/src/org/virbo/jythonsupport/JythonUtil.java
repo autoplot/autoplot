7754c09a7f2ea6f821161fdebf56c031e5969b0a
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-25 19:33:42 +0000

    explicitly set the Python cache dir to autoplot_data to support jumbojars.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2956 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonSupport/src/org/virbo/jythonsupport/JythonUtil.java
+++ JythonSupport/src/org/virbo/jythonsupport/JythonUtil.java
@@ -4,8 +4,11 @@
  */
 package org.virbo.jythonsupport;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Properties;
 import org.python.core.Py;
+import org.python.core.PySystemState;
 import org.python.util.PythonInterpreter;
 
 /**
@@ -24,7 +27,9 @@ public class JythonUtil {
      * @throws java.io.IOException
      */
     public static PythonInterpreter createInterpreter(boolean sandbox) throws IOException {
-        System.err.println("jython cache dir: " + Py.getSystemState().cachedir );
+        if ( PySystemState.cachedir==null ) {
+            System.setProperty( "python.cachedir", System.getProperty("user.home")+"/autoplot_data/pycache" );
+        }
         PythonInterpreter interp = new PythonInterpreter();
         Py.getAdapter().addPostClass(new PyQDataSetAdapter());
 
