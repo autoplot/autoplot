2ef3926c18476904a6b20f30fd224711cc85c199
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-25 21:08:47 +0000

    remove org.das.graph stuff from default application python imports.  I'm not sure where these are used, but there's no good reason for them to be imported.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2961 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/appContextImports.py
+++ VirboAutoplot/src/org/virbo/autoplot/appContextImports.py
@@ -1,5 +1,4 @@
 from org.das2 import DasApplication
-from org.das2.graph import DasCanvas, DasCanvasComponent, DasPlot, DasAxis, DasRow, DasColumn, EventsRenderer, SeriesRenderer, DefaultPlotSymbol
 
 from java.awt import Color
 from javax.swing import JFrame, JComponent, JLayeredPane
