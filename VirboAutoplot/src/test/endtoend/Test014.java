83ededbdfce24895dc27dbba5a5414dd0f074954
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-08-28 01:05:17 +0000

    tests of guess cadence.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@2989 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/test/endtoend/Test014.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.endtoend;
+
+import java.awt.Graphics2D;
+import java.io.IOException;
+import org.das2.graph.DasAxis;
+import org.das2.graph.DasCanvas;
+import org.das2.graph.Painter;
+import org.virbo.dataset.DataSetOps;
+import org.virbo.dataset.DataSetUtil;
+import static org.virbo.autoplot.ScriptContext.*;
+import org.virbo.dataset.MutablePropertyDataSet;
+import org.virbo.dataset.QDataSet;
+import org.virbo.dataset.RankZeroDataSet;
+import org.virbo.dsops.Ops;
+import org.virbo.jythonsupport.Util;
+
+
+/**
+ * challenging guess cadence
+ * @author jbf
+ */
+public class Test014 {
+
+
+    public static void doTest( int id, String uri, QDataSet ds ) throws Exception {
+     
+        long t0= System.currentTimeMillis();
+        String label= String.format( "test014_%03d", id );
+        double t;
+
+        if ( ds==null ) ds= Util.getDataSet( uri );
+
+        QDataSet dep0= (QDataSet) ds.property( QDataSet.DEPEND_0 );
+
+        t= (System.currentTimeMillis()-t0)/1000.;
+        System.err.printf( "Read data in %9.3f seconds (%s): %s\n", t, label, uri );
+
+        final QDataSet cadence= DataSetUtil.guessCadenceNew( dep0, null );
+        
+        t= (System.currentTimeMillis()-t0)/1000.;
+        System.err.printf( "Guess cadence in %9.3f seconds (%s): %s\n", t, label, uri );
+        System.err.printf( "cadence= %s (scale type=%s): \n", cadence.toString(), cadence.property(QDataSet.SCALE_TYPE) );
+
+        QDataSet diff= Ops.diff(dep0);
+        System.err.printf( "diffs(dep0)[0:3]= %f %f %f %s\n",
+                diff.value(0), diff.value(1), diff.value(2),
+                String.valueOf(diff.property(QDataSet.UNITS)) );
+        MutablePropertyDataSet hist= (MutablePropertyDataSet) Ops.autoHistogram( diff );
+
+        DasAxis xAxis= getDocumentModel().getPlots(0).getXaxis().getController().getDasAxis();
+
+        plot( hist );
+        setCanvasSize( 600, 600 );
+
+        final DasCanvas cc= getDocumentModel().getCanvases(0).getController().getDasCanvas();
+        xAxis= getDocumentModel().getPlots(0).getXaxis().getController().getDasAxis();
+
+        xAxis.setLabel("UNITS=%{UNITS}");
+
+        Painter p = new Painter() {
+            public void paint(Graphics2D g) {
+                DasAxis xAxis= getDocumentModel().getPlots(0).getXaxis().getController().getDasAxis();
+                final int ix= (int)xAxis.transform( DataSetUtil.asDatum((RankZeroDataSet)cadence) );
+                g.drawLine( ix, 0, ix, cc.getHeight() );
+            }
+        };
+        cc.addTopDecorator( p );
+
+        int i= uri.lastIndexOf("/");
+
+        setTitle(uri.substring(i+1)+"!c cadence="+cadence );
+
+        writeToPng( String.format( "test014_%03d.png", id ) );
+
+        cc.removeTopDecorator(p);
+        
+        hist.putProperty( QDataSet.TITLE, uri + "!c cadence="+cadence );
+        
+        hist.putProperty( QDataSet.LABEL, label );
+
+        hist.putProperty( QDataSet.LABEL, label );
+        
+        formatDataSet( hist, label+".qds");
+
+    }
+    
+    public static void main(String[] args) throws InterruptedException, IOException, Exception {
+        try {
+
+            getDocumentModel().getOptions().setAutolayout(false);
+            getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
+
+            QDataSet ds;
+
+            ds= Util.getDataSet( "vap:file:/home/jbf/ct/hudson/data.backup/cdf/c2_waveform_wbd_200704170840_u01.cdf?WBD_Elec[::1090]" );
+            QDataSet ant= Util.getDataSet( "vap:file:/home/jbf/ct/hudson/data.backup/cdf/c2_waveform_wbd_200704170840_u01.cdf?ANTENNA[::1090]" );
+            QDataSet r= Ops.where( Ops.eq( ant, DataSetUtil.asDataSet(2) ) );
+            ds= DataSetOps.applyIndex( ds, 0, r, true );
+            doTest( 0, "vap:file:/home/jbf/ct/hudson/data.backup/cdf/c2_waveform_wbd_200704170840_u01.cdf?WBD_Elec[::1090]", ds );
+            doTest( 1, "vap:file:///home/jbf/ct/hudson/data.backup/cdf/i8_15sec_mag_19731030_v02.cdf?F1_Average_B_15s", null );
+            doTest( 2, "file:/home/jbf/ct/hudson/data.backup/xls/2008-lion and tiger summary.xls?sheet=Samantha+tiger+lp+lofreq&firstRow=53&column=Complex_Modulus&depend0=Frequency", null );
+            System.exit(0);  // TODO: something is firing up the event thread
+        } catch (RuntimeException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+    }
+}
