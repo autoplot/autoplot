e487a500a72c68954c473c4014a1a0a753263d05
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-09-19 11:12:52 +0000

    set layout column so that colorbar is visible.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@3225 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test015.java
+++ VirboAutoplot/src/test/endtoend/Test015.java
@@ -52,6 +52,8 @@ public class Test015 {
     public static void main(String[] args) throws InterruptedException, IOException, Exception {
         try {
             getDocumentModel().getOptions().setAutolayout(false);
+            getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
+            
             doTest( 0, "vap+cef:file:///home/jbf/ct/hudson/data.backup/cef/C1_CP_PEA_CP3DXPH_DNFlux__20020811_140000_20020811_150000_V061018.cef?Data__C1_CP_PEA_CP3DXPH_DNFlux" );
             doTest( 1, "vap+cef:file:///home/jbf/ct/hudson/data.backup/cef/C1_CP_CIS-CODIF_HS_H1_PSD__20020117_000000_20020117_240000_V070824.cef?3d_ions__C1_CP_CIS-CODIF_HS_H1_PSD");
             doTest( 2, "vap+cef:file:///home/jbf/ct/hudson/data.backup/cef/C1_CP_EDI_EGD__20050215_V03.cef?tof__C1_CP_EDI_EGD" );
