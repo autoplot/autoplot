817cb4180e0f3cbaca2a0e3edf19db13b51ebf26
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-09-22 10:33:04 +0000

    add headless switch.  cleanup to make more like the others.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@3235 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test003.java
+++ VirboAutoplot/src/test/endtoend/Test003.java
@@ -6,7 +6,7 @@
 package test.endtoend;
 
 import java.io.IOException;
-import org.virbo.autoplot.ScriptContext;
+import static org.virbo.autoplot.ScriptContext.*;
 
 /**
  * Reiner's stuff
@@ -14,30 +14,40 @@ import org.virbo.autoplot.ScriptContext;
  */
 public class Test003 {
     public static void main(String[] args) throws InterruptedException, IOException, Exception {
+
+        boolean headless= true;
+
         try {
-            ScriptContext.getDocumentModel().getOptions().setAutolayout(false);
-            ScriptContext.getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
-
-            ScriptContext.load( "file:///home/jbf/ct/lanl/hudson/geo_pitch_stack_test1_local.vap" );
-            ScriptContext.setCanvasSize( 800, 600 );
-            ScriptContext.writeToPng( "test003_001.png" );
-
-            ScriptContext.reset();
-
-            ScriptContext.load( "file:///home/jbf/ct/hudson/vap/contextOverview2.vap" );
-            ScriptContext.setCanvasSize( 800, 600 );
-            ScriptContext.writeToPng( "test003_002.png" );
-
-            ScriptContext.reset();
+            if ( !headless ) createGui();
             
-            ScriptContext.load( "file:///home/jbf/ct/hudson/vap/lanl_97A_sopa_panel_slices_2.vap" );
-            ScriptContext.setCanvasSize( 800, 600 );
-            ScriptContext.writeToPng( "test003_003.png" );
+            getDocumentModel().getOptions().setAutolayout(false);
+            getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
+            getDocumentModel().getCanvases(0).setFitted(false);
+            setCanvasSize( 800, 600 );
+            
+            load( "file:///home/jbf/ct/lanl/hudson/geo_pitch_stack_test1_local.vap?canvases[0].fitted=false" );
+            setCanvasSize( 800, 600 );
+            writeToPng( "test003_001.png" );
+            save( "/home/jbf/tmp/test003_001.vap");
 
-            System.exit(0);  // TODO: something is firing up the event thread
+            reset();
+            
+            load( "file:///home/jbf/ct/hudson/vap/contextOverview2.vap?canvases[0].fitted=false" );
+            setCanvasSize( 800, 600 );
+            writeToPng( "test003_002.png" );
+            save( "/home/jbf/tmp/test003_002.vap");
+
+            reset();
+
+            load( "file:///home/jbf/ct/hudson/vap/lanl_97A_sopa_panel_slices_2.vap?canvases[0].fitted=false" );
+            setCanvasSize( 800, 600 );
+            writeToPng( "test003_003.png" );
+            save( "/home/jbf/tmp/test003_003.vap");
+
+            if ( headless ) System.exit(0);
         } catch ( RuntimeException ex ) {
             ex.printStackTrace();
-            System.exit(1);
+            if ( headless ) System.exit(1);
         }
     }
 }
