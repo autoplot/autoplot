206bccc1e3361bab1ea54ac68b6b1bf50e803d19
edjackson <edjackson@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-09-30 19:56:03 +0000

    Added mouse click handling to GridPngWalkView
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@3326 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/autoplot/pngwalk/GridPngWalkView.java
+++ VirboAutoplot/src/org/autoplot/pngwalk/GridPngWalkView.java
@@ -1,11 +1,11 @@
 package org.autoplot.pngwalk;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferedImage;
 
 /**
@@ -22,6 +22,7 @@ public class GridPngWalkView extends PngWalkView {
         super(seq);
 
         addMouseListener(new MouseAdapter() {
+
             @Override
             public void mouseClicked(MouseEvent e) {
                 selectCellAt(e.getX(), e.getY());
@@ -32,12 +33,25 @@ public class GridPngWalkView extends PngWalkView {
     @Override
     public Dimension getPreferredSize() {
         int w = getParent().getWidth();
-        int h = thumbSize*(1+(seq.size()+1)/Math.max(1, w/thumbSize));
+        int h = thumbSize * (1 + (seq.size() + 1) / Math.max(1, w / thumbSize));
         //System.err.printf("w=%d, h=%d%n",w ,h);
-        return new Dimension(w,h);
+        return new Dimension(w, h);
     }
+
     private void selectCellAt(int x, int y) {
-        //seq.setIndex(n);
+        int cellsPerRow = Math.max(1, this.getWidth() / thumbSize);
+        if (x > cellsPerRow * thumbSize) {
+            return;
+        }
+        int col = x / thumbSize;
+        int row = y / thumbSize;
+
+        int n = row * cellsPerRow + col;
+        if (n >= seq.size()) {
+            return;
+        }
+
+        seq.setIndex(n);
     }
 
     public int getThumbSize() {
@@ -45,8 +59,12 @@ public class GridPngWalkView extends PngWalkView {
     }
 
     public void setThumbSize(int size) {
-        if (size < MIN_THUMB_SIZE) size = MIN_THUMB_SIZE;
-        if (size > MAX_THUMB_SIZE) size = MAX_THUMB_SIZE;
+        if (size < MIN_THUMB_SIZE) {
+            size = MIN_THUMB_SIZE;
+        }
+        if (size > MAX_THUMB_SIZE) {
+            size = MAX_THUMB_SIZE;
+        }
         thumbSize = size;
     }
 
@@ -58,16 +76,23 @@ public class GridPngWalkView extends PngWalkView {
         // Fit as many thumbnails as possible horizontally (at least one), then go to a new row
         int cellsPerRow = Math.max(1, this.getWidth() / thumbSize);
 
-        for (int row=0; row < (seq.size()/cellsPerRow + 1); row++) {
-            for (int col=0; col < cellsPerRow; col++) {
-                int n = (row*cellsPerRow) + col;
-                if(n >= seq.size()) break;
-                g2.draw(new Ellipse2D.Double(col*thumbSize+2, row*thumbSize+2, thumbSize-4, thumbSize-4));
-                BufferedImage thumb = seq.imageAt(n).getThumbnail(thumbSize-4);
-                g2.drawImage(thumb, col*thumbSize+(thumbSize-thumb.getWidth())/2, row*thumbSize+(thumbSize-thumb.getHeight())/2 , null);
+        for (int row = 0; row < (seq.size() / cellsPerRow + 1); row++) {
+            for (int col = 0; col < cellsPerRow; col++) {
+                int n = (row * cellsPerRow) + col;
+                if (n >= seq.size()) {
+                    break;
+                }
+                if (seq.getIndex() == n) {
+                    Color oldColor = g2.getColor();
+                    g2.setColor(Color.orange);
+                    g2.fillRect(col * thumbSize + 2, row * thumbSize + 2, thumbSize - 4, thumbSize - 4);
+                    g2.setColor(oldColor);
+                }
+                //g2.draw(new Ellipse2D.Double(col * thumbSize + 2, row * thumbSize + 2, thumbSize - 4, thumbSize - 4));
+                BufferedImage thumb = seq.imageAt(n).getThumbnail(thumbSize - 4);
+                g2.drawImage(thumb, col * thumbSize + (thumbSize - thumb.getWidth()) / 2, row * thumbSize + (thumbSize - thumb.getHeight()) / 2, null);
 
             }
         }
     }
-
 }
