e5d80d9858412603b26d0092fc9d01c5761c9a71
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-10-08 02:16:53 +0000

    move in the bookmarks, etc from the old version of the application.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@3373 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/autoplot/pngwalk/PngWalkTool1.java
+++ VirboAutoplot/src/org/autoplot/pngwalk/PngWalkTool1.java
@@ -11,12 +11,15 @@
 
 package org.autoplot.pngwalk;
 
-import java.awt.Dimension;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -25,12 +28,22 @@ import java.util.prefs.Preferences;
 import java.util.regex.Pattern;
 import javax.swing.AbstractAction;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import org.das2.components.TearoffTabbedPane;
+import org.das2.datum.DatumRange;
+import org.das2.util.ArgumentList;
 import org.virbo.autoplot.bookmarks.Bookmark;
 import org.virbo.datasource.DataSetSelector;
 import org.xml.sax.SAXException;
+import org.das2.util.TimeParser;
+import org.das2.util.filesystem.FileSystem.FileSystemOfflineException;
+import org.virbo.autoplot.ScriptContext;
+import org.virbo.autoplot.bookmarks.BookmarksManager;
+import org.virbo.autoplot.bookmarks.BookmarksManagerModel;
+import org.virbo.datasource.DataSetURI;
 
 /**
  *
@@ -49,19 +62,168 @@ public class PngWalkTool1 extends javax.swing.JPanel {
 
     int returnTabIndex=0; // index of the tab we left to look at the single panel view.  TODO: account for tear off.
 
-    public static void main( String[] args ) {
-        JFrame parent= new JFrame("PNG Walk Tool 1");
 
-        PngWalkTool1 r= new PngWalkTool1();
+    public static void main(String[] args) {
 
-        r.setTemplate( "file:///tmp/pngwalk/*.png" );
-        
-        parent.getContentPane().add(r);
+        DataSetURI.init();  // FtpFileSystem implementation
 
-        parent.pack();
-        parent.setVisible(true);
-        parent.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        System.err.println("this is pngwalk 20091007");
+        final ArgumentList alm = new ArgumentList("AutoPlotUI");
+        alm.addBooleanSwitchArgument("nativeLAF", "n", "nativeLAF", "use the system look and feel");
+        alm.addOptionalPositionArgument(0, "template",  "file:/tmp/pngwalk/product_$Y$m$d.png", "initial template to use.");
 
+        alm.process(args);
+
+        if (alm.getBooleanValue("nativeLAF")) {
+            try {
+                javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        String template = alm.getValue("template"); // One Slash!!
+        //final String template=  "file:/home/jbf/temp/product_$Y$m$d.png" ; // One Slash!!
+        //final String template=  "file:/net/spot3/home/jbf/fun/pics/2001minnesota/.*JPG" ;
+        //final String template= "file:/home/jbf/public_html/voyager/VGPW_0201/BROWSE/V1/.*.PNG";
+        //final String template= "file:///net/spot3/home/jbf/fun/pics/20080315_tenerife_masca_hike/IMG_.*.JPG";
+        //final String template= "http://www.swpc.noaa.gov/ftpdir/lists/hpi/plots/pmap_$Y_$m_$d_...._S_.*_.*_.*_.*.gif";
+
+        start( template, null );
+
+    }
+
+    public static PngWalkTool1 start( String template, Window parent ) {
+
+        final PngWalkTool1 tool = new PngWalkTool1();
+
+        if ( template!=null ) {
+            tool.setTemplate(template);
+        } else {
+            Preferences prefs = Preferences.userNodeForPackage(PngWalkTool.class);
+            String srecent = prefs.get( PngWalkTool.PREF_RECENT,"");
+            if ( srecent.equals("") ) {
+                tool.setTemplate("file:/tmp/pngwalk/product_$Y$m$d.png");
+            } else {
+                try {
+                    List<Bookmark> books = Bookmark.parseBookmarks(srecent);
+                    tool.setTemplate( ((Bookmark.Item)books.get(0)).getUrl() );
+                } catch (SAXException ex) {
+                    Logger.getLogger(DemoPngWalk.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(DemoPngWalk.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                tool.setTemplate("file:/tmp/pngwalk/product_$Y$m$d.png");
+            }
+
+        }
+
+        PngWalkTool1.ActionEnabler enabler= new PngWalkTool1.ActionEnabler() {
+            public boolean isActionEnabled(String filename) {
+                String s = filename;
+                String template = tool.getTemplate();
+                int i0 = template.indexOf("_$Y");
+                if ( i0==-1 ) i0= template.indexOf("_%Y");
+                int i1 = s.indexOf(".png");
+                if ( i1==-1 || i0==-1 ) return false;
+                //String timeRange = s.substring(i0 + 1, i1);
+                String productFile = template.substring(0, i0) + ".vap";
+                try {
+                    return WalkUtil.fileExists(productFile);
+                } catch (FileSystemOfflineException ex) {
+                    Logger.getLogger(DemoPngWalk.class.getName()).log(Level.SEVERE, null, ex);
+                    return false;
+                } catch (URISyntaxException ex) {
+                    Logger.getLogger(DemoPngWalk.class.getName()).log(Level.SEVERE, null, ex);
+                    return false;
+                }
+            }
+        };
+
+        final int op= parent==null ? JFrame.EXIT_ON_CLOSE : JFrame.DISPOSE_ON_CLOSE;
+
+        tool.addFileAction( enabler, "autoplot", new AbstractAction("Launch Autoplot") {
+            public void actionPerformed(ActionEvent e) {
+                String s = tool.getSelectedFile();
+                String template = tool.getTemplate();
+                int i0 = template.indexOf("_$Y");
+                if ( i0==-1 ) i0= template.indexOf("_%Y");
+                int i1 = s.indexOf(".png");
+                if ( i1==-1 ) return;
+                TimeParser tp= TimeParser.create( template.substring(i0 + 1, i1) );
+                String timeRange = s.substring(i0 + 1, i1);
+                try {
+                    DatumRange dr= tp.parse(timeRange).getTimeRange();
+                    timeRange= dr.toString().replaceAll(" ", "+");
+                } catch ( ParseException ex ) {
+                    throw new RuntimeException(ex);
+                }
+                String productFile = template.substring(0, i0) + ".vap";
+
+                final String suri = productFile + "?timeRange=" + timeRange;
+
+                Runnable run = new Runnable() {
+                    public void run() {
+                        try {
+                            ScriptContext.createGui();
+                            ScriptContext.plot(suri);
+                            ((JFrame)ScriptContext.getViewWindow()).setDefaultCloseOperation(op);
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(DemoPngWalk.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                };
+                new Thread(run).start();
+            }
+        });
+
+        JFrame frame = new JFrame("PNG Walk Tool");
+
+        if ( parent==null ) {
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        } else {
+            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        }
+
+        frame.setJMenuBar( createMenuBar(tool,frame) );
+
+        frame.getContentPane().add(tool);
+
+
+        frame.pack();
+
+        frame.setVisible(true);
+
+        return tool;
+    }
+
+    private static JMenuBar createMenuBar( final PngWalkTool1 tool, final JFrame f ) {
+        JMenuBar result= new JMenuBar();
+        JMenu fileMenu= new JMenu("File");
+        fileMenu.add( new AbstractAction( f.getDefaultCloseOperation()==JFrame.EXIT_ON_CLOSE ? "Exit" : "Close" ) {
+            public void actionPerformed(ActionEvent e) {
+                f.dispose();
+            }
+        } );
+        result.add(fileMenu);
+
+        final JMenu bookmarksMenu= new JMenu("Bookmarks");
+        final BookmarksManager man= new BookmarksManager(f,true);
+
+        man.getModel().addPropertyChangeListener( BookmarksManagerModel.PROP_LIST, new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent evt) {
+                man.updateBookmarks( bookmarksMenu, tool.getSelector() );
+            }
+        });
+        man.setVisible(false);
+        man.setPrefNode("pngwalk");
+
+        man.updateBookmarks( bookmarksMenu, tool.getSelector() );
+
+        result.add( bookmarksMenu );
+
+        return result;
     }
 
 
@@ -70,7 +232,7 @@ public class PngWalkTool1 extends javax.swing.JPanel {
         initComponents();
         dataSetSelector1.setEnableDataSource(false);
 
-        Preferences prefs = Preferences.userNodeForPackage(PngWalkTool1.class);
+        Preferences prefs = Preferences.userNodeForPackage(PngWalkTool.class);
         String srecent = prefs.get(PREF_RECENT,"");
 
         if ( !srecent.equals("") ) {
