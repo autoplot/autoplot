55742ad1f116484778583fa1cfd4f20cdc787582
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-10-26 16:25:53 +0000

    catch exceptions for each test so many components will run.  I couldn't figure out why my test wasn't producing new files for URIs that should have worked...
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@3492 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test006.java
+++ VirboAutoplot/src/test/endtoend/Test006.java
@@ -19,41 +19,51 @@ import org.virbo.jythonsupport.Util;
 public class Test006 {
 
 
-    public static void doTest( int id, String uri ) throws Exception {
-        QDataSet ds;
-        long t0= System.currentTimeMillis();
-        ds= Util.getDataSet( uri );
-        double t= (System.currentTimeMillis()-t0)/1000.;
-        MutablePropertyDataSet hist= (MutablePropertyDataSet) Ops.autoHistogram(ds);
-        hist.putProperty( QDataSet.TITLE, uri );
-        String label= String.format( "test006_%03d", id );
-        hist.putProperty( QDataSet.LABEL, label );
-        formatDataSet( hist, label+".qds");
+    public static Exception doTest( int id, String uri, Exception ex ) throws Exception {
+        try {
+            QDataSet ds;
+            long t0= System.currentTimeMillis();
+            ds= Util.getDataSet( uri );
+            double t= (System.currentTimeMillis()-t0)/1000.;
+            MutablePropertyDataSet hist= (MutablePropertyDataSet) Ops.autoHistogram(ds);
+            hist.putProperty( QDataSet.TITLE, uri );
+            String label= String.format( "test006_%03d", id );
+            hist.putProperty( QDataSet.LABEL, label );
+            formatDataSet( hist, label+".qds");
 
-        QDataSet dep0= (QDataSet) ds.property( QDataSet.DEPEND_0 );
-        if ( dep0!=null ) {
-            MutablePropertyDataSet hist2= (MutablePropertyDataSet) Ops.autoHistogram(dep0);
-            formatDataSet( hist2, label+".dep0.qds");
-        } else {
-            PrintWriter pw= new PrintWriter( label+".dep0.qds" );
-            pw.println("no dep0");
-            pw.close();
-        }
+            QDataSet dep0= (QDataSet) ds.property( QDataSet.DEPEND_0 );
+            if ( dep0!=null ) {
+                MutablePropertyDataSet hist2= (MutablePropertyDataSet) Ops.autoHistogram(dep0);
+                formatDataSet( hist2, label+".dep0.qds");
+            } else {
+                PrintWriter pw= new PrintWriter( label+".dep0.qds" );
+                pw.println("no dep0");
+                pw.close();
+            }
 
-        System.err.printf( "Read in %9.3f seconds (%s): %s\n", t, label, uri );
+            System.err.printf( "Read in %9.3f seconds (%s): %s\n", t, label, uri );
+            } catch ( Exception e ) {
+                e.printStackTrace();
+                if ( ex!=null ) ex= e;
+            }
+            return ex;
     }
     
     public static void main(String[] args) throws InterruptedException, IOException, Exception {
         try {
 
-            doTest( 0, "ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/omni2_$Y.dat?column=field17&timerange=1963&timeFormat=$Y+$j+$H&time=field0&validMax=999" );
-            doTest( 1, "http://goes.ngdc.noaa.gov/data/avg/2004/A1050412.TXT?skip=23&timeFormat=$y$m$d+$H$M&column=E1&time=YYMMDD" );
-            doTest( 2, "file:///home/jbf/ct/hudson/data.backup/dat/V11979066.CSV?depend0=field0&column=field13" );
-            doTest( 3, "file:///home/jbf/ct/hudson/data.backup/dat/power.dat.txt" );  //case where guess cadence is failing, though it shouldn't.
+            Exception e=null;
+            e= doTest( 0, "ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/omni2_$Y.dat?column=field17&timerange=1963&timeFormat=$Y+$j+$H&time=field0&validMax=999", e );
+            e= doTest( 1, "http://goes.ngdc.noaa.gov/data/avg/2004/A1050412.TXT?skip=23&timeFormat=$y$m$d+$H$M&column=E1&time=YYMMDD", e );
+            e= doTest( 2, "file:///home/jbf/ct/hudson/data.backup/dat/V11979066.CSV?depend0=field0&column=field13", e );
+            e= doTest( 3, "file:///home/jbf/ct/hudson/data.backup/dat/power.dat.txt", e );  //case where guess cadence is failing, though it shouldn't.
 
             //doTest( 2, "vap:http://vho.nasa.gov/mission/soho/celias_pm_30sec/1998.txt?time=YY&column=GSE_X&timeFormat=$y+$b+$d+$(ignore):$H:$M:$S" );
 
-
+            if ( e!=null ) {
+                throw e;
+            }
+            
             System.exit(0);  // TODO: something is firing up the event thread
         } catch (RuntimeException ex) {
             ex.printStackTrace();
