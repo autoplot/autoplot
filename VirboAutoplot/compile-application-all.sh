ee4aaf8e493f2bfa2dfb04fbc67127129562e1bb
jdv227 <jdv227@0c594bb0-8543-0410-a25e-d8ec440261db>
2009-11-10 20:31:33 +0000

    fixing error: properties for panel editor not copied into jumbo jar; changing use of JAVA_HOME to make slightly more mac friendly; commenting out proguard
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@3639 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/compile-application-all.sh
+++ VirboAutoplot/compile-application-all.sh
@@ -6,13 +6,20 @@
 #
 # CDF Support will be awkward because of the binaries.  Support this for the hudson platform.
 
-# set JAVA5_HOME and JAVA6_HOME
-if [ "" = "$JAVA5_HOME" ]; then
-    JAVA5_HOME=/usr/local/jdk1.5.0_17/
+# You should defined JAVA_HOME before running this script.
+# If its not defined, there is a small attempt made to use something.
+#
+# On a PC or Linux, JAVA_HOME will probably be set, and no changes will be needed.
+# On a Mac, you might have to set JAVA_HOME manually.
+# On OS 10.5.8, this worked:
+#  JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
+
+if [ "" = "$JAVA_HOME" ]; then
+    JAVA_HOME=/usr/local/jdk1.5.0_17/
 fi
-if [ "" = "$JAVA6_HOME" ]; then
-    JAVA6_HOME=/usr/local/jre1.6.0_14/
-fi
+
+JAVAC=$JAVA_HOME/bin/javac
+JAR=$JAVA_HOME/bin/jar
 
 rm -r -f temp-src/
 mkdir temp-src/
@@ -82,6 +89,12 @@ for i in `ls ../*/src/META-INF/$file` ; do
    cat $i >> temp-classes/META-INF/$file
 done
 
+file=org.virbo.datasource.DataSourceEditorPanel.extensions
+touch temp-classes/META-INF/$file
+for i in `ls ../*/src/META-INF/$file` ; do
+   cat $i >> temp-classes/META-INF/$file
+done
+
 file=helpsets.txt
 touch temp-classes/META-INF/$file
 for i in `ls ../*/src/META-INF/$file` ; do
@@ -129,27 +142,27 @@ hasErrors=0
 # compile key java classes.
 echo "compile sources..."
 cd temp-src
-if ! $JAVA5_HOME/bin/javac -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/virbo/autoplot/AutoPlotUI.java; then hasErrors=1; fi
-if ! $JAVA5_HOME/bin/javac -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/virbo/autoplot/JythonMain.java; then hasErrors=1; fi
-if ! $JAVA5_HOME/bin/javac -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/virbo/autoplot/AutoplotHelpViewer.java; then hasErrors=1; fi
-if ! $JAVA5_HOME/bin/javac -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/autoplot/pngwalk/DemoPngWalk.java; then hasErrors=1; fi
-if ! $JAVA5_HOME/bin/javac -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/das2/beans/*.java; then hasErrors=1; fi
-if ! $JAVA5_HOME/bin/javac -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/das2/util/awt/*.java; then hasErrors=1; fi
-if ! $JAVA5_HOME/bin/javac -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 test/endtoend/*.java; then hasErrors=1; fi
+if ! $JAVAC -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/virbo/autoplot/AutoPlotUI.java; then hasErrors=1; fi
+if ! $JAVAC -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/virbo/autoplot/JythonMain.java; then hasErrors=1; fi
+if ! $JAVAC -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/virbo/autoplot/AutoplotHelpViewer.java; then hasErrors=1; fi
+if ! $JAVAC -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/autoplot/pngwalk/DemoPngWalk.java; then hasErrors=1; fi
+if ! $JAVAC -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/das2/beans/*.java; then hasErrors=1; fi
+if ! $JAVAC -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/das2/util/awt/*.java; then hasErrors=1; fi
+if ! $JAVAC -target 1.5 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 test/endtoend/*.java; then hasErrors=1; fi
 cat ../temp-classes/META-INF/org.virbo.datasource.DataSourceFactory.extensions | cut -d' ' -f1
 for i in `cat ../temp-classes/META-INF/org.virbo.datasource.DataSourceFactory.extensions | cut -d' ' -f1 | sed 's/\./\//g'`; do
-   echo $JAVA5_HOME/bin/javac -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java
-   if ! $JAVA5_HOME/bin/javac -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java; then hasErrors=1; fi
+   echo $JAVAC -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java
+   if ! $JAVAC -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java; then hasErrors=1; fi
 done
 cat ../temp-classes/META-INF/org.virbo.datasource.DataSourceFormat.extensions | cut -d' ' -f1
 for i in `cat ../temp-classes/META-INF/org.virbo.datasource.DataSourceFormat.extensions | cut -d' ' -f1 | sed 's/\./\//g'`; do
-   echo $JAVA5_HOME/bin/javac -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java
-   if ! $JAVA5_HOME/bin/javac -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java; then hasErrors=1; fi
+   echo $JAVAC -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java
+   if ! $JAVAC -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java; then hasErrors=1; fi
 done
 cat ../temp-classes/META-INF/org.virbo.datasource.DataSourceEditorPanel.extensions | cut -d' ' -f1
 for i in `cat ../temp-classes/META-INF/org.virbo.datasource.DataSourceEditorPanel.extensions | cut -d' ' -f1 | sed 's/\./\//g'`; do
-   echo $JAVA5_HOME/bin/javac -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java
-   if ! $JAVA5_HOME/bin/javac -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java; then hasErrors=1; fi
+   echo $JAVAC -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java
+   if ! $JAVAC -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 $i.java; then hasErrors=1; fi
 done
 cd ..
 echo "done compile sources."
@@ -163,12 +176,15 @@ echo "make jumbo jar file..."
 cd temp-classes
 
 mkdir -p ../dist/
-$JAVA5_HOME/bin/jar cmf ../temp-src/MANIFEST.MF ../dist/AutoplotAll.jar *
+$JAR cmf ../temp-src/MANIFEST.MF ../dist/AutoplotAll.jar *
 cd ..
 echo "done make jumbo jar file..."
 
-echo "proguard/pack200 stuff..."
-#proguard is compiled for Java 6.  This needs to be fixed.
-$JAVA6_HOME/bin/java -jar ../APLibs/lib/proguard.jar @apApplicationAll.proguard
-$JAVA6_HOME/bin/pack200 dist/AutoplotAll.pro.jar.pack.gz dist/AutoplotAll.pro.jar
-echo "done proguard/pack200 stuff."
+# Progaurd stuff on hold for now:
+# 
+#echo "proguard/pack200 stuff..."
+##proguard is compiled for Java 6.  This needs to be fixed.
+#$JAVA6_HOME/bin/java -jar ../APLibs/lib/proguard.jar @apApplicationAll.proguard
+#$JAVA6_HOME/bin/pack200 dist/AutoplotAll.pro.jar.pack.gz dist/AutoplotAll.pro.jar
+#echo "done proguard/pack200 stuff."
+
