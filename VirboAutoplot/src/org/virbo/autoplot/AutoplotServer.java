786e28fc2633c6a2c863c4aa4b559e8acfd05bcb
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-02-15 21:55:22 +0000

    add resolution and cache parameters for serving datasets.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@5899 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/AutoplotServer.java
+++ VirboAutoplot/src/org/virbo/autoplot/AutoplotServer.java
@@ -9,17 +9,24 @@
 
 package org.virbo.autoplot;
 
+import java.io.File;
 import java.io.FileOutputStream;
+import org.das2.dataset.AverageTableRebinner;
+import org.das2.dataset.RebinDescriptor;
+import org.das2.datum.Datum;
+import org.das2.datum.DatumRange;
+import org.das2.datum.DatumRangeUtil;
 import org.das2.datum.Units;
 import org.das2.graph.DasCanvas;
 import static org.virbo.autoplot.ScriptContext.*;
 
 import org.das2.util.ArgumentList;
+import org.das2.util.filesystem.FileSystem;
 import org.das2.util.monitor.NullProgressMonitor;
-import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.autoplot.dom.Application;
+import org.virbo.dataset.DataSetUtil;
 import org.virbo.dataset.QDataSet;
-import org.virbo.datasource.DataSetURI;
+import org.virbo.dsops.Ops;
 import org.virbo.qstream.SimpleStreamFormatter;
 
 
@@ -50,6 +57,8 @@ public class AutoplotServer {
         alm.addOptionalSwitchArgument("format", "f", "format", "png", "output format png or pdf or qds (dflt=png)");
         alm.addOptionalSwitchArgument("outfile", "o", "outfile", "-", "output filename or -");
         alm.addOptionalSwitchArgument("timeRange", "t", "timeRange", "", "timerange for TimeSeriesBrowse datasources" );
+        alm.addOptionalSwitchArgument("resolution", "r", "resolution", "", "resolution requirement for timerange request");
+        alm.addOptionalSwitchArgument("cache", "c", "cache", "", "location where files are downloaded, default is $HOME/autoplot_data/cache");
         
         alm.requireOneOf( new String[] { "uri", "vap" } );
         alm.process(args);
@@ -59,7 +68,21 @@ public class AutoplotServer {
 
         String timeRange= alm.getValue("timeRange");
         //String timeRange="2010-01-04";
-        
+        String resolution= alm.getValue("resolution");
+
+        String cache= alm.getValue("cache");
+        if ( !cache.equals("") ) {
+            File fcache= new File( cache );
+            if ( !fcache.mkdirs() ) {
+                System.err.println( "unable to make dirs for cache="+fcache );
+            }
+            if ( !fcache.canWrite() ) {
+                System.err.println( "unable to write to cache="+fcache );
+            }
+            FileSystem.settings().setLocalCacheDir( new File( cache ) );
+            System.err.println( "using cache dir "+FileSystem.settings().getLocalCacheDir() );
+        }
+
         if ( suri.equals("") && vap.equals("") ) {
             alm.printUsage();
             System.err.println("Either uri or vap must be specified.");
@@ -117,8 +140,31 @@ public class AutoplotServer {
                     System.err.printf( "   suri=%s\n", suri );
                     ds= org.virbo.jythonsupport.Util.getDataSet(suri);
                 }
-                
+
+                System.err.println("loaded ds=" + ds );
+                if ( !resolution.equals("") ) {
+                    if ( ds==null ) {
+                        throw new IllegalArgumentException("ds is null, resolution is only usable with qds mode");
+                    }
+                    QDataSet dep0= (QDataSet) ds.property(QDataSet.DEPEND_0);
+                    Datum res= Units.seconds.parse(resolution);
+
+                    AverageTableRebinner rebin= new AverageTableRebinner();
+                    DatumRange tr;
+                    if ( timeRange.equals("") ) {
+                        tr= DataSetUtil.asDatumRange( Ops.extent( dep0 ),true );
+                    } else {
+                        tr= DatumRangeUtil.parseTimeRange(timeRange);
+                    }
+                    int nbin= (int)Math.ceil( tr.width().divide(res).doubleValue( Units.dimensionless ) );
+                    RebinDescriptor ddx= new RebinDescriptor( tr.min(), tr.max(), nbin, false );
+                    ds= rebin.rebin( ds, ddx, null );
+                }
+
             } else {
+                if ( !timeRange.equals("") )  {
+                    System.err.println("timeRange parameter has no effect in this mode (format="+format+")");
+                }
                 plot(suri);
             }
         }
@@ -142,11 +188,22 @@ public class AutoplotServer {
                 writeToPdf( outfile );
             }
         } else if ( format.equals("qds") ) {
-            if ( outfile.equals("-") ) {
-                new SimpleStreamFormatter().format( ds, System.out, true );
+            if ( ds==null ) {
+                if ( outfile.equals("-") ) {
+                    System.out.print( ":00:000000<exception/>TODO: format this\n");
+                } else {
+                    FileOutputStream fout= new FileOutputStream(outfile);
+                    fout.write( ":00:000000<exception/>TODO: format this\n".getBytes());
+                }
             } else {
-                FileOutputStream fout= new FileOutputStream(outfile);
-                new SimpleStreamFormatter().format( ds, fout, true );
+                if ( outfile.equals("-") ) {
+                    System.err.println("formatting stream to System.out");
+                    new SimpleStreamFormatter().format( ds, System.out, true );
+                } else {
+                    System.err.println("formatting stream to file "+outfile);
+                    FileOutputStream fout= new FileOutputStream(outfile);
+                    new SimpleStreamFormatter().format( ds, fout, true );
+                }
             }
         }
 
