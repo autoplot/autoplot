648065103842a22003f46b2849d2575b9ec53f96
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-03-31 17:00:25 +0000

    include LANL CRRES file
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@6274 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test030.java
+++ VirboAutoplot/src/test/endtoend/Test030.java
@@ -20,6 +20,36 @@ import static org.virbo.autoplot.ScriptContext.*;
  */
 public class Test030 {
 
+    /**
+     * this reads the ascii file into a bundle and tries to plot the last one.
+     * This removes most of the dependence on the AsciiDataSource.
+     *
+     * @param id
+     * @param uri
+     */
+    public static void doTestBundle( int id, String uri ) throws Exception {
+
+        System.err.printf( "reading %s...\n", uri );
+        QDataSet ds= Util.getDataSet( uri );
+        System.err.printf( "   read %s.\n", String.valueOf(ds) );
+
+        QDataSet bds= (QDataSet) ds.property(QDataSet.BUNDLE_1);
+
+        QDataSet x= DataSetOps.unbundle(ds,0);
+        QDataSet y= DataSetOps.unbundle(ds,bds.length()-1);
+        plot( x, y );
+        setTitle( uri );
+
+        String label= String.format( "test030_%03d", id );
+
+        writeToPng( label+".png" );
+        //((MutablePropertyDataSet)bundle1).putProperty( QDataSet.LABEL, uri );
+        formatDataSet( ds, label+".qds");
+        formatDataSet( ds, label+".txt");
+
+
+    }
+
     public static void doTest( int id, String uri ) {
 
         try {
@@ -27,9 +57,7 @@ public class Test030 {
             QDataSet ds= Util.getDataSet( uri );
             System.err.printf( "   read %s.\n", String.valueOf(ds) );
 
-            QDataSet bds= (QDataSet) ds.property(QDataSet.BUNDLE_1);
-            
-            plot( DataSetOps.unbundle(ds,0), DataSetOps.unbundle(ds,1) );
+            plot( ds );
             setTitle( uri );
 
             String label= String.format( "test030_%03d", id );
@@ -47,8 +75,13 @@ public class Test030 {
     }
 
     public static void main(String[] args) throws Exception  {
-        doTest( 0, TestSupport.TEST_DATA + "dat/headers/demo1.txt?rank2" );
-        doTest( 1, TestSupport.TEST_DATA + "dat/headers/proton_density.dat?rank2" );
+
+        doTestBundle( 0, TestSupport.TEST_DATA + "dat/headers/proton_density.dat?rank2" );
+
+        //TODO: DEPEND_0 is lost here.
+        doTest( 1, TestSupport.TEST_DATA + "dat/headers/proton_density.dat?column=Proton_Density" );
+
+        doTestBundle( 2, TestSupport.TEST_DATA + "dat/headers/CRRES_mod.txt?rank2" );
 
         System.exit(0);  // TODO: something is firing up the event thread
     }
