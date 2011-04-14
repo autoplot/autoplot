87752b65f1815ea7a828f4c2f433061d1cd8a41f
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-04-14 19:27:47 +0000

    commit empty test500, used by the plasma wave group.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@6410 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/test/endtoend/Test500.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.endtoend;
+
+/**
+ * Mark this number as allocated to the Plasma Wave Group at U. Iowa.  They don't use this, but they have a test script.
+ * See http://apps-pw.physics.uiowa.edu/hudson/job/autoplot-test500/
+ * @author jbf
+ */
+public class Test500 {
+
+}
