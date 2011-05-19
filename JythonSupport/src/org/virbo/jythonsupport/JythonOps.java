6479562ee6a2756e1ca1cfa15e184cbf648ed11b
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-05-19 21:49:16 +0000

    clean-up of coerce stuff.  coerceToDs renamed to "dataset" function which makes a dataset.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@6540 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonSupport/src/org/virbo/jythonsupport/JythonOps.java
+++ JythonSupport/src/org/virbo/jythonsupport/JythonOps.java
@@ -68,26 +68,30 @@ public class JythonOps {
         return result;
     }
 
-    public static QDataSet coerce( PyObject arg0 ) {
-        System.err.println("======================================================");
-        System.err.println("coerce( PyObject ) command that makes a QDataSet is deprecated--use coerceToDs( PyObject ) instead.");
-        System.err.println("native python coerce command will be available soon.  Contact faden @ cottagesystems.com if you need assistance.");
-        System.err.println("  sleeping for 3 seconds.");
-        System.err.println("======================================================");
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return coerceToDs( arg0 );
-    }
+//    public static QDataSet coerce( PyObject arg0 ) {
+//        System.err.println("======================================================");
+//        System.err.println("coerce( PyObject ) command that makes a QDataSet is deprecated--use coerceToDs( PyObject ) instead.");
+//        System.err.println("native python coerce command will be available soon.  Contact faden @ cottagesystems.com if you need assistance.");
+//        System.err.println("  sleeping for 3 seconds.");
+//        System.err.println("======================================================");
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException ex) {
+//            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return coerceToDs( arg0 );
+//    }
 
+    public static QDataSet coerceToDs( PyObject arg0 ) {
+        return dataset( arg0 );
+    }
+    
     /**
      * coerce a python array into a QDataSet.
      * @param arg0
      * @return
      */
-    public static QDataSet coerceToDs( PyObject arg0 ) {
+    public static QDataSet dataset( PyObject arg0 ) {
         if ( arg0 instanceof PyQDataSet ) {
             return ((PyQDataSet)arg0).rods;
         } else if ( arg0 instanceof PyList ) {
