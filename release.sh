d6f70aaf89328a9791589dcf7d8626d9e3d1cd9c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-05-20 11:49:02 +0000

    last commit didn't have comment for: svn merge -r6527:6541  https://autoplot.svn.sourceforge.net/svnroot/autoplot/autoplot/branches/autoplot2010/ .
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@6544 0c594bb0-8543-0410-a25e-d8ec440261db

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ ant do-tag
 cd ../VirboAutoplot
 ant do-tag
 
-export AP_VERSION=20110521
+export AP_VERSION=20110521 
 
 ant jar -Ddefault_release_tag=$AP_VERSION
 ant jnlp-release -Ddefault_release_tag=$AP_VERSION
