c8fbddae390c3bcfcae0e84982e25ea779115cdf
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-05-25 12:00:57 +0000

    add Jython libraries to path.  See src/tests/endtoend/Test029.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@6578 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test029.java
+++ VirboAutoplot/src/test/endtoend/Test029.java
@@ -5,6 +5,7 @@
 package test.endtoend;
 
 import java.io.IOException;
+import java.io.InputStream;
 import org.python.core.PyException;
 import org.python.core.PyObject;
 import org.python.core.PyReflectedFunction;
@@ -24,43 +25,66 @@ import org.virbo.jythonsupport.JythonUtil;
  */
 public class Test029 {
     public static void main(String[] args) throws IOException  {
+        int status= testBuiltIns();
+        if ( status!=0 )             System.exit(status);
 
+        status= testJythonLib();
+        if ( status!=0 )             System.exit(status);
+
+    }
+
+    /**
+     * ensure that Python 2.2.1 Lib directory is available, with python libraries
+     * (http://sourceforge.net/tracker/index.php?func=detail&aid=3134982&group_id=199733&atid=970682)
+     * @throws IOException
+     */
+    private static int testJythonLib() throws IOException {
+        // see if "Lib/glob.py" is on path.  As of 20110525, this shows it is on the path.
+        java.net.URL s= Test029.class.getResource("/Lib/glob.py");
+        InputStream in= s.openStream();
+
+        int c;
+        while ( (c=in.read())!=-1 ) System.err.print( (char)c );
+        in.close();
+
+        InteractiveInterpreter interp = JythonUtil.createInterpreter(false);
+        interp.exec("import glob\n");
+        PyObject res= interp.eval("print glob.glob('*')\n");
+        System.err.println(res);
+        return 0;
+    }
+
+    private static int testBuiltIns() throws IOException {
         // Python2.7 built-ins.  This comes from http://docs.python.org/library/functions.html.  The
         // params are removed and we evaluate each one.
-        String builtIns = "abs() 	divmod() 	input() 	open() 	staticmethod() "
-            + "all() 	enumerate() 	int() 	ord() 	str() "
-            + "any() 	eval() 	isinstance() 	pow() 	sum() "
-            + "basestring() 	execfile() 	issubclass() 	print() 	super() "
-            + "bin() 	file() 	iter() 	property() 	tuple() "
-            + "bool() 	filter() 	len() 	range() 	type() "
-            + "bytearray() 	float() 	list() 	raw_input() 	unichr() "
-            + "callable() 	format() 	locals() 	reduce() 	unicode()"
-            + "chr() 	frozenset() 	long() 	reload() 	vars()"
-            + "classmethod() 	getattr() 	map() 	repr() 	xrange()"
-            + "cmp() 	globals() 	max() 	reversed() 	zip()"
-            + "compile() 	hasattr() 	memoryview() 	round() 	__import__()"
-            + "complex() 	hash() 	min() 	set() 	apply()"
-            + "delattr() 	help() 	next() 	setattr() 	buffer()"
-            + "dict() 	hex() 	object() 	slice() 	coerce()"
-            + "dir() 	id() 	oct() 	sorted() 	intern()";
-        String[] bb= builtIns.replaceAll("\\(\\)", " " ).split("\\s+");
+        //
+        // note abs() is redefined but behaves like the built-in.
+        String builtIns = "abs() 	divmod() 	input() 	open() 	staticmethod() " + "all() 	enumerate() 	int() 	ord() 	str() " + "any() 	eval() 	isinstance() 	pow() 	sum() " + "basestring() 	execfile() 	issubclass() 	print() 	super() " + "bin() 	file() 	iter() 	property() 	tuple() " + "bool() 	filter() 	len() 	range() 	type() " + "bytearray() 	float() 	list() 	raw_input() 	unichr() " + "callable() 	format() 	locals() 	reduce() 	unicode()" + "chr() 	frozenset() 	long() 	reload() 	vars()" + "classmethod() 	getattr() 	map() 	repr() 	xrange()" + "cmp() 	globals() 	max() 	reversed() 	zip()" + "compile() 	hasattr() 	memoryview() 	round() 	__import__()" + "complex() 	hash() 	min() 	set() 	apply()" + "delattr() 	help() 	next() 	setattr() 	buffer()" + "dict() 	hex() 	object() 	slice() 	coerce()" + "dir() 	id() 	oct() 	sorted() 	intern()";
+        String supported= "abs() pow()";
+        String notYetSupported= "all() any()  print()  bin()  bytearray()   format()  frozenset()   reversed()   memoryview()   set()   help()   next()   buffer()   sorted() ";
 
-        InteractiveInterpreter interp= JythonUtil.createInterpreter(false);  // this is with Autoplot add-ons.
+        String[] bb = builtIns.replaceAll("\\(\\)", " ").split("\\s+");
+        InteractiveInterpreter interp = JythonUtil.createInterpreter(false); // this is with Autoplot add-ons.
         //InteractiveInterpreter interp = new InteractiveInterpreter();  // this is our goal.
-        
-        for ( int i=0; i<bb.length; i++ ) {
-            String b= bb[i];
+        for (int i = 0; i < bb.length; i++) {
+            String b = bb[i];
             try {
-                PyObject result= interp.eval( b );
-                if ( result instanceof PyReflectedFunction ) {
-                    System.err.println( "!!!" + b + ": " + result);
+                PyObject result = interp.eval(b);
+                if (result instanceof PyReflectedFunction) {
+                    System.err.println("!!!" + b + ": " + result);
+                    if ( !supported.contains(b) ) {
+                        return -3;
+                    }
                 } else {
-                    System.err.println( "   " + b + ": " + result);
+                    System.err.println("   " + b + ": " + result);
                 }
-            } catch ( PyException ex ) {
-                System.err.println("Error finding symbol \""+b+"\"" );
+            } catch (PyException ex) {
+                System.err.println("Error finding symbol \"" + b + "\"");
+                if ( !notYetSupported.contains(b) ) {
+                    return -2;
+                }
             }
         }
-
+        return 0;
     }
 }
