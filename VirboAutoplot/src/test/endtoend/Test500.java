48ec88228dd560a02ec7bdb5ac9475334c90c95e
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-06-16 01:11:13 +0000

    java implementation of test500 for debugging
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@6764 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test500.java
+++ VirboAutoplot/src/test/endtoend/Test500.java
@@ -19,6 +19,18 @@ public class Test500 {
         String[] args2= {  "--vap=/home/jbf/autoplot/test/test500_002.vap", "--outfile=/tmp/test500_002.png", "--noexit" };
         org.virbo.autoplot.AutoplotServer.main( args2 );
 
+        String[] args3= {  "--vap=/home/jbf/autoplot/test/test500_003.vap", "--outfile=/tmp/test500_003.png", "--noexit" };
+        org.virbo.autoplot.AutoplotServer.main( args3 );
+
+        String[] args4= {  "--vap=/home/jbf/autoplot/test/test500_004.vap", "--outfile=/tmp/test500_004.png", "--noexit" };
+        org.virbo.autoplot.AutoplotServer.main( args4 );
+
+        String[] args5= {  "--vap=/home/jbf/autoplot/test/test500_005.vap", "--outfile=/tmp/test500_005.png", "--noexit" };
+        org.virbo.autoplot.AutoplotServer.main( args5 );
+
+        String[] args6= {  "--vap=/home/jbf/autoplot/test/test500_006.vap", "--outfile=/tmp/test500_006.png", "--noexit" };
+        org.virbo.autoplot.AutoplotServer.main( args6 );
+
         System.err.println("DONE...");
     }
 }
