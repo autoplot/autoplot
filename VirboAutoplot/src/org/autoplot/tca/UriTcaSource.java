73e5b469f393ec1dd3e045ab0b7c708267a59524
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-06-17 22:15:21 +0000

    improve support for rank 1 datasets. get the labels.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@6778 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/autoplot/tca/UriTcaSource.java
+++ VirboAutoplot/src/org/autoplot/tca/UriTcaSource.java
@@ -13,6 +13,7 @@ import org.das2.datum.TimeUtil;
 import org.das2.datum.Units;
 import org.das2.util.monitor.NullProgressMonitor;
 import org.virbo.dataset.AbstractQFunction;
+import org.virbo.dataset.BundleDataSet.BundleDescriptor;
 import org.virbo.dataset.DDataSet;
 import org.virbo.dataset.DRank0DataSet;
 import org.virbo.dataset.DataSetUtil;
@@ -34,6 +35,7 @@ public class UriTcaSource extends AbstractQFunction {
 
     TimeSeriesBrowse tsb;
     QDataSet ds;
+    QDataSet bundleDs;
     DataSource dss;
     Exception ex;
     QDataSet error;
@@ -66,6 +68,30 @@ public class UriTcaSource extends AbstractQFunction {
         try {
             if ( read ) {
                 ds= dss.getDataSet( new NullProgressMonitor() );
+                bundleDs= (QDataSet)ds.property(QDataSet.BUNDLE_1);
+                if ( bundleDs==null ) {
+                    if ( ds.rank()==1 ) { // just a single param, go ahead and support this.
+                        DDataSet bds1= DDataSet.createRank2(1,0);
+                        String name= (String) ds.property(QDataSet.NAME);
+                        String label= (String) ds.property(QDataSet.LABEL);
+                        bds1.putProperty( QDataSet.NAME, 0, name==null ? "ds0" : name );
+                        bds1.putProperty( QDataSet.LABEL, 0, label==null ? ( name==null ? "" : name ) : label );
+                        bundleDs= bds1;
+                    } else {
+                        DDataSet bds1= DDataSet.createRank2(ds.length(0),0);
+                        QDataSet dep1= (QDataSet) ds.property(QDataSet.DEPEND_1);
+                        Units u= dep1==null ? Units.dimensionless : SemanticOps.getUnits(dep1);
+                        for ( int i=0; i<ds.length(0); i++ ) {
+                            String c= "";
+                            String name= ( dep1!=null ? u.createDatum(dep1.value(i)).toString() : (String)ds.property(QDataSet.NAME) );
+                            String label= (String) ds.property(QDataSet.LABEL);
+                            bds1.putProperty( QDataSet.NAME, i, "ds"+i );
+                            bds1.putProperty( QDataSet.LABEL, i, label==null ?  ( name==null ? "" : name ) : label );
+                        }
+                        bundleDs= bds1;
+                    }
+                }
+                read= false;
             }
             if ( ds==null ) {
                 return new JoinDataSet( errorNoDs );
@@ -75,10 +101,13 @@ public class UriTcaSource extends AbstractQFunction {
             QDataSet d0= parm.slice(0);
             QDataSet findex= Ops.findex( dep0, d0 ); // TODO: param.slice(0) does findex support rank 0?
 
-            if ( findex.value()>=0 && findex.value()<=dep0.length()-1 ) {
-                int ii= (int)(findex.value());
-                double aa= findex.value() - ii;
-                QDataSet result= ( aa<0.5 ) ? ds.slice(ii) : ds.slice(ii+1);
+            if ( findex.value()>=-0.5 && findex.value()<dep0.length()-0.5 ) {
+                int ii= (int)( findex.value() + 0.5 ); // nearest neighbor
+                QDataSet result= ds.slice(ii);
+                if ( result.rank()==0 ) {
+                    result= new JoinDataSet( result );
+                }
+                ((MutablePropertyDataSet)result).putProperty( QDataSet.BUNDLE_0, bundleDs );
                 return result;
             } else {
                 return new JoinDataSet( error );
