195a481c16c6cd8adae356351ed7e0056fea9562
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-07-01 14:12:44 +0000

    new two-jar release is standard release
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@6895 0c594bb0-8543-0410-a25e-d8ec440261db

--- release.sh
+++ release.sh
@@ -3,19 +3,35 @@
 # release script for the Autoplot website
 export rpwd=`pwd`
 cd dasCore
-ant do-tag
+ant -quiet do-tag  #TODO: why must we still do this?
 
 cd ../VirboAutoplot
-ant do-tag
+ant -quiet do-tag
 
-export AP_VERSION=20110624 
+export AP_VERSION=20110701
 
-ant jar -Ddefault_release_tag=$AP_VERSION
-ant jnlp-release -Ddefault_release_tag=$AP_VERSION
+# TODO: these are already in another script, but should probably clean this up.  Right now the keystore file is the security...
+export KEYPASS=virbo1
+export STOREPASS=dolphin
 
-sh ./jumbojar.sh  
-mv dist/autoplot.jar dist/jnlp/
-rsync -e ssh -av dist/jnlp/ root@aurora.gmu.edu:/var/www/autoplot/jnlp/$AP_VERSION/
+#SINGLE JAR RELEASE
+#sh ./jumbojar.sh
+#mv dist/autoplot.jar dist/jnlp/
+#rsync -e ssh -av dist/jnlp/ root@aurora.gmu.edu:/var/www/autoplot/jnlp/$AP_VERSION/
+
+#STANDARD TWO-JAR (volatile and stable) RELEASE
+sh ./compile-application.sh
+
+# these are left over from the do-tag stuff.
+cd ..
+rm -r VirboAutoplot/dist/lib
+rm -r VirboAutoplot/dist/VirboAutoplot.jar
+rm -r VirboAutoplot/dist/README.TXT
+
+rm -r VirboAutoplot/dist/AutoplotStable.jar
+rm -r VirboAutoplot/dist/AutoplotStable.jar.pack.gz
+
+#rsync -e ssh -av dist/jnlp/ root@aurora.gmu.edu:/var/www/autoplot/jnlp/$AP_VERSION/
 
 echo "If this is to be a new production version, then update the latest link in /var/www/autoplot/jnlp/"
 
