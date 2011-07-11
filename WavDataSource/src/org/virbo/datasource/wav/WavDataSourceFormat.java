d9821295f239bd5401d90ee94f9ee34f5f869e3d
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-07-11 20:14:49 +0000

    correct location of DataSourceFormat in source code.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@7005 0c594bb0-8543-0410-a25e-d8ec440261db

--- WavDataSource/src/org/virbo/datasource/wav/WavDataSourceFormat.java
+++ WavDataSource/src/org/virbo/datasource/wav/WavDataSourceFormat.java
@@ -23,7 +23,7 @@ import org.virbo.binarydatasource.BufferDataSet;
 import org.virbo.dataset.QDataSet;
 import org.virbo.dataset.QubeDataSetIterator;
 import org.virbo.datasource.URISplit;
-import org.virbo.datasource.datasource.DataSourceFormat;
+import org.virbo.datasource.DataSourceFormat;
 
 /**
  * Format data to binary file.
