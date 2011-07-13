ce1abca2bb7421eac06646971c72ea2e1fdcfd85
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-07-13 20:01:12 +0000

    use ftp site for omni file again to support use at RPWG.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@7063 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test005.java
+++ VirboAutoplot/src/test/endtoend/Test005.java
@@ -82,8 +82,8 @@ public class Test005 {
             plot("http://autoplot.org/data/autoplot.xml");
             writeToPng("test005_demo6.png");
             xxx("demo6");
-            //String omniSrc= "ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/";
-            String omniSrc= "file:/home/jbf/ct/hudson/data.backup/dat/";
+            String omniSrc= "ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/";
+            //String omniSrc= "file:/home/jbf/ct/hudson/data.backup/dat/";
             plot( omniSrc + "/omni2_1963.dat");
             writeToPng("test005_demo7.png");
             xxx("demo7");
