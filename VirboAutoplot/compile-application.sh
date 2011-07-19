49e4c32f4204037f01df3ea785e3095f10eb1b89
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-07-19 02:09:39 +0000

    check exit status on wget stable jars.  It took a few minutes for me to figure out what was going on.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@7122 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/compile-application.sh
+++ VirboAutoplot/compile-application.sh
@@ -56,9 +56,15 @@ rm -r -f temp-volatile-classes/
 mkdir temp-volatile-classes
 
 echo "copy jar file classes..."
-wget -O AutoplotStable.jar ${HUDSON_URL}/job/autoplot-jar-stable/lastSuccessfulBuild/artifact/autoplot/VirboAutoplot/dist/AutoplotStable.jar |& head -20
-wget -O AutoplotStable.jar.pack.gz ${HUDSON_URL}/job/autoplot-jar-stable/lastSuccessfulBuild/artifact/autoplot/VirboAutoplot/dist/AutoplotStable.jar.pack.gz |& head -20
+wget -O AutoplotStable.jar ${HUDSON_URL}/job/autoplot-jar-stable/lastSuccessfulBuild/artifact/autoplot/VirboAutoplot/dist/AutoplotStable.jar # 2>&1 | head -100
+wget -O AutoplotStable.jar.pack.gz ${HUDSON_URL}/job/autoplot-jar-stable/lastSuccessfulBuild/artifact/autoplot/VirboAutoplot/dist/AutoplotStable.jar.pack.gz # 2>&1 | head -100
+#TODO: check exit status, and stop if it fails.
 echo "done copy jar file classes."
+
+if [ $? -ne 0 ]; then
+   echo "wget fails: wget -O AutoplotStable.jar ${HUDSON_URL}/job/autoplot-jar-stable/lastSuccessfulBuild/artifact/autoplot/VirboAutoplot/dist/AutoplotStable.jar"
+   exit -1
+fi
 
 echo "copy sources..."
 for i in \
