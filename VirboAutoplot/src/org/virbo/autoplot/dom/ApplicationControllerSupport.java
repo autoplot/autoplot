3872c1955b4af41109cd7fd8251c77082bbf8cbf
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-07-21 12:09:04 +0000

    bugfix 3370241: trigger clean-up of DataSourceFilters by setting URI to null before resetting it.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@7150 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/ApplicationControllerSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/ApplicationControllerSupport.java
@@ -4,6 +4,9 @@
  */
 package org.virbo.autoplot.dom;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author jbf
@@ -16,12 +19,59 @@ public class ApplicationControllerSupport {
         this.controller = controller;
     }
 
+//    /**
+//     * remove unused Dsfs, recursing if vap+internal is not used.
+//     * this is not used, because other code handles this.
+//     * see also DomUtil.dataSourceUsages, which uses controllers.
+//     * @return
+//     */
+//    void cleanOrphanedDsfs( ) {
+//        Application dom= controller.application;
+//        Map<String,Integer> useCount= new HashMap();
+//        for ( PlotElement pe: dom.plotElements ) {
+//            String s= pe.getDataSourceFilterId();
+//            Integer i= useCount.get(s);
+//            if ( i==null ) i= 1; else i=i+1;
+//            useCount.put( s,i );
+//        }
+//        for ( DataSourceFilter dsf: dom.dataSourceFilters ) {
+//            String uri= dsf.getUri();
+//            if ( uri==null ) uri=""; //(Artifact 3373605)
+//            if ( uri.startsWith("vap+internal:") ) {
+//                String[] ids= uri.substring(13).split(",");
+//                for ( String s: ids ) {
+//                    Integer i= useCount.get(s);
+//                    if ( i==null ) i= 1; else i=i+1;
+//                    useCount.put( s,i );
+//                }
+//            }
+//        }
+//        boolean recurse= false;
+//        for ( int i=dom.dataSourceFilters.size()-1; i>=0; i-- ) {
+//            DataSourceFilter dsf= dom.dataSourceFilters.get(i);
+//            String s= dsf.id;
+//            Integer c= useCount.get(s);
+//            if ( c==null ) {
+//                if ( dsf.getUri().startsWith("vap+internal:") ) {
+//                    recurse= true;
+//                }
+//                controller.deleteDataSourceFilter(dsf);
+//            }
+//        }
+//        if ( recurse ) { // recurse in case any are free now.
+//            cleanOrphanedDsfs();
+//        }
+//
+//    }
+
     void plot( Plot plot, PlotElement panel, String primaryUri) {
         if ( panel==null ) panel = controller.addPlotElement(plot, null ); // timeseriesbrowse
+        panel.getController().getDataSourceFilter().setUri(null); // this has the side effect of removing parents
         panel.getController().getDataSourceFilter().setUri(primaryUri);
     }
 
     void plot( Plot plot, PlotElement panel, String secondaryUri, String primaryUri) {
+        panel.getController().getDataSourceFilter().setUri(null);
         DataSourceFilter dsf1 = controller.addDataSourceFilter();
         DataSourceFilter dsf2 = controller.addDataSourceFilter();
         if ( panel==null ) panel = controller.addPlotElement(plot, null ); // timeseriesbrowse
@@ -35,6 +85,7 @@ public class ApplicationControllerSupport {
         DataSourceFilter dsf2 = controller.addDataSourceFilter();
         DataSourceFilter dsf3 = controller.addDataSourceFilter();
         if (panel==null) panel = controller.addPlotElement(plot, null ); // timeseriesbrowse
+        panel.getController().getDataSourceFilter().setUri(null);
         panel.getController().getDataSourceFilter().setUri("vap+internal:" +  dsf1.getId() + "," + dsf2.getId()+","+dsf3.getId() );
         dsf1.setUri(secondaryUri);
         dsf2.setUri(teriaryUri);
