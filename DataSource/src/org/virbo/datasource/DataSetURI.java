6800973777aef0f71ca6ff6c9c08742feb7d9733
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-08-15 20:05:38 +0000

    check for ampersand in filename extension, as in ".../WAV_2011227_SRV_V17.PKT&type=B" where the user used ampersand instead of question mark.  We simply give a more precise error now.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@7328 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/DataSetURI.java
+++ DataSource/src/org/virbo/datasource/DataSetURI.java
@@ -782,6 +782,9 @@ public class DataSetURI {
         if ( !( surl.startsWith("vap") ) ) {
             URISplit split2= URISplit.parse(surl);
             String vapScheme= URISplit.implicitVapScheme(split2);
+            if ( vapScheme.contains("&") ) {
+                throw new IllegalArgumentException("Address contains ampersand in what looks like a filename: "+surl);
+            }
             if ( vapScheme.equals("") ) {
                 //one last hangout for generic "vap" type.  Better hope they don't use it...  Call it vap+X so it's easier to debug.
                 vapScheme="vap+X";
