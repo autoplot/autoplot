61ddcef0600c5762fc079835fda1879e6d28a5f4
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-09-07 12:28:56 +0000

    last merge accidentially committed temporary changes to the Java/cdf reader
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@7455 0c594bb0-8543-0410-a25e-d8ec440261db

--- CdfJavaDataSource/src/gov/nasa/gsfc/voyager/cdf/VariableDataLocator.java
+++ CdfJavaDataSource/src/gov/nasa/gsfc/voyager/cdf/VariableDataLocator.java
@@ -1,5 +1,5 @@
 package gov.nasa.gsfc.voyager.cdf;
-public interface VariableDataLocator {
+public interface VariableDataLocator {  
     /**
      * Gets a list of regions that contain data for the variable.
      * Each element of the vector describes a region as an int[3] array.
