9d34d7e3ec18d552f79536e7b575c8bc33f7b924
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-09-23 11:53:23 +0000

    bugfix: datums were formatted with units.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@7616 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/autoplot/csv/CsvDataSourceFormat.java
+++ DataSourcePack/src/org/autoplot/csv/CsvDataSourceFormat.java
@@ -15,7 +15,6 @@ import org.das2.datum.format.DatumFormatter;
 import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.dataset.QDataSet;
 import org.virbo.dataset.SemanticOps;
-import org.virbo.datasource.DataSetURI;
 import org.virbo.datasource.DataSourceFormat;
 import org.virbo.datasource.URISplit;
 
