1eb3fbfdecb4f970cc0d2ebad3a44d4919cebf32
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2011-10-31 11:33:18 +0000

    comment out Jared's test in Test024.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@7951 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test024.java
+++ VirboAutoplot/src/test/endtoend/Test024.java
@@ -45,12 +45,8 @@ public class Test024 {
         ScriptContext.writeToPng("test024_002");
 
     }
-    public static void main( String[] args )  {
-        try {
 
-            example1();
-            example2();
-            
+    public static void example3() throws Exception {
         org.virbo.idlsupport.APDataSet apds  = new org.virbo.idlsupport.APDataSet();
         apds.setDataSetURL("vap:file:///home/jbf/ct/hudson/data.backup/xls/hourlyForecast.xls?column=Temperature_F&depend0=Rel_Humidity_");
         apds.doGetDataSet();
@@ -58,30 +54,35 @@ public class Test024 {
 
         apds  = new org.virbo.idlsupport.APDataSet();
         apds.setDataSetURI("vap+das2server:http://www-pw.physics.uiowa.edu/das/das2Server?dataset=das2_1/cassini/cassiniLrfc&key=33696757&start_time=2010-01-11T11:15:00.000Z&end_time=2010-01-11T21:45:00.000Z&-lfdr+ExEw+-mfdr+ExEw+-mfr+13ExEw+-hfr+ABC12EuEvEx+-n+hfr_snd+-n+lp_rswp+-n+bad_data+-n+dpf_zero+-n+mfdr_mfr2+-n+mfr3_hfra+-n+hf1_hfrc+-a+-b+30+-bgday=" );
+
+        apds.doGetDataSet();
+        System.err.println( apds.toString() );
+
+        Object o;
+        o= apds.slice("ds_1", 0 );
+        double[] d= (double[])o;
+        System.err.println( String.format( "apds.slice(%s,0)=double[%d]", "ds_1", d.length ) );
+
+        o= apds.slice("ds_2", 0 );
+        d= (double[])o;
+        System.err.println( String.format( "apds.slice(%s,0)=double[%d]", "ds_2", d.length ) );
+
+        System.err.println( String.format("apds.rank()=%d", apds.rank() ) );
+        System.err.println( String.format("apds.length()=%d", apds.length() ) );
+
+        System.err.print( "apds.lengths(0)=" );
+        int[] lens= apds.lengths(0);
+        for ( int i:lens ) System.err.print ( i + "," );
+        System.err.println("");
+
+    }
+
+    public static void main( String[] args )  {
         try {
-            apds.doGetDataSet();
-            System.err.println( apds.toString() );
 
-            Object o;
-            o= apds.slice("ds_1", 0 );
-            double[] d= (double[])o;
-            System.err.println( String.format( "apds.slice(%s,0)=double[%d]", "ds_1", d.length ) );
-
-            o= apds.slice("ds_2", 0 );
-            d= (double[])o;
-            System.err.println( String.format( "apds.slice(%s,0)=double[%d]", "ds_2", d.length ) );
-
-            System.err.println( String.format("apds.rank()=%d", apds.rank() ) );
-            System.err.println( String.format("apds.length()=%d", apds.length() ) );
-
-            System.err.print( "apds.lengths(0)=" );
-            int[] lens= apds.lengths(0);
-            for ( int i:lens ) System.err.print ( i + "," );
-            System.err.println("");
-            
-        } catch ( Exception ex ) {  // we're getting a headless exception because it can't query for user/password
-            ex.printStackTrace();
-        }
+            example1();
+            example2();
+            //Jared's dataset is not working--ask Ed about this.... example3();  // Jared's slice
 
         } catch ( Exception ex ) {
             ex.printStackTrace();
