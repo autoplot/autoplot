689d3b72e7006bfc33f6ad876bc79ce502831799
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-01-11 22:25:55 +0000

    add Dan's rbsp-b_WFR-spectral-matrix_emfisis-L1_20121015120844_v1.2.2.cdf where it does a poor job of guessing cadence
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@8524 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test014.java
+++ VirboAutoplot/src/test/endtoend/Test014.java
@@ -139,6 +139,8 @@ public class Test014 {
             doTest( 2, "file:/home/jbf/ct/hudson/data.backup/xls/2008-lion and tiger summary.xls?sheet=Samantha+tiger+lp+lofreq&firstRow=53&column=Complex_Modulus&depend0=Frequency", null );
             doTest( 3, "file:/home/jbf/ct/hudson/data.backup/dat/cl_ttag_study.dat?column=field0", null );
             doTest( 5, "file:///home/jbf/ct/hudson/data.backup/dat/power.dat.txt", null );
+
+            doTest( 6, "file:///home/jbf/ct/hudson/data.backup/cdf/rbsp-b_WFR-spectral-matrix_emfisis-L1_20121015120844_v1.2.2.cdf?BuBu", null );
             System.exit(0);  // TODO: something is firing up the event thread
         } catch ( Exception ex) {
             ex.printStackTrace();
