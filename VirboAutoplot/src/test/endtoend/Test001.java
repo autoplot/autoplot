ab137d4b0b25d8aa35faed0fc560fdd6fc7acb7e
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-02-05 13:29:36 +0000

    update test001_003 to use up-to-date autohistogram output that hints at the render type.  This was motivated by the bugfix where the render type was mucked with from original autoplot code, now flagging this plot as having an error (http://sarahandjeremy.net:8080/hudson/job/autoplot-test001/2882/).
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@8700 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test001.java
+++ VirboAutoplot/src/test/endtoend/Test001.java
@@ -27,7 +27,7 @@ public class Test001 {
             setCanvasSize( 800, 600 );
             writeToPng( "test001_001.png" );
 
-            ds= Util.getDataSet( "file:/home/jbf/ct/hudson/data.backup/xls/2008-lion and tiger summary.xls?sheet=Samantha+tiger+lp+lofreq&firstRow=53&column=Complex_Modulus&depend0=Frequency" );
+            ds= Util.getDataSet( TestSupport.TEST_DATA+"xls/2008-lion and tiger summary.xls?sheet=Samantha+tiger+lp+lofreq&firstRow=53&column=Complex_Modulus&depend0=Frequency" );
             plot( 0, "xaxis should be log", ds );
 
             MutablePropertyDataSet mds= (MutablePropertyDataSet) findgen(50,50);
@@ -43,9 +43,9 @@ public class Test001 {
             writeToPng( "test001_002.png" );
 
             reset();
-            plot( "file:/home/jbf/ct/hudson/data.backup/xls/2008-lion and tiger summary.xls?sheet=Samantha+tiger+lp+lofreq&firstRow=53&column=Complex_Modulus&depend0=Frequency" );
+            plot( TestSupport.TEST_DATA+"xls/2008-lion and tiger summary.xls?sheet=Samantha+tiger+lp+lofreq&firstRow=53&column=Complex_Modulus&depend0=Frequency" );
             // this causes bad things as of 2009-08-12.
-            plot( "file:/home/jbf/ct/hudson/data.backup/qds/hist2.qds" );
+            plot( TestSupport.TEST_DATA+"qds/hist2.qds" );
             writeToPng( "test001_003.png" );
 
             System.exit(0);  // TODO: something is firing up the event thread
