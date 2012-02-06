0f69e4bab676a65e13587516f57ac6e585e7542c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-02-06 18:54:49 +0000

    demo extension.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@8713 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ APExtension/src/apextension/Extension.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package apextension;
+
+/**
+ * Demo of extension to Autoplot.  Note an extension will have restricted security, so do not attempt to load and save files.
+ * @author jbf
+ */
+public class Extension {
+    public static double total( double a, double b ) {
+        return a+b;
+    }
+}
