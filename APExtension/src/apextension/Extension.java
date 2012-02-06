c135459a07e4348744395e389df739b46917d5e8
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-02-06 18:55:39 +0000

    demo extension.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@8714 0c594bb0-8543-0410-a25e-d8ec440261db

--- APExtension/src/apextension/Extension.java
+++ APExtension/src/apextension/Extension.java
@@ -7,6 +7,13 @@ package apextension;
 
 /**
  * Demo of extension to Autoplot.  Note an extension will have restricted security, so do not attempt to load and save files.
+ *
+ * create a jar file from this project, and load it like so:
+ * import sys
+ * sys.path.append("/home/jbf/APExtension.jar")
+ * from apextension import Extension
+ * print Extension.total( 3,4 )
+ * 
  * @author jbf
  */
 public class Extension {
