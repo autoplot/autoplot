c83c3d07a0f45c444497999c9e2b86e4acd57238
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-02-09 13:17:50 +0000

    rte_0076363774_20120128_090924: sloppiness in the trim function of DDataSet resulted in an error when plot(x,y,z) of TSB dataset.  trim didn't trim the PLANE_0 property.  Now DataSetUtil.trimProperties is used in all the ArrayDataSet implementations.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@8737 0c594bb0-8543-0410-a25e-d8ec440261db

--- CdfJavaDataSource/src/org/virbo/cdf/TrDDataSet.java
+++ CdfJavaDataSource/src/org/virbo/cdf/TrDDataSet.java
@@ -583,9 +583,10 @@ public final class TrDDataSet extends TrArrayDataSet implements WritableDataSet,
         double[] newback = new double[noff2-noff1];
         System.arraycopy( this.back, noff1, newback, 0, noff2-noff1 );
         TrDDataSet result= new TrDDataSet( nrank, end-start, len1, len2, len3, newback );
-        DataSetUtil.putProperties( DataSetUtil.getProperties(this), result );
-        QDataSet dep0= (QDataSet) property(QDataSet.DEPEND_0);
-        if ( dep0!=null ) result.putProperty( QDataSet.DEPEND_0, dep0.trim(start, end) );
+        Map<String,Object> props= DataSetUtil.getProperties(this);
+        Map<String,Object> depProps= DataSetUtil.trimProperties( this, start, end );
+        props.putAll(depProps);
+        DataSetUtil.putProperties( props, result );
         return result;
     }
 
