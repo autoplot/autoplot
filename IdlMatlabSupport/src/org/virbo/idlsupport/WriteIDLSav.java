6ef3d57b97c4240e02e13bd4a95298aee9c9f38c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-02-10 23:46:18 +0000

    initial code for exporting to idlsav file, based on http://www.physics.wisc.edu/~craigm/idl/savefmt/savefmt.html
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@8749 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ IdlMatlabSupport/src/org/virbo/idlsupport/WriteIDLSav.java
@@ -0,0 +1,198 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.virbo.idlsupport;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.Channel;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.util.Calendar;
+
+/**
+ * write data to IDL Save File
+ * http://www.physics.wisc.edu/~craigm/idl/savefmt/node20.html
+ * @author jbf
+ */
+public final class WriteIDLSav {
+    public static final int DATATYPE_DOUBLE = 5;
+    public static final int RECTYPE_ENDMARKER = 6;
+    public static final int RECTYPE_TIMESTAMP = 10;
+    public static final int RECTYPE_VARIABLE = 2;
+    public static final int RECTYPE_VERSION = 14;
+    public static final int VARFLAG_ARRAY = 4;
+
+    private static ByteBuffer timestamp() {
+        ByteBuffer date= writeString( Calendar.getInstance().toString() );
+        ByteBuffer user= writeString( "USER" );
+        ByteBuffer host= writeString( "HOST" ); //TODO: get these from java props
+        ByteBuffer result= ByteBuffer.allocateDirect( 256*4 + date.limit() + user.limit() + host.limit() );
+        result.position(256*4);
+        result.put(date);
+        result.put(user);
+        result.put(host);
+        result.flip();
+        return result;
+    }
+
+    private static ByteBuffer version() {
+        ByteBuffer format= ByteBuffer.allocateDirect(4);
+        format.putInt(7);
+        ByteBuffer arch= writeString( "JAVA_ARCH");
+        ByteBuffer os= writeString( "JAVA_OS");
+        ByteBuffer release= writeString("AUTOPLOT");
+        ByteBuffer result= ByteBuffer.allocateDirect( 4 + arch.limit() + os.limit() + release.limit() );
+        result.put( format );
+        result.put( arch );
+        result.put( os );
+        result.put( release );
+        result.flip();
+        return result;
+    }
+
+    static final ByteBuffer getBytesStr( String s ) {
+        return ByteBuffer.wrap(s.getBytes());
+    }
+
+    static final ByteBuffer getBytesByte( byte b ) {
+        return ByteBuffer.wrap(new byte[]{b});
+    }
+
+    private static  ByteBuffer writeString( String s ) {
+        ByteBuffer result= ByteBuffer.allocateDirect(s.length()+4);
+        result.putInt(s.length());
+        try {
+            result.put(s.getBytes("US-ASCII"));
+        } catch ( UnsupportedEncodingException ex ) { } ; // this doesn't happen with JavaSE
+
+        result.flip();
+        return result;
+    }
+
+    private static ByteBuffer writeArrayDesc( Object data ) {
+        int nmax= 8; // ?? see python code.
+        int capacity= ( 8 + nmax ) * 4; //TODO: 1D
+        int eleLen= 8; // bytes
+        int ndims= 1;
+
+        ByteBuffer result= ByteBuffer.allocateDirect( capacity );
+        result.putInt( 8 ); // normal array.  There's a 64-bit array read as well that is not implemented.
+        result.putInt( eleLen ); // bytes per element TODO: only 8 byte.
+        result.putInt( Array.getLength(data)*eleLen ); //TODO: 1D
+        result.putInt( Array.getLength(data) );
+        result.putInt( ndims );
+        result.putInt( 0 );
+        result.putInt( 0 );
+
+        result.putInt( nmax );
+
+        for ( int i=0; i<nmax; i++ ) {
+            result.putInt( i==0 ? Array.getLength(data) : 1 ); //TODO: guess
+        }
+
+        result.flip();
+        return result;
+    }
+
+    private static ByteBuffer writeTypeDesc( Object data ) {
+        if ( data.getClass().isArray() ) {
+            return writeArrayDesc( data );
+        } else {
+            throw new RuntimeException("not implemented");
+        }
+    }
+
+    private static ByteBuffer writeDoubleArray( double[] data ) {
+        ByteBuffer buf= ByteBuffer.allocateDirect( data.length*8 );
+        for ( int i=0; i<data.length; i++ ) {
+            buf.putDouble(data[i]);
+        }
+        buf.flip();
+        return buf;
+    }
+
+    private static ByteBuffer writeTypeDescArray( Object data ) {
+
+        ByteBuffer arrayDesc= writeArrayDesc(data);
+        ByteBuffer result= ByteBuffer.allocateDirect( 8 + arrayDesc.limit() );
+        result.putInt( DATATYPE_DOUBLE );
+        result.putInt( VARFLAG_ARRAY);
+        result.put(arrayDesc);
+
+        result.flip();
+        return result;
+    }
+
+    private static ByteBuffer variable( String name, Object data ) {
+
+        ByteBuffer nameBuf= writeString( name.toUpperCase() );
+        ByteBuffer typedesc= writeTypeDesc( data );
+
+        ByteBuffer varData;
+        if ( data.getClass().isArray() && data.getClass().getComponentType()==double.class ) {
+            varData= writeDoubleArray( (double[])data );
+        } else {
+            throw new RuntimeException("not supported "+data.getClass());
+        }
+
+        ByteBuffer result= ByteBuffer.allocateDirect( nameBuf.limit() + typedesc.limit() + 4 + varData.limit() );
+
+        result.put(nameBuf);
+        result.put(typedesc);
+        result.putInt(7);
+        result.put(varData);
+
+        result.flip();
+        return result;
+    }
+
+    private static ByteBuffer endMarker() {
+        return ByteBuffer.allocate(0);
+    }
+
+
+    private static void writeRecord( FileChannel ch, int recType, ByteBuffer buf ) throws IOException {
+        ByteBuffer rec= ByteBuffer.allocateDirect( buf.limit()+16 );
+        rec.order( ByteOrder.LITTLE_ENDIAN );
+
+        rec.putInt( recType );
+        rec.putLong( buf.limit() );
+
+        rec.putInt( 0 ); // skip 4 bytes.
+        rec.put(buf);
+
+        rec.flip();
+
+        System.err.println( "writing "+recType + " to buffer bytes "+ rec.limit() );
+        ch.write(rec);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+
+        FileOutputStream fos = new FileOutputStream(new File("/tmp/channel.idlsav"));
+        FileChannel ch = fos.getChannel();
+
+        ch.write(getBytesStr("SR"));
+
+        ch.write(getBytesByte((byte) 0));
+        ch.write(getBytesByte((byte) 4));
+
+        writeRecord( ch, RECTYPE_TIMESTAMP, timestamp() );
+        writeRecord( ch, RECTYPE_VERSION, version() );
+        writeRecord( ch, RECTYPE_VARIABLE, variable( "xyxyxyxy", new double[] { 45,46,47,48,49 } ) );
+        writeRecord( ch, RECTYPE_ENDMARKER, endMarker() );
+        ch.close();
+
+        // variable
+throw new IllegalArgumentException("This is a work in progress....");
+
+    }
+}
