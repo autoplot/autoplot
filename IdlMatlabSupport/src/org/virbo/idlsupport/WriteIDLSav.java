93701e085780bd40dd515a746193cff719435fb1
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-02-19 23:56:50 +0000

    first commit after getting empty idlsave to format.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@8820 0c594bb0-8543-0410-a25e-d8ec440261db

--- IdlMatlabSupport/src/org/virbo/idlsupport/WriteIDLSav.java
+++ IdlMatlabSupport/src/org/virbo/idlsupport/WriteIDLSav.java
@@ -34,13 +34,13 @@ public final class WriteIDLSav {
         ByteBuffer date= writeString( "Sat Feb 11 08:43:55 2012" ); //Calendar.getInstance().toString() );
         ByteBuffer user= writeString( "jbf" );
         ByteBuffer host= writeString( "Jeremy-Fadens-MacBook-Air.local" ); //TODO: get these from java props
-        ByteBuffer result= ByteBuffer.allocateDirect( 256*4 + date.limit() + user.limit() + host.limit() );
-        for ( int i=0; i<256*4; i++ ) {
+        ByteBuffer result= ByteBuffer.allocateDirect( 257*4 + date.limit() + user.limit() + host.limit() );
+        for ( int i=0; i<257*4; i++ ) {
             result.put((byte)0);
         }
-        result.put(date);
-        result.put(user);
-        result.put(host);
+        result.put(date); System.err.println(4+result.position());
+        result.put(user); System.err.println(4+result.position());
+        result.put(host); System.err.println(4+result.position());
         result.flip();
         return result;
     }
@@ -48,11 +48,13 @@ public final class WriteIDLSav {
     private static ByteBuffer version() {
         ByteBuffer format= ByteBuffer.allocateDirect(4);
         format.order(ByteOrder.BIG_ENDIAN);
-        format.putInt(7);
-        ByteBuffer arch= writeString( "JAVA_ARCH");
-        ByteBuffer os= writeString( "JAVA_OS");
-        ByteBuffer release= writeString("AUTOPLOT");
-        ByteBuffer result= ByteBuffer.allocateDirect( 4 + arch.limit() + os.limit() + release.limit() );
+        format.putInt(9);
+        format.flip();
+        ByteBuffer arch= writeString( "x86_64");
+        ByteBuffer os= writeString( "darwin");
+        ByteBuffer release= writeString("8.1");
+        ByteBuffer result= ByteBuffer.allocateDirect( 4 + 4 + arch.limit() + os.limit() + release.limit() );
+        result.putInt( 0 );
         result.put( format );
         result.put( arch );
         result.put( os );
@@ -70,13 +72,16 @@ public final class WriteIDLSav {
     }
 
     private static  ByteBuffer writeString( String s ) {
-        ByteBuffer result= ByteBuffer.allocateDirect(s.length()+4);
+        int len= 4 * (int)Math.ceil( ( s.length()+4 ) / 4. );
+        ByteBuffer result= ByteBuffer.allocateDirect(len);
         result.order( ByteOrder.BIG_ENDIAN );
         result.putInt(s.length());
         try {
             result.put(s.getBytes("US-ASCII"));
         } catch ( UnsupportedEncodingException ex ) { } ; // this doesn't happen with JavaSE
-
+        for ( int i=result.position(); i<result.limit(); i++  ) {
+            result.put((byte)0);
+        }
         result.flip();
         return result;
     }
@@ -107,9 +112,37 @@ public final class WriteIDLSav {
         return result;
     }
 
+    private static ByteBuffer writeShortDesc( Object data ) {
+        int nmax= 8; // ?? see python code.
+        int capacity= ( 8 + nmax ) * 4; //TODO: 1D
+        int eleLen= 8; // bytes
+        int ndims= 1;
+
+        ByteBuffer result= ByteBuffer.allocateDirect( capacity );
+        result.order(ByteOrder.BIG_ENDIAN);
+        result.putInt( 8 ); // normal array.  There's a 64-bit array read as well that is not implemented.
+        result.putInt( eleLen ); // bytes per element TODO: only 8 byte.
+        result.putInt( Array.getLength(data)*eleLen ); //TODO: 1D
+        result.putInt( Array.getLength(data) );
+        result.putInt( ndims );
+        result.putInt( 0 );
+        result.putInt( 0 );
+
+        result.putInt( nmax );
+
+        for ( int i=0; i<nmax; i++ ) {
+            result.putInt( i==0 ? Array.getLength(data) : 1 ); //TODO: guess
+        }
+
+        result.flip();
+        return result;
+    }
+
     private static ByteBuffer writeTypeDesc( Object data ) {
         if ( data.getClass().isArray() ) {
             return writeArrayDesc( data );
+        } else if ( data.getClass()==short.class ) {
+            return writeShortDesc( data );
         } else {
             throw new RuntimeException("not implemented");
         }
@@ -162,11 +195,14 @@ public final class WriteIDLSav {
     }
 
     private static ByteBuffer endMarker() {
-        return ByteBuffer.allocate(0);
+        ByteBuffer result= ByteBuffer.allocate(4);
+        for ( int i=0; i<4; i++ ) result.put((byte)0);
+        result.flip();
+        return result;
     }
 
 
-    private static void writeRecord( FileChannel ch, int recType, ByteBuffer buf ) throws IOException {
+     private static int writeRecord( FileChannel ch, int recType, ByteBuffer buf, int pos ) throws IOException {
 
         int len= (int)( 4 * Math.ceil( ( buf.limit()+12. ) / 4 ) );
 
@@ -174,19 +210,28 @@ public final class WriteIDLSav {
         rec.order( ByteOrder.BIG_ENDIAN );
 
         rec.putInt( recType );
-        rec.putInt( len );
 
+        if ( recType==RECTYPE_ENDMARKER ) {
+            rec.putInt( 0 );
+        } else {
+            rec.putInt( pos + len );
+        }
         rec.putInt( 0 ); // skip 4 bytes.
         rec.put(buf);
 
+        int padBytes=0;
         for ( int i=rec.position(); i<rec.limit(); i++ ) {
             rec.put((byte)0);  // pad out to 4 byte boundary
+            padBytes++;
         }
 
         rec.flip();
 
-        System.err.println( "writing "+recType + " to buffer bytes "+ rec.limit() );
+        System.err.println( "writing "+recType + " to buffer bytes "+ pos + " to " + ( pos + rec.limit() ) + " " + padBytes );
         ch.write(rec);
+
+        pos+= rec.limit();
+        return pos;
     }
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
@@ -199,10 +244,12 @@ public final class WriteIDLSav {
         ch.write(getBytesByte((byte) 0));
         ch.write(getBytesByte((byte) 4));
 
-        writeRecord( ch, RECTYPE_TIMESTAMP, timestamp() );
-        writeRecord( ch, RECTYPE_VERSION, version() );
-        //writeRecord( ch, RECTYPE_VARIABLE, variable( "xyxyxyxy", new double[] { 45,46,47,48,49 } ) );
-        writeRecord( ch, RECTYPE_ENDMARKER, endMarker() );
+        int pos= 4;
+        pos= writeRecord( ch, RECTYPE_TIMESTAMP, timestamp(), pos );
+        pos= writeRecord( ch, RECTYPE_VERSION, version(), pos );
+        //writeRecord( ch, RECTYPE_VARIABLE, variable( "xyxyxyxy", new double[] { 45,46,47,48,49 } ), pos );
+        writeRecord( ch, RECTYPE_VARIABLE, variable( "myvar", (short)99 ), pos );
+        pos= writeRecord( ch, RECTYPE_ENDMARKER, endMarker(), pos );
         ch.close();
 
         // variable
