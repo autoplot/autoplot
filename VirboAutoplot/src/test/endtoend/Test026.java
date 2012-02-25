d30a546e07c7c6cf017b9ca98779b33ea1048714
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-02-25 13:40:50 +0000

    parsing tests from test009 are moved to here.  This are redundant tests, but at least now they are all in one place.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@8875 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test026.java
+++ VirboAutoplot/src/test/endtoend/Test026.java
@@ -4,6 +4,7 @@
  */
 package test.endtoend;
 
+import org.das2.datum.DatumRangeUtil;
 import org.das2.datum.DatumUtil;
 import org.das2.datum.DatumRange;
 import static org.das2.datum.DatumRangeUtil.*;
@@ -14,6 +15,49 @@ import static org.das2.datum.DatumRangeUtil.*;
  * @author jbf
  */
 public class Test026 {
+
+    private static boolean testTimeRange( String norm, String test ) {
+        DatumRange dr1= DatumRangeUtil.parseTimeRangeValid(test);
+        DatumRange dr2= DatumRangeUtil.parseTimeRangeValid(norm);
+        if ( !dr1.equals(dr2) ) {
+            throw new IllegalStateException("fail after parsing test->"+dr1.toString()+" width="+DatumUtil.asOrderOneUnits(dr1.width()) );
+        } else {
+            String format= DatumRangeUtil.formatTimeRange(dr1);
+            dr1= DatumRangeUtil.parseTimeRangeValid(format);
+            if ( !dr1.equals(dr2) ) {
+               throw new IllegalStateException("fail after format:  format="+format+" width="+DatumUtil.asOrderOneUnits(dr1.width()) );
+            }
+        }
+        return true;
+    }
+
+    /**
+     * test against a list of time ranges.  This may serve as a guide for how to
+     * format time strings.  Each test is a string to parse against an easy-to-parse
+     * explicit range.  
+     *
+     * commented entries are strings that fail in the parser but should handled.
+     */
+    public static void testTimeRangeFormatParse() {
+        // testTimeRange( easy-to-parse norm, test string ).
+        testTimeRange( "2001-11-03 23:00 to 2001-11-05 00:00", "2001-11-03 23:00 to 2001-11-04 24:00"  );
+        testTimeRange( "2001-01-01 00:00 to 2002-01-01 00:00", "2001"  );
+        testTimeRange( "2001-01-01 00:00 to 2004-01-01 00:00", "2001-2004"  ); // das2 "-" is exclusive
+        testTimeRange( "2001-01-01 00:00 to 2004-01-01 00:00", "2001 to 2004"  ); // das2 to is exclusive
+        testTimeRange( "2001-01-01 00:00 to 2004-01-01 00:00", "2001 through 2003"  ); // das2 through is inclusive
+        testTimeRange( "2001-06-01 00:00 to 2001-07-01 00:00", "2001 Jun" );
+        testTimeRange( "2001-06-01 00:00 to 2001-08-01 00:00", "2001 Jun through July" );
+        testTimeRange( "2001-06-01 00:00 to 2001-07-01 00:00", "2001 Jun to July" );
+        testTimeRange( "2001-06-08 00:00 to 2001-06-09 00:00", "2001 Jun 8" );
+        testTimeRange( "2001-06-01 00:00 to 2001-07-01 00:00", "2001 Jun to July" );
+        testTimeRange( "2001-06-08 00:00 to 2001-06-09 00:00", "2001 Jun 8 00:00 to 24:00" );
+        testTimeRange( "2001-01-01 00:00 to 2001-01-06 00:00", "2001 Jan 01 span 5 day" );
+        testTimeRange( "2001-01-01 05:00 to 2001-01-01 07:00", "2001 Jan 01 05:00 span 2 hr" );
+        testTimeRange( "2010-09-01 00:00 to 2010-09-02 00:00", "2010-244" );  // day of year is three digits (001-366)
+        testTimeRange( "2010-03-01 00:00 to 2010-03-02 00:00", "2010-060" );
+        //testTimeRange( "2000 01 span 5 d", "2000-jan-01 to 2000-jan-06" );
+
+    }
 
     public static void doTest(int id, String test, String ref) throws Exception {
 
@@ -66,6 +110,8 @@ public class Test026 {
 
             //month boundaries crossing year boundary caused problems.
             doTest(35, "Aug 1969 through Sep 1970", "Aug 1 1969 to Oct 1 1970" );
+
+            testTimeRangeFormatParse(); // these are tests that used to be in test009.
 
             System.exit(0);  // TODO: something is firing up the event thread
         } catch (Exception ex) {
