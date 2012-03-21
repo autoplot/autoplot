2c546c17c598936d1f2126ccbc821bb46c0a7179
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-03-21 20:33:25 +0000

    add script to build single (jumbo) jar
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@9074 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/makeSingleJar.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#make the single jar ("jumbojar") on the server side so the release need only be transferred once.  Before we would copy both multi-jar and single-jar versions at once.  This should be run in the directory with AutoplotStable.jar in it.
+# This should be run immediately after the autoplot-release script is run, and from within the dist folder.
+
+if [ ! -e AutoplotVolatile.jar ]; then
+  echo "folder should contain AutoplotVolatile.jar"
+  exit -1
+fi
+
+mkdir tjar
+cd tjar
+unzip ../AutoplotStable.jar
+unzip -o ../AutoplotVolatile.jar
+zip -r ../autoplot.jar.1 *
+
+cd ..
+cat ../../bin/starterScript.sh > autoplot.jar
+cat autoplot.jar.1 >> autoplot.jar
+rm autoplot.jar.1
+rm -r tjar
