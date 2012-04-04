4867bafe357b7b56f3babe0dcfcaa3c71aad96eb
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-04-04 11:56:37 +0000

    return a more precise status message.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@9193 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/virbo/autoplot/bookmarks/RemoteStatus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.autoplot.bookmarks;
+
+/**
+ * holder for status of remote bookmarks
+ * @author jbf
+ */
+public class RemoteStatus {
+    String remoteURL; // location we tried to read
+    int depth;
+    int status;  // Bookmark.Folder.REMOTE_STATUS_*
+    boolean remoteRemote; // true indicates there are more remote bookmarks to load in.
+}
