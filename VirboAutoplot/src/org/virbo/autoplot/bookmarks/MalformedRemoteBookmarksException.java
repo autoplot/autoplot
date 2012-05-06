0194c08baadd2a01c9f9a79384ae779d223c6a1d
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-05-06 12:24:39 +0000

    add a specific exception type.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@9526 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/virbo/autoplot/bookmarks/MalformedRemoteBookmarksException.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.autoplot.bookmarks;
+
+/**
+ * marker class for MalformedRemoteBookmarksException
+ * @author jbf
+ */
+public class MalformedRemoteBookmarksException extends Exception {
+    public MalformedRemoteBookmarksException( String msg ) {
+        super(msg);
+    }
+}
