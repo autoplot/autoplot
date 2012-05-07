8afadc97f7f8278f73666cc24b9c6b4d78c88c74
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-05-07 12:20:31 +0000

    special handling for Html Files and Empty files.  Move openBrowser into DataSource so that DataSetSelector can use it.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@9542 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/DataSourceUtil.java
+++ DataSource/src/org/virbo/datasource/DataSourceUtil.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLDecoder;
@@ -31,6 +32,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
 import org.das2.datum.DatumRange;
 import org.das2.datum.DatumRangeUtil;
 import org.das2.datum.TimeUtil;
@@ -649,6 +651,47 @@ public class DataSourceUtil {
         }
 
         return spec;
+    }
+
+    /**
+     * open the URL in a browser.   Borrowed from http://www.centerkey.com/java/browser/.  See also openBrowser in VirboAutoplot,
+     * which this replaces.
+     */
+    public static void openBrowser(String url) {
+        final String errMsg = "Error attempting to launch web browser";
+        String osName;
+        try {
+            osName =System.getProperty("os.name", "applet" );
+        } catch (SecurityException ex) {
+            osName= "applet";
+        }
+        try {
+            if (osName.startsWith("Mac OS")) {
+                Class fileMgr = Class.forName("com.apple.eio.FileManager");
+                Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[]{String.class});
+                openURL.invoke(null, new Object[]{url});
+            } else if (osName.startsWith("Windows")) {
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else if (osName.equals("applet")) {
+                throw new RuntimeException("applets can't start browser yet");
+                //TODO: this shouldn't be difficult, just get the AppletContext.
+            } else { //assume Unix or Linux
+                String[] browsers = {"firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape"};
+                String browser = null;
+                for (int count = 0; count < browsers.length && browser == null; count++) {
+                    if (Runtime.getRuntime().exec(new String[]{"which", browsers[count]}).waitFor() == 0) {
+                        browser = browsers[count];
+                    }
+                }
+                if (browser == null) {
+                    throw new Exception("Could not find web browser");
+                } else {
+                    Runtime.getRuntime().exec(new String[]{browser, url});
+                }
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, errMsg + ":\n" + e.getLocalizedMessage());
+        }
     }
 
     /**
--- VirboAutoplot/src/org/virbo/autoplot/AutoplotUtil.java
+++ VirboAutoplot/src/org/virbo/autoplot/AutoplotUtil.java
@@ -85,6 +85,7 @@ import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QubeDataSetIterator;
 import org.virbo.dataset.RankZeroDataSet;
 import org.virbo.dataset.SemanticOps;
+import org.virbo.datasource.DataSourceUtil;
 import org.virbo.datasource.URISplit;
 import org.virbo.dsops.Ops;
 import org.virbo.dsutil.AutoHistogram;
@@ -983,35 +984,7 @@ public class AutoplotUtil {
      * open the URL in a browser.   Borrowed from http://www.centerkey.com/java/browser/.
      */
     public static void openBrowser(String url) {
-        final String errMsg = "Error attempting to launch web browser";
-        String osName = AutoplotUtil.getProperty("os.name", "applet");
-        try {
-            if (osName.startsWith("Mac OS")) {
-                Class fileMgr = Class.forName("com.apple.eio.FileManager");
-                Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[]{String.class});
-                openURL.invoke(null, new Object[]{url});
-            } else if (osName.startsWith("Windows")) {
-                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
-            } else if (osName.equals("applet")) {
-                throw new RuntimeException("applets can't start browser yet");
-                //TODO: this shouldn't be difficult, just get the AppletContext.
-            } else { //assume Unix or Linux
-                String[] browsers = {"firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape"};
-                String browser = null;
-                for (int count = 0; count < browsers.length && browser == null; count++) {
-                    if (Runtime.getRuntime().exec(new String[]{"which", browsers[count]}).waitFor() == 0) {
-                        browser = browsers[count];
-                    }
-                }
-                if (browser == null) {
-                    throw new Exception("Could not find web browser");
-                } else {
-                    Runtime.getRuntime().exec(new String[]{browser, url});
-                }
-            }
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, errMsg + ":\n" + e.getLocalizedMessage());
-        }
+        DataSourceUtil.openBrowser(url);
     }
 
     public static Document readDoc(InputStream is) throws SAXException, IOException, ParserConfigurationException {
