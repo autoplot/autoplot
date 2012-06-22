1eb61bb5c0e317f11aeca48b77ee0809338654b0
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-06-22 14:56:39 +0000

    make into proper Jemmy test.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@9870 0c594bb0-8543-0410-a25e-d8ec440261db

--- VATesting/src/test/Test_3pt7_ZoomPan.java
+++ VATesting/src/test/Test_3pt7_ZoomPan.java
@@ -5,7 +5,12 @@
 
 package test;
 
+import org.das2.datum.DatumRange;
+import org.netbeans.jemmy.Scenario;
 import java.awt.event.MouseWheelEvent;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.das2.graph.DasPlot;
 import org.netbeans.jemmy.drivers.input.MouseEventDriver;
 import org.virbo.autoplot.dom.Application;
@@ -17,32 +22,62 @@ import static org.virbo.autoplot.ScriptContext.*;
  *
  * @author jbf
  */
-public class Test_3pt7_ZoomPan {
-    public static void main( String[] args ) throws Exception {
-        createGui();
-        AutoplotUI app = (AutoplotUI) getViewWindow();
-        JFrameOperator mainFrame = new JFrameOperator(app);
-        waitUntilIdle();
+public class Test_3pt7_ZoomPan implements Scenario {
 
-        Application dom= getDocumentModel();
+    private static boolean close( double t, double d ) {
+        return Math.abs( (t-d)/t ) < 0.001;
+    }
 
-        DasPlot p= dom.getController().getPlot().getController().getDasPlot();
-        MouseWheelEvent e;
-        e= new MouseWheelEvent( p, 0, System.currentTimeMillis(), 0, 300, 300, 0, false, MouseWheelEvent.WHEEL_UNIT_SCROLL, 1, 1 );
-        p.getDasMouseInputAdapter().getSecondaryModule().mouseWheelMoved(e);
+    public int runIt(Object o) {
+        DatumRange dr0;
+        DatumRange dr1;
+        DatumRange dr2;
 
-        writeToPng( "Test_3pt7_ZoomPan.001.png");
+        try {
+            createGui();
+            AutoplotUI app = (AutoplotUI) getViewWindow();
+            JFrameOperator mainFrame = new JFrameOperator(app);
+            waitUntilIdle();
+            Application dom = getDocumentModel();
+            DasPlot p = dom.getController().getPlot().getController().getDasPlot();
+            MouseWheelEvent e;
+            e = new MouseWheelEvent(p, 0, System.currentTimeMillis(), 0, 300, 300, 0, false, MouseWheelEvent.WHEEL_UNIT_SCROLL, 1, 1);
+            p.getDasMouseInputAdapter().getSecondaryModule().mouseWheelMoved(e);
+            writeToPng("Test_3pt7_ZoomPan.001.png");
+            dr0= p.getXAxis().getDatumRange();
 
-        e= new MouseWheelEvent( p, 0, System.currentTimeMillis(), 0, 300, 300, 0, false, MouseWheelEvent.WHEEL_UNIT_SCROLL, 1, -1 );
-        p.getDasMouseInputAdapter().getSecondaryModule().mouseWheelMoved(e);
+            e = new MouseWheelEvent(p, 0, System.currentTimeMillis(), 0, 300, 300, 0, false, MouseWheelEvent.WHEEL_UNIT_SCROLL, 1, -1);
+            p.getDasMouseInputAdapter().getSecondaryModule().mouseWheelMoved(e);
+            writeToPng("Test_3pt7_ZoomPan.002.png");
+            dr1= p.getXAxis().getDatumRange();
 
-        writeToPng( "Test_3pt7_ZoomPan.002.png");
+            dom.getController().getPlot().setIsotropic(true);
+            e = new MouseWheelEvent(p, 0, System.currentTimeMillis(), 0, 300, 300, 0, false, MouseWheelEvent.WHEEL_UNIT_SCROLL, 1, 1);
+            p.getXAxis().getDasMouseInputAdapter().getSecondaryModule().mouseWheelMoved(e);
+            writeToPng("Test_3pt7_ZoomPan.003.png");
+            dr2= p.getXAxis().getDatumRange();
 
-        dom.getController().getPlot().setIsotropic(true);
-        e= new MouseWheelEvent( p, 0, System.currentTimeMillis(), 0, 300, 300, 0, false, MouseWheelEvent.WHEEL_UNIT_SCROLL, 1, 1 );
-        p.getXAxis().getDasMouseInputAdapter().getSecondaryModule().mouseWheelMoved(e);
+            if ( dr0.width().value()==125.0
+                    && dr1.width().value()==100.0
+                    && dr1.width().value()==100.0
+                    && close( dr2.width().value(), 158.31 ) ) {
+                return 0;
+            } else {
+                return 1;
+            }
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Test_3pt7_ZoomPan.class.getName()).log(Level.SEVERE, null, ex);
+            return 1;
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Test_3pt7_ZoomPan.class.getName()).log(Level.SEVERE, null, ex);
+            return 1;
+        }
 
-        writeToPng( "Test_3pt7_ZoomPan.003.png");
-        
+    }
+
+    public static void main(String[] argv) {
+	String[] params = {"test.Test_3pt7_ZoomPan"};
+	org.netbeans.jemmy.Test.main(params);
     }
 }
