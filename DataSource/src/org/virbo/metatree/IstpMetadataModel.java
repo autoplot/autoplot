212028404cf47ca9161ae45660cbe110beb14aa1
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-06-25 15:49:47 +0000

    remove debug message showing when DEPEND_0 was a String instead of another Map.  This happens often and should not be considered a failure.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@9896 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/metatree/IstpMetadataModel.java
+++ DataSource/src/org/virbo/metatree/IstpMetadataModel.java
@@ -347,7 +347,7 @@ public class IstpMetadataModel extends MetadataModel {
                 Object o= attrs.get(key);
                 if ( o==null ) continue;
                 if ( !( o instanceof Map ) ) {
-                    new RuntimeException("String where Map was expected").printStackTrace();
+                    //new RuntimeException("String where Map was expected").printStackTrace();
                     //TODO: track this down: vap:http://cdaweb.gsfc.nasa.gov/istp_public/data/fast/ies/1998/fa_k0_ies_19980102_v02.cdf?ion_0
                     continue;
                 }
