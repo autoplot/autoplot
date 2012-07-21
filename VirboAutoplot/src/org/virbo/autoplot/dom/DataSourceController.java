c7e6726bbfe248109bcca6865c883e6853f004d4
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-07-21 12:04:14 +0000

    look at plot.context and plot.xaxis.range in doesPlotElementSupportTsb.  Reiner and Seth's histograms v. time...
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10074 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/DataSourceController.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/DataSourceController.java
@@ -257,9 +257,13 @@ public class DataSourceController extends DomNodeController {
      * @return
      */
     private boolean doesPlotElementSupportTsb( PlotElement p ) {
-        return p.isAutoComponent() || (
-                !( p.getComponent().contains("|slice0") || p.getComponent().contains("|collapse0") ) );
-
+        Plot plot= p.getController().getApplication().getController().getPlotFor(p);
+        if ( plot==null ) return false;
+        if ( UnitsUtil.isTimeLocation( plot.getXaxis().getRange().getUnits() ) 
+                || UnitsUtil.isTimeLocation( plot.getContext().getUnits() ) ) return true;
+        return false;
+        //return p.isAutoComponent() || (
+        //        !( p.getComponent().contains("|slice0") || p.getComponent().contains("|collapse0") ) );
     }
 
     public synchronized void resetDataSource(boolean valueWasAdjusting,DataSource dataSource) {
