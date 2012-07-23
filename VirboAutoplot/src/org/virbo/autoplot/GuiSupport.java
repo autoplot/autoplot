7a87824f392e7ecfbba935307cb4749002de101b
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-07-23 20:04:14 +0000

    bugfix: Bill pointed out that regardless of which Autoplot window he did a file->print to->pdf, he would get the same image.  This was because of an old bugfix that allowed George to print the slice of a window, where we kept track of a focus canvas.  This is reverted back to the old code, since you can print directly from the slices now.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10099 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/GuiSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/GuiSupport.java
@@ -829,8 +829,8 @@ public class GuiSupport {
     public static Action getPrintAction( final Application app, final Component parent,final String ext) {
         return new AbstractAction("Print as "+ext.toUpperCase()) {
             public void actionPerformed(ActionEvent e) {
-                final DasCanvas canvas= DasCanvas.getFocusCanvas();
-                //final DasCanvas canvas = app.getController().getDasCanvas();
+                //final DasCanvas canvas= DasCanvas.getFocusCanvas();
+                final DasCanvas canvas = app.getController().getDasCanvas();
                 final JFileChooser fileChooser = new JFileChooser();
                 fileChooser.setDialogTitle("Print to "+ext.toUpperCase());
                 fileChooser.setFileFilter(getFileNameExtensionFilter( ext + " files", ext ));
