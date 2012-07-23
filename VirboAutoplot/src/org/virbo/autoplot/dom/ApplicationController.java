021aaa9ad7bd060d55d25d7e0546c56370d0cd5f
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-07-23 10:42:20 +0000

    reset actions where leftover dasCanvasComponents are removed from event thread need to occur on the event thread.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10079 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/ApplicationController.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/ApplicationController.java
@@ -1455,33 +1455,48 @@ public class ApplicationController extends DomNodeController implements RunLater
                 dsf.getController().histogram=null;
             }
 
-            //go ahead and check for leftover das2 plots and renderers that might have been left from a bug.  rfe3324592
-            DasCanvasComponent[] dccs= canvas.controller.getDasCanvas().getCanvasComponents();
-            for ( int i=0; i<dccs.length; i++ ) {
-                if ( dccs[i] instanceof DasPlot ) {
-                    DasPlot p= (DasPlot)dccs[i];
-                    boolean okay=false;
-                    for ( Plot pp: application.getPlots() ) {
-                        if ( pp.getController().getDasPlot()==p ) okay=true;
-                    }
-                    if ( !okay ) {
-                        canvas.controller.getDasCanvas().remove(p);
-                    } else {
-                        Renderer[] rr= p.getRenderers();
-                        for ( int j=0; j<rr.length; j++ ) {
-                            okay= false;
-                            for ( PlotElement pes: application.getPlotElements() ) {
-                               if ( pes.getController().getRenderer()==rr[j] ) okay=true;
+            // reset das2 stuff which may be in a bad state.  This must be done on the event thread.
+            Runnable run= new Runnable() {
+                public void run() {
+                    //go ahead and check for leftover das2 plots and renderers that might have been left from a bug.  rfe3324592
+                    DasCanvasComponent[] dccs= canvas.controller.getDasCanvas().getCanvasComponents();
+                    for ( int i=0; i<dccs.length; i++ ) {
+                        if ( dccs[i] instanceof DasPlot ) {
+                            DasPlot p= (DasPlot)dccs[i];
+                            boolean okay=false;
+                            for ( Plot pp: application.getPlots() ) {
+                                if ( pp.getController().getDasPlot()==p ) okay=true;
                             }
                             if ( !okay ) {
-                                p.removeRenderer(rr[j]);
+                                canvas.controller.getDasCanvas().remove(p);
+                            } else {
+                                Renderer[] rr= p.getRenderers();
+                                for ( int j=0; j<rr.length; j++ ) {
+                                    okay= false;
+                                    for ( PlotElement pes: application.getPlotElements() ) {
+                                       if ( pes.getController().getRenderer()==rr[j] ) okay=true;
+                                    }
+                                    if ( !okay ) {
+                                        p.removeRenderer(rr[j]);
+                                    }
+                                }
                             }
                         }
                     }
                 }
+            };
+
+            try {
+                if ( SwingUtilities.isEventDispatchThread() ) {
+                    run.run();
+                } else {
+                    SwingUtilities.invokeAndWait(run);
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ApplicationController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (InvocationTargetException ex) {
+                Logger.getLogger(ApplicationController.class.getName()).log(Level.SEVERE, null, ex);
             }
-
-
 
 
         } finally {
