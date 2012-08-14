22ec14ba5165293b3405ec551007a74ad0116c2f
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-08-14 01:00:18 +0000

    rte 1243117384 print message so I can debug on linux...
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10197 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/PlotElementController.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/PlotElementController.java
@@ -1579,15 +1579,19 @@ public class PlotElementController extends DomNodeController {
             peleCopy.getPlotDefaults().getYaxis().setAutoRange(true);
             peleCopy.getPlotDefaults().getZaxis().setAutoRange(true);
 
-            if ( peleCopy.getPlotDefaults()==null ) {
+            //if ( peleCopy.getPlotDefaults()==null ) {
                 System.err.println("plotDefaults is null here!");
-            }
+                System.err.println("1.1"+peleCopy.getPlotDefaults().toString());
+                System.err.println("1.2"+peleCopy.getPlotDefaults().getXaxis().toString());
+                System.err.println("1.3"+peleCopy.getPlotDefaults().getXaxis().getRange().toString());
+
+            //}
 
             if ( logger.isLoggable(Level.FINEST) ) {
                 System.err.println("1"+peleCopy.toString());
-                System.err.println("1"+peleCopy.getPlotDefaults().toString());
-                System.err.println("1"+peleCopy.getPlotDefaults().getXaxis().toString());
-                System.err.println("1"+peleCopy.getPlotDefaults().getXaxis().getRange().toString());
+                System.err.println("1.1"+peleCopy.getPlotDefaults().toString());
+                System.err.println("1.2"+peleCopy.getPlotDefaults().getXaxis().toString());
+                System.err.println("1.3"+peleCopy.getPlotDefaults().getXaxis().getRange().toString());
                 System.err.println("2"+peleCopy.getPlotDefaults().getYaxis().getRange().toString());
 
                 logger.finest( String.format( "done, autorange  x:%s, y:%s ",
