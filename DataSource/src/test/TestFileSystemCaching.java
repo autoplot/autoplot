b34634340f91ab778a75485bc9a18727f4444f4b
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-08-14 17:27:53 +0000

    add test/TestFileSystemCaching
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10211 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ DataSource/src/test/TestFileSystemCaching.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test;
+
+import ftpfs.FTPBeanFileSystemFactory;
+import org.das2.util.filesystem.FileObject;
+import org.das2.util.filesystem.FileSystem;
+import org.das2.util.filesystem.KeyChain;
+
+/**
+ *
+ * @author jbf
+ */
+public class TestFileSystemCaching {
+    public static void main( String[] args ) throws Exception {
+
+        FileSystem.registerFileSystemFactory( "ftp", new FTPBeanFileSystemFactory() );
+        
+        FileSystem fs= FileSystem.create( "ftp://jbf@localhost/tmp/testData/");
+        FileObject fo= fs.getFileObject( "data_2010_03_02_v1.00.qds" );
+        System.err.println( fo.getClass() );
+        System.err.println( fo.lastModified() );
+        System.err.println( fo.getSize() );
+
+        KeyChain.getDefault().writeKeysFile();
+        
+        fs.listDirectory("/a");
+        fo= fs.getFileObject("/a/afile.txt");
+        System.err.println( fo.lastModified() );
+        System.err.println( fo.getSize() );
+
+        fs.listDirectory("/a");
+        fo= fs.getFileObject("/a/afile.txt");
+        System.err.println( fo.lastModified() );
+        System.err.println( fo.getSize() );
+
+
+    }
+}
