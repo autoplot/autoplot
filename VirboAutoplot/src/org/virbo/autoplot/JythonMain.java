a4deada5d60bc339331ad3a2d813ed272ddaf1e6
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-08-29 22:18:29 +0000

    any arguments following the script name are passed in as params.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10324 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/JythonMain.java
+++ VirboAutoplot/src/org/virbo/autoplot/JythonMain.java
@@ -12,9 +12,11 @@ package org.virbo.autoplot;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
 import org.python.core.Py;
 import org.python.core.PyDictionary;
 import org.python.util.PythonInterpreter;
+import org.virbo.datasource.DataSourceUtil;
 
 
 /**
@@ -32,6 +34,34 @@ public class JythonMain {
     public static PythonInterpreter getInterpreter() {
         return interp;
     }
+
+    private static void insertArgs( String[] argv ) {
+        interp.exec("params=dict()"); // untested.
+        int iargv=-1;  // skip the zeroth one, it is the name of the script
+        for (String s : argv ) {
+            int ieq= s.indexOf("=");
+            if ( ieq>0 ) {
+                String snam= s.substring(0,ieq).trim();
+                if ( DataSourceUtil.isJavaIdentifier(snam) ) {
+                    String sval= s.substring(ieq+1).trim();
+                    interp.exec("params['" + snam + "']='" + sval+"'");
+                } else {
+                    if ( snam.startsWith("-") ) {
+                        System.err.println("script arguments should not start with -, they should be name=value");
+                    }
+                    System.err.println("bad parameter: "+ snam);
+                }
+            } else {
+                if ( iargv>=0 ) {
+                    interp.exec("params['arg_" + iargv + "']='" + s +"'" );
+                    iargv++;
+                } else {
+                    //System.err.println("skipping parameter" + s );
+                    iargv++;
+                }
+            }
+        }
+    }
     
     public static void main(String[] args) throws Exception {
 
@@ -48,6 +78,9 @@ public class JythonMain {
             in= System.in;
             inIdentifier= "<stdin>";
         } else {
+            if ( args.length>1 ) {
+                insertArgs( Arrays.copyOfRange(args, 1, args.length ) );
+            }
             File f= new File( args[0] );
             if ( !f.exists() ) {
                 System.err.println("File does not exist: "+f);
