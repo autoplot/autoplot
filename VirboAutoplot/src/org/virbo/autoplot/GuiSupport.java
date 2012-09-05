d9b3e522b1acdc2cccb31c1d96468fb119575b15
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-09-05 22:53:35 +0000

    bugfix: export data "Processed Data or Component" didn't work when there was a component.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10379 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/GuiSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/GuiSupport.java
@@ -310,7 +310,7 @@ public class GuiSupport {
                 DatumRange xbounds= p.getXAxis().getDatumRange();
                 QDataSet dsout=  pe.getController().getDataSet();
                 if ( dsf.getController().getTsb()!=null ) {
-                    dsout= DataSetOps.sprocess( pe.getComponent(), dsout, DasProgressPanel.createFramed(parent, "process TSB timeseries at native resolution") );
+                    dsout= DataSetOps.processDataSet( pe.getComponent(), dsout, DasProgressPanel.createFramed(parent, "process TSB timeseries at native resolution") );
                     dsout= SemanticOps.trim( dsout, xbounds, null );
                     format.formatData( uriOut, dsout, mon );
                 } else {
@@ -320,7 +320,7 @@ public class GuiSupport {
             } else if ( dscontrol.equals("plotElement") ) {
                 QDataSet dsout=  pe.getController().getDataSet();
                 if ( dsf.getController().getTsb()!=null ) {
-                    dsout= DataSetOps.sprocess( pe.getComponent(), dsout, DasProgressPanel.createFramed(parent, "process TSB timeseries at native resolution") );
+                    dsout= DataSetOps.processDataSet( pe.getComponent(), dsout, DasProgressPanel.createFramed(parent, "process TSB timeseries at native resolution") );
                     format.formatData( uriOut, dsout, mon );
                 } else {
                     format.formatData( uriOut, dsout, mon );
