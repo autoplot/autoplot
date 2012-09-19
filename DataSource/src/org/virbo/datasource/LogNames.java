b7fd7b95c187e5d02e776cad7f0671e7962f508a
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-09-19 10:42:50 +0000

    bug 3567923: giant logger refactoring.   Introduce LogNames constants to encourage standard names and to make it easier to locate the use of a logger.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10503 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ DataSource/src/org/virbo/datasource/LogNames.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.datasource;
+
+/**
+ * Useful Constants for retrieving loggers, to be used like so: Logger.getLogger(LogNames.APDSS).  This
+ * is intended to encourage use of standard names, and minimize the number of log channels.
+ * See http://www.autoplot.org/developer.logging .
+ * @author jbf
+ */
+public class LogNames {
+
+    public static final String APDSS= "apdss";
+
+    public static final String APDSS_UPDATING= "apdss.updating";
+
+    public static final String APDSS_CDAWEB= "apdss.cdaweb";
+
+    /**
+     * native CDF logger
+     */
+    public static final String APDSS_CDFN= "apdss.cdfn";
+
+    /**
+     * pure-Java CDF logger
+     */
+    public static final String APDSS_CDFJAVA= "apdss.cdfjava";
+
+    /**
+     * HTML tables reader.
+     */
+    public static final String APDSS_HTML= "apdss.html";
+
+    public static final String APDSS_ASCII= "apdss.ascii";
+
+    /**
+     * Comma separated values reader.
+     */
+    public static final String APDSS_CSV= "apdss.csv";
+
+    /*
+     * data set selector
+     */
+    public static final String APDSS_DSS= "apdss.dss";
+
+    public static final String APDSS_URI= "apdss.uri";
+
+
+}
