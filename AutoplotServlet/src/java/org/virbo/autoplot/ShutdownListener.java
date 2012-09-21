a4d36ab15d924ab33cadbe20db46438d4b1deb1d
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-09-21 15:04:58 +0000

    logger cleanup.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10543 0c594bb0-8543-0410-a25e-d8ec440261db

--- AutoplotServlet/src/java/org/virbo/autoplot/ShutdownListener.java
+++ AutoplotServlet/src/java/org/virbo/autoplot/ShutdownListener.java
@@ -5,6 +5,7 @@
 
 package org.virbo.autoplot;
 
+import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import org.das2.system.RequestProcessor;
@@ -15,12 +16,14 @@ import org.das2.system.RequestProcessor;
  */
 public class ShutdownListener implements ServletContextListener {
 
+    private static final Logger logger= Logger.getLogger("autoplot.servlet" );
+
     public void contextInitialized(ServletContextEvent sce) {
-        System.err.println("startup !!!");
+        logger.fine("startup includes das2 RequestProcessor.");
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
-        System.err.println("shutdown !!!");
+        logger.fine("shutdown message received, shutting down das2 RequestProcessor.");
         RequestProcessor.shutdown();
     }
 
