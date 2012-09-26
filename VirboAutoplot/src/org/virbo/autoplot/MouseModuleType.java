25a34f87b79da85efed07aa3b5aade19c3e233ea
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-09-26 20:54:23 +0000

    add mouseModuleType to user preferences.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10620 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/virbo/autoplot/MouseModuleType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.autoplot;
+
+/**
+ * Enumeration to store the current mouse module preference.
+ * @author jbf
+ */
+public enum MouseModuleType {
+    crosshairDigitizer,
+    zoomX,
+    boxZoom,   
+}
