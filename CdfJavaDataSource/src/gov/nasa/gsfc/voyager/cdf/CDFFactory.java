00106b8802fade9f82640ef278e018fc5537382c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-10-12 21:14:48 +0000

    clone ByteBuffer using allocateDirect, which might be able to utilize memory outside of Java's space.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@10783 0c594bb0-8543-0410-a25e-d8ec440261db

--- CdfJavaDataSource/src/gov/nasa/gsfc/voyager/cdf/CDFFactory.java
+++ CdfJavaDataSource/src/gov/nasa/gsfc/voyager/cdf/CDFFactory.java
@@ -81,7 +81,7 @@ public class CDFFactory {
      * @return
      */
     public static ByteBuffer clone(ByteBuffer original) {
-       ByteBuffer clone = ByteBuffer.allocate(original.capacity());
+       ByteBuffer clone = ByteBuffer.allocateDirect(original.capacity());
        original.rewind();//copy from the beginning
        clone.put(original);
        original.rewind();
