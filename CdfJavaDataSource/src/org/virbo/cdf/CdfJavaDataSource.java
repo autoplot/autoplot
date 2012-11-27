d3cd14618474944278e8fe89283f809f021dc801
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-11-27 18:39:07 +0000

    3590417: kludge for rbspa_pre_ect-mageis-L2_20121031_v1.0.0.cdf?FEDO
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11014 0c594bb0-8543-0410-a25e-d8ec440261db

--- CdfJavaDataSource/src/org/virbo/cdf/CdfJavaDataSource.java
+++ CdfJavaDataSource/src/org/virbo/cdf/CdfJavaDataSource.java
@@ -669,6 +669,12 @@ public class CdfJavaDataSource extends AbstractDataSource {
                 }
 
                 if ( lablDs!=null && ( depDs==null || depDs.rank()==2 || depDs.rank()==1 && depDs.length()<100 ) ) { // Reiner has a file where DEPEND_1 is defined, but is just 0,1,2,3,...
+                        // kludge for Seth's file file:///home/jbf/ct/lanl/data.backup/seth/rbspa_pre_ect-mageis-L2_20121031_v1.0.0.cdf?FEDO
+                        if ( depDs.rank()==2 && DataSetUtil.asDatum(lablDs.slice(0)).toString().equals("channel00") ) {
+                            if ( depDs.value(0,0)==depDs.value(depDs.length()-1,0) ) {
+                                lablDs= (MutablePropertyDataSet)depDs.slice(0);
+                            }
+                        }
                         QDataSet bundleDs= lablDs;
                         result.putProperty( "BUNDLE_"+idep, DataSetUtil.toBundleDs(bundleDs) );
                 }
