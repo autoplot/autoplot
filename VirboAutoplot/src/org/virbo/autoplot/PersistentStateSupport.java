512036d06112ab2d0e31b6885ce581d1056c9f96
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2012-12-21 13:12:22 +0000

    bugfix 3597950: currentFile can be null, so check for this before checking content.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11119 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/PersistentStateSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/PersistentStateSupport.java
@@ -527,6 +527,7 @@ public class PersistentStateSupport {
      * @return
      */
     public String getCurrentFile() {
+        if ( currentFile==null ) return null;
         if ( currentFile.startsWith("file://") ) { 
             return currentFile.substring(7);
         } else if ( currentFile.startsWith("file:") ) {
