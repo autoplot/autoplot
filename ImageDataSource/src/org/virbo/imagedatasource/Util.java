9f4afad9d031e1919367ef8bf610552e65a03d9f
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-01-02 20:57:42 +0000

    add utility for parsing GPS locations
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11153 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ ImageDataSource/src/org/virbo/imagedatasource/Util.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.imagedatasource;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jbf
+ */
+public class Util {
+    /**
+     * returns 45.5 from "45°30'"
+     * @param s
+     * @return
+     */
+    public static double parseGPSString( String s ) {
+        double[] d= new double[4];
+        int i=0;
+        int oldi= i;
+        i= s.indexOf("°"); // degree symbol
+        if ( i>-1 ) {
+            d[0]= Double.parseDouble( s.substring(oldi,i) );
+            oldi=i+1;
+        }
+
+        i= s.indexOf("'"); // '
+        if ( i>-1 ) {
+            d[1]= Double.parseDouble( s.substring(oldi,i) );
+            oldi=i+1;
+        }
+        i= s.indexOf("\""); // "
+        if ( i>-1 ) {
+            d[2]= Double.parseDouble( s.substring(oldi,i) );
+        }
+
+        return d[0] + d[1]/60 + d[2]/(60*60);
+    }
+}
