a6c9a2fc72d50886da9a108634983262db7c33ec
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-01-31 18:42:17 +0000

    use get.cgi, not get3.cgi.  Bob's moved the server...
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11230 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test007.java
+++ VirboAutoplot/src/test/endtoend/Test007.java
@@ -59,7 +59,7 @@ public class Test007 {
             doTest( 0, "vap+tsds:http://timeseries.org/get.cgi?StartDate=19980101&EndDate=20090101&ppd=1&ext=bin&out=tsml&param1=NGDC_NOAA15_SEM2-33-v0" );
             doTest( 1, "vap+tsds:http://tsds.net/cgi-bin/get.cgi?StartDate=20000101&EndDate=20010101&ext=bin&out=tsml&ppd=24&filter=mean&param1=OMNI_OMNI2-41-v1");
             doTest( 2, "vap+tsds:http://tsds.net/cgi-bin/get.cgi?StartDate=19910101&EndDate=20041231&ext=bin&out=tsml&ppd=24&filter=mean&param1=Augsburg_ULF-6-v1" );
-            doTest( 3, "vap+tsds:http://timeseries.org/get3.cgi?StartDate=19930101&EndDate=20031231&ppd=1&ext=bin&out=ncml&param1=Kanekal_SAMPEX_elo_1hour-1-v0" );
+            doTest( 3, "vap+tsds:http://timeseries.org/get.cgi?StartDate=19930101&EndDate=20031231&ppd=1&ext=bin&out=ncml&param1=Kanekal_SAMPEX_elo_1hour-1-v0" );
 
             System.exit(0);  // TODO: something is firing up the event thread
         } catch ( Exception ex) {
