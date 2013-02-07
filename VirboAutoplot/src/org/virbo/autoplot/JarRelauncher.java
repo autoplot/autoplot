648d07024a33fc5d3addada3e029195d4b3f9798
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-02-07 13:26:26 +0000

    add JarRelauncher so JVM gets more memory than 256MB default when double-clicking a jar file.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11268 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/virbo/autoplot/JarRelauncher.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.autoplot;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * The only way to get an executable jar to have more memory is to have a little java process that
+ * launches a bigger one that has more memory available to it.
+ *
+ * http://stackoverflow.com/questions/12201878/specifying-jvm-heap-sizes-in-jar?rq=1
+ *
+ * This is also Ed West's recommendation.
+ * 
+ * @author jbf
+ */
+public class JarRelauncher {
+    public static void main( String[] args ) {
+        String myPath= System.getProperty("pwd"); //TODO: check this
+        if ( System.getProperty("os.family").equals("Windows") ) {
+            String javaPath = System.getProperty("java.home") + "\\bin\\java.exe";
+            try {
+                Runtime.getRuntime().exec("\\" + javaPath + " -Xmx1000M -jar " + myPath + "autoplot.jar ");
+            } catch (IOException ex) {
+                Logger.getLogger(JarRelauncher.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
