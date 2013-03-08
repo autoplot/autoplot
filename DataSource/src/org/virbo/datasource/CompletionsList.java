64630a33af76a63e6a8138257ad16bd02b4df0d6
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-03-08 19:17:07 +0000

    put in separator
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11475 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/CompletionsList.java
+++ DataSource/src/org/virbo/datasource/CompletionsList.java
@@ -5,14 +5,18 @@
 
 package org.virbo.datasource;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.util.List;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.JSeparator;
+import org.virbo.datasource.DataSetURI.CompletionResult;
 
 /**
  *
@@ -21,34 +25,44 @@ import javax.swing.JPopupMenu;
 public class CompletionsList {
     
     public interface CompletionListListener {
-        public void itemSelected( DataSetURI.CompletionResult item );
+        public void itemSelected( CompletionResult item );
     }
     
-    public static JPopupMenu fillPopupNew( final List<DataSetURI.CompletionResult> completions,
+    public static JPopupMenu fillPopupNew( final List<CompletionResult> completions,
             final String labelprefix, 
             JPopupMenu popupMenu, 
             final CompletionListListener listener ) {
 
         JMenu subMenu = null;
 
+        JComponent menuItem;
+        
         int i = 0;
         while (i < completions.size()) {
             int stopAt = Math.min(i + 30, completions.size());
             while (i < stopAt) {
-                final DataSetURI.CompletionResult s1 = completions.get(i);
-                String label = s1.label;
-                if (label.startsWith(labelprefix)) {
-                    label = label.substring(labelprefix.length());
-                }
-                Action a = new AbstractAction(label) {
-                    public void actionPerformed(ActionEvent ev) {
-                        listener.itemSelected(s1);
+                final CompletionResult s1 = completions.get(i);
+                if ( s1==CompletionResult.SEPARATOR ) {
+                    //menuItem= new javax.swing.JButton("HELLO");
+                    menuItem= new javax.swing.JSeparator();
+                    //menuItem.setMinimumSize( new Dimension(20,20) );  Linux suppresses these...
+                    
+                } else {
+                    String label = s1.label;
+                    if (label.startsWith(labelprefix)) {
+                        label = label.substring(labelprefix.length());
                     }
-                };
-                JMenuItem menuItem= new JMenuItem(a);
-                if ( s1.doc!=null ) menuItem.setToolTipText(s1.doc);
-                if ( !s1.label.endsWith("/") && s1.completion.contains("?") ) {
-                    if ( s1.maybePlot ) menuItem.setIcon( new javax.swing.ImageIcon( CompletionsList.class.getResource("/org/virbo/datasource/go-small.png")) );
+                    Action a = new AbstractAction(label) {
+                        public void actionPerformed(ActionEvent ev) {
+                            listener.itemSelected(s1);
+                        }
+                    };
+                    JMenuItem item= new JMenuItem(a);
+                    if ( s1.doc!=null ) item.setToolTipText(s1.doc);
+                    if ( !s1.label.endsWith("/") && s1.completion.contains("?") ) {
+                        if ( s1.maybePlot ) item.setIcon( new javax.swing.ImageIcon( CompletionsList.class.getResource("/org/virbo/datasource/go-small.png")) );
+                    }
+                    menuItem= item;
                 }
                 if (subMenu == null) {
                     popupMenu.add(menuItem);
