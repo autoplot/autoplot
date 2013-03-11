05d8f62a0554343988e11614258686028653eb97
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-03-11 22:48:35 +0000

    commit commented code that shows why we can't easily delegate to the existing completions.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11493 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonDataSource/src/org/autoplot/inline/InlineDataSourceFactory.java
+++ JythonDataSource/src/org/autoplot/inline/InlineDataSourceFactory.java
@@ -6,7 +6,10 @@
 package org.autoplot.inline;
 
 import java.net.URI;
+import java.util.List;
+import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.datasource.AbstractDataSourceFactory;
+import org.virbo.datasource.CompletionContext;
 import org.virbo.datasource.DataSource;
 
 /**
@@ -19,5 +22,25 @@ public class InlineDataSourceFactory extends AbstractDataSourceFactory {
     public DataSource getDataSource(URI uri) throws Exception {
         return new InlineDataSource( uri );
     }
+
+    @Override
+    public List<CompletionContext> getCompletions(CompletionContext cc, ProgressMonitor mon) throws Exception {
+//        PythonInterpreter interp = JythonUtil.createInterpreter(false);
+//        URL imports = JythonOps.class.getResource("imports.py");
+//        interp.execfile(imports.openStream());
+//        String frag= "ripple";
+//        org.das2.jythoncompletion.CompletionContext cc1= 
+//                CompletionSupport.getCompletionContext( "x="+frag, cc.completablepos+2, 0, 0, 0 );
+//        //String frag= cc.completable;
+//        //TODO: where to get CompletionResultSet?
+//        
+//        JythonCompletionTask.getLocalsCompletions( interp, cc1, null );
+//        PyStringMap locals= (PyStringMap) interp.getLocals();
+//        
+//        return super.getCompletions(cc, mon);
+        return super.getCompletions(cc, mon);
+    }
+
+
 
 }
