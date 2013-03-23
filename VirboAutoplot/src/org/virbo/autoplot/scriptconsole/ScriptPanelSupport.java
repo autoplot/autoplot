0864e99b9afcfdf6ffe07d018f86cb596a56c83e
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-03-23 19:29:11 +0000

    set the progress panel parent.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11552 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/scriptconsole/ScriptPanelSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/scriptconsole/ScriptPanelSupport.java
@@ -338,7 +338,7 @@ public class ScriptPanelSupport {
                     public void run() {
                         int offset = 0;
                         //ProgressMonitor mon= //DasProgressPanel.createComponentPanel(model.getCanvas(),"running script");
-                        ProgressMonitor mon= DasProgressPanel.createFramed("running script");
+                        ProgressMonitor mon= DasProgressPanel.createFramed( SwingUtilities.getWindowAncestor(panel), "running script");
                         
                         try {
                             if (file != null && ( file.exists() && file.canWrite() || file.getParentFile().canWrite() ) ) {
