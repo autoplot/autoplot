170e1151f4289a82ea0315d6fa46c2a34669bf0c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-04-18 17:43:04 +0000

    trivial fix imports
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11749 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/TimeSeriesBrowseController.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/TimeSeriesBrowseController.java
@@ -12,7 +12,6 @@ import java.beans.PropertyDescriptor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
