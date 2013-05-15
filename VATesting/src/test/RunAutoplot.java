79605acaffec8cb122b450189547cc340fc1ec8e
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-05-15 20:11:55 +0000

    add RunAutoplot as a quick and dirty way to get the profiler back.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11889 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VATesting/src/test/RunAutoplot.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import org.virbo.autoplot.AutoplotUI;
+
+/**
+ * Introduced because the VirboAutoplot project no longer supported profiling in
+ * Netbeans.
+ * @author jbf
+ */
+public class RunAutoplot {
+    public static void main( String[] args ) {
+        AutoplotUI.main(args);
+    }
+}
