4c91c964d32ece14abcf8dbb43a2000cb754afc8
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-05-18 13:36:02 +0000

    add fftPower test to test001.  start keeping track of times.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@11910 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test001.java
+++ VirboAutoplot/src/test/endtoend/Test001.java
@@ -6,10 +6,13 @@
 package test.endtoend;
 
 import java.io.IOException;
+import org.das2.util.monitor.NullProgressMonitor;
+import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QDataSet;
 import static org.virbo.dsops.Ops.*;
 import static org.virbo.autoplot.ScriptContext.*;
+import org.virbo.dsops.Ops;
 import org.virbo.jythonsupport.Util;
 
 /**
@@ -21,12 +24,30 @@ public class Test001 {
         try {
             getDocumentModel().getOptions().setAutolayout(false);
             getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
+            
+            long t0;
+            
+            System.err.println( "--- test001_001 ---" );
+            t0= System.currentTimeMillis();
             QDataSet ds= Util.getDataSet( "http://www.autoplot.org/data/fireworks.wav" );
             plot( 0, ds );
             plot( 1, fftWindow( ds, 512 ) );
             setCanvasSize( 800, 600 );
             writeToPng( "test001_001.png" );
-
+            System.err.println( String.format( "test001_001 completed in %d ms", System.currentTimeMillis()-t0 ) );
+            
+            System.err.println( "--- test001_004 ---" );
+            t0= System.currentTimeMillis();
+            ds= Util.getDataSet( "http://www.autoplot.org/data/fireworks.wav" );
+            plot( 0, ds );
+            plot( 1, fftPower( ds, Ops.windowFunction( FFTFilterType.Hanning, 1024 ), 2, new NullProgressMonitor() ) );
+            setCanvasSize( 800, 600 );
+            writeToPng( "test001_004.png" );
+            System.err.println( String.format( "test001_004 completed in %d ms", System.currentTimeMillis()-t0 ) );            
+                  
+            
+            System.err.println( "--- test001_002 ---" );
+            t0= System.currentTimeMillis();
             ds= Util.getDataSet( TestSupport.TEST_DATA+"xls/2008-lion and tiger summary.xls?sheet=Samantha+tiger+lp+lofreq&firstRow=53&column=Complex_Modulus&depend0=Frequency" );
             plot( 0, "xaxis should be log", ds );
 
@@ -41,12 +62,15 @@ public class Test001 {
 
             setCanvasSize( 800, 600 );
             writeToPng( "test001_002.png" );
+            System.err.println( String.format( "test001_002 completed in %d ms", System.currentTimeMillis()-t0 ) );            
 
             reset();
+            System.err.println( "--- test001_003 ---" );
             plot( TestSupport.TEST_DATA+"xls/2008-lion and tiger summary.xls?sheet=Samantha+tiger+lp+lofreq&firstRow=53&column=Complex_Modulus&depend0=Frequency" );
             // this causes bad things as of 2009-08-12.
             plot( TestSupport.TEST_DATA+"qds/hist2.qds" );
             writeToPng( "test001_003.png" );
+            System.err.println( String.format( "test001_003 completed in %d ms", System.currentTimeMillis()-t0 ) );
 
             System.exit(0);  // TODO: something is firing up the event thread
         } catch ( Exception ex ) {
