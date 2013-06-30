973271066bb191b19e21b3d094f10fe395e0efaa
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-06-30 16:46:37 +0000

    handle aggregations.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12154 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/state/EmbedDataExperiment.java
+++ VirboAutoplot/src/org/virbo/autoplot/state/EmbedDataExperiment.java
@@ -10,10 +10,20 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+import org.das2.util.filesystem.FileSystem;
 import org.das2.util.monitor.NullProgressMonitor;
 import org.virbo.autoplot.dom.Application;
 import org.virbo.autoplot.dom.DataSourceFilter;
@@ -53,7 +63,47 @@ public class EmbedDataExperiment {
         public void write(int b) throws IOException {
             out.write(b);
         }
-        
+
+        @Override
+        public void write(byte[] b, int off, int len) throws IOException {
+            out.write(b, off, len);
+        }
+    }
+    
+    /**
+     * return a list of all the resources used in the DOM.
+     * @param dom
+     * @return 
+     */
+    private static Set<URI> getResources( Application dom ) {
+        Set<URI> result= new HashSet();
+        for ( DataSourceFilter dsf: dom.getDataSourceFilters() ) {
+            String uri = dsf.getUri();
+            URISplit split= URISplit.parse(uri);
+            if ( split.resourceUri!=null ) {
+                if ( DataSetURI.isAggregating(split.resourceUri.toString()) ) {
+                    try {
+                        String [] rr= DataSetURI.unaggregate( split.resourceUri.toASCIIString(), dom.getTimeRange() );
+                        for ( String r: rr ) {
+                            try {
+                                result.add( new URI( r ) );
+                            } catch (URISyntaxException ex) {
+                                Logger.getLogger(EmbedDataExperiment.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                    } catch (FileSystem.FileSystemOfflineException ex) {
+                        Logger.getLogger(EmbedDataExperiment.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (UnknownHostException ex) {
+                        Logger.getLogger(EmbedDataExperiment.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IOException ex) {
+                        Logger.getLogger(EmbedDataExperiment.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } else {
+                    result.add( split.resourceUri );
+                }
+            }
+        }
+        return result;
     }
     
     public static void save( Application dom3, File f ) throws FileNotFoundException, IOException {
@@ -62,15 +112,18 @@ public class EmbedDataExperiment {
 
         FileOutputStream fout= new FileOutputStream(f);
         final ZipOutputStream out = new ZipOutputStream( fout );
+        for ( URI uri: getResources(dom) ) {
+            String name= uri.toString().replaceAll("://","/");
+            File file1= DataSetURI.getFile(uri,new NullProgressMonitor());
+            writeToZip( out, name, file1 );
+        }
         for ( DataSourceFilter dsf: dom.getDataSourceFilters() ) {
             String uri = dsf.getUri();
             URISplit split= URISplit.parse(uri);
             if ( split.resourceUri!=null ) {
-                //TODO: aggregations...
                 String name= split.resourceUri.toString().replaceAll("://","/");
-                File file1= DataSetURI.getFile(split.resourceUri,new NullProgressMonitor());
-                writeToZip( out, name, file1 );
-                dsf.setUri("%{PWD}/"+name+"?"+split.params);
+                split.file= "%{PWD}/"+name;
+                dsf.setUri( URISplit.format(split) );
             }
         }
         ZipEntry e= new ZipEntry("default.vap");
