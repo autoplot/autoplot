0edd04e78720f9d5139bd26f42725aea17db00ba
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-07-02 12:05:18 +0000

    javadoc
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12159 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/ChangesSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/ChangesSupport.java
@@ -147,8 +147,10 @@ public final class ChangesSupport {
     public static final String PROP_VALUEADJUSTING = "valueAdjusting";
 
     /**
-     * the bean state is rapidly changing.
-     * @return
+     * the bean state is rapidly changing.  This
+     * returns the lock message, or null if the value
+     * is not adjusting.
+     * @return null or a message indicating that the value is adjusting.
      */
     public String isValueAdjusting() {
         return valueIsAdjusting;
