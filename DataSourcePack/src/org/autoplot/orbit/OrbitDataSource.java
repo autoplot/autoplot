316fb4966be6481614d790c6141ae3f0ef206e06
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-07-02 14:35:44 +0000

    add TSB
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12168 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/autoplot/orbit/OrbitDataSource.java
+++ DataSourcePack/src/org/autoplot/orbit/OrbitDataSource.java
@@ -8,13 +8,14 @@ import java.net.URI;
 import java.text.ParseException;
 import java.util.logging.Level;
 import org.das2.datum.DatumRange;
-import org.das2.datum.DatumRangeUtil;
 import org.das2.datum.Orbits;
 import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QDataSet;
 import org.virbo.datasource.AbstractDataSource;
+import org.virbo.datasource.DefaultTimeSeriesBrowse;
 import org.virbo.datasource.URISplit;
+import org.virbo.datasource.capability.TimeSeriesBrowse;
 import org.virbo.dsops.Ops;
 
 /**
@@ -24,24 +25,27 @@ import org.virbo.dsops.Ops;
 class OrbitDataSource extends AbstractDataSource {
 
     String sc;
-    DatumRange tr;
+    TimeSeriesBrowse tsb;
     
     public OrbitDataSource(URI uri) {
         super(uri);
+        tsb= new DefaultTimeSeriesBrowse();
         sc= params.get( URISplit.PARAM_ARG_0 );
         String str= params.get( URISplit.PARAM_TIME_RANGE );
         if ( str!=null ) {
             try {
-                tr= DatumRangeUtil.parseTimeRange(str);
+                tsb.setURI(uri.toASCIIString());
             } catch (ParseException ex) {
                 logger.log(Level.SEVERE, null, ex);
             }
         }
+        addCability(TimeSeriesBrowse.class, tsb );
     }
 
     @Override
     public QDataSet getDataSet(ProgressMonitor mon) throws Exception {
         Orbits o= Orbits.getOrbitsFor(sc);
+        DatumRange tr= tsb.getTimeRange();
         String s= o.getOrbit(tr.min());
         
         QDataSet result= null;
