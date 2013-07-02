a36e2ec70dd0c5f083f8117706a8f32eefb27bad
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-07-02 14:37:04 +0000

    add TSB
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12169 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/autoplot/orbit/OrbitDataSourceFactory.java
+++ DataSourcePack/src/org/autoplot/orbit/OrbitDataSourceFactory.java
@@ -6,11 +6,17 @@ package org.autoplot.orbit;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.datasource.AbstractDataSourceFactory;
 import org.virbo.datasource.CompletionContext;
 import org.virbo.datasource.DataSource;
+import org.virbo.datasource.DefaultTimeSeriesBrowse;
+import org.virbo.datasource.URISplit;
+import org.virbo.datasource.capability.TimeSeriesBrowse;
 
 /**
  * Orbit data sources added for demonstration purposes, but it will also be useful.
@@ -26,17 +32,51 @@ public class OrbitDataSourceFactory extends AbstractDataSourceFactory {
     @Override
     public List<CompletionContext> getCompletions(CompletionContext cc, ProgressMonitor mon) throws Exception {
         if ( cc.context==CompletionContext.CONTEXT_PARAMETER_NAME ) {
-            List<CompletionContext> result= new ArrayList<CompletionContext>();
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "rbspa-pp", "RBSP-A (Van Allen Probe A)") );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "rbspb-pp", "RBSP-B (Van Allen Probe B)") );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "crres", "CRRES" ) );
-            result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "cassini", "Cassini Spacecraft" ) );
-            return result;
+            
+            List<CompletionContext> ccresult= new ArrayList<CompletionContext>();
+            
+            Map<String,String> names= new LinkedHashMap();
+            names.put( "rbspa-pp",  "RBSP-A (Van Allen Probe A)" );
+            names.put( "rbspb-pp",  "RBSP-B (Van Allen Probe B)" );
+            names.put( "crres",     "CRRES" );
+            names.put( "cassini",   "Cassini Spacecraft" );
+            for ( Entry<String,String> n : names.entrySet() ) {
+                CompletionContext cc1= new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, 
+                        n.getKey(), this, "arg_0", n.getValue(), null, true );
+                ccresult.add(cc1);
+            }
+            return ccresult;
         } else {
-            return super.getCompletions(cc, mon); //To change body of generated methods, choose Tools | Templates.
+            return super.getCompletions(cc, mon); 
         }
         
     }
+
+    @Override
+    public <T> T getCapability(Class<T> clazz) {
+        if ( clazz==TimeSeriesBrowse.class ) {
+            return (T) new DefaultTimeSeriesBrowse();
+        } else {
+            return null;
+        }
+        
+    }
+
+    @Override
+    public boolean reject(String surl, List<String> problems, ProgressMonitor mon) {
+        URISplit split = URISplit.parse(surl);
+        Map map = URISplit.parseParams(split.params);
+        if ( DefaultTimeSeriesBrowse.reject( map, problems) ) {
+            return true;
+        } else {
+            if ( !map.containsKey( URISplit.PARAM_ARG_0 ) ) {
+                problems.add( "no spacecraft" );
+                return true;
+            }
+        }
+        return super.reject(surl, problems, mon); //To change body of generated methods, choose Tools | Templates.
+    }
+    
     
     
 }
