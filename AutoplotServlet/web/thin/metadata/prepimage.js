82af824001aad2ef2736d8143febe8be2cbeb456
rweigel <rweigel@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-07-07 14:30:59 +0000

    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12202 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ AutoplotServlet/web/thin/metadata/prepimage.js
@@ -0,0 +1,21 @@
+function prepimage(id) {
+	
+	PLOTINFO[id] = {};
+	PLOTINFO[id].src = $("#"+id).attr("src"); 
+	
+	$('#'+id)
+		.attr('src',PLOTINFO[id].src)
+		.load(function () {
+			id = $(this).attr("id"); // ID of loaded image.
+			// Load the data
+			ImageInfo.loadInfo(PLOTINFO[id].src, mycallback);
+
+			// Callback function for when metadata extracted
+			function mycallback() {
+				splotInfo = ImageInfo.getField(PLOTINFO[id].src, "data")['plotInfo'];
+				PLOTINFO[id] = $.parseJSON(splotInfo);
+				// When image is loaded and metadata extracted, set onclick attribute.
+				$("#"+id).attr("onclick","cross(event)");
+			}
+		})
+}
