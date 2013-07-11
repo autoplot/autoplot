5ca89f208deed53177416f9e781367f66921c109
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-07-11 13:43:27 +0000

    add test to lock in the Ops.dataset function.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12237 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test029.java
+++ VirboAutoplot/src/test/endtoend/Test029.java
@@ -6,10 +6,17 @@ package test.endtoend;
 
 import java.io.IOException;
 import java.io.InputStream;
+import org.das2.datum.DatumRangeUtil;
+import org.das2.datum.TimeUtil;
+import org.das2.datum.Units;
 import org.python.core.PyException;
 import org.python.core.PyObject;
 import org.python.core.PyReflectedFunction;
 import org.python.util.InteractiveInterpreter;
+import org.virbo.dataset.DDataSet;
+import org.virbo.dataset.DataSetUtil;
+import org.virbo.dataset.QDataSet;
+import org.virbo.dsops.Ops;
 import org.virbo.jythonsupport.JythonUtil;
 
 /**
@@ -25,7 +32,13 @@ import org.virbo.jythonsupport.JythonUtil;
  */
 public class Test029 {
     public static void main(String[] args) throws IOException  {
-        int status= testBuiltIns();
+        
+        int status;
+        
+        status= testStringToDataSet();
+        if ( status!=0 )             System.exit(status);
+            
+        status= testBuiltIns();
         if ( status!=0 )             System.exit(status);
 
         status= testJythonLib();
@@ -92,5 +105,74 @@ public class Test029 {
             }
         }
         return 0;
+    }
+    
+    private static int equiv( QDataSet norm, QDataSet test ) {
+        return Ops.equivalent( norm, test ) ? 0 : 1;
+    }
+    
+    /**
+     * Test the codes that convert strings into DataSets, to serve as documentation for this
+     * central feature.  This is JSON, but taken dangerously further.  In JSON, 
+     * 0 is an integer, 0.0 is a float, '0.0' is a string, and ['0.0'] is an array.  Here we
+     * tread out into deep water where '2014-01-01T00:00' is a time datum and 
+     * '2014-01-01T00:00/2014-01-04T00:00' is a datum range.  We have a
+     * system that supports ordinal data, so when is the string now datum from this
+     * ordinal set?
+     *   int, float, double, etc to Rank 0 datasets
+     *   List&lt;Number&gt; to Rank 1 datasets.
+     *   Java arrays of Number to Rank 1-4 qubes datasets
+     *   Strings to rank 0 datasets with units ("5 s" "2014-01-01T00:00")
+     *   Strings to rank 1 bins ('2014-01-01T00:00/2014-01-04T00:00')
+     *   Datums to rank 0 datasets
+     *   DatumRanges to rank 1 bins
+     * Other things to watch out for:
+     *   ':'  Python slice 
+     *   '1996:1998'  Python slice
+     *   '2014'  Is this a year long time range, integer, or ISO8601 date?  (It's an integer.)
+     *   '2014/2016'  Is this an ISO8601 time range, or the ratio of two numbers?  (There was a das2 code that allowed simple expressions).
+     *   '5 to 40 kg' is not supported (presently), because this is completely new water...
+     * @return 
+     */
+    private static int testStringToDataSet() {
+        QDataSet r0= DataSetUtil.asDataSet( 0, Units.dimensionless );
+        QDataSet r1= Ops.indgen(4);
+        QDataSet t1= DataSetUtil.asDataSet( TimeUtil.toDatum( new int[] { 2014,1,1,0,0,0,0 } ) );
+        QDataSet r0_2014= DataSetUtil.asDataSet( 2014 );
+        QDataSet r0_2014_s= DataSetUtil.asDataSet( 2014, Units.seconds );
+        QDataSet tr1= DataSetUtil.asDataSet( DatumRangeUtil.parseISO8601Range("2014-01-01T00:00/2014-01-04T00:00") );
+        
+        int r;
+        
+        r= equiv( r0, Ops.dataset("0") );
+        if ( r!=0 ) throw new IllegalStateException("failed to make dataset: 0");
+
+        r= equiv( t1, Ops.dataset("2014-01-01T00:00" ) );
+        if ( r!=0 ) throw new IllegalStateException("failed to make dataset: time");
+
+        r= equiv( tr1, Ops.dataset("2014-01-01T00:00/2014-01-04T00:00" ) );
+        if ( r!=0 ) throw new IllegalStateException("failed to make dataset: time range");
+
+        r= equiv( tr1, Ops.dataset("2014-01-01T00:00/P3D" ) );
+        if ( r!=0 ) throw new IllegalStateException("failed to make dataset: time range");
+
+        r= equiv( tr1, Ops.dataset("P3D/2014-01-04T00:00" ) );
+        if ( r!=0 ) throw new IllegalStateException("failed to make dataset: time range");
+
+        r= equiv( r0_2014, Ops.dataset("2014" ) );
+        if ( r!=0 ) throw new IllegalStateException("failed to make dataset: datum");
+
+        r= equiv( r0_2014, Ops.dataset("2014." ) );
+        if ( r!=0 ) throw new IllegalStateException("failed to make dataset: datum with decimal");
+
+        r= equiv( r0_2014_s, Ops.dataset("2014s" ) );
+        if ( r!=0 ) throw new IllegalStateException("failed to make dataset: datum with units");
+        
+        r= equiv( r1, Ops.dataset( new int[] { 0,1,2,3 } ) );
+        if ( r!=0 ) throw new IllegalStateException("failed to make dataset: array");
+        
+        
+        return 0;
+        
     }
 }
