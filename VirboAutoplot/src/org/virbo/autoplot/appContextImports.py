97c9c4f68f6b7d8e673a407ed924a92be022a9ab
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-07-19 22:58:28 +0000

    simply import everything from autoplotapp.py. NOTE: There was a bunch of stuff imported that probably shouldn't have.  I'm taking that out, because there's no justifying their use.  Codes will have to import Color , JFrames, etc...
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12319 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/appContextImports.py
+++ VirboAutoplot/src/org/virbo/autoplot/appContextImports.py
@@ -1,10 +1,10 @@
-from org.das2 import DasApplication
+## why was this ever imported: from org.das2 import DasApplication
 
-from java.awt import Color
-from javax.swing import JFrame, JComponent, JLayeredPane
+## why was this ever imported: from java.awt import Color
+## why was this ever imported: from javax.swing import JFrame, JComponent, JLayeredPane
 
 #print 'import stuff for autoplot'
-from org.virbo.autoplot.ScriptContext import *
-from org.virbo.autoplot import *
+from autoplotapp import *
+## why was this ever imported: from org.virbo.autoplot import *
 
 
