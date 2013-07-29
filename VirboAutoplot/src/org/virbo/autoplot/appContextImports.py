b9abab6b0aff27aa1a72603712f246a854cca61a
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-07-29 21:16:53 +0000

    put the import for Color back in, so you could say Color.RED.  This is done a lot and is harmless.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12349 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/appContextImports.py
+++ VirboAutoplot/src/org/virbo/autoplot/appContextImports.py
@@ -1,10 +1,10 @@
 ## why was this ever imported: from org.das2 import DasApplication
-
-## why was this ever imported: from java.awt import Color
 ## why was this ever imported: from javax.swing import JFrame, JComponent, JLayeredPane
+## why was this ever imported: from org.virbo.autoplot import *
+
+from java.awt import Color   # this is used a lot, and seems harmless...
 
 #print 'import stuff for autoplot'
 from autoplotapp import *
-## why was this ever imported: from org.virbo.autoplot import *
 
 
