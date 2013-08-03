d0b74b53a7dedf4b4c62b45662026043e5e2894a
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-08-03 11:31:56 +0000

    getMessage(Exception) introduced after http://www.rbsp-ect.lanl.gov/data_pub/rbspa/hope/level2/rbspa_pre_ect-hope-sci-L2_$Y$m$d_v$(v,sep).cdf?FPDU&timerange=2012-11-02 was difficult to debug.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12384 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/DataSourceUtil.java
+++ DataSource/src/org/virbo/datasource/DataSourceUtil.java
@@ -764,6 +764,25 @@ public class DataSourceUtil {
         
         return name;
     }
+    
+    /**
+     * return a one-line string representation of the exception.  This was introduced
+     * when a NullPointerException was represented as "null", and it was somewhat
+     * unclear about what was going on.
+     * @param ex
+     * @return 
+     */
+    public static String getMessage( Exception ex ) {
+        if ( ex instanceof NullPointerException ) {
+            return ex.toString();
+        } else {
+            if ( ex.getMessage().length()<5 ) {
+                return ex.toString() + ": " +ex.getMessage();
+            } else {
+                return ex.getMessage();
+            }
+        }
+    }
 
     public static void main(String[] args ) {
         String surl= "http://cdaweb.gsfc.nasa.gov/istp_public/data/polar/hyd_h0/2000/po_h0_hyd_20000109_v01.cdf?ELECTRON_DIFFERENTIAL_ENERGY_FLUX";
