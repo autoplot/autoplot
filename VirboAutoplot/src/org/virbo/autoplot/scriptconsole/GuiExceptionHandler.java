0523ce05c586c2facc05c13e61f6b653fc1dbd85
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-08-06 12:19:35 +0000

    choices[4] (usually "Submit") should be the default.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12400 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/scriptconsole/GuiExceptionHandler.java
+++ VirboAutoplot/src/org/virbo/autoplot/scriptconsole/GuiExceptionHandler.java
@@ -764,7 +764,7 @@ public final class GuiExceptionHandler implements ExceptionHandler {
             Component parent= appModel==null ? null : SwingUtilities.getWindowAncestor(appModel.getCanvas());
             Icon icon= new ImageIcon( AutoplotUtil.getAutoplotIcon() );
             int option= JOptionPane.showOptionDialog( parent, form, "Submit Exception Report",
-                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, icon, choices, choices[3] )  ;
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, icon, choices, choices[4] )  ;
             if ( option==2 ) {
                 return;
                 
