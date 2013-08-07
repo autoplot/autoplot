e626c842c808d19b50da0d76c731635ff6def27a
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-08-07 15:42:09 +0000

    remove unused imports (one of which caused a warning during compile).
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12430 0c594bb0-8543-0410-a25e-d8ec440261db

--- CDAWebDataSource/src/org/autoplot/cdaweb/CDAWebDB.java
+++ CDAWebDataSource/src/org/autoplot/cdaweb/CDAWebDB.java
@@ -5,19 +5,13 @@
 
 package org.autoplot.cdaweb;
 
-import com.sun.org.apache.xml.internal.serialize.OutputFormat;
-import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
