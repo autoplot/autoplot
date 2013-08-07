53723cf1b41456cea076e37bb9df29551b2ffacf
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-08-07 11:47:08 +0000

    add -q to wget calls in compile_application.sh
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12420 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/compile-application.sh
+++ VirboAutoplot/compile-application.sh
@@ -80,9 +80,9 @@ mkdir temp-volatile-src/
 rm -r -f temp-volatile-classes/
 mkdir temp-volatile-classes
 
-echo "copy jar file classes..."
-${WGET} -O AutoplotStable.jar ${HUDSON_URL}/job/autoplot-jar-stable/lastSuccessfulBuild/artifact/autoplot/VirboAutoplot/dist/AutoplotStable.jar # 2>&1 | head -100
-${WGET} -O AutoplotStable.jar.pack.gz ${HUDSON_URL}/job/autoplot-jar-stable/lastSuccessfulBuild/artifact/autoplot/VirboAutoplot/dist/AutoplotStable.jar.pack.gz # 2>&1 | head -100
+echo "copy jar file classes using wget -q..."
+${WGET} -q -O AutoplotStable.jar ${HUDSON_URL}/job/autoplot-jar-stable/lastSuccessfulBuild/artifact/autoplot/VirboAutoplot/dist/AutoplotStable.jar # 2>&1 | head -100
+${WGET} -q -O AutoplotStable.jar.pack.gz ${HUDSON_URL}/job/autoplot-jar-stable/lastSuccessfulBuild/artifact/autoplot/VirboAutoplot/dist/AutoplotStable.jar.pack.gz # 2>&1 | head -100
 #TODO: check exit status, and stop if it fails.
 if [ $? -ne 0 ]; then
    echo "wget fails: $WGET -O AutoplotStable.jar ${HUDSON_URL}/job/autoplot-jar-stable/lastSuccessfulBuild/artifact/autoplot/VirboAutoplot/dist/AutoplotStable.jar"
