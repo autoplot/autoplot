2e73e22fce94c45df8796d134f90ff586a80f37b
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-08-12 13:50:33 +0000

    add support for curl.  add AP_CURL and AP_WGET environment variables.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12473 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/autoplot/wgetfs/WGetFileSystemFactory.java
+++ DataSource/src/org/autoplot/wgetfs/WGetFileSystemFactory.java
@@ -11,13 +11,47 @@ import org.das2.util.filesystem.FileSystem;
 import org.das2.util.filesystem.FileSystemFactory;
 
 /**
- * Implemented to relieve all the annoying http problems we see at LANL.
+ * Implemented to relieve all the annoying http problems we see at LANL.  If
+ * the system is a Mac, then curl is used, otherwise wget is used.  Either
+ * way, if the system environment property AP_WGET, WGET, AP_CURL, CURL is set
+ * then the implied executable is used.
+ * 
  * @author jbf
  */
 public class WGetFileSystemFactory implements FileSystemFactory {
 
+    /**
+     * support curl too, since it is on macs by default.
+     */
+    protected static boolean useCurl= true;
+    
+    /**
+     * the executable to spawn.  See createFileSystem for use of AP_WGET or AP_CURL environment variables.
+     */
+    protected static String exe=null;
+    
     @Override
     public FileSystem createFileSystem(URI root) throws FileSystem.FileSystemOfflineException, UnknownHostException, FileNotFoundException {
+        synchronized ( FileSystem.class ) {
+            if ( exe==null ) {
+                useCurl= false;
+                exe= System.getProperty("AP_WGET");
+                if ( exe==null || exe.length()==0 ) {
+                    exe= System.getProperty("AP_CURL");
+                    if ( exe!=null && exe.length()>0 ) useCurl= true;
+                } 
+                if ( exe==null || exe.length()==0 ) {
+                    if ( System.getProperty("os.name").startsWith("Mac") ) {
+                        useCurl= true;
+                        exe= "curl";
+                    } else {
+                        useCurl= false;
+                        exe= "wget";
+                    }
+                }
+            }
+        }
+        
         return WGetFileSystem.createWGetFileSystem(root);
     }
     
