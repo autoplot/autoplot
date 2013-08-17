718ed5e4be44007ad76696da80c7c89c6682c091
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-08-17 12:13:07 +0000

    add popup menu items for named character references and unicode.  There was no way to discover this functionality.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12491 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/AxisPanel.java
+++ VirboAutoplot/src/org/virbo/autoplot/AxisPanel.java
@@ -587,6 +587,8 @@ public class AxisPanel extends javax.swing.JPanel {
         processMenu= new JPopupMenu();
         processMenu.add( createMenuItem( tf, "%{CONTEXT}", "Context from the dataset, such as slice location or component" ) );
         processMenu.add( createMenuItem( tf, "!c", "Line Break", "Insert new line escape character" ) );
+        processMenu.add( createMenuItem( tf, "&epsilon;", "Insert named character reference for epsilon symbol") );
+        processMenu.add( createMenuItem( tf, "&#0229;", "Insert unicode symbol") );        
         return processMenu;
     }
 
