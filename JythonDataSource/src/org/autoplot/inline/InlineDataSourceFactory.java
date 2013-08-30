af61187f756aa78ab7853544bdda45da2e2ca043
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-08-30 17:42:36 +0000

    finally allow for completions.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12658 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonDataSource/src/org/autoplot/inline/InlineDataSourceFactory.java
+++ JythonDataSource/src/org/autoplot/inline/InlineDataSourceFactory.java
@@ -6,11 +6,19 @@
 package org.autoplot.inline;
 
 import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import org.das2.jythoncompletion.CompletionSupport;
+import org.das2.jythoncompletion.DefaultCompletionItem;
+import org.das2.jythoncompletion.JythonCompletionTask;
 import org.das2.util.monitor.ProgressMonitor;
+import org.python.util.PythonInterpreter;
 import org.virbo.datasource.AbstractDataSourceFactory;
 import org.virbo.datasource.CompletionContext;
 import org.virbo.datasource.DataSource;
+import org.virbo.jythonsupport.JythonOps;
+import org.virbo.jythonsupport.JythonUtil;
 
 /**
  *
@@ -25,20 +33,30 @@ public class InlineDataSourceFactory extends AbstractDataSourceFactory {
 
     @Override
     public List<CompletionContext> getCompletions(CompletionContext cc, ProgressMonitor mon) throws Exception {
-//        PythonInterpreter interp = JythonUtil.createInterpreter(false);
-//        URL imports = JythonOps.class.getResource("imports.py");
-//        interp.execfile(imports.openStream());
-//        String frag= "ripple";
-//        org.das2.jythoncompletion.CompletionContext cc1= 
-//                CompletionSupport.getCompletionContext( "x="+frag, cc.completablepos+2, 0, 0, 0 );
-//        //String frag= cc.completable;
-//        //TODO: where to get CompletionResultSet?
-//        
-//        JythonCompletionTask.getLocalsCompletions( interp, cc1, null );
-//        PyStringMap locals= (PyStringMap) interp.getLocals();
-//        
-//        return super.getCompletions(cc, mon);
-        return super.getCompletions(cc, mon);
+        List<CompletionContext> result= new ArrayList();
+        if ( cc.context==CompletionContext.CONTEXT_PARAMETER_NAME ) {
+            if ( cc.completable.length()==0 ) {
+                result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "", "enter at least one character" ) );
+            } else {
+                PythonInterpreter interp = JythonUtil.createInterpreter(false);
+                URL imports = JythonOps.class.getResource("imports.py");
+                interp.execfile(imports.openStream());
+                String frag= cc.completable;
+                org.das2.jythoncompletion.CompletionContext cc1= CompletionSupport.getCompletionContext( "x="+frag, cc.completablepos+2, 0, 0, 0 );        
+                List<DefaultCompletionItem> r=  JythonCompletionTask.getLocalsCompletions( interp, cc1 );
+                
+                for ( DefaultCompletionItem item: r ) {
+                    result.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, item.getComplete() ) );
+                }   
+            }
+        }
+        
+        return result;
+    }
+
+    @Override
+    public boolean reject(String surl, List<String> problems, ProgressMonitor mon) {
+        return super.reject(surl, problems, mon); //To change body of generated methods, choose Tools | Templates.
     }
 
 
