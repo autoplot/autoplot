352ac03b57cabc4f2a494d5734d2fd3db3ffc111
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-09-01 12:33:37 +0000

    rename ScriptServlet2 to SecureScriptServlet, to use a more descriptive name.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12697 0c594bb0-8543-0410-a25e-d8ec440261db

--- AutoplotServlet/src/java/org/virbo/autoplot/SecureScriptServlet.java
+++ AutoplotServlet/src/java/org/virbo/autoplot/SecureScriptServlet.java
@@ -23,13 +23,18 @@ import org.python.util.PythonInterpreter;
 /**
  * This provides a generic method for adding a function to the server via Jython scripts.  Introduced
  * to support wildcard-de-globing, this could be used for a number of different ways.
+ * 
+ * Unlike the ScriptServlet, this requires a reference to a script that already exists on the server, instead of 
+ * running possibly malicious updated code.
  *
  * http://localhost:8084/AutoplotServlet/ScriptServlet2?resourceURI=http://autoplot.org/data/versioning/data_$Y_$m_$d_v$v.qds&timerange=2010-03
  *
  * @author jbf
  */
-public class ScriptServlet2 extends HttpServlet {
+public class SecureScriptServlet extends HttpServlet {
    
+    private static final Logger logger= Logger.getLogger("autoplot.servlet");
+    
     private static String maybeQuoteString(String sval) {
         boolean isNumber= false;
         try {
