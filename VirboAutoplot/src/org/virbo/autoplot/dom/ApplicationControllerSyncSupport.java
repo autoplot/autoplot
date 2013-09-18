9f33f16dc5edcc42eefdf4699ff64404e7bb7227
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-09-18 14:50:00 +0000

    take out "I wonder if this branch is ever used" message.  It is, and now it seems obvious that it would be.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12886 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/ApplicationControllerSyncSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/ApplicationControllerSyncSupport.java
@@ -110,7 +110,6 @@ public class ApplicationControllerSyncSupport {
 
         for ( Plot p: application.getPlots() ) {
             if ( p.controller==null ) {
-                logger.info("I wonder if this branch is ever used...");
                 Row row;
                 if ( p.getRowId().equals("") ) {
                     row= application.controller.getCanvas().marginRow;
@@ -136,7 +135,6 @@ public class ApplicationControllerSyncSupport {
                 if ( SwingUtilities.isEventDispatchThread() ) {
                     run.run();
                 } else {
-                    logger.info("I wonder if this branch is ever used...");
                     run.run();
                     //try {
                     //    SwingUtilities.invokeAndWait(run);
