462908a75bd4b27dd67c4d63ab4e052bdf61d1b8
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-09-19 12:25:27 +0000

    add test038 to test components of jython scripts.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12896 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test038.java
+++ VirboAutoplot/src/test/endtoend/Test038.java
@@ -9,7 +9,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.python.util.PythonInterpreter;
 import org.virbo.jythonsupport.JythonUtil;
