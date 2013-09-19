32df1f73fd7aa1a1f87694eb0a6de48acaa5972f
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-09-19 12:24:55 +0000

    add test038 to test components of jython scripts.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@12895 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/test/endtoend/Test038.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.endtoend;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.python.util.PythonInterpreter;
+import org.virbo.jythonsupport.JythonUtil;
+import org.virbo.jythonsupport.JythonUtil.Param;
+
+/**
+ *
+ * @author jbf
+ */
+public class Test038 {
+    
+    static final Logger logger= Logger.getLogger("autoplot");
+
+    /**
+     * test the getGetParams for a script, seeing if we can reduce 
+     * and run the script within interactive time.
+     * 
+     * @param file
+     * @throws Exception 
+     */
+    private static void doTestGetParams( String testId, String file ) {
+        long t0= System.currentTimeMillis();
+        System.err.println("== test "+testId+": "+ file + " ==" );
+        
+        try {
+            String script= JythonUtil.readScript( new FileReader(file) );
+            String scrip= org.virbo.jythonsupport.JythonUtil.simplifyScriptToGetParams(script,true);
+            File f= new File(file);
+            String fout= "./test038_"+f.getName();
+            FileWriter fw= new FileWriter(fout);
+            List<Param> parms= org.virbo.jythonsupport.JythonUtil.getGetParams( script );
+            try {
+                fw.append(scrip);
+            } finally {
+                fw.close();
+            }
+            for ( Param p: parms ) {
+                System.err.println(p);
+            }
+            System.err.println( String.format( "read params in %d millis: %s\n", System.currentTimeMillis()-t0, file ) );
+        } catch ( Exception ex ) {
+            ex.printStackTrace();
+            System.err.println( String.format( "failed within %d millis: %s\n", System.currentTimeMillis()-t0, file ) );
+        }
+
+    }
+    
+    /**
+     * Autoplot reduces jython scripts to get not much more than the getParam calls so the
+     * script can be executed quickly.
+     * @throws Exception 
+     */
+    public static void testGetParams() {
+        doTestGetParams("001","/home/jbf/ct/hudson/script/test038/trivial.jy");
+        doTestGetParams("002","/home/jbf/ct/hudson/script/test038/demoParms.jy");
+        doTestGetParams("003","/home/jbf/ct/hudson/script/test038/rbsp/emfisis/background_removal_wfr.jyds");
+    }
+    
+    public static void main( String[] args ) throws IOException {
+    
+        long t0= System.currentTimeMillis();
+        PythonInterpreter interp= JythonUtil.createInterpreter(true);
+        interp.eval("1+2");
+        System.err.println( String.format( "first initialize in %d millis\n", System.currentTimeMillis()-t0 ) );
+        
+        testGetParams();
+
+    }
+}
