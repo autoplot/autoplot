94e908eceff633edf229afec03c687d81d79ada7
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-10-11 22:23:32 +0000

    add  fce_A_2.jyds to test038, which tests if/elif/elif
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@13007 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test038.java
+++ VirboAutoplot/src/test/endtoend/Test038.java
@@ -71,6 +71,7 @@ public class Test038 {
         doTestGetParams("004","/home/jbf/ct/hudson/script/test038/rbsp/emfisis/background_removal_wfr.jyds");
         doTestGetParams("005","/home/jbf/ct/hudson/script/test038/demoParms2.jy");
         doTestGetParams("006","/home/jbf/ct/hudson/script/test038/fce_A.jyds");
+        doTestGetParams("007","/home/jbf/ct/hudson/script/test038/fce_A_2.jyds");
     }
     
     public static void main( String[] args ) throws IOException {
