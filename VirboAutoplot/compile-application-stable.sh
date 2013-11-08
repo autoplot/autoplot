6ca33666154fda0604c4912ccc9bf9797821ee61
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-11-08 11:49:07 +0000

    hide private info when building
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@13210 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/compile-application-stable.sh
+++ VirboAutoplot/compile-application-stable.sh
@@ -112,8 +112,8 @@ $JAVA_HOME/bin/pack200 --repack dist/AutoplotStable2.jar dist/AutoplotStable1.ja
 mv dist/AutoplotStable2.jar dist/AutoplotStable.jar
 rm dist/AutoplotStable1.jar
 
-echo "sign the jar files..."
-echo "  use set +x to hide private info"
+#echo "sign the jar files..."
+#echo "  use set +x to hide private info"
 #set +x
 #echo $JAVA_HOME/bin/jarsigner -keypass \"$KEYPASS\" -storepass \"$STOREPASS\" $JARSIGNER_OPTS dist/AutoplotStable.jar \"$ALIAS\"
 #if ! $JAVA_HOME/bin/jarsigner -keypass \"$KEYPASS\" -storepass \"$STOREPASS\" $JARSIGNER_OPTS dist/AutoplotStable.jar \"$ALIAS\"; then
@@ -130,13 +130,13 @@ echo $JAVA_HOME/bin/pack200 --repack dist/AutoplotStable.jar
 $JAVA_HOME/bin/pack200 --repack dist/AutoplotStable.jar
 
 echo "sign a second time (again with set +x to hide info...)"
-#set +x
+set +x
 echo $JAVA_HOME/bin/jarsigner -keypass \"$KEYPASS\" -storepass \"$STOREPASS\" $JARSIGNER_OPTS dist/AutoplotStable.jar \"$ALIAS\"
 if ! $JAVA_HOME/bin/jarsigner -keypass \"$KEYPASS\" -storepass \"$STOREPASS\" $JARSIGNER_OPTS dist/AutoplotStable.jar \"$ALIAS\"; then
    echo "Failed to sign resources!"
    exit 1
 fi
-#set -x
+set -x
 
 echo "pack the jar file..."
 $JAVA_HOME/bin/pack200 dist/AutoplotStable.jar.pack.gz dist/AutoplotStable.jar
