4c8b1a77700408ea6ded3066958b1566fbd40d3a
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-11-09 13:38:10 +0000

    netbeans suggestions. rename parseFormatStr to parseLayoutStr, deprecate parseFormatStr.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@13221 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/RowController.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/RowController.java
@@ -37,6 +37,7 @@ public class RowController extends DomNodeController {
         final List<String> minList= Arrays.asList( DasDevicePosition.PROP_MINIMUM, DasDevicePosition.PROP_EMMINIMUM, DasDevicePosition.PROP_PTMINIMUM );
         final List<String> maxList= Arrays.asList( DasDevicePosition.PROP_MAXIMUM, DasDevicePosition.PROP_EMMAXIMUM, DasDevicePosition.PROP_PTMAXIMUM );
         PropertyChangeListener list= new PropertyChangeListener() {
+            @Override            
             public void propertyChange(PropertyChangeEvent evt) {
                 if ( maxList.contains( evt.getPropertyName() ) ) {
                     row.setBottom( DasDevicePosition.formatLayoutStr(dasRow, false ) );
@@ -48,9 +49,10 @@ public class RowController extends DomNodeController {
 
         dasRow.addPropertyChangeListener(list);
         list= new PropertyChangeListener() {
+            @Override            
             public void propertyChange(PropertyChangeEvent evt) {
                 try {
-                    double[] dd= DasDevicePosition.parseFormatStr((String)evt.getNewValue());
+                    double[] dd= DasDevicePosition.parseLayoutStr((String)evt.getNewValue());
                     if ( evt.getPropertyName().equals(Row.PROP_TOP) ) {
                         dasRow.setMin( dd[0], dd[1], (int) dd[2] );
                     } else if ( evt.getPropertyName().equals(Row.PROP_BOTTOM) ) {
