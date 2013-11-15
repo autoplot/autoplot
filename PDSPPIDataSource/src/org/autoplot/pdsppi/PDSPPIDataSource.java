2f612dbf9cecedc8fb8563dac9fc7424a9e5693d
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-11-15 01:48:58 +0000

    play with code to identify fill automatically.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@13277 0c594bb0-8543-0410-a25e-d8ec440261db

--- PDSPPIDataSource/src/org/autoplot/pdsppi/PDSPPIDataSource.java
+++ PDSPPIDataSource/src/org/autoplot/pdsppi/PDSPPIDataSource.java
@@ -7,11 +7,18 @@ package org.autoplot.pdsppi;
 import java.io.File;
 import java.net.URI;
 import java.net.URL;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.das2.util.LoggerManager;
 import org.das2.util.monitor.ProgressMonitor;
 import org.virbo.dataset.DataSetOps;
+import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QDataSet;
 import org.virbo.datasource.AbstractDataSource;
 import org.virbo.datasource.DataSetURI;
+import org.virbo.dsops.Ops;
 import org.virbo.spase.VOTableReader;
 
 /**
@@ -20,6 +27,8 @@ import org.virbo.spase.VOTableReader;
  */
 public class PDSPPIDataSource extends AbstractDataSource {
 
+    private static final Logger logger= LoggerManager.getLogger("apdss.pdsppi");
+    
     PDSPPIDataSource( URI uri ) {
         super(uri);
     }
@@ -34,7 +43,22 @@ public class PDSPPIDataSource extends AbstractDataSource {
         File f= DataSetURI.downloadResourceAsTempFile( new URL(url), 3600, mon );
         mon.setProgressMessage("reading data");
         QDataSet ds= read.readTable( f.toString(), mon );
-        return DataSetOps.unbundle( ds, param );
+        QDataSet result= DataSetOps.unbundle( ds, param );
+        
+        QDataSet ah= Ops.autoHistogram(result);
+        Map<String,Object> up= (Map<String,Object>) ah.property(QDataSet.USER_PROPERTIES);
+        Map<Double,Integer> outl= (Map<Double,Integer>) up.get("outliers");
+        Integer outlierCount= (Integer) up.get("outlierCount");
+        if ( outlierCount>10 ) {
+            for ( Entry<Double,Integer> out: outl.entrySet() ) {
+                if ( out.getValue()>outlierCount*8/10 ) { // ID FILL
+                    logger.log(Level.FINE, "identified fill: {0}", out.getKey());
+                    ((MutablePropertyDataSet)result).putProperty(QDataSet.FILL_VALUE, out.getKey() );
+                    break;
+                }
+            }
+        }
+        return result;
         
     }
     
