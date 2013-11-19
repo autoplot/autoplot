53aa55f16dacdbbcb1edbfca9fab76eadfaa9c5e
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-11-19 00:09:35 +0000

    experiment with embedding the JNLP to satisfy new Java security restrictions
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@13308 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/compile-application.sh
+++ VirboAutoplot/compile-application.sh
@@ -157,7 +157,7 @@ echo "done special handling of META-INF stuff."
 
 echo "=== copy resources..."
 cd temp-volatile-src
-for i in $( find * -name '*.png' -o -name '*.gif' -o -name '*.html' -o -name '*.py' -o -name '*.jy' -o -name '*.xsl' -o -name '*.xsd' -o -name '*.CSV' ); do
+for i in $( find * -name '*.png' -o -name '*.gif' -o -name '*.html' -o -name '*.py' -o -name '*.jy' -o -name '*.xsl' -o -name '*.xsd' -o -name '*.CSV' -o -name '*.JNLP' ); do
    mkdir -p $(dirname ../temp-volatile-classes/$i)
    cp $i ../temp-volatile-classes/$i
 done
