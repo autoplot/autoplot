e5333fabe03e3d8c9a4d4506349c21834a1adbfd
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2013-12-04 21:50:54 +0000

    bugfix: Jon had a case where DELTA_PLUS point to itself, causing a stack overflow...
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@13376 0c594bb0-8543-0410-a25e-d8ec440261db

--- CdfJavaDataSource/src/org/virbo/cdf/CdfJavaDataSource.java
+++ CdfJavaDataSource/src/org/virbo/cdf/CdfJavaDataSource.java
@@ -733,7 +733,9 @@ public class CdfJavaDataSource extends AbstractDataSource {
         boolean doPlusMinus= depend==false;
         Object deltaPlus= thisAttributes.get( "DELTA_PLUS_VAR" );
         Object deltaMinus= thisAttributes.get( "DELTA_MINUS_VAR" );
-        if ( doPlusMinus && ( deltaPlus!=null && deltaPlus instanceof String ) && (  deltaMinus!=null && deltaMinus instanceof String ) ) {
+        if ( doPlusMinus 
+                && ( deltaPlus!=null && deltaPlus instanceof String && !deltaPlus.equals(svariable) ) 
+                && (  deltaMinus!=null && deltaMinus instanceof String ) && !deltaPlus.equals(svariable) ) {
             Variable var= cdf.getVariable((String)deltaPlus);
             if ( var!=null ) {
                 QDataSet delta= wrapDataSet( cdf, (String)deltaPlus, constraints, !var.recordVariance(), false, null ); //TODO: slice1
