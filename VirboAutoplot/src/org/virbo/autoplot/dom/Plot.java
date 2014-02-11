f009c62c82c5aef27ba51511eb447a34dcefe5c9
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-02-11 19:05:40 +0000

    Chris pointed out that displayTitle property didn't load properly.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@13841 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/Plot.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/Plot.java
@@ -291,6 +291,7 @@ public class Plot extends DomNode {
         if (!exclude.contains(PROP_TICKS_URI)) this.setTicksURI(that.getTicksURI());
         if (!exclude.contains(PROP_LEGENDPOSITION)) this.setLegendPosition(that.getLegendPosition());
         if (!exclude.contains(PROP_DISPLAYLEGEND)) this.setDisplayLegend(that.isDisplayLegend());
+        if (!exclude.contains(PROP_DISPLAYTITLE)) this.setDisplayTitle(that.isDisplayTitle());
     }
 
     @Override
@@ -339,6 +340,8 @@ public class Plot extends DomNode {
         if (!b) result.add(new PropertyChangeDiff(PROP_LEGENDPOSITION, that.legendPosition, this.legendPosition ));
         b= that.displayLegend==this.displayLegend;
         if (!b) result.add(new PropertyChangeDiff(PROP_DISPLAYLEGEND, that.displayLegend, this.displayLegend ));
+        b= that.displayTitle==this.displayTitle;
+        if (!b) result.add(new PropertyChangeDiff(PROP_DISPLAYTITLE, that.displayTitle, this.displayTitle ));
         result.addAll(DomUtil.childDiffs( PROP_XAXIS, this.getXaxis().diffs(that.getXaxis())));
         result.addAll(DomUtil.childDiffs( PROP_YAXIS, this.getYaxis().diffs(that.getYaxis())));
         result.addAll(DomUtil.childDiffs( PROP_ZAXIS, this.getZaxis().diffs(that.getZaxis())));
