86c0b76a7314ef1e7d00bd88903c2402294eea54
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-02-11 14:02:44 +0000

    bugfix: findbugs DLS_DEAD_LOCAL_STORE showed that the copied array was not used.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@13833 0c594bb0-8543-0410-a25e-d8ec440261db

--- CdfJavaDataSource/src/org/virbo/cdf/TrArrayDataSet.java
+++ CdfJavaDataSource/src/org/virbo/cdf/TrArrayDataSet.java
@@ -36,13 +36,13 @@ public abstract class TrArrayDataSet extends AbstractDataSet {
         if ( c.isArray() ) throw new IllegalArgumentException("input must be 1-D array");
         if ( copy ) {
             arr= Array.newInstance( c, Array.getLength(array) );
-            System.arraycopy( array, 0, arr, 0, Array.getLength(array) );
+            System.arraycopy( array, 0, arr, 0, Array.getLength(array) ); // this is guarenteed to be an array.
         } else {
             arr= array;
         }
-        if ( c==double.class ) return TrDDataSet.wrap( (double[])array, qube );
-        if ( c==float.class ) return TrFDataSet.wrap( (float[])array, qube );
-        if ( c==long.class ) return TrLDataSet.wrap( (long[])array, qube ); 
+        if ( c==double.class ) return TrDDataSet.wrap( (double[])arr, qube );
+        if ( c==float.class ) return TrFDataSet.wrap( (float[])arr, qube );
+        if ( c==long.class ) return TrLDataSet.wrap( (long[])arr, qube ); 
         //if ( c==int.class ) return TrIDataSet.wrap( (int[])array, qube );
         //if ( c==short.class ) return TrSDataSet.wrap( (short[])array, qube );
         //if ( c==byte.class ) return TrBDataSet.wrap( (byte[])array, qube );
