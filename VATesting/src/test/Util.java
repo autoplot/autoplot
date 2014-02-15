21606105322b269d2f09512aef2a1b7a541efc4f
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-02-15 14:11:01 +0000

    add setLAF
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@13901 0c594bb0-8543-0410-a25e-d8ec440261db

--- VATesting/src/test/Util.java
+++ VATesting/src/test/Util.java
@@ -5,7 +5,10 @@
 
 package test;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JMenuItem;
+import javax.swing.UnsupportedLookAndFeelException;
 import org.das2.graph.DasPlot;
 import org.netbeans.jemmy.ComponentChooser;
 import org.netbeans.jemmy.operators.JPopupMenuOperator;
@@ -20,8 +23,8 @@ public class Util {
     /**
      * push the context menu item identified by the items.  This is a
      * list of regular expressions identifying the levels.
-     * @param c
-     * @param items
+     * @param c the focus plot
+     * @param items labels, eg [ "Plot Style", "Series" ]
      * @return the JMenuItem found, which has been pushed.
      */
     public static JMenuItem pushContextMenu( DasPlot c, String[] items ) {
@@ -35,5 +38,22 @@ public class Util {
             }
             JMenuItem item= op.pushMenu( push );
             return item;
+    }
+
+    /**
+     * set the look and feel to the local platform
+     */
+    static void setLAF() {
+        try {
+            javax.swing.UIManager.setLookAndFeel(javax.swing.UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
