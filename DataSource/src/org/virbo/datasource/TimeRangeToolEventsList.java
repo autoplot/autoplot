8cd29ad5b19e8883a7124724ec22aa8b95957938
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-02-19 02:22:21 +0000

    introduce gui that loads a list of events and fires off range events
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@13922 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ DataSource/src/org/virbo/datasource/TimeRangeToolEventsList.java
@@ -0,0 +1,411 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.virbo.datasource;
+
+import java.awt.Color;
+import java.text.ParseException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.DefaultListModel;
+import javax.swing.SwingUtilities;
+import org.das2.components.DasProgressPanel;
+import org.das2.datum.Datum;
+import org.das2.datum.DatumRange;
+import org.das2.datum.DatumRangeUtil;
+import org.das2.datum.Units;
+import org.das2.event.DataRangeSelectionEvent;
+import org.das2.util.LoggerManager;
+import org.das2.util.monitor.ProgressMonitor;
+import org.virbo.dataset.DataSetOps;
+import org.virbo.dataset.DataSetUtil;
+import org.virbo.dataset.QDataSet;
+import org.virbo.dataset.SemanticOps;
+import org.virbo.dsops.Ops;
+
+/**
+ * Introduce tool to make it easy to browse around a list of events.
+ * This is in the events list format, and will probably become part of the
+ * timerange GUI.
+ * 
+ * @author jbf
+ */
+public class TimeRangeToolEventsList extends javax.swing.JPanel {
+
+    private static final Logger logger= LoggerManager.getLogger("apdss.gui");
+    
+    QDataSet currentDataSet= null;
+    
+    /**
+     * Creates new form TimeRangeToolEventsList
+     */
+    public TimeRangeToolEventsList() {
+        initComponents();
+    }
+
+    private DatumRange getRange( int i ) {
+        QDataSet ds1= currentDataSet.slice(i).trim(0,2); // it's a shame this doesn't get the units right...
+        Units tu= (Units)((QDataSet)currentDataSet.property(QDataSet.BUNDLE_1)).property(QDataSet.UNITS,0);
+        return DatumRange.newDatumRange( ds1.value(0), ds1.value(1), tu );
+    }
+    private void fillList() {
+        DefaultListModel lm= new DefaultListModel();
+        for ( int i=0; i<currentDataSet.length(); i++ ) {
+            String ss= getRange(i).toString();
+            lm.add(lm.getSize(),ss);
+        }
+        intervalsList.setModel( lm );
+    }
+    
+        /**
+     * make canonical rank 2 bundle dataset of min,max,color,text
+     * @param vds
+     * @return
+     */
+    public static QDataSet makeCanonical( QDataSet vds ) {
+
+        QDataSet xmins;
+        QDataSet xmaxs;
+        QDataSet colors;
+        QDataSet msgs;
+
+        Color defaultColor= new Color( 240,240,240,0 ); // transparent
+        
+        if ( vds==null ) {
+            return null;
+        }
+        if ( vds.rank()==2 ) {
+            QDataSet dep0= (QDataSet) vds.property(QDataSet.DEPEND_0);
+            if ( dep0==null ) {
+                xmins= DataSetOps.unbundle( vds,0 );
+                xmaxs= DataSetOps.unbundle( vds,1 );
+
+                if ( vds.length(0)>3 ) {
+                    colors= DataSetOps.unbundle( vds,2 );
+                } else {
+                    colors= Ops.replicate( defaultColor.getRGB(), xmins.length() );
+                }
+                
+            } else if ( dep0.rank()==2 ) {
+                if ( SemanticOps.isBins(dep0) ) {
+                    xmins= DataSetOps.slice1( dep0, 0 );
+                    xmaxs= DataSetOps.slice1( dep0, 1 );
+                    colors= Ops.replicate( 0x808080, xmins.length() );
+                    Units u0= SemanticOps.getUnits(xmins );
+                    Units u1= SemanticOps.getUnits(xmaxs );
+                    if ( !u1.isConvertableTo(u0) && u1.isConvertableTo(u0.getOffsetUnits()) ) {
+                        xmaxs= Ops.add( xmins, xmaxs );
+                    }
+                } else {
+                    throw new IllegalArgumentException( "DEPEND_0 is rank 2 but not bins" );
+                }
+                
+            } else  if ( dep0.rank() == 1 ) {
+                Datum width= SemanticOps.guessXTagWidth( dep0, null ).divide(2);
+                xmins= Ops.subtract( dep0, org.virbo.dataset.DataSetUtil.asDataSet(width) );
+                xmaxs= Ops.add( dep0, org.virbo.dataset.DataSetUtil.asDataSet(width) );
+                colors= Ops.replicate( defaultColor.getRGB(), xmins.length() );
+
+            } else {
+                throw new IllegalArgumentException( "rank 2 dataset must have dep0 of rank 1 or rank 2 bins" );
+            }
+
+            msgs= DataSetOps.unbundle( vds, vds.length(0)-1 );
+
+        } else if ( vds.rank()==1 ) {
+            QDataSet dep0= (QDataSet) vds.property(QDataSet.DEPEND_0);
+            if ( dep0==null ) {
+                xmins= vds;
+                xmaxs= vds;
+                msgs= vds;
+            } else if ( dep0.rank() == 2  ) {
+                if ( SemanticOps.isBins(dep0) ) {
+                    xmins= DataSetOps.slice1( dep0, 0 );
+                    xmaxs= DataSetOps.slice1( dep0, 1 );
+                    Units u0= SemanticOps.getUnits(xmins );
+                    Units u1= SemanticOps.getUnits(xmaxs );
+                    if ( !u1.isConvertableTo(u0) && u1.isConvertableTo(u0.getOffsetUnits()) ) {
+                        xmaxs= Ops.add( xmins, xmaxs );
+                    }
+                    msgs= vds;
+                } else {
+                    throw new IllegalArgumentException( "DEPEND_0 is rank 2 but not bins" );
+                }
+            } else if ( dep0.rank() == 1 ) {
+                Datum width= SemanticOps.guessXTagWidth( dep0, null );
+                if ( width!=null ) {
+                    width= width.divide(2);
+                } else {
+                    QDataSet sort= Ops.sort(dep0);
+                    QDataSet diffs= Ops.diff( DataSetOps.applyIndex(dep0,0,sort,false) );
+                    QDataSet w= Ops.reduceMin( diffs,0 );
+                    width= DataSetUtil.asDatum(w);                    
+                }
+                xmins= Ops.subtract( dep0, org.virbo.dataset.DataSetUtil.asDataSet(width) );
+                xmaxs= Ops.add( dep0, org.virbo.dataset.DataSetUtil.asDataSet(width) );                
+                msgs= vds;
+            } else {
+                throw new IllegalArgumentException( "dataset is not correct form" );
+            }
+            Color c0= defaultColor;
+            Color c1= new Color( c0.getRed(), c0.getGreen(), c0.getBlue(), c0.getAlpha()==255 ? 128 : c0.getAlpha() );
+            int irgb= c1.getRGB();
+            
+            colors= Ops.replicate( irgb, xmins.length() );
+
+        } else if ( vds.rank()==0 ) {
+            xmins= Ops.replicate(vds,1); // increase rank from 0 to 1.
+            xmaxs= xmins;
+            Color c0= defaultColor;
+            Color c1= new Color( c0.getRed(), c0.getGreen(), c0.getBlue(), c0.getAlpha()==255 ? 128 : c0.getAlpha() );
+            int irgb= c1.getRGB();
+            colors= Ops.replicate( irgb, xmins.length() );
+            msgs= Ops.replicate(vds,1);
+        } else {            
+            throw new IllegalArgumentException( "dataset must be rank 0, 1 or 2" );
+        }
+
+        Units u0= SemanticOps.getUnits( xmins );
+        Units u1= SemanticOps.getUnits( xmaxs );
+
+        if ( u1.isConvertableTo( u0.getOffsetUnits() ) && !u1.isConvertableTo(u0) ) { // maxes are dt instead of stopt.
+            xmaxs= Ops.add( xmins, xmaxs );
+        }
+
+        QDataSet lds= Ops.bundle( xmins, xmaxs, colors, msgs );
+        
+        return lds;
+
+    }
+
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
+     */
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jScrollPane1 = new javax.swing.JScrollPane();
+        intervalsList = new javax.swing.JList();
+        currentDataSetDL = new javax.swing.JComboBox();
+        rescaleComboBox = new javax.swing.JComboBox();
+        jLabel1 = new javax.swing.JLabel();
+        prevButton = new javax.swing.JButton();
+        nextButton = new javax.swing.JButton();
+
+        intervalsList.setModel(new javax.swing.AbstractListModel() {
+            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
+            public int getSize() { return strings.length; }
+            public Object getElementAt(int i) { return strings[i]; }
+        });
+        intervalsList.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                intervalsListValueChanged(evt);
+            }
+        });
+        jScrollPane1.setViewportView(intervalsList);
+
+        currentDataSetDL.setEditable(true);
+        currentDataSetDL.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        currentDataSetDL.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                currentDataSetDLActionPerformed(evt);
+            }
+        });
+
+        rescaleComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "none", "-5%,105%", "-10%,110%" }));
+        rescaleComboBox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                rescaleComboBoxActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Rescale:");
+
+        prevButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/virbo/datasource/prevPrev.png"))); // NOI18N
+        prevButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                prevButtonActionPerformed(evt);
+            }
+        });
+
+        nextButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/virbo/datasource/nextNext.png"))); // NOI18N
+        nextButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                nextButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane1)
+            .addComponent(currentDataSetDL, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(rescaleComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 115, Short.MAX_VALUE)
+                .addComponent(prevButton, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(nextButton)
+                .addContainerGap())
+        );
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {nextButton, prevButton});
+
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(currentDataSetDL, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rescaleComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1)
+                    .addComponent(prevButton, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(nextButton))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 234, Short.MAX_VALUE))
+        );
+
+        layout.linkSize(javax.swing.SwingConstants.VERTICAL, new java.awt.Component[] {nextButton, prevButton});
+
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void currentDataSetDLActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_currentDataSetDLActionPerformed
+        final String uri= (String)currentDataSetDL.getSelectedItem();
+        Runnable run= new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    DataSource dsource = DataSetURI.getDataSource(uri);
+                    ProgressMonitor mon= DasProgressPanel.createFramed(SwingUtilities.getWindowAncestor(TimeRangeToolEventsList.this),"Loading Events File...");
+                    currentDataSet= dsource.getDataSet(mon);
+                    currentDataSet= makeCanonical(currentDataSet);
+                    currentDataSetDL.setEnabled(true);
+                    fillList( );
+                } catch (Exception ex) {
+                    Logger.getLogger(TimeRangeToolEventsList.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        };
+        currentDataSetDL.setEnabled(false);
+        new Thread(run).start();
+                
+    }//GEN-LAST:event_currentDataSetDLActionPerformed
+
+    /** Registers DataRangeSelectionListener to receive events.
+     * @param listener The listener to register.
+     */
+    public void addDataRangeSelectionListener(org.das2.event.DataRangeSelectionListener listener) {
+        listenerList.add(org.das2.event.DataRangeSelectionListener.class, listener);
+    }
+    
+    /** Removes DataRangeSelectionListener from the list of listeners.
+     * @param listener The listener to remove.
+     */
+    public synchronized void removeDataRangeSelectionListener(org.das2.event.DataRangeSelectionListener listener) {
+        listenerList.remove(org.das2.event.DataRangeSelectionListener.class, listener);
+    }
+    
+    
+    /** Notifies all registered listeners about the event.
+     *
+     * @param event The event to be fired
+     */
+    private void fireDataRangeSelectionListenerDataRangeSelected(DataRangeSelectionEvent event) {
+        Object[] listeners = listenerList.getListenerList();
+        for (int i = listeners.length-2; i>=0; i-=2) {
+            if (listeners[i]==org.das2.event.DataRangeSelectionListener.class) {
+                ((org.das2.event.DataRangeSelectionListener)listeners[i+1]).dataRangeSelected(event);
+            }
+        }
+    }
+    
+    private void intervalsListValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_intervalsListValueChanged
+        if ( evt.getValueIsAdjusting() ) {
+            return;
+        } else {
+            fireSelection();
+        }
+    } 
+    
+    private void fireSelection() {
+        int i1= intervalsList.getMinSelectionIndex();
+        int i2= intervalsList.getMaxSelectionIndex();
+        DatumRange fire= null;
+        String rescale= (String)rescaleComboBox.getSelectedItem();
+        for ( int i=i1; i<=i2; i++ ) {
+            if ( intervalsList.isSelectedIndex(i) ) {
+                if ( fire==null ) {
+                    fire= getRange(i);
+                } else {
+                    fire= DatumRangeUtil.union( fire, getRange(i) );
+                }
+            }
+        }
+        if ( fire==null ) {
+            return;
+        }
+        if ( rescaleComboBox.getSelectedIndex()>0 ) {
+            try {
+                fire= DatumRangeUtil.rescale( fire,rescale );
+            } catch (ParseException ex) {
+                logger.log(Level.SEVERE, null, ex);
+            }
+        }
+        fireDataRangeSelectionListenerDataRangeSelected( new DataRangeSelectionEvent(this,fire.min(),fire.max()) );
+        
+    }//GEN-LAST:event_intervalsListValueChanged
+
+    private void prevButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_prevButtonActionPerformed
+        int i0= intervalsList.getMinSelectionIndex();
+        int i1= intervalsList.getMaxSelectionIndex();
+        if ( i0==-1 ) {
+            i0= 0;
+            i1= 0;  
+        }
+        int de= i1-i0+1;
+        i0= Math.max(0,i0-1);
+        i1= i0 + de-1;
+        int[] indexes= new int[de];
+        for ( int i=i0; i<=i1; i++ ) {
+            indexes[i-i0]= i;
+        }
+        intervalsList.setSelectedIndices(indexes);
+    }//GEN-LAST:event_prevButtonActionPerformed
+
+    private void nextButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nextButtonActionPerformed
+        int i0= intervalsList.getMinSelectionIndex();
+        int i1= intervalsList.getMaxSelectionIndex();
+        if ( i0==-1 ) {
+            i0= 0;
+            i1= 0;  
+        }
+        int de= i1-i0+1;
+        i1= Math.min(intervalsList.getModel().getSize()-1,i1+1);
+        i0= i1-de+1;
+        int[] indexes= new int[de];
+        for ( int i=i0; i<=i1; i++ ) indexes[i-i0]= i;
+        intervalsList.setSelectedIndices(indexes);
+    }//GEN-LAST:event_nextButtonActionPerformed
+
+    private void rescaleComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rescaleComboBoxActionPerformed
+        fireSelection();
+    }//GEN-LAST:event_rescaleComboBoxActionPerformed
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox currentDataSetDL;
+    private javax.swing.JList intervalsList;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JButton nextButton;
+    private javax.swing.JButton prevButton;
+    private javax.swing.JComboBox rescaleComboBox;
+    // End of variables declaration//GEN-END:variables
+}
