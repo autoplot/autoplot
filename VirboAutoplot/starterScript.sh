cb589a38c0ceb09d0a99be1369044b1211370328
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-03-25 18:48:37 +0000

    new launch script allows -h and --headless.  allow -J to pass arguments to Java.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@14136 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/starterScript.sh
+++ VirboAutoplot/starterScript.sh
@@ -15,10 +15,35 @@
 # This file is still a valid jar file and may be inspected by standard tools
 # such as "unzip".
 
+JAVA_ARGS=""
+AP_ARGS=""
+
+memIsImplicit=1
+
+for i in "$@"; do
+   echo $i
+   if [[ $i == -J-Xmx* ]]; then
+      JAVA_ARGS="${JAVA_ARGS} ${i:2}";
+      memIsImplicit=0
+   elif [[ $i == -J* ]]; then
+      JAVA_ARGS="${JAVA_ARGS} ${i:2}";
+   elif [[ $i == '--headless' ]]; then
+      JAVA_ARGS="${JAVA_ARGS} -Djava.awt.headless=true";
+   elif [[ $i == '-h' ]]; then
+      JAVA_ARGS="${JAVA_ARGS} -Djava.awt.headless=true";
+   else
+      AP_ARGS="${AP_ARGS} $i";
+   fi
+done
+
+if [[ $memIsImplicit == 1 ]]; then 
+   JAVA_ARGS="${JAVA_ARGS} -Xmx1000M ";
+fi
+
 if [ "${JAVA_HOME}" -a \( -x "${JAVA_HOME}"/bin/java \) ]; then
-        exec "${JAVA_HOME}"/bin/java -Xmx1000M -jar $0 "$@"
+      exec "${JAVA_HOME}"/bin/java ${JAVA_ARGS} -jar $0 "${AP_ARGS}"
 else
-        exec /usr/bin/env java -Xmx1000M -jar $0 "$@"
+      exec /usr/bin/env java ${JAVA_ARGS} -jar $0 "${AP_ARGS}"
 fi
 
 # JAR FILE DATA STARTS AFTER THIS TEXT #
