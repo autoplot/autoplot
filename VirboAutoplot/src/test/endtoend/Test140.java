96aec01933e65e3b759bcc4c06ad04708216d246
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-04-17 10:20:57 +0000

    replace escapes so that pngwalk can be used to look at the results.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@14247 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test140.java
+++ VirboAutoplot/src/test/endtoend/Test140.java
@@ -156,6 +156,7 @@ public class Test140 {
             id= id.replaceAll("%3D","=");
             id= id.replaceAll("%26","_");
             id= id.replaceAll("%7E","_"); // twiddle
+            id= id.replaceAll("%2B","_"); // colon
             if ( id.length()>150 ) { // ext4 filename length limits...
                 id= id.substring(0,150) + "..." + String.format( "%016d", id.hashCode() );
             }
