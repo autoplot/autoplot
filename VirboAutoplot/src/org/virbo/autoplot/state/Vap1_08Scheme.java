a154019f4261ea8f82ce4332e47d351a6db5addd
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-05-05 17:39:32 +0000

    introduce v1.08 scheme.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@14345 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/virbo/autoplot/state/Vap1_08Scheme.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.autoplot.state;
+
+import org.virbo.autoplot.dom.DomNode;
+import org.w3c.dom.Element;
+
+/**
+ * in vap 1.08, we add the properties:
+ * Autoranging property is restored.  In v1.07 no VAP file could have ranges that would be 
+ * set after the data was loaded.
+ * @author jbf
+ */
+public class Vap1_08Scheme extends AbstractVapScheme {
+
+    public String getId() {
+        return "1.08";
+    }
+
+    public boolean resolveProperty(Element element, DomNode node) {
+        return false;
+    }
+
+}
