0fc9781a2d4e2e6a9dddfa2a02d9c46d239e501e
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-06-13 21:09:55 +0000

    whoops
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@14659 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/compile-application-all.sh
+++ VirboAutoplot/compile-application-all.sh
@@ -193,7 +193,7 @@ if ! $JAVAC -target 1.6 -source 1.6 -cp ../temp-classes:. -d ../temp-classes -Xm
 if ! $JAVAC -target 1.6 -source 1.6 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/das2/fsm/FileStorageModelNew.java; then echo "*****"; hasErrors=1; fi  # some scripts use this old name.
 if ! $JAVAC -target 1.6 -source 1.6 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/das2/math/filter/*.java; then echo "*****"; hasErrors=1; fi  
 if ! $JAVAC -target 1.6 -source 1.6 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/das2/components/DataPointRecorderNew.java; then echo "*****"; hasErrors=1; fi  
-if ! $JAVAC -target 1.6 -source 1.6 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org.das2.graph/Auralizor.java; then echo "*****"; hasErrors=1; fi  
+if ! $JAVAC -target 1.6 -source 1.6 -cp ../temp-classes:. -d ../temp-classes -Xmaxerrs 10 org/das2/graph/Auralizor.java; then echo "*****"; hasErrors=1; fi  
 
 
 cat ../temp-classes/META-INF/org.virbo.datasource.DataSourceFactory.extensions | cut -d' ' -f1
