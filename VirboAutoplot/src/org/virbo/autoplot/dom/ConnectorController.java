183b31acaee2eba2f993fe399c2c38624a07b4d2
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-07-23 21:42:26 +0000

    script to demo /home/jbf/project/rbsp/study/craig/20140722/connectorControllerExperiment.jy needed access to the das ColumnColumnConnector object so it can add the listener.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@14776 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/ConnectorController.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/ConnectorController.java
@@ -14,6 +14,7 @@ public class ConnectorController extends DomNodeController  {
     
     Connector connector;
     Application dom;
+    ColumnColumnConnector dasConnector;
     
     public ConnectorController( Application dom, Connector connector ) {
         super(connector);
@@ -23,12 +24,17 @@ public class ConnectorController extends DomNodeController  {
     
     public void bindTo( ColumnColumnConnector c ) {
         ApplicationController ac= dom.controller;
+        dasConnector= c;
         ac.bind( this.connector, Connector.PROP_BOTTOMCURTAIN, c, ColumnColumnConnector.PROP_BOTTOM_CURTAIN ); 
         ac.bind( this.connector, Connector.PROP_CURTAINOPACITYPERCENT, c, ColumnColumnConnector.PROP_CURTAIN_OPACITY_PERCENT );
         ac.bind( this.connector, Connector.PROP_FILL, c, ColumnColumnConnector.PROP_FILL );
         ac.bind( this.connector, Connector.PROP_FILLCOLOR, c, ColumnColumnConnector.PROP_FILL_COLOR );
         ac.bind( this.connector, Connector.PROP_VISIBLE, c, "visible" );
         ac.bind( this.connector, Connector.PROP_COLOR, c, "foreground" );
+    }
+    
+    public ColumnColumnConnector getDasConnector() {
+        return this.dasConnector;
     }
     
     public void removeBindings() {
