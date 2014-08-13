e17ba8ecab1e197bcce4d0f95dcad660809a53f3
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-08-13 14:32:51 +0000

    findbugs ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@14872 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/autoplot/wgetfs/WGetFileSystemFactory.java
+++ DataSource/src/org/autoplot/wgetfs/WGetFileSystemFactory.java
@@ -23,40 +23,46 @@ public class WGetFileSystemFactory implements FileSystemFactory {
     /**
      * support curl too, since it is on macs by default.
      */
-    static boolean useCurl= true;
-    
+    private static boolean useCurl= true;
+
     /**
      * the executable to spawn.  See createFileSystem for use of AP_WGET or AP_CURL environment variables.
      */
-    static String exe=null;
+    private static String exe=null;
+    
+    private static void doInitialize() {
+        if ( exe==null ) {
+            useCurl= false;
+            exe= System.getProperty("AP_WGET");
+            if ( exe!=null && ( exe.length()<2 || exe.equalsIgnoreCase("true") || exe.equalsIgnoreCase("false") ) ) {
+                exe= null;
+                throw new IllegalArgumentException("AP_WGET must be set to the name of the command to run, or empty string");
+            }
+            if ( exe==null || exe.length()==0 ) {
+                exe= System.getProperty("AP_CURL");
+                if ( exe!=null && ( exe.length()<2 || exe.equalsIgnoreCase("true") || exe.equalsIgnoreCase("false") ) ) {
+                   exe= null;
+                   throw new IllegalArgumentException("AP_CURL must be set to the name of the command to run, or empty string");
+                }
+                if ( exe!=null && exe.length()>0 ) useCurl= true;
+            } 
+            if ( exe==null || exe.length()==0 ) {
+                if ( System.getProperty("os.name").startsWith("Mac") ) {
+                    useCurl= true;
+                    exe= "curl";
+                } else {
+                    useCurl= false;
+                    exe= "wget";
+                }
+            }
+        }        
+    }
     
     @Override
     public FileSystem createFileSystem(URI root) throws FileSystem.FileSystemOfflineException, UnknownHostException, FileNotFoundException {
         synchronized ( FileSystem.class ) {
             if ( exe==null ) {
-                useCurl= false;
-                exe= System.getProperty("AP_WGET");
-                if ( exe!=null && ( exe.length()<2 || exe.equalsIgnoreCase("true") || exe.equalsIgnoreCase("false") ) ) {
-                    exe= null;
-                    throw new IllegalArgumentException("AP_WGET must be set to the name of the command to run, or empty string");
-                }
-                if ( exe==null || exe.length()==0 ) {
-                    exe= System.getProperty("AP_CURL");
-                    if ( exe!=null && ( exe.length()<2 || exe.equalsIgnoreCase("true") || exe.equalsIgnoreCase("false") ) ) {
-                       exe= null;
-                       throw new IllegalArgumentException("AP_CURL must be set to the name of the command to run, or empty string");
-                    }
-                    if ( exe!=null && exe.length()>0 ) useCurl= true;
-                } 
-                if ( exe==null || exe.length()==0 ) {
-                    if ( System.getProperty("os.name").startsWith("Mac") ) {
-                        useCurl= true;
-                        exe= "curl";
-                    } else {
-                        useCurl= false;
-                        exe= "wget";
-                    }
-                }
+                doInitialize();
             }
         }
         
