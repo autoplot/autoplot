46761ae578e48ecc5d45827842ccdee4c6bc4d35
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-10-16 17:49:20 +0000

    Initial coding of the filters GUI stuff.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@15223 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ DataSource/src/org/virbo/filters/FilterEditorPanel.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.filters;
+
+/**
+ *
+ * @author mmclouth
+ */
+public interface FilterEditorPanel {
+    String getFilter();
+    void setFilter( String filter );
+}
---
+++ DataSource/src/org/virbo/filters/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.virbo.filters;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mmclouth
+ */
+public class Test {
+    public static void main( String[] args ) {
+        FilterEditorPanel p= new AddFilterEditorPanel();
+        p.setFilter("|add(50)");
+        JOptionPane.showMessageDialog( null, p );
+        System.err.println( p.getFilter() );
+    }
+    
+    
+}
