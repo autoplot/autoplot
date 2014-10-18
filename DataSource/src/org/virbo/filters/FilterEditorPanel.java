f00c7f4f2564346890d299e4c59f4f017f368f0c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-10-18 02:41:44 +0000

    javadoc
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@15257 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/filters/FilterEditorPanel.java
+++ DataSource/src/org/virbo/filters/FilterEditorPanel.java
@@ -7,7 +7,11 @@
 package org.virbo.filters;
 
 /**
- *
+ * Interface for adding small GUIs to control each of the filters.  For example
+ * "|divide(5)" is controlled with a GUI that accepts the float parameter that 
+ * might check that the operand is not zero.  These should each implement get
+ * and setFilter, and fire off a property change event when the value is changed,
+ * so the GUI can be interactively.
  * @author mmclouth
  */
 public interface FilterEditorPanel {
