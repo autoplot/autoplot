e99c1db78904bc5668eb3a068b04a10bbc4a2862
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-10-20 20:08:38 +0000

    add AbstractFilterEditorPanel, which can be extended to implement most filters.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@15266 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ DataSource/src/org/virbo/filters/AbstractFilterEditorPanel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.virbo.filters;
+
+import javax.swing.JPanel;
+import org.virbo.dataset.QDataSet;
+
+/**
+ * Implements the typical filter, where we don't care about the input data and
+ * the filter itself implements the GUI.
+ * @author jbf
+ */
+public abstract class AbstractFilterEditorPanel extends JPanel implements FilterEditorPanel {
+    @Override
+    public abstract void setFilter( String filter );
+    @Override
+    public abstract String getFilter( );
+    @Override
+    public void setInput( QDataSet ds ) {
+        // do nothing, ignore input
+    }
+}
