7c85274fd11ba50e1d741c15e26e0b99ff098780
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-10-29 16:32:17 +0000

    400: experiment with getting coordinates
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@15357 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/autoplot/pngwalk/SinglePngWalkView.java
+++ VirboAutoplot/src/org/autoplot/pngwalk/SinglePngWalkView.java
@@ -2,10 +2,13 @@ package org.autoplot.pngwalk;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.RenderingHints;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.MalformedURLException;
 
 /**
  * An implementation of PngWalkView to display a single image.
@@ -16,6 +19,8 @@ public class SinglePngWalkView extends PngWalkView {
 //    private boolean sizeValid = false;
     private transient BufferedImage cacheImage;
 
+    Rectangle imageLocation= null;
+    
     public SinglePngWalkView(WalkImageSequence s) {
         super(s);
         setShowCaptions(true);
@@ -33,8 +38,34 @@ public class SinglePngWalkView extends PngWalkView {
                     getPopup().show(e.getComponent(),e.getX(), e.getY());
                 }
             }
+            
+            @Override
+            public void mouseClicked(MouseEvent e) { 
+                Rectangle lrect= imageLocation;
+                if ( imageLocation==null ) return;
+                BufferedImage i = seq.currentImage().getImage();
+                double factor = (double) lrect.getWidth() / (double) i.getWidth(null);
+                System.err.println( lrect );
+                //TODO: this assume x is limiting. paintImageCentered might return the scaling.
+                int imageX= (int)( ( e.getX() - lrect.x ) / factor );
+                int imageY= (int)( ( e.getY() - lrect.y ) / factor );
+                
+                doLookupMetadata( imageX, imageY );
+                
+            }
+            
         } );
 
+    }
+    
+    /**
+     * look up the richPng metadata within the png images.
+     * @param x
+     * @param y 
+     */
+    private void doLookupMetadata( int x, int y ) {
+        System.err.println( String.format( "%d %d ", x, y ) );
+        
     }
 
     @Override
@@ -50,11 +81,12 @@ public class SinglePngWalkView extends PngWalkView {
         BufferedImage i = seq.currentImage().getImage();
         
         if (i!=null && i.getWidth(this) >0 && i.getHeight(this) > 0) {
-            paintImageCentered(i, g2, seq.currentImage().getCaption());
+            imageLocation= paintImageCentered(i, g2, seq.currentImage().getCaption());
             cacheImage = i;
         } else {
-            if (cacheImage != null)
-                paintImageCentered(cacheImage, g2, seq.currentImage().getCaption());
+            if (cacheImage != null) {
+                imageLocation= paintImageCentered(cacheImage, g2, seq.currentImage().getCaption());
+            }
             paintImageCentered(loadingImage, g2);
         }
 
