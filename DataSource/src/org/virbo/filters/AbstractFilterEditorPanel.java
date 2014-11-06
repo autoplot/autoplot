4a5c8a413a38b7d82e534d61ac391433e6e8a271
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-11-06 21:33:14 +0000

    trivial whitespace. add logger for implementations to use.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@15452 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/filters/AbstractFilterEditorPanel.java
+++ DataSource/src/org/virbo/filters/AbstractFilterEditorPanel.java
@@ -4,7 +4,9 @@
  */
 package org.virbo.filters;
 
+import java.util.logging.Logger;
 import javax.swing.JPanel;
+import org.das2.util.LoggerManager;
 import org.virbo.dataset.QDataSet;
 
 /**
@@ -13,14 +15,20 @@ import org.virbo.dataset.QDataSet;
  * @author jbf
  */
 public abstract class AbstractFilterEditorPanel extends JPanel implements FilterEditorPanel {
+    
+    protected static final Logger logger= LoggerManager.getLogger("apdss.filters");
+    
     @Override
     public abstract void setFilter( String filter );
+
     @Override
     public abstract String getFilter( );
+
     @Override
     public void setInput( QDataSet ds ) {
         // do nothing, ignore input
     }
+    
     @Override
     public JPanel getPanel() {
         return this;
