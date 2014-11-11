03ddedf5b4a6ddd50d1e9f66180176f4c33aef91
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-11-11 13:18:47 +0000

    refactor PngWalkTool1 is finally renamed to PngWalkTool.  I was surprised I hadn't done this, and it's just before the B branch is started.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@15504 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/autoplot/pngwalk/PngWalkTool1.java
+++ VirboAutoplot/src/org/autoplot/pngwalk/PngWalkTool1.java
@@ -9,13 +9,15 @@ import java.awt.Window;
 
 /**
  * Launcher for PngWalkTool1.  The "1" in PngWalkTool1 was to indicate it was provisional.  Now it's official.
+ * This is a half-hearted attempt to support Jython scripts that used the PngWalkTool1 name.  Please let us know if this
+ * needs attention, at autoplot@groups.google.com
+ * 
  * @author jbf
  */
 public class PngWalkTool1 {
     
-    public static PngWalkTool1 start( String template, final Window parent ) {
-        PngWalkTool.start(template, parent);
-        return null;
+    public static PngWalkTool start( String template, final Window parent ) {
+        return PngWalkTool.start(template, parent);
     }
 
     public static void main( String[] args ) {
