ac096dc8d8d3f4025a75f700eacc28f062ee1858
mmclouth <mmclouth@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-11-13 23:08:19 +0000

    Modified NoArgFilterEditorPanel to return the same filter that it was given with getFilter() and setFilter().
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@15533 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/filters/NoArgFilterEditorPanel.java
+++ DataSource/src/org/virbo/filters/NoArgFilterEditorPanel.java
@@ -30,25 +30,37 @@ public class NoArgFilterEditorPanel extends AbstractFilterEditorPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
+        jLabel1.setText("jLabel1");
+
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(0, 400, Short.MAX_VALUE)
+            .add(layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jLabel1)
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(0, 300, Short.MAX_VALUE)
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .add(jLabel1)
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 
     @Override
     public void setFilter(String filter) {
-        String s= filter;
+        s= filter;
+        jLabel1.setText(s);
     }
 
     @Override
