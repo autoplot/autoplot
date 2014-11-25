ff8ef0bf2bf65c400b08e4d4c578ba697bb0d2b7
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-11-25 14:41:06 +0000

    go through and clean up all the references to old web folders that no longer exist.  Put in new fits demo that verifies the zip fs support as well.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@15607 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test017.java
+++ VirboAutoplot/src/test/endtoend/Test017.java
@@ -153,6 +153,7 @@ public class Test017 {
 
         //This fails because negative CADENCE and MONOTONIC=true.
 
+        // This shows that I'm doing something wrong with fits files.  Using FITS liberator it looks good, with AP it is distorted.
         "020 http://www.spacetelescope.org/static/projects/fits_liberator/datasets/m42/m42_40min_red.zip/m42_40min_red.fits",
         //"020 vap:http://www.astro.princeton.edu/~frei/Gcat_htm/Catalog/Fits/n4013_lR.fits",
 
