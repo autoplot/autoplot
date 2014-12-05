3251a1682e9c32c52d3f2db2c498f7a3c8ab2c33
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2014-12-05 20:02:03 +0000

    add new target for Preferences, so the user's prefs are in one location.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@15684 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/autoplot/Prefs.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.autoplot;
+
+/**
+ * class for containing preferences
+ * 
+ * @author jbf
+ */
+public class Prefs {
+    public static final String PREF_EXPORT_FOLDER= "exportFolder";
+    public static final String PREF_EXPORT_FILE= "exportFile";
+    public static final String PREF_EXPORT_EXT= "exportExt";
+}
