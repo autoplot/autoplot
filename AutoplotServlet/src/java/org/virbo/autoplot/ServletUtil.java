7a5f376ab6bc5e3238016a1da83841e06046c5cf
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-01-07 15:32:16 +0000

    introduce id=name, so that data servers can hide internal names.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@15867 0c594bb0-8543-0410-a25e-d8ec440261db

--- AutoplotServlet/src/java/org/virbo/autoplot/ServletUtil.java
+++ AutoplotServlet/src/java/org/virbo/autoplot/ServletUtil.java
@@ -4,12 +4,20 @@
  */
 package org.virbo.autoplot;
 
-import java.text.ParseException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
-import org.das2.graph.DasDevicePosition;
+import org.virbo.datasource.AutoplotSettings;
 
 /**
- *
+ * Utilities for the servlets
  * @author jbf
  */
 public class ServletUtil {
@@ -31,4 +39,76 @@ public class ServletUtil {
 
     }
    
+    private static Map<String,String> idMap=null;
+    private static long idMapFresh= 0;
+    
+    /**
+     * return the id map, checking no more than once per 5 seconds, and
+     * creating an empty file if one is not found.  
+     * See HOME/autoplot_data/server/
+     * 
+     * @return map from one string to another.
+     * @throws java.io.IOException
+     */
+    public static Map<String,String> getIdMap() throws IOException {
+        long currentTimeMillis= System.currentTimeMillis();
+        if ( currentTimeMillis-idMapFresh < 5000 ) return idMap;
+        File apd= new File( AutoplotSettings.settings().resolveProperty( AutoplotSettings.PROP_AUTOPLOTDATA ) );
+        if ( !apd.exists() ) {
+            if ( !apd.mkdirs() ) {
+                throw new IllegalArgumentException("unable to make autoplot directory: "+apd);
+            }
+        }
+        File sd= new File( apd, "server" );
+        if ( !sd.exists() ) {
+            if ( !sd.mkdirs() ) {
+                throw new IllegalArgumentException("unable to make server directory: "+sd);
+            }
+        }
+        File ff= new File( sd, "ids.txt" );
+        if ( !ff.exists() ) {
+            try {
+                BufferedWriter w= new BufferedWriter( new FileWriter( ff ) );
+                w.write("# map from regex to local reference.  See http://autoplot.org/servlet_guide.");
+                w.newLine();
+                w.close();
+            } catch ( IOException ex ) {
+                throw ex;
+            }
+            idMapFresh= currentTimeMillis;
+            idMap= new HashMap();
+            return idMap;
+        }
+        long idMapFreshNow= ff.lastModified();
+        if ( idMap==null || idMapFreshNow>idMapFresh ) {
+            synchronized ( ServletUtil.class ) {
+                if ( idMap==null || idMapFreshNow>idMapFresh ) { 
+                    Map<String,String> idMapLocal= new LinkedHashMap<String, String>();
+                    try {
+                        BufferedReader r= new BufferedReader( new FileReader( ff ) );
+                        String s= r.readLine();
+                        while ( s!=null ) {
+                            int i= s.indexOf("#");
+                            if ( i>-1 ) s= s.substring(0,i);
+                            s= s.trim();
+                            if ( s.length()>0 ) {
+                                String[] ss= s.split("\\s+");
+                                if ( ss.length!=2 ) {
+                                    System.err.println("skipping malformed line: "+s);
+                                } else {
+                                    idMapLocal.put(ss[0],ss[1]);
+                                }
+                            }
+                            s= r.readLine();
+                        }
+                    } catch ( IOException ex ) {
+                        throw ex; 
+                    }
+                    idMap= idMapLocal;
+                    idMapFresh= currentTimeMillis;
+                }
+            }
+        }
+        return idMap;
+    }
 }
