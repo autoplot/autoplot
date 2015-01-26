5aebd5e5f6a6239ffca43e7fcc34eba5ed599f37
mmclouth <mmclouth@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-01-26 21:53:59 +0000

    Added Test_3pt12_FilterChainGUI.java.  Jemmy Test tests the filters tree on the data panel and adds "Slice0" and "Add" filters.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@16113 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VATesting/src/org/autoplot/test/Test_3pt12_FilterChainGUI.java
@@ -0,0 +1,133 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.autoplot.test;
+
+
+import java.awt.Component;
+import java.awt.Point;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+import javax.swing.tree.TreePath;
+import org.netbeans.jemmy.Scenario;
+import org.netbeans.jemmy.operators.DialogOperator;
+import org.netbeans.jemmy.operators.JMenuBarOperator;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JCheckBoxOperator;
+import org.netbeans.jemmy.operators.JFrameOperator;
+import org.netbeans.jemmy.operators.JLabelOperator;
+import org.netbeans.jemmy.operators.JListOperator;
+import org.netbeans.jemmy.operators.JPopupMenuOperator;
+import org.netbeans.jemmy.operators.JRadioButtonOperator;
+import org.netbeans.jemmy.operators.JScrollPaneOperator;
+import org.netbeans.jemmy.operators.JSpinnerOperator;
+import org.netbeans.jemmy.operators.JSpinnerOperator.NumberSpinnerOperator;
+import org.netbeans.jemmy.operators.JTabbedPaneOperator;
+import org.netbeans.jemmy.operators.JTableOperator;
+import org.netbeans.jemmy.operators.JTextComponentOperator;
+import org.netbeans.jemmy.operators.JTextFieldOperator;
+import org.netbeans.jemmy.operators.JTreeOperator;
+import org.netbeans.jemmy.operators.ListOperator;
+import org.virbo.autoplot.AutoplotUI;
+import org.virbo.autoplot.ScriptContext;
+import static org.virbo.autoplot.ScriptContext.save;
+import static org.virbo.autoplot.ScriptContext.writeToPng;
+import util.RegexComponentChooser;
+
+/**
+ *
+ * @author kenziemclouth
+ */
+public class Test_3pt12_FilterChainGUI implements Scenario {
+    
+    @Override
+    public int runIt(Object o) {
+
+        try {
+            ScriptContext.createGui();
+            
+            AutoplotUI app= (AutoplotUI) ScriptContext.getViewWindow();
+            
+            JFrameOperator mainFrame = new JFrameOperator(app);
+        
+            // wait for the application to be in the "ready" state.
+            new JLabelOperator(mainFrame).waitText( AutoplotUI.READY_MESSAGE );
+            
+            Thread.sleep(500);
+            
+            new JTextFieldOperator(app.getDataSetSelector().getEditor()).setText("vap+inline:ripples(100,110)+randn(100)/50+outerProduct(ones(100),randn(110)/50)");
+            new JButtonOperator(app.getDataSetSelector().getGoButton()).clickMouse();
+            
+            new JMenuBarOperator( mainFrame ).pushMenu("Options|Enable Feature|Data Panel", "|");
+            
+            Thread.sleep(500);
+            new JLabelOperator(mainFrame).waitText( AutoplotUI.READY_MESSAGE );
+            
+            new JTabbedPaneOperator( app.getTabs() ).selectPage("data");
+
+            JButtonOperator search = new JButtonOperator(mainFrame,7);
+            search.pushNoBlock();
+            
+            //add the "slice0()" filter
+            DialogOperator editFilterFrame = new DialogOperator( new RegexComponentChooser( "Edit Filters") );           
+            new JButtonOperator(editFilterFrame).push();           
+            DialogOperator addFilterFrame = new DialogOperator( new RegexComponentChooser( "Add Filter") );           
+            JTreeOperator filterTree = new JTreeOperator(addFilterFrame);
+            filterTree.clickOnPath(filterTree.getPathForRow(2), 2);  //open Data Set Operations branch
+            filterTree.selectRow(11); //select Slice0
+            new JButtonOperator(addFilterFrame,"OK").clickMouse();           
+            new JSpinnerOperator(editFilterFrame).getIncreaseOperator().clickMouse(10);
+            new JButtonOperator(editFilterFrame, "OK").clickMouse();
+            
+            search.pushNoBlock();
+            
+            //add the "add" filter
+            editFilterFrame = new DialogOperator( new RegexComponentChooser( "Edit Filters") );           
+            new JButtonOperator(editFilterFrame).push();
+            addFilterFrame = new DialogOperator( new RegexComponentChooser( "Add Filter") );
+            filterTree = new JTreeOperator(addFilterFrame);
+            filterTree.selectRow(7); //select Add
+            new JButtonOperator(addFilterFrame,"OK").clickMouse();
+            new JTextFieldOperator(editFilterFrame, "1.").setText("5");
+            new JButtonOperator(editFilterFrame, "OK").clickMouse();
+            
+            new JTabbedPaneOperator( app.getTabs() ).selectPage("canvas");
+            Thread.sleep(5000);
+            System.err.println("Done!");
+            
+            writeToPng("Test_3pt12_FilterChainGUI.png"); // Leave artifacts for testing.
+            save("Test_3pt12_FilterChainGUI.vap");
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            return(0);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Test_3pt12_FilterChainGUI.class.getName()).log(Level.SEVERE, null, ex);
+            return(1);
+        } catch (IOException ex) {
+            Logger.getLogger(Test_3pt12_FilterChainGUI.class.getName()).log(Level.SEVERE, null, ex);
+            return(2);
+        }
+        
+    }
+    public static void main(String[] argv) {
+	String[] params = {"org.autoplot.test.Test_3pt12_FilterChainGUI"};
+	org.netbeans.jemmy.Test.main(params);
+    }
+    
+}
