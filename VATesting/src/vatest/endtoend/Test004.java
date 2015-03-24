5dd767009b69f047bc82905c2b4e06b1cfe3a7f5
aizuchi <aizuchi@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-03-24 19:48:01 +0000

    Refactored to for Java 8 static imports. Removed unused imports. Using local test data tree.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@16512 0c594bb0-8543-0410-a25e-d8ec440261db

--- VATesting/src/vatest/endtoend/Test004.java
+++ VATesting/src/vatest/endtoend/Test004.java
@@ -6,7 +6,13 @@
 package vatest.endtoend;
 
 import java.io.IOException;
-import org.virbo.autoplot.ScriptContext;
+import static java.lang.System.err;
+import static java.lang.System.exit;
+import static org.virbo.autoplot.ScriptContext.getDocumentModel;
+import static org.virbo.autoplot.ScriptContext.load;
+import static org.virbo.autoplot.ScriptContext.setCanvasSize;
+import static org.virbo.autoplot.ScriptContext.writeToPng;
+import static vatest.endtoend.VATestSupport.TEST_VAP;
 
 /**
  * Test Jan's products
@@ -15,17 +21,17 @@ import org.virbo.autoplot.ScriptContext;
 public class Test004 {
     public static void main(String[] args) {
         try {
-            ScriptContext.getDocumentModel().getOptions().setAutolayout(false);
-            ScriptContext.getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
+            getDocumentModel().getOptions().setAutolayout(false);
+            getDocumentModel().getCanvases(0).getMarginColumn().setRight("100%-10em");
 
-            ScriptContext.load( "/home/jbf/ct/hudson/vap/merka_celias_test004_v1.04.vap" );
-            System.err.println( "#### model pending changes: " + ScriptContext.getDocumentModel().getController().isPendingChanges() );
-            ScriptContext.setCanvasSize( 992, 711 );
-            ScriptContext.writeToPng( "test004.png" );
-            System.exit(0);  // TODO: something is firing up the event thread
-        } catch ( Exception ex ) {
+            load( TEST_VAP + "merka_celias_test004_v1.04.vap" );
+            err.println("#### model pending changes: " + getDocumentModel().getController().isPendingChanges() );
+            setCanvasSize( 992, 711 );
+            writeToPng( "test004.png" );
+            exit(0);  // TODO: something is firing up the event thread
+        } catch ( IOException | InterruptedException ex ) {
             ex.printStackTrace();
-            System.exit(1);
+            exit(1);
         }
     }
 }
