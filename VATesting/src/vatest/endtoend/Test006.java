ad2b0c99386ef8aecc199859a5a81681eb507d4b
aizuchi <aizuchi@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-03-25 19:24:17 +0000

    URL fixed. Imports cleaned up.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@16521 0c594bb0-8543-0410-a25e-d8ec440261db

--- VATesting/src/vatest/endtoend/Test006.java
+++ VATesting/src/vatest/endtoend/Test006.java
@@ -4,13 +4,19 @@
  */
 package vatest.endtoend;
 
-import java.io.IOException;
 import java.io.PrintWriter;
+import static java.lang.String.format;
+import static java.lang.System.currentTimeMillis;
+import static java.lang.System.err;
+import static java.lang.System.exit;
 import static org.virbo.autoplot.ScriptContext.*;
 import org.virbo.dataset.MutablePropertyDataSet;
 import org.virbo.dataset.QDataSet;
-import org.virbo.dsops.Ops;
-import org.virbo.jythonsupport.Util;
+import static org.virbo.dataset.QDataSet.DEPEND_0;
+import static org.virbo.dataset.QDataSet.LABEL;
+import static org.virbo.dataset.QDataSet.TITLE;
+import static org.virbo.dsops.Ops.autoHistogram;
+import static org.virbo.jythonsupport.Util.getDataSet;
 
 /**
  * Misc tests
@@ -22,26 +28,26 @@ public class Test006 {
     public static Exception doTest( int id, String uri, Exception ex ) throws Exception {
         try {
             QDataSet ds;
-            long t0= System.currentTimeMillis();
-            ds= Util.getDataSet( uri );
-            double t= (System.currentTimeMillis()-t0)/1000.;
-            MutablePropertyDataSet hist= (MutablePropertyDataSet) Ops.autoHistogram(ds);
-            hist.putProperty( QDataSet.TITLE, uri );
-            String label= String.format( "test006_%03d", id );
-            hist.putProperty( QDataSet.LABEL, label );
+            long t0= currentTimeMillis();
+            ds= getDataSet( uri );
+            double t= (currentTimeMillis()-t0)/1000.;
+            MutablePropertyDataSet hist= (MutablePropertyDataSet) autoHistogram(ds);
+            hist.putProperty(TITLE, uri );
+            String label= format( "test006_%03d", id );
+            hist.putProperty(LABEL, label );
             formatDataSet( hist, label+".qds");
 
-            QDataSet dep0= (QDataSet) ds.property( QDataSet.DEPEND_0 );
+            QDataSet dep0= (QDataSet) ds.property(DEPEND_0);
             if ( dep0!=null ) {
-                MutablePropertyDataSet hist2= (MutablePropertyDataSet) Ops.autoHistogram(dep0);
+                MutablePropertyDataSet hist2= (MutablePropertyDataSet) autoHistogram(dep0);
                 formatDataSet( hist2, label+".dep0.qds");
             } else {
-                PrintWriter pw= new PrintWriter( label+".dep0.qds" );
-                pw.println("no dep0");
-                pw.close();
+                try (PrintWriter pw = new PrintWriter( label+".dep0.qds" )) {
+                    pw.println("no dep0");
+                }
             }
 
-            System.err.printf( "Read in %9.3f seconds (%s): %s\n", t, label, uri );
+            err.printf( "Read in %9.3f seconds (%s): %s\n", t, label, uri );
             } catch ( Exception e ) {
                 e.printStackTrace();
                 if ( ex!=null ) ex= e;
@@ -53,7 +59,7 @@ public class Test006 {
         try {
 
             Exception e=null;
-            e= doTest( 0, "ftp://nssdcftp.gsfc.nasa.gov/spacecraft_data/omni/omni2_$Y.dat?column=field17&timerange=1963&timeFormat=$Y+$j+$H&time=field0&validMax=999", e );
+            e= doTest( 0, "ftp://spdf.gsfc.nasa.gov/pub/data/omni/low_res_omni/omni2_$Y.dat?column=field17&timerange=1963&timeFormat=$Y+$j+$H&time=field0&validMax=999", e );
             e= doTest( 1, "http://goes.ngdc.noaa.gov/data/avg/2004/A1050412.TXT?skip=23&timeFormat=$y$m$d+$H$M&column=E1&time=YYMMDD", e );
             e= doTest( 2, "vap+dat:file:///home/jbf/ct/hudson/data.backup/dat/V11979066.CSV?depend0=field0&column=field13", e );
             e= doTest( 3, "file:///home/jbf/ct/hudson/data.backup/dat/power.dat.txt", e );  //case where guess cadence is failing, though it shouldn't.
@@ -65,10 +71,10 @@ public class Test006 {
                 throw e;
             }
             
-            System.exit(0);  // TODO: something is firing up the event thread
+            exit(0);  // TODO: something is firing up the event thread
         } catch ( Exception ex) {
             ex.printStackTrace();
-            System.exit(1);
+            exit(1);
         }
     }
 }
