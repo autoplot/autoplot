fa757923577a06be9d14ba2cb0ed6d3152fc4700
mmclouth <mmclouth@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-03-25 19:15:00 +0000

    Added fftMultiThread() that has a parameter for number of threads..  Still needs testing!
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@16520 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/Experiment1.java
+++ VirboAutoplot/src/test/Experiment1.java
@@ -6,6 +6,7 @@
 package test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.das2.components.DasProgressPanel;
@@ -32,38 +33,48 @@ public class Experiment1 {
     private static double speed;
     
     public static void main( String[] args ) throws InterruptedException {
-        int size = 5;
+        int size = 15;
         double[] speedArray2 = new double[size];
         double[] speedArray4 = new double[size];
+        double[] speedArrayMulti = new double[size];
         double[] speedArray8 = new double[size];
         
-        for( int i = 0; i< size ; i = i + 1) {
-            doTwoThreads();
-            speedArray2[i] = speed;
+        //for( int i = 0; i< size ; i = i + 1) {
+          //  doTwoThreads();
+            //speedArray2[i] = speed;
             //System.err.print("Four threads:" + i);
-        }
-        for( int i = 0; i< size ; i = i + 1) {
-            doFourThreads();
-            speedArray4[i] = speed;
-            //System.err.print("Four threads:" + i);
-        }
+        //}
         for( int i = 0; i< size ; i = i + 1) {
             doEightThreads();
             speedArray8[i] = speed;
             //System.err.print("Four threads:" + i);
         }
         for( int i = 0; i< size ; i = i + 1) {
+            fftMultiThread(8);
+            speedArrayMulti[i] = speed;
+            //System.err.print("Four threads:" + i);
+        }
+        //for( int i = 0; i< size ; i = i + 1) {
+          //  doEightThreads();
+            //speedArray8[i] = speed;
+            //System.err.print("Four threads:" + i);
+        //}
+        for( int i = 0; i< size ; i = i + 1) {
+            System.err.println("speedArray8[" + i + "]=" + speedArray8[i]+ "     speedArrayMulti[" + i + "]=" + speedArrayMulti[i]);
             //System.err.println("speedArray2[" + i + "]=" + speedArray2[i] + "     speedArray4[" + i + "]=" + speedArray4[i]);
             //System.err.println("speedArray4[" + i + "]=" + speedArray4[i] + "     speedArray8[" + i + "]=" + speedArray8[i]);
-            System.err.println("speedArray2[" + i + "]=" + speedArray2[i] + "     speedArray4[" + i + "]=" + speedArray4[i] + "     speedArray8[" + i + "]=" + speedArray8[i]);
+           // System.err.println("speedArray2[" + i + "]=" + speedArray2[i] + "     speedArray4[" + i + "]=" + speedArray4[i] + "     speedArray8[" + i + "]=" + speedArray8[i]);
         }
         //QDataSet timesTwo = Ops.dataset(speedArray2);
-        QDataSet timesFour = Ops.dataset(speedArray4);
+        //QDataSet timesFour = Ops.dataset(speedArray4);
+        QDataSet timesMulti = Ops.dataset(speedArrayMulti);
         QDataSet timesEight = Ops.dataset(speedArray8);
         
+        ScriptContext.setLayout(4,1);
         //ScriptContext.createGui();
-        //ScriptContext.plot(1, timesTwo);
-        ScriptContext.plot(2, timesFour);
+        //ScriptContext.plot(0, timesTwo);
+        //ScriptContext.plot(1, timesFour);
+        ScriptContext.plot(2, timesMulti);
         ScriptContext.plot(3, timesEight);
         
         try {
@@ -132,17 +143,73 @@ public class Experiment1 {
     }
     private static final int DATASET_SIZE = 10000;
     
+    private static ArrayList<Runnable> run = new ArrayList<Runnable>();
+    private static ArrayList<ProgressMonitor> mon = new ArrayList<ProgressMonitor>();
+    
+    private static void fftMultiThread(int threads) throws InterruptedException {
+        run.clear();
+        mon.clear();
+        
+        final QDataSet ds= Ops.ripplesWaveformTimeSeries(DATASET_SIZE);
+
+        final QDataSet[] out = new QDataSet[threads+1];
+        ProgressMonitor temp =  getMonitor("original task");
+        mon.add(temp);
+        long t0 = System.currentTimeMillis();
+        out[0]= Ops.fftPower(ds, 512, mon.get(0) );
+        
+        while (!mon.get(0).isFinished()) {
+            Thread.sleep(200);
+        }
+        long time = System.currentTimeMillis() - t0;
+        
+        ScriptContext.setLayout(threads+1,1);
+        ScriptContext.plot( 0, out[0] );
+       
+        for( int i = 0; i< threads ; i = i + 1) {
+            temp= getMonitor("task" + (i+1));
+            mon.add(temp);
+            final int j = i+1;
+            final int thr = threads;
+            Runnable run0= new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    out[j]= Ops.fftPower(ds.trim(((j-1)*DATASET_SIZE)/thr,(j*DATASET_SIZE)/thr), 512, mon.get(j) );
+                    ScriptContext.plot( j, out[j] );
+                } catch ( Exception ex) {
+                    ex.printStackTrace();
+                }
+            }
+            };
+            run.add(run0);
+            if (i==0) {
+                t0 = System.currentTimeMillis();
+            }
+            new Thread(run.get(i)).start();
+        }
+        
+        while( !mon.get(threads - 1).isFinished() && !mon.get(threads).isFinished()  ) {
+            Thread.sleep(200);
+        }
+        long time1 = System.currentTimeMillis() - t0;
+        speed = ((double) time) / time1;
+        
+        try {
+            ScriptContext.writeToPng("/tmp/Experiment1_graphs.png");
+        } catch (IOException ex) {
+            Logger.getLogger(Experiment1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
     private static void doFourThreads() throws InterruptedException {
         //ScriptContext.createGui();
         final QDataSet ds= Ops.ripplesWaveformTimeSeries(DATASET_SIZE);
 
-        
-        //QDataSet[] out = new QDataSet[4];
         final ProgressMonitor mon0= getMonitor("original task");
         long t0 = System.currentTimeMillis();
         QDataSet out = Ops.fftPower(ds, 512, mon0 );
-        //out[0]= Ops.fftPower(ds, 512, mon0 );
-        
+       
         while (!mon0.isFinished()) {
             Thread.sleep(200);
         }
@@ -152,18 +219,15 @@ public class Experiment1 {
         ScriptContext.setLayout(5,1);
         
         //ScriptContext.plot( 0, out );
-        //for( int i = 0; i< 5 ; i = i + 1) {
-          //  final ProgressMonitor mon1= getMonitor("task" + (i+1));
-            //out[i]= Ops.fftPower(ds.trim((i*DATASET_SIZE)/4,((i+1)*DATASET_SIZE)/4), 512, mon1 );
-            
-            //};
-        //}
+       
+      
         
         final ProgressMonitor mon1= getMonitor("task 1");
         final ProgressMonitor mon2= getMonitor("task 2");
         final ProgressMonitor mon3= getMonitor("task 3");
         final ProgressMonitor mon4= getMonitor("task 4");
-                
+              
+       
         Runnable run1= new Runnable() {
             @Override
             public void run() {
