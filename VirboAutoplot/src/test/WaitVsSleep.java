bc7d03713e621d8671fd77b5231aa9366957a5fd
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-04-20 12:49:39 +0000

    code to experiment with using wait vs sleep vs "the right way"
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@16711 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/test/WaitVsSleep.java
@@ -0,0 +1,64 @@
+
+package test;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.virbo.autoplot.AutoplotUI;
+
+/**
+ * demonstrate the difference between wait and sleep
+ * @author faden@cottagesystems.com
+ */
+public class WaitVsSleep {
+    static double d= 0;
+    
+    public static void sleepDemo() {
+        Runnable run= new Runnable() {
+            public void run() {
+                while ( true ) {
+                    try {
+                        Thread.sleep(10);
+                        if ( d==1 ) {
+                            break;
+                        }
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(WaitVsSleep.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            }
+        };
+        new Thread( run,"sleep" ).start();
+    }
+    
+    
+    public static void waitDemo() {
+        Runnable run= new Runnable() {
+            public void run() {
+                synchronized ( WaitVsSleep.class ) {
+                    while ( true ) {
+                        try {
+                            WaitVsSleep.class.wait(10);
+                            if ( d==1 ) {
+                                break;
+                            }
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(WaitVsSleep.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+                }
+            }
+        };
+        new Thread( run,"sleep" ).start();
+    }
+    
+        
+    public static void main( String[] args ) {
+        waitDemo();
+        //sleepDemo();
+        String pid= AutoplotUI.getProcessId("???");
+        JOptionPane.showMessageDialog( null, "Click Okay to stop loop PID="+pid );
+        d=1;
+    }
+    
+}
