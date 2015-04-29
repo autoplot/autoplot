0a7b95408cfec3df30d19444c010aeeeb6bbbfa5
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-04-29 11:39:07 +0000

    1376: comment the place where the filename is available.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@16761 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/scriptconsole/ScriptPanelSupport.java
+++ VirboAutoplot/src/org/virbo/autoplot/scriptconsole/ScriptPanelSupport.java
@@ -361,6 +361,7 @@ public class ScriptPanelSupport {
         if (ex instanceof PySyntaxError) {
             logger.log(Level.SEVERE, ex.getMessage(), ex);
             int lineno = offset + ((PyInteger) ex.value.__getitem__(1).__getitem__(1)).getValue();
+            //String filename= String.valueOf( (ex.value.__getitem__(1).__getitem__(3)) );
             //int col = ((PyInteger) ex.value.__getitem__(1).__getitem__(2)).getValue();
             annotationsSupport.annotateLine(lineno, "error", ex.toString(),interp);
         } else {
