51cdca2b19112dc1ee404c8a1bd77a5e1998422a
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-05-15 16:16:45 +0000

    tweak to GUI to clarify that DEPO was never coded.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@16840 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/autoplot/html/HtmlTableDataSourceEditorPanel.java
+++ DataSourcePack/src/org/autoplot/html/HtmlTableDataSourceEditorPanel.java
@@ -49,13 +49,11 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
     Map<String, String> params;
     URISplit split;
 
+    @Override
     public boolean reject( String url ) throws IOException, URISyntaxException {
         split = URISplit.parse(url);
         FileSystem fs = FileSystem.create( DataSetURI.getWebURL( DataSetURI.toUri(split.path) ).toURI() );
-        if ( fs.isDirectory( split.file.substring(split.path.length()) ) ) {
-            return true;
-        }
-        return false;
+        return fs.isDirectory( split.file.substring(split.path.length()) );
     }
     
     public boolean prepare(String uri, Window parent, ProgressMonitor mon) throws Exception {
@@ -67,6 +65,7 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
         return true;
     }
 
+    @Override
     public void markProblems(List<String> problems) {
         
     }
@@ -83,6 +82,7 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
 
         jTable1.getColumnModel().getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 
+            @Override
             public void valueChanged(ListSelectionEvent e) {
                 if (e.getValueIsAdjusting()) {
                     return;
@@ -94,6 +94,7 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
 
         jTable1.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 
+            @Override
             public void valueChanged(ListSelectionEvent e) {
                 if (e.getValueIsAdjusting()) {
                     return;
@@ -118,7 +119,7 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
                 }
                 if ( currentTool==Tool.DEPEND_0 ) {
                     params.put(PROP_DEP0, name);
-                    dep0Columns.setSelectedItem(name);
+                    //dep0Columns.setSelectedItem(name);
                     clearTool(); // otherwise we would respond to deselection event
                 } else if ( currentTool==Tool.COLUMN ) {
                     params.put(PROP_COLUMN, name);
@@ -148,7 +149,7 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
             }
             if (currentTool == Tool.DEPEND_0) {
                 params.put(PROP_DEP0, name);
-                dep0Columns.setSelectedItem(name);
+                //dep0Columns.setSelectedItem(name);
 
             } else if (currentTool == Tool.COLUMN) {
                 params.put(PROP_COLUMN, name);
@@ -186,6 +187,7 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
     Action createToolAction(final String label, final Tool t) {
         return new AbstractAction(label) {
 
+            @Override
             public void actionPerformed(ActionEvent e) {
                 if ( e.getSource() instanceof JToggleButton ) {
                     if ( jTable1.getSelectionModel().isSelectionEmpty() ) {
@@ -228,38 +230,20 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
         jTable1 = new javax.swing.JTable();
         jPanel1 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        dep0Columns = new javax.swing.JComboBox();
         columnsComboBox = new javax.swing.JComboBox();
         jToggleButton2 = new javax.swing.JToggleButton();
-        jToggleButton3 = new javax.swing.JToggleButton();
         jLabel2 = new javax.swing.JLabel();
         tableComboBox = new javax.swing.JComboBox();
         jLabel1 = new javax.swing.JLabel();
         firstRowTextField = new javax.swing.JFormattedTextField();
         jToggleButton1 = new javax.swing.JToggleButton();
+        jLabel5 = new javax.swing.JLabel();
 
         jTable1.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_OFF);
         jScrollPane1.setViewportView(jTable1);
 
         jLabel3.setText("Column:");
         jLabel3.setToolTipText("Select the column to plot");
-
-        jLabel4.setText("Depends On:");
-        jLabel4.setEnabled(false);
-
-        dep0Columns.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-        dep0Columns.setEnabled(false);
-        dep0Columns.addItemListener(new java.awt.event.ItemListener() {
-            public void itemStateChanged(java.awt.event.ItemEvent evt) {
-                dep0ColumnsItemStateChanged(evt);
-            }
-        });
-        dep0Columns.addFocusListener(new java.awt.event.FocusAdapter() {
-            public void focusGained(java.awt.event.FocusEvent evt) {
-                dep0ColumnsFocusGained(evt);
-            }
-        });
 
         columnsComboBox.setEditable(true);
         columnsComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
@@ -277,11 +261,6 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
         jToggleButton2.setAction(createToolAction( "column", Tool.COLUMN ));
         jToggleButton2.setText("Select");
         jToggleButton2.setToolTipText("Select the column to plot by pushing this button and then clicking on a column of the table.\n");
-
-        jToggleButton3.setAction(createToolAction( "depend0", Tool.DEPEND_0 ));
-        jToggleButton3.setText("Select");
-        jToggleButton3.setToolTipText("Select the column containing the indepenent variable to plot against by pressing this button and then clicking on the table.\n");
-        jToggleButton3.setEnabled(false);
 
         jLabel2.setText("Table:");
         jLabel2.setToolTipText("Select the table within the html to use.  Note tables are often used for layout on web pages, so \nthere will probably be more tables than expected.\n");
@@ -304,34 +283,34 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
         jToggleButton1.setToolTipText("Select the first row to start the data by clicking on this button then a row of the table.\n");
         jToggleButton1.setEnabled(false);
 
+        jLabel5.setFont(jLabel5.getFont().deriveFont(jLabel5.getFont().getSize()-2f));
+        jLabel5.setText("Note the HTML parser has never been able to parse an independent param.  See File->Add Plot.");
+
         org.jdesktop.layout.GroupLayout jPanel1Layout = new org.jdesktop.layout.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(jPanel1Layout.createSequentialGroup()
-                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
-                    .add(jLabel4)
-                    .add(jLabel3))
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(31, 31, 31)
                 .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                     .add(jPanel1Layout.createSequentialGroup()
-                        .add(dep0Columns, 0, 447, Short.MAX_VALUE)
+                        .add(jLabel3)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(jToggleButton3)
-                        .add(18, 18, 18)
-                        .add(jLabel1)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(firstRowTextField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 94, Short.MAX_VALUE)
-                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(jToggleButton1))
-                    .add(jPanel1Layout.createSequentialGroup()
                         .add(columnsComboBox, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 190, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                         .add(jToggleButton2)
                         .add(36, 36, 36)
                         .add(jLabel2)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                        .add(tableComboBox, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 158, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)))
+                        .add(tableComboBox, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 158, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                    .add(jPanel1Layout.createSequentialGroup()
+                        .add(jLabel5, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 568, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                        .add(18, 18, 18)
+                        .add(jLabel1)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(firstRowTextField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 96, Short.MAX_VALUE)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(jToggleButton1)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -345,13 +324,10 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
                     .add(tableComboBox, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(jLabel4)
-                    .add(dep0Columns, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(jToggleButton3, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 24, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                     .add(jToggleButton1)
                     .add(firstRowTextField, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                    .add(jLabel1))
-                .addContainerGap(12, Short.MAX_VALUE))
+                    .add(jLabel1)
+                    .add(jLabel5)))
         );
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
@@ -359,17 +335,31 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(jPanel1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 823, Short.MAX_VALUE)
+            .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 854, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
                 .add(jPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 381, Short.MAX_VALUE)
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 393, Short.MAX_VALUE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void tableComboBoxItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_tableComboBoxItemStateChanged
+        final String ltable = (String) tableComboBox.getSelectedItem();
+        params.put(PROP_TABLE, ltable);
+        setTable(ltable);
+    }//GEN-LAST:event_tableComboBoxItemStateChanged
+
+    private void columnsComboBoxFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_columnsComboBoxFocusGained
+
+    }//GEN-LAST:event_columnsComboBoxFocusGained
+
+    private void columnsComboBoxItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_columnsComboBoxItemStateChanged
+        params.put(PROP_COLUMN, (String) columnsComboBox.getSelectedItem());
+    }//GEN-LAST:event_columnsComboBoxItemStateChanged
 
 
     private void resetFile() {
@@ -443,26 +433,6 @@ public class HtmlTableDataSourceEditorPanel extends javax.swing.JPanel implement
         }
 
     }
-
-private void tableComboBoxItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_tableComboBoxItemStateChanged
-    final String ltable = (String) tableComboBox.getSelectedItem();
-    params.put(PROP_TABLE, ltable);
-    setTable(ltable);
-}//GEN-LAST:event_tableComboBoxItemStateChanged
-
-private void dep0ColumnsItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_dep0ColumnsItemStateChanged
-    params.put(PROP_DEP0, (String) dep0Columns.getSelectedItem());
-}//GEN-LAST:event_dep0ColumnsItemStateChanged
-
-private void dep0ColumnsFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_dep0ColumnsFocusGained
-}//GEN-LAST:event_dep0ColumnsFocusGained
-
-private void columnsComboBoxItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_columnsComboBoxItemStateChanged
-    params.put(PROP_COLUMN, (String) columnsComboBox.getSelectedItem());
-}//GEN-LAST:event_columnsComboBoxItemStateChanged
-
-private void columnsComboBoxFocusGained(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_columnsComboBoxFocusGained
-}//GEN-LAST:event_columnsComboBoxFocusGained
     protected File file = null;
     public static final String PROP_FILE = "file";
 
@@ -494,6 +464,7 @@ private void columnsComboBoxFocusGained(java.awt.event.FocusEvent evt) {//GEN-FI
     public static final String PROP_COLUMN = "column";
     public static final String PROP_DEP0 = "depend0";
 
+    @Override
     public JPanel getPanel() {
         return this;
     }
@@ -506,6 +477,7 @@ private void columnsComboBoxFocusGained(java.awt.event.FocusEvent evt) {//GEN-FI
         }
     }
 
+    @Override
     public void setURI(String url) {
         try {
             split = URISplit.parse(url);
@@ -521,7 +493,7 @@ private void columnsComboBoxFocusGained(java.awt.event.FocusEvent evt) {//GEN-FI
             } 
 
             if ( params.get(PROP_COLUMN)!=null ) columnsComboBox.setSelectedItem( params.get(PROP_COLUMN) );
-            if ( params.get(PROP_DEP0)!=null ) dep0Columns.setSelectedItem( params.get(PROP_DEP0) );
+            //if ( params.get(PROP_DEP0)!=null ) dep0Columns.setSelectedItem( params.get(PROP_DEP0) );
 
             firstRowTextField.setValue(getIntValue(PROP_FIRST_ROW, 1));
 
@@ -534,6 +506,7 @@ private void columnsComboBoxFocusGained(java.awt.event.FocusEvent evt) {//GEN-FI
 
     }
 
+    @Override
     public String getURI() {
 
         split.params = URISplit.formatParams(params);
@@ -543,18 +516,16 @@ private void columnsComboBoxFocusGained(java.awt.event.FocusEvent evt) {//GEN-FI
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JComboBox columnsComboBox;
-    public javax.swing.JComboBox dep0Columns;
     public javax.swing.JFormattedTextField firstRowTextField;
     public javax.swing.JLabel jLabel1;
     public javax.swing.JLabel jLabel2;
     public javax.swing.JLabel jLabel3;
-    public javax.swing.JLabel jLabel4;
+    public javax.swing.JLabel jLabel5;
     public javax.swing.JPanel jPanel1;
     public javax.swing.JScrollPane jScrollPane1;
     public javax.swing.JTable jTable1;
     public javax.swing.JToggleButton jToggleButton1;
     public javax.swing.JToggleButton jToggleButton2;
-    public javax.swing.JToggleButton jToggleButton3;
     public javax.swing.JComboBox tableComboBox;
     // End of variables declaration//GEN-END:variables
 }
