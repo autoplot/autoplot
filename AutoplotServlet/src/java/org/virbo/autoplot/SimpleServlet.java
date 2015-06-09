8ae9aa96a2aaf09306693934e2ad6eb39013821e
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-06-09 14:57:08 +0000

    add ServletInfo so we can get settings from the client side.  Update version number.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@16923 0c594bb0-8543-0410-a25e-d8ec440261db

--- AutoplotServlet/src/java/org/virbo/autoplot/SimpleServlet.java
+++ AutoplotServlet/src/java/org/virbo/autoplot/SimpleServlet.java
@@ -80,7 +80,7 @@ import org.virbo.dsops.Ops;
 public class SimpleServlet extends HttpServlet {
 
     private static final Logger logger= Logger.getLogger("autoplot.servlet" );
-    public static final String version= "v20150212.2049";
+    public static final String version= "v20150609.0955";
 
     static FileHandler handler;
 
