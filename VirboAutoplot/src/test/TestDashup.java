4c897d68db7c31b8d22ce41dbe41017ffbeef34c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-06-23 22:07:47 +0000

    development of Dashup
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17000 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/TestDashup.java
+++ VirboAutoplot/src/test/TestDashup.java
@@ -6,7 +6,9 @@
 package test;
 
 import java.util.Arrays;
+import javax.swing.JDialog;
 import javax.swing.JOptionPane;
+import org.virbo.jythonsupport.ui.DataMashUp;
 import org.virbo.jythonsupport.ui.NamedURIListTool;
 
 /**
@@ -14,11 +16,26 @@ import org.virbo.jythonsupport.ui.NamedURIListTool;
  * @author jbf
  */
 public class TestDashup {
-       public static void main( String[] args ) {
+    public static void main( String[] args ) {
+        //test1();
+        test2();
+    }
+    
+    public static void test2() {
+        DataMashUp dmu= new DataMashUp();
+        JDialog dia= new JDialog();
+        dia.setResizable(true);
+        dia.getContentPane().add(dmu);
+        dia.pack();
+        dia.setVisible(true);
+        //JOptionPane.showConfirmDialog( null, dmu, "Data Mashup, or Dashup", JOptionPane.OK_OPTION );
+    }
+    
+    public static void test1() {
         NamedURIListTool n= new NamedURIListTool();
         
         n.setIds( Arrays.asList( "data1", "data2" ) );
-        n.setUris( Arrays.asList("http://autoplot.org/data/autoplot.org?","http://autoplot.org/data/autoplot.org?") );
+        n.setUris( Arrays.asList("http://autoplot.org/data/autoplot.cdf?","http://autoplot.org/data/autoplot.cdf?") );
         n.refresh();
         JOptionPane.showConfirmDialog(null,n);
     }
