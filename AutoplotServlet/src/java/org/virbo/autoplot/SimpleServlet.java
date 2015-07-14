c3699f873b51b9edcdf0eb5f266f967df550dd89
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-07-14 11:39:42 +0000

    version 20150714.0636
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17114 0c594bb0-8543-0410-a25e-d8ec440261db

--- AutoplotServlet/src/java/org/virbo/autoplot/SimpleServlet.java
+++ AutoplotServlet/src/java/org/virbo/autoplot/SimpleServlet.java
@@ -80,7 +80,7 @@ import org.virbo.dsops.Ops;
 public class SimpleServlet extends HttpServlet {
 
     private static final Logger logger= Logger.getLogger("autoplot.servlet" );
-    public static final String version= "v20150702.1015";
+    public static final String version= "v20150714.0636";
 
     static FileHandler handler;
 
