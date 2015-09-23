a798b6a6dc57cef83108ca89854be64e9e961837
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-09-23 15:09:39 +0000

    add abstract class to implement functionality for DataSourceFormatEditorPanels, so that arguments are consistent.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17514 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/AbstractDataSourceFormat.java
+++ DataSource/src/org/virbo/datasource/AbstractDataSourceFormat.java
@@ -34,12 +34,35 @@ public abstract class AbstractDataSourceFormat implements DataSourceFormat {
         return resourceUri;
     }
 
+    /**
+     * return the string parameter.  Note setUri must be called with
+     * the input URI.
+     * @param name the parameter name.
+     * @param deflt the default value should the parameter be missing.
+     * @return the value
+     */    
     public String getParam( String name, String deflt ) {
         String result= params.get(name);
         if ( result==null ) {
             return deflt;
         } else {
             return result;
+        }
+    }
+    
+    /**
+     * return the boolean parameter.  Note setUri must be called with
+     * the input URI.
+     * @param name the parameter name.
+     * @param deflt the default value should the parameter be missing or misformed.
+     * @return the value
+     */
+    public boolean getBooleanParam( String name, boolean deflt ) {
+        String s= params.get(name);
+        if ( s==null || s.length()==0 ) {
+            return deflt;
+        } else {
+            return 'F'==s.substring(0,1).toUpperCase().charAt(0);
         }
     }
 }
