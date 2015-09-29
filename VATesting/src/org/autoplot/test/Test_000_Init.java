ca38d2070f5e7f29ca2a9e8533fda765daa5c390
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-09-29 15:44:08 +0000

    introduce init so that the mouse position on the screen doesn't affect results.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17556 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VATesting/src/org/autoplot/test/Test_000_Init.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.autoplot.test;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.netbeans.jemmy.Scenario;
+import org.virbo.autoplot.AutoplotUI;
+import static org.virbo.autoplot.ScriptContext.createGui;
+import static org.virbo.autoplot.ScriptContext.getApplicationModel;
+import static org.virbo.autoplot.ScriptContext.getViewWindow;
+import org.virbo.autoplot.scriptconsole.DumpRteExceptionHandler;
+
+/**
+ * Verify that Jemmy will work, set the mouse position for tests.
+ * @author jbf
+ */
+public class Test_000_Init implements Scenario {
+
+    @Override
+    public int runIt(Object param) {
+        
+        getApplicationModel().setExceptionHandler( new DumpRteExceptionHandler() );
+            
+        createGui();
+        AutoplotUI app = (AutoplotUI) getViewWindow();
+        
+        try {
+            new Robot().mouseMove(app.getX() + 100, app.getY() + 100);
+        } catch (AWTException ex) {
+            Logger.getLogger(Test_037_ZoomPan.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+        
+    }
+    
+    public static void main(String[] argv) {
+	String[] params = {"org.autoplot.test.Test_000_Init"};
+	org.netbeans.jemmy.Test.main(params);
+    }    
+
+}
