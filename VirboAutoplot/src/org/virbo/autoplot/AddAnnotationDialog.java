1b2c3f373fecc45a043ba735666e165e4d5c4ad6
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-10-12 22:43:58 +0000

    add annotation dialog (NEEDS REVIEW)
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17683 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/AddAnnotationDialog.java
+++ VirboAutoplot/src/org/virbo/autoplot/AddAnnotationDialog.java
@@ -18,6 +18,10 @@ public class AddAnnotationDialog extends javax.swing.JPanel {
         initComponents();
     }
 
+    public String getText() {
+        return jTextField1.getText();
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
