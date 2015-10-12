a1ba49beb3532375f5f818b73fa5e89e1470e64a
aluthens <aluthens@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-10-12 22:09:36 +0000

    moved html header and closing into main method so all of the 'doTests' will run in between to populate a table
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17680 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test026.java
+++ VirboAutoplot/src/test/endtoend/Test026.java
@@ -106,7 +106,10 @@ public class Test026 {
     }
     
     public static void writeToHTML(int id, DatumRange test, DatumRange ref){
+        /*
         String outputFolder= "/home/aluthens/batch/test026/";
+        
+        String outputFolder= ".";
         String filePath= outputFolder+"Examples.html";
         
         File f= new File(filePath);
@@ -115,19 +118,30 @@ public class Test026 {
         String headerString="<head><title>Test 026</title></head>";
         String bodyString="<body style=\"background-color: #6B6B6B; margin=0;\">";
         String headerOpen= "<div style=\"top: 0px; margin-right=0px; font-size:40px; background-color:black; color:white;height:100px;\">"
-                    + "TEST 026" + "</div>"; 
+                    + "TEST 026" + "</div>";
+        String tableOpen= "<table border=\"1\" style=\"width:100%; color:white;\">";
+        String htmlClose= "</table></body></html>";
+        */
         
-        String table= "<table border=\"1\" style=\"width:100%; color:white;\"><tr><td><strong>Test Number:</strong> " 
-                + id + "</td><td><strong>Test: </strong> " + test + "</td><td><strong>Ref: </strong> " + ref + "</td></tr></table>";
-        String htmlClose= "</body></html>";
+        String outputFolder= ".";
+        String filePath= outputFolder+"Examples.html";
+        File f= new File(filePath);
+        
+        
+        String table1=  "<tr><td><strong>Test Number:</strong> " + id + "</td>" +
+                        "<td><strong>Test: </strong> " + test + "</td>" + 
+                        "<td><strong>Ref: </strong> " + ref + "</td></tr>";
+        
+        
         
         try ( BufferedWriter bw = new BufferedWriter(new FileWriter(f)) ) {
-            bw.write(htmlOpen); //opens html
-            bw.write(headerString); //writes html header
-            bw.write(bodyString); //opens body and gives style
-            bw.write(headerOpen); //writes header of webpage
-            bw.write(table);
-            bw.write(htmlClose); //closes body and html
+            //bw.write(htmlOpen); //opens html
+            //bw.write(headerString); //writes html header
+            //bw.write(bodyString); //opens body and gives style
+            //bw.write(headerOpen); //writes header of webpage
+            //bw.write(tableOpen);
+            bw.write(table1);
+            //bw.write(htmlClose); //closes body and html
         }
         catch(IOException e){
             System.out.println("ERROR.");
@@ -137,9 +151,27 @@ public class Test026 {
 
     public static void main(String[] args) {
         try {
-
-            // These tests check DatumRange parsing.
+            //write to HTML
+            String outputFolder= ".";
+            String filePath= outputFolder+"Examples.html";
+        
+            File f= new File(filePath);
+        
+            String htmlOpen= "<html>";
+            String headerString="<head><title>Test 026</title></head>";
+            String bodyString="<body style=\"background-color: #6B6B6B; margin=0;\">";
+            String headerOpen= "<div style=\"top: 0px; margin-right=0px; font-size:40px; background-color:black; color:white;height:100px;\">"
+                                + "TEST 026" + "</div>";
+            String tableOpen= "<table border=\"1\" style=\"width:100%; color:white;\">";
+            String htmlClose= "</table></body></html>";
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+            bw.write(htmlOpen); //opens html
+            bw.write(headerString); //writes html header
+            bw.write(bodyString); //opens body and gives style
+            bw.write(headerOpen); //writes header of webpage
+            bw.write(tableOpen);
             
+            //doTests
             doTestDR( 70, "0 to 35", DatumRange.newDatumRange(0,35,Units.dimensionless) );
             doTestDR( 71, "0to35", DatumRange.newDatumRange(0,35,Units.dimensionless) );
             doTestDR( 72, "0 to 35 apples", DatumRange.newDatumRange(0,35,SemanticOps.lookupUnits("apples")) );
@@ -209,6 +241,7 @@ public class Test026 {
             doTest(44, "1972/now-P1D", "1972-01-01T00:00/" + now.subtract(1,Units.days), micros );
             doTest(45, "now-P10D/now-P1D", new DatumRange( now.subtract(10,Units.days), now.subtract(1,Units.days) ).toString(), micros );
             
+            bw.write(htmlClose); //closes body and html
             System.exit(0);  // TODO: something is firing up the event thread
         } catch (Exception ex) {
             ex.printStackTrace();
