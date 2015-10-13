fbff89dce971c92c30351946ece8466e970062ef
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-10-13 15:00:38 +0000

    introduce test049 for various units tests of new codes.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17685 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/test/endtoend/Test049.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.endtoend;
+
+import org.virbo.datasource.DataSourceUtil;
+
+/**
+ * Unit tests added as new codes are introduced.  This will serve mostly
+ * as documentation, but also it doesn't hurt to test.
+ * @author jbf
+ */
+public class Test049 {
+    
+    private static void test000() {
+        if ( ! DataSourceUtil.unescapeParam( "energy.gt(1e+3)" ).equals( "energy.gt(1e+3)" ) ) throw new AssertionError("001");
+        if ( ! DataSourceUtil.unescapeParam( "energy.within(1e+3+to+1e+5)" ).equals( "energy.within(1e+3 to+1e+5)" ) )  throw new AssertionError("002");
+    }
+    
+    public static void main( String[] args ) {
+        test000();
+    }
+}
