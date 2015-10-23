a8b8689178018087db9e5db590dec4a518c1f915
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-10-23 17:28:48 +0000

    bugfix: alpha channel now works.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17791 0c594bb0-8543-0410-a25e-d8ec440261db

--- ImageDataSource/src/org/virbo/imagedatasource/ImageDataSourceFormat.java
+++ ImageDataSource/src/org/virbo/imagedatasource/ImageDataSourceFormat.java
@@ -25,6 +25,7 @@ import org.virbo.datasource.DataSourceFormat;
  */
 public class ImageDataSourceFormat implements DataSourceFormat {
 
+    @Override
     public void formatData(String uri, QDataSet data, ProgressMonitor mon) throws Exception {
         BufferedImage im;
 
@@ -42,10 +43,11 @@ public class ImageDataSourceFormat implements DataSourceFormat {
 
         } else if ( data.length(0,0)==4 ) {
             im= new BufferedImage( data.length(), data.length(0), BufferedImage.TYPE_INT_ARGB);
-            r= DataSetOps.slice2( data, 0 );
-            g= DataSetOps.slice2( data, 1 );
-            b= DataSetOps.slice2( data, 2 );
-            alpha= DataSetOps.slice2( data, 3 );
+            alpha= DataSetOps.slice2( data, 0 );
+            r= DataSetOps.slice2( data, 1 );
+            g= DataSetOps.slice2( data, 2 );
+            b= DataSetOps.slice2( data, 3 );
+            
             w= data.length();
             h= data.length(0);
 
@@ -74,13 +76,20 @@ public class ImageDataSourceFormat implements DataSourceFormat {
         if ( alpha==null ) {
             for ( int i=0; i<w; i++ ) {
                 for ( int j=0; j<h; j++ ) {
-                    im.setRGB( i, h-j-1, (int)( r.value(i,j)*256*256 + g.value(i,j)*256 + b.value(i,j) ) );
+                    im.setRGB( i, h-j-1, 
+                            (int)(r.value(i,j))*256*256 
+                            + (int)(g.value(i,j))*256 
+                            + (int)b.value(i,j) );
                 }
             }
         } else {
             for ( int i=0; i<w; i++ ) {
                 for ( int j=0; j<h; j++ ) {
-                    im.setRGB( i, h-j-1, (int)( alpha.value(i,j)*256*256*256 + r.value(i,j)*256*256 + g.value(i,j)*256 + b.value(i,j) ) );
+                    im.setRGB( i, h-j-1, 
+                            (int)(alpha.value(i,j)) *256*256*256 
+                            + (int)(r.value(i,j))*256*256 
+                            + (int)(g.value(i,j))*256
+                            + (int)(b.value(i,j)) );
                 }
             }
 
@@ -99,10 +108,12 @@ public class ImageDataSourceFormat implements DataSourceFormat {
 
     }
 
+    @Override
     public boolean canFormat(QDataSet ds) {
         return ds.rank()==3 && ( ds.length()<5 || ds.length(0,0)<5 );
     }
 
+    @Override
     public String getDescription() {
         return "Image Format";
     }
