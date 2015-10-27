7092659c78e124b8bb01fcef58823af1515cfea9
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-10-27 19:02:14 +0000

    demo of file watcher.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17818 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/test/DemoFileWatcher.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import static test.WatchDir.cast;
+
+/**
+ *
+ * @author jbf
+ */
+public class DemoFileWatcher {
+    public static void main( String[] args ) throws IOException, InterruptedException {
+        
+        File file= new File("/home/jbf/tmp/externalModificationFile.txt");
+        WatchService watcher = FileSystems.getDefault().newWatchService();
+                
+        Path fpath= file.toPath();
+        
+        Path parent= fpath.getParent();
+        parent.register( watcher, StandardWatchEventKinds.ENTRY_MODIFY );
+        
+        while ( true ) {
+            WatchKey key= watcher.take();
+            for ( WatchEvent e : key.pollEvents() ) {
+                
+                WatchEvent<Path> ev = cast(e);
+                Path name = ev.context();
+                
+                if ( parent.resolve(name).equals(fpath) ) {
+                    System.err.println(ev.kind() + " " + ev.context() );
+                } else {
+                    System.err.println("     "+ev);
+                }
+
+            }
+            
+            if ( !key.reset() ) return;
+            
+        }
+
+    }
+            
+        
+}
