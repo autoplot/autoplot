e7d597d8aaa379d46aa766b1533315764b0569ca
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-10-30 15:53:32 +0000

    add fontSize property to plots and axes.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17850 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/dom/PlotController.java
+++ VirboAutoplot/src/org/virbo/autoplot/dom/PlotController.java
@@ -1711,9 +1711,11 @@ public class PlotController extends DomNodeController {
         ac.bind( this.plot, Plot.PROP_ISOTROPIC, p, DasPlot.PROP_ISOTROPIC );
         ac.bind( this.plot, Plot.PROP_DISPLAYTITLE, p, DasPlot.PROP_DISPLAYTITLE );
         ac.bind( this.plot, Plot.PROP_DISPLAYLEGEND, p, DasPlot.PROP_DISPLAYLEGEND );
+        ac.bind( this.plot, Plot.PROP_FONTSIZE, p, DasPlot.PROP_FONTSIZE );
         ac.bind( dom.options, Options.PROP_PRINTINGLOGLEVEL, p, DasPlot.PROP_PRINTINGLOGLEVEL );
         ac.bind( dom.options, Options.PROP_DISPLAYLOGLEVEL, p, DasPlot.PROP_LOG_LEVEL );
         ac.bind( dom.options, Options.PROP_LOGMESSAGETIMEOUTSEC, p, DasPlot.PROP_LOG_TIMEOUT_SEC );
+        
     }
 
     /**
