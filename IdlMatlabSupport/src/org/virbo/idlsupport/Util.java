90236d428739c4e6e177dba4962b732edf1a6267
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-11-03 22:34:27 +0000

    support getting the editor in IDL
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@17923 0c594bb0-8543-0410-a25e-d8ec440261db

--- IdlMatlabSupport/src/org/virbo/idlsupport/Util.java
+++ IdlMatlabSupport/src/org/virbo/idlsupport/Util.java
@@ -5,12 +5,20 @@
 
 package org.virbo.idlsupport;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import org.das2.util.AboutUtil;
+import org.das2.util.monitor.NullProgressMonitor;
 import org.virbo.dataset.QDataSet;
 import org.virbo.datasource.DataSetSelectorSupport;
+import org.virbo.datasource.DataSourceEditorPanel;
+import org.virbo.datasource.DataSourceEditorPanelUtil;
+import org.virbo.datasource.DataSourceRegistry;
 
 /**
  *
@@ -60,5 +68,60 @@ public class Util {
         Logger.getLogger( "das2.dasml" ).setLevel( Level.ALL );
         Logger.getLogger( "das2" ).setLevel( Level.ALL );
         Logger.getAnonymousLogger().setLevel( Level.ALL );
+    }
+    
+    /**
+     * normally a Util class is just a bunch of static methods, but its easier
+     * to get at the methods if we can create an instance.
+     */
+    public Util() {
+        
+    }
+    
+    /**
+     * return an array of the sources that can be discovered.
+     * @return an array of the sources that can be discovered.
+     */
+    public String[] getDiscoverySources() {
+        DataSourceRegistry registry= DataSourceRegistry.getInstance();
+        List<String> exts= registry.getSourceEditorExtensions();
+        List<String> result= new ArrayList();
+        
+        for ( String ext: exts ) {
+            String uri= "vap+" + ext.substring(1) + ":";
+            try {
+                DataSourceEditorPanel p = (DataSourceEditorPanel) DataSourceEditorPanelUtil.getEditorByExt( ext );
+                if ( p.reject(uri) ) {
+                    System.err.printf("           (nope) %s: %s\n", ext, p);
+                } else {
+                    result.add(uri);
+                }
+            } catch (Exception ex) {
+                System.err.printf("           (exception) %s  %s\n", ext, ex );
+            }
+        }
+        return result.toArray( new String[result.size()] );
+        
+    }
+    
+    /**
+     * bring up the editor for this URI, or partial URI (vap+cdaweb:).  This
+     * was introduced to test Java GUIs in IDL.
+     * @param uri
+     * @return
+     * @throws Exception 
+     */
+    public String enterEditor( String uri ) throws Exception {
+        DataSourceEditorPanel p = (DataSourceEditorPanel) DataSourceEditorPanelUtil.getDataSourceEditorPanel( new URI(uri) );
+        if ( p.reject(uri) ) {
+            System.err.printf("           (nope) %s: %s\n", uri, p);
+            return uri;
+        } else {
+            //for fun, let's try to enter the GUI.
+            p.prepare( uri, null, new NullProgressMonitor() );
+            p.setURI(uri);
+            JOptionPane.showMessageDialog( null, p.getPanel() );
+            return p.getURI();
+        }
     }
 }
