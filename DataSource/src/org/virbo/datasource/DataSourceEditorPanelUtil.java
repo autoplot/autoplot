2a80dfaa57b73619fddd3189fbf930a79f9c38be
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-11-21 12:59:44 +0000

    add a more convenient method for getting editors.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@18043 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/DataSourceEditorPanelUtil.java
+++ DataSource/src/org/virbo/datasource/DataSourceEditorPanelUtil.java
@@ -1,19 +1,53 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.virbo.datasource;
 
+import java.awt.Component;
 import java.lang.reflect.Constructor;
 import java.net.URI;
+import java.util.logging.Logger;
+import javax.swing.JPanel;
+import org.das2.util.LoggerManager;
+import org.das2.util.monitor.NullProgressMonitor;
 import org.virbo.aggregator.AggregatingDataSourceEditorPanel;
 
 /**
- *
+ * Utilities for URLs.
  * @author jbf
  */
 public class DataSourceEditorPanelUtil {
+    
+    private static final Logger logger= LoggerManager.getLogger("apdss.uri");
+    
+    /**
+     * get an editor for the URI.  This must be inserted into the GUI, using getPanel.
+     * @param parent
+     * @param uri
+     * @return the editor panel.
+     * @throws IllegalArgumentException when the editor panel throws an exception.
+     */
+    public static DataSourceEditorPanel getDataSourceEditorPanel( JPanel parent, String uri ) {
+        logger.entering("org.virbo.datasource.DataSourceEditorPanelUtil", "getDataSourceEditorPanel");
+        DataSourceEditorPanel edit;
+        edit = DataSourceEditorPanelUtil.getDataSourceEditorPanel( uri );
+        if ( edit==null ) {
+            throw new IllegalArgumentException("can''t get editor for " + uri); // shouldn't happen now.
+        } else {
+            try {
+                if ( !edit.reject( uri ) ) {
+                    edit.prepare( uri, null, new NullProgressMonitor() );
+                    edit.setURI( uri );
+                    JPanel editPanel= edit.getPanel();
+                    editPanel.setAlignmentX( Component.LEFT_ALIGNMENT );
+                    parent.add( editPanel );
+                }
+            } catch ( Exception ex ) {
+                throw new IllegalArgumentException(ex);
+            }
+        }
+        logger.exiting("org.virbo.datasource.DataSourceEditorPanelUtil", "getDataSourceEditorPanel");
+        return edit;
+    }
+    
     /**
      * @param uri
      * @return an EditorPanel or null if one is not found.
@@ -37,8 +71,8 @@ public class DataSourceEditorPanelUtil {
                 DataSourceEditorPanel edit = getEditorByExt(eext);
                 if (edit != null) {
                     result.setDelegateEditorPanel(edit);
+                    result.setName( edit.getPanel().getName() );
                 }
-                result.setName( edit.getPanel().getName() );
                 return result;
             } else {
                 return new AggregatingDataSourceEditorPanel();
