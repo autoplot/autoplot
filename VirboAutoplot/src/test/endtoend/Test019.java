995c03c9ee8f09831ec13c1c8bd8aad298bab9ab
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2015-12-13 14:54:02 +0000

    update RBSP-A orbit 31.  LANL has updated their orbit intervals
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@18142 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/test/endtoend/Test019.java
+++ VirboAutoplot/src/test/endtoend/Test019.java
@@ -176,7 +176,7 @@ public class Test019 {
         // The following shows a bug where it doesn't consider the length of $H and just stops on the next period.
         // A field cannot contain the following delimiter.
         //  testTimeParser1( "$(j,Y=2012).*.$H", "017.x.y.z.02", "2012-01-17T02:00:00/03:00:00");
-        testTimeParser1( "$(o;id=rbspa-pp)", "31",  "2012-09-10T14:45:51.316Z/2012-09-10T23:44:37.174Z");
+        testTimeParser1( "$(o;id=rbspa-pp)", "31",  "2012-09-10T14:48:30.914Z 2012-09-10T23:47:34.973Z");
         //testTimeParser1( "$(j,Y=2012)$(hrinterval,names=01|02|03|04)", "01702", "2012-01-17T06:00/18:00");
         testTimeParser1( "$-1Y $-1m $-1d $H$M", "2012 03 30 1620", "2012-03-30T16:20 to 2012-03-30T16:21" );
         testTimeParser1( "$Y",            "2012",     "2012-01-01T00:00 to 2013-01-01T00:00");
