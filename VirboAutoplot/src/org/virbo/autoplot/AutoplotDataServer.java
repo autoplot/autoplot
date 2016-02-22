2903ef156ef572dc91d2f6006bc67256708453c4
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-02-22 20:23:36 +0000

    bugfix: whoops, restore code that would jump out of the loop once some data was found, this was correct.  The bug was elsewhere, because %25Y was not recognized as an aggregation with TimeSeriesBrowse.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@18353 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/virbo/autoplot/AutoplotDataServer.java
+++ VirboAutoplot/src/org/virbo/autoplot/AutoplotDataServer.java
@@ -132,7 +132,7 @@ public class AutoplotDataServer {
                         logger.log( Level.FINE, "  --> {0} )", ds1 );
                         writeData( format, out, ds1, ascii );
                         someValid= true;
-                        continue;
+                        break;
                     }
                     if ( ds1.rank()==1 ) {
                         QDataSet xrange= Ops.extent( SemanticOps.xtagsDataSet(ds1) );
