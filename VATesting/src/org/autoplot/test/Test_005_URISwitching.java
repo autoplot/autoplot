cd7a744823664f1ef81d567355ab7302b51d6a2c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-03-29 11:16:36 +0000

    bug 1545: add test005 to headful tests after appreciating bug.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@18574 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VATesting/src/org/autoplot/test/Test_005_URISwitching.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.autoplot.test;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.das2.datum.LoggerManager;
+import org.netbeans.jemmy.Scenario;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JTextFieldOperator;
+import org.virbo.autoplot.AutoplotUI;
+import org.virbo.autoplot.ScriptContext;
+import static org.virbo.autoplot.ScriptContext.createGui;
+import static org.virbo.autoplot.ScriptContext.writeToPng;
+import org.virbo.datasource.DataSetSelector;
+
+/**
+ * Run through the bookmarks.  This was introduced after a shocking bug
+ * wasn't appreciated until after release, and I realized there was no
+ * headful tests that switched render types.
+ * 
+ * @author jbf
+ */
+public class Test_005_URISwitching implements Scenario {
+
+    private static final Logger logger= LoggerManager.getLogger("vatesting");
+    
+    @Override
+    public int runIt(Object o) {
+        try {
+            createGui();
+            
+            AutoplotUI app= (AutoplotUI) ScriptContext.getViewWindow();
+            
+            DataSetSelector dss= app.getDataSetSelector();
+            new JTextFieldOperator( dss.getEditor() ).setText("http://autoplot.org/data/hsi_qlimg_5050601_001.fits");
+            new JButtonOperator( dss.getGoButton() ).clickMouse();
+
+            writeToPng( "Test_005_URISwitching.demo012.png" );
+            
+            new JTextFieldOperator( dss.getEditor() ).setText("ftp://spdf.gsfc.nasa.gov/pub/data/omni/low_res_omni/omni2_1963.dat?column=field17");
+            new JButtonOperator( dss.getGoButton() ).clickMouse();
+            writeToPng( "Test_005_URISwitching.demo007.png" );
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Test_005_URISwitching.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
+    }
+     
+    public static void main(String[] argv) {
+	String[] params = {"org.autoplot.test.Test_005_URISwitching"};
+	org.netbeans.jemmy.Test.main(params);
+    }
+}
