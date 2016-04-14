c0349f163c4c5201c001f723d63024cd059af1a9
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-04-14 15:31:48 +0000

    change the name of the SAMP client.  Only allow one client to be registered.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@18701 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/autoplot/AddSampListener.java
@@ -0,0 +1,99 @@
+
+package org.autoplot;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.astrogrid.samp.Message;
+import org.astrogrid.samp.Metadata;
+import org.astrogrid.samp.xmlrpc.StandardClientProfile;
+import org.astrogrid.samp.gui.GuiHubConnector;
+import org.astrogrid.samp.client.AbstractMessageHandler;
+import org.astrogrid.samp.client.HubConnection;
+import org.das2.util.LoggerManager;
+import org.virbo.datasource.DataSetSelector;
+
+/**
+ * Listener for the Cluster Final Archive SAMP protocol.  Other clients
+ * include the SOHO archive viewer, and Ulysses, will include the
+ * Cluster Final Archive viewer.
+ * @author jbf
+ */
+public class AddSampListener {
+
+    private static final Logger logger= LoggerManager.getLogger("autoplot.samp");
+    
+    private static final Map<Integer,AbstractMessageHandler> listeners= new HashMap();
+    
+    private static void maybePlot( DataSetSelector sel, String uri ) {
+        sel.setValue(uri);
+        sel.maybePlot(true);
+    }
+
+    /**
+     * Add the SAMP listener to Autoplot.
+     * @param sel the Autoplot URI address bar
+     */
+    public synchronized static void addSampListener( final DataSetSelector sel ) {
+        
+        AbstractMessageHandler l= listeners.get(sel.hashCode()); //why is my HashMap not working???
+        if ( listeners.size()>0 ) {
+            logger.info("handler is already running.");
+            return;
+        }
+        
+        logger.info( "starting up SAMP listener" );
+
+        StandardClientProfile profile = StandardClientProfile.getInstance();
+        GuiHubConnector hubConnector = new GuiHubConnector(profile);
+        hubConnector.setAutoconnect(3);
+
+        Metadata meta = new Metadata();
+        meta.setName("Autoplot");
+        meta.setDescriptionText("Autoplot");
+        meta.setIconUrl("http://autoplot.org/wiki/images/Logo32x32.png");
+        meta.setDocumentationUrl("http://autoplot.org");
+
+        hubConnector.declareMetadata(meta);
+
+        class ConcreteMessageReceiver extends AbstractMessageHandler {
+            String mType;
+
+            ConcreteMessageReceiver( String mType ) {
+                super( mType );
+                this.mType= mType;
+            }
+
+            @Override
+            public Map processCall( HubConnection connection, String senderId, Message message ) {
+                logger.log(Level.FINE, "got message: {0}", message.toString());
+                String s= (String) message.getParam("url");
+
+                if ( s.startsWith( "file://localhost" ) ) s= s.substring(16);
+                if ( s.startsWith( "file://" ) ) s= s.substring(7);
+                String ext= s.endsWith(".cdf") ? ext="cdf" : null;
+                if ( "cdf".equals(ext) || mType.equals("table.load.cdf") ) {
+                    maybePlot( sel, "vap+cdf:" + s );
+                } else if ( mType.equals("image.load.fits") )  {
+                    maybePlot( sel, "vap+fits:" + s );
+                }  else if ( mType.equals("table.load.fits") ) {
+                    maybePlot( sel, "vap+fits:" + s );
+                }
+                return null;
+            }
+        }
+        
+        listeners.put( sel.hashCode(), l );
+
+        ConcreteMessageReceiver messageHandler;
+        messageHandler= new ConcreteMessageReceiver("image.load.fits");
+        hubConnector.addMessageHandler(messageHandler);
+        messageHandler= new ConcreteMessageReceiver("table.load.fits");
+        hubConnector.addMessageHandler(messageHandler);
+        messageHandler= new ConcreteMessageReceiver("table.load.cdf");
+        hubConnector.addMessageHandler(messageHandler);
+        hubConnector.declareSubscriptions(hubConnector.computeSubscriptions());
+    }
+    
+}
