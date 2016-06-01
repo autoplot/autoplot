e2a021aadea30fab90686cdee31444a31b10877b
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-06-01 20:01:42 +0000

    bug 1595: verify the length of the time format.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@19019 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/autoplot/pngwalk/CreatePngWalkDialog.java
+++ VirboAutoplot/src/org/autoplot/pngwalk/CreatePngWalkDialog.java
@@ -147,6 +147,9 @@ public class CreatePngWalkDialog extends javax.swing.JPanel {
             params.batchUriName="$o";
         } else {
             params.batchUriName="";
+            if ( params.timeFormat.length()<2 ) {
+                 throw new IllegalArgumentException("Time Format must be at least two characters in length");
+            }
         }
         
         params.outputFormat= pngFormatCB.isSelected() ? "png" : "pdf";
@@ -284,7 +287,6 @@ public class CreatePngWalkDialog extends javax.swing.JPanel {
 
         timeFormatCB.setEditable(true);
         timeFormatCB.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "$Y$m$d", "$Y", "$Y$(m,span=3)", "$Y$m", "$Y_$j", "$Y$m$d_$H", "$Y$m$d_$H$M", "$Y$m$d_$H$M$S", "$(o,id=rbspa-pp)", "$(o,id=http://das2.org/wiki/index.php/Orbits/crres)" }));
-        timeFormatCB.setEnabled(true);
 
         timeRangeToolButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/virbo/datasource/calendar.png"))); // NOI18N
         timeRangeToolButton.setToolTipText("Use time range tool");
@@ -330,6 +332,10 @@ public class CreatePngWalkDialog extends javax.swing.JPanel {
 
         batchUriNameCB.setText("Events file specifies product names");
         batchUriNameCB.setToolTipText("<html>The events file contains the file name, so for example instead of product_$Y$m$d,\n<br>just use the last column when generating the filename.\n<br><tt>2000-01-09T06:50:41.155Z\t2000-01-09T13:46:34.224Z\ta/o1\t</tt>\n");
+
+        binding = org.jdesktop.beansbinding.Bindings.createAutoBinding(org.jdesktop.beansbinding.AutoBinding.UpdateStrategy.READ_WRITE, eventsFileRadioButton, org.jdesktop.beansbinding.ELProperty.create("${selected}"), batchUriNameCB, org.jdesktop.beansbinding.BeanProperty.create("enabled"));
+        bindingGroup.addBinding(binding);
+
         batchUriNameCB.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 batchUriNameCBActionPerformed(evt);
