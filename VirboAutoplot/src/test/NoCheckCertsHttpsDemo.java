3772e20ea6fc43a512a9c9a3be3b16992cead365
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-06-04 12:37:22 +0000

    add noCheckCertificate setting to disable certificate checks.  LANL's website is suddenly not working for me.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@19032 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/test/NoCheckCertsHttpsDemo.java
@@ -0,0 +1,70 @@
+package test;
+
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
+/**
+ *
+ * @author jbf
+ */
+public class NoCheckCertsHttpsDemo {
+
+    /**
+     *  fix for
+     *    Exception in thread "main" javax.net.ssl.SSLHandshakeException:
+     *       sun.security.validator.ValidatorException:
+     *           PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException:
+     *               unable to find valid certification path to requested target
+     */
+    public static void main(String[] args) throws Exception {
+
+        TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                }
+
+                public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                }
+
+            }
+        };
+
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+        URL url = new URL("https://www.rbsp-ect.lanl.gov/data_pub/");
+        URLConnection con = url.openConnection();
+        Reader reader = new InputStreamReader(con.getInputStream());
+        while (true) {
+            int ch = reader.read();
+            if (ch == -1) {
+                break;
+            }
+            System.out.print((char) ch);
+        }
+    }
+}
