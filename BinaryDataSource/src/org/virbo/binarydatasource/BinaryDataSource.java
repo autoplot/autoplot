6eb2e95e707d578df11b32bca1b15bf95fe144b5
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-07-22 12:18:43 +0000

    1637: binary source fails to read large JADE file.  Improve the wording by using longs for intermediate calculations.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@19217 0c594bb0-8543-0410-a25e-d8ec440261db

--- BinaryDataSource/src/org/virbo/binarydatasource/BinaryDataSource.java
+++ BinaryDataSource/src/org/virbo/binarydatasource/BinaryDataSource.java
@@ -205,13 +205,8 @@ public class BinaryDataSource extends AbstractDataSource {
             fieldCount= recSizeBits / BufferDataSet.bitCount(columnType);
         }
 
-        final int frecCount;
-        if ( ( recSizeBits % 8 )==0 ) {
-            frecCount= Math.min( length / recSizeBytes, recCount );
-        } else {
-            frecCount= Math.min( length * 8 / recSizeBits, recCount ); // Note this limits size to less than 1/8 GB.
-        }
-
+        final int frecCount= Math.min( (int)( (long)length * 8L / recSizeBits ), recCount ); // Note this limits size to less than 1/8 GB.
+        
         int[] rank2= null;
 
         String o = params.get("rank2");
