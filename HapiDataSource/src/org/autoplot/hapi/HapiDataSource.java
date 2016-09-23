e2d7edcee177fcddcaf0b19db23efdb40065a2ec
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-09-23 16:05:00 +0000

    correct the logger manager.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@19497 0c594bb0-8543-0410-a25e-d8ec440261db

--- HapiDataSource/src/org/autoplot/hapi/HapiDataSource.java
+++ HapiDataSource/src/org/autoplot/hapi/HapiDataSource.java
@@ -19,6 +19,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.das2.datum.DatumRange;
 import org.das2.datum.Units;
+import org.das2.util.LoggerManager;
 import org.das2.util.monitor.ProgressMonitor;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -39,7 +40,7 @@ import org.virbo.dsutil.DataSetBuilder;
  */
 public class HapiDataSource extends AbstractDataSource {
 
-    protected final static Logger logger= Logger.getLogger("apdss.hapi");
+    protected final static Logger logger= LoggerManager.getLogger("apdss.hapi");
     
     TimeSeriesBrowse tsb;
     
