6f1b258c1c9a4bc71ca3fe411ae80539671dc294
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-09-29 13:44:39 +0000

    don't use abbrevs for time duration units, which is exclusive of non-native speakers.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@19549 0c594bb0-8543-0410-a25e-d8ec440261db

--- HapiServerDemo/src/java/org/autoplot/hapiserver/Util.java
+++ HapiServerDemo/src/java/org/autoplot/hapiserver/Util.java
@@ -1,24 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.autoplot.hapiserver;
 
 /**
- *
+ * helpful functions.
  * @author jbf
  */
 public class Util {
+    
+    /**
+     * return the duration in a easily-human-consumable form.
+     * @param dt the duration in milliseconds.
+     * @return a duration like "2.6 hours"
+     */
     public static String getDurationForHumans( long dt ) {
         if ( dt<2*1000 ) {
-            return dt+" ms";
+            return dt+" milliseconds";
         } else if ( dt<2*60000 ) {
-            return String.format("%.1f",dt/1000.)+"s";
+            return String.format("%.1f",dt/1000.)+" seconds";
         } else if ( dt<2*3600000 ) {
-            return String.format("%.1f",dt/60000.)+" min";
+            return String.format("%.1f",dt/60000.)+" minutes";
         } else if ( dt<2*86400000 ) {
-            return String.format("%.1f",dt/3600000.)+" hr";
+            return String.format("%.1f",dt/3600000.)+" hours";
         } else {
             return String.format("%.1f",dt/86400000.)+" days";
         }
