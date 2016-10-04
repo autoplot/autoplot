7bf0bbca7f55ccf2231b1be73e9cf1e865955acb
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-10-04 13:01:36 +0000

    introduce streaming versions of the HtmlParser.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@19574 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ DataSourcePack/src/org/autoplot/html/AsciiTableStreamer.java
@@ -0,0 +1,262 @@
+
+package org.autoplot.html;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.das2.datum.EnumerationUnits;
+import org.das2.datum.TimeParser;
+import org.das2.datum.Units;
+import org.das2.util.LoggerManager;
+import org.virbo.dataset.AbstractDataSet;
+import org.virbo.dataset.DDataSet;
+import org.virbo.dataset.QDataSet;
+import org.virbo.dsops.Ops;
+
+/**
+ * Generic class for converting a table of ASCII strings to QDataSet stream.
+ * This supports streaming sources by reporting QDataSet records as they arrive.
+ * @author jbf
+ */
+public class AsciiTableStreamer implements Iterator<QDataSet> {
+
+    private static final Logger logger= LoggerManager.getLogger("apdss.html");
+            
+    QDataSet desc = null;  // bundle descriptor
+    List<Units> units = null;
+    Units defaultUnits= null;
+    List<String> labels = null;
+    List<String> names = null;
+    List<String> format= null;
+    List<Double> fillValues= null;
+    
+    List<QDataSet> records;
+    QDataSet recordDescriptor;
+    
+    int fieldCount= -1;
+    boolean initializedFields= false;
+    
+    /**
+     * true indicates that there may be more records, while false asserts no more records.
+     */
+    boolean hasNextRecord= true;
+    
+    public AsciiTableStreamer() {
+        records= Collections.synchronizedList( new LinkedList<QDataSet>() );
+    }
+    
+    protected void setUnits(String units) {
+        this.defaultUnits= Units.lookupUnits(units);
+    }
+    
+    private void setUnitsAndFormat( List<String> values ) {
+        for (int i = 0; i < fieldCount; i++) {
+            String field = values.get(i).trim();
+            boolean isTime= false;
+            try {
+                if ( TimeParser.isIso8601String(field) ) { // allow ISO8601 times.
+                    Units.cdfTT2000.parse(field);
+                    isTime= true;
+                } else if ( field.matches("\\d+/\\d+/\\d+") ) {
+                    Units.cdfTT2000.parse(field);
+                    isTime= true;
+                }
+            } catch (ParseException ex) {
+                Logger.getLogger(AsciiTableStreamer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            if ( units.get(i)==null ) {
+                if ( field.contains("$") ) {
+                    units.set(i,Units.dollars);
+                    format.set(i,"%.2f");
+                } else if ( field.endsWith("%") ) {
+                    units.set(i,Units.percent);
+                    format.set(i,null);
+                } else if ( isTime ) {
+                    units.set(i,Units.us2000);
+                    format.set(i,null);
+                } else {
+                    try {
+                        Integer.parseInt(field);
+                        units.set(i,Units.dimensionless);
+                        format.set(i,"%d");
+                    } catch ( NumberFormatException ex ) {
+                        try {
+                            Double.parseDouble(field);
+                            units.set(i,Units.dimensionless);
+                            format.set(i,null);
+                        } catch ( NumberFormatException ex2 ) {
+                            String[] ss= field.split("\\s",-2);  // "3.4 sec"
+                            if ( ss.length>1 ) {
+                                try {
+                                    Double.parseDouble(ss[0]);
+                                    units.set(i,Units.lookupUnits( field.substring( ss[0].length() ).trim() ) );
+                                    format.set(i,null);
+                                } catch ( NumberFormatException ex3 ) {
+                                    units.set( i, new EnumerationUnits("default") );
+                                    format.set(i,null);
+                                }
+                            } else {
+                                units.set( i, new EnumerationUnits("default") );
+                                format.set(i,null);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    protected void addRecord(List<String> values) {
+        if ( fieldCount==-1 ) {
+            return;
+        }
+        if ( initializedFields==false ) {
+            setUnitsAndFormat(values);
+            initializedFields= true;
+        }
+        
+        DDataSet result= DDataSet.createRank1(fieldCount);
+        
+        for (int i = 0; i < fieldCount; i++) {
+            String field = values.get(i).trim();
+            if ( field.trim().length()==0 ) {
+                result.putValue( i, fillValues.get(i) );
+            } else {
+                try {
+                    Units u= units.get(i);
+                    double d;
+                    if ( u instanceof EnumerationUnits ) {
+                        d= ((EnumerationUnits)u).createDatum(field).doubleValue(u);
+                    } else {
+                        d= u.parse( field ).doubleValue( u );
+                    }
+                    result.putValue( i, d);
+                } catch (ParseException ex) {
+                    result.putValue( i, fillValues.get(i) );
+                }
+            }
+        }
+        if ( desc==null ) {
+            desc=  getBundleDescriptor();
+        }
+        
+        sendRecord( result );
+    }
+
+    private void sendRecord( QDataSet result ) {
+        records.add(result);
+    }
+    
+    
+    
+    protected void initialize(List<String> values) {
+        fieldCount = values.size();
+
+        units = new ArrayList<>(fieldCount);
+        for (int i = 0; i < fieldCount; i++) {
+            units.add(i, defaultUnits ); // null here means we can reset.
+        }
+        format= new ArrayList<>(fieldCount);
+        labels = new ArrayList<>(fieldCount);
+        names = new ArrayList<>(fieldCount);
+        fillValues= new ArrayList<>(fieldCount);
+        if (labels.isEmpty()) {
+            for (int i = 0; i < fieldCount; i++) {
+                labels.add(i, values.get(i));
+                names.add(i, Ops.safeName(values.get(i)));
+                format.add("");
+                fillValues.add(-1e38);
+            }
+        }
+
+    }
+
+    /**
+     * return true if the header has been set.
+     * @return true if the header has been set.
+     */
+    public boolean hasHeader() {
+        return this.fieldCount>-1;
+    }
+    
+    protected void addHeader(List<String> values) {
+        if (fieldCount == -1) {
+            initialize(values);
+        }
+    }
+
+    protected void addUnits(List<String> units) {
+    }
+
+    protected void addUnits(int icol, String units) {
+    }
+
+    private QDataSet getBundleDescriptor() {
+        return new AbstractDataSet() {
+            @Override
+            public int rank() {
+                return 2;
+            }
+
+            @Override
+            public Object property(String name, int i) {
+                switch (name) {
+                    case QDataSet.LABEL:
+                        return labels.get(i);
+                    case QDataSet.NAME:
+                        return names.get(i);
+                    case QDataSet.FORMAT:
+                        return format.get(i);
+                    case QDataSet.UNITS:
+                        return units.get(i);
+                    default:
+                        break;
+                }
+                return property(name);
+            }
+
+            @Override
+            public double value(int i0, int i1) {
+                return 0;
+            }
+
+            @Override
+            public int length() {
+                return labels.size();
+            }
+
+            @Override
+            public int length(int i) {
+                return 0;
+            }
+        };
+    }
+
+    @Override
+    public boolean hasNext() {
+        if ( !records.isEmpty() ) return true;
+        while ( !initializedFields && hasNextRecord ) {
+            Thread.yield();
+        }
+        while ( records.isEmpty() && hasNextRecord ) {
+            Thread.yield();
+        }
+        return hasNextRecord;
+    }
+
+    @Override
+    public QDataSet next() {
+        while ( records.isEmpty() ) {
+            Thread.yield();
+        }
+        QDataSet result= records.remove(0);
+        //result.putProperty(QDataSet.BUNDLE_1, desc);
+        return result;
+    }
+
+}
---
+++ DataSourcePack/src/org/autoplot/html/HtmlParserStreamer.java
@@ -0,0 +1,189 @@
+
+package org.autoplot.html;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.html.HTML;
+import javax.swing.text.html.HTMLEditorKit;
+
+/**
+ * Stream the table into an AsciiTableStreamer.
+ * @author jbf
+ */
+public class HtmlParserStreamer extends HTMLEditorKit.ParserCallback {
+
+    private static final Logger logger= Logger.getLogger("apdss.html");
+
+    int tableCount = -1;
+    int itable=0;
+    String stable= null;
+
+    boolean inTable = false;
+    List<String> currentRow;
+
+    int fieldCount = -1;
+    boolean inField = false;
+    String fieldText = "";
+    
+    int recordCount= -1;
+
+    boolean isHeader= false;
+
+    int icolspan;
+
+    AsciiTableStreamer ascii= new AsciiTableStreamer();
+
+    List<String> tables= new ArrayList();
+
+    /**
+     * set the table to read.
+     * @param name 
+     */
+    void setTable( String name ) {
+        int i= name.indexOf(":");
+        if ( i>-1 ) name= name.substring(0,i);
+        this.stable= name;
+        try {
+            itable= Integer.parseInt(name);
+        } catch ( NumberFormatException ex ) {
+            itable= -1;
+        }
+    }
+
+    /**
+     * set the units for the columns.
+     * @param units 
+     */
+    void setUnits(String units) {
+        ascii.setUnits( units );
+    }
+    
+    @Override
+    public void handleText(char[] data, int pos) {
+        if (inField) {
+            fieldText += new String(data);
+            if (fieldText.length() > 30) {
+                fieldText = fieldText.substring(0, 27) + "...";
+            }
+        }
+
+    }
+    
+    private List<String> currentTableName= new ArrayList(); // tables can be nested
+    int nest=0;
+    
+    @Override
+    public void handleStartTag(HTML.Tag t, MutableAttributeSet a, int pos) {
+        logger.fine("startTag "+t + " @"+pos);
+        if ( t==HTML.Tag.TABLE ) {
+            logger.fine("startTag table");
+            tableCount++;
+            nest++;
+            recordCount= 0;
+            String tableName= (String)a.getAttribute("id");
+            if ( tableName==null ) tableName= ""+tableCount;
+            currentTableName.add( tableName );
+            if ( itable>-1 ) {
+                if ( tableCount==itable ) inTable= true;
+            } else {
+                if ( stable.equals( a.getAttribute("id") ) ) inTable= true;
+            }
+        } else if ( inTable ) {
+            if (t == HTML.Tag.TR) {
+                currentRow = new ArrayList<>();
+            } else if (t == HTML.Tag.TH) {
+                String colspan= (String) a.getAttribute(HTML.Attribute.COLSPAN);
+                icolspan= ( colspan!=null ) ? Integer.parseInt(colspan) : 1;
+                inField = true;
+                fieldText = "";
+                isHeader= true;
+            } else if (t == HTML.Tag.TD) {
+                inField = true;
+                fieldText = "";
+                isHeader= false;
+            }
+        }
+    }
+
+    @Override
+    public void handleEndTag(HTML.Tag t, int pos) {
+        logger.log(Level.FINE, "endTag {0} @{1}", new Object[]{t, pos});
+        if (t == HTML.Tag.TABLE) {
+                    
+            nest--;
+            String dim;
+            if ( fieldCount>-1 ) {
+                dim= recordCount + " rows, "+fieldCount+ " colums";
+            } else {
+                dim= recordCount + " rows";
+            }
+            //if ( nest==0 ) {
+                if ( currentTableName.isEmpty() ) throw new IllegalArgumentException("table html syntax");
+                tables.add( currentTableName.remove(0) + ": "+dim );
+            //}
+            if ( inTable ) inTable= false;
+        } else if ( inTable ) {
+            if (t == HTML.Tag.TR) {
+                if (fieldCount == -1) {
+                    fieldCount = currentRow.size();
+                }
+                if (currentRow.size() != fieldCount) {
+                    logger.fine("skipping row because of field count");
+                    return;
+                }
+                if ( isHeader ) {
+                    ascii.addHeader( currentRow );
+                } else {
+                    if ( ascii.hasHeader()==false ) {
+                        List<String> values= new ArrayList();
+                        boolean haveNumber= false;
+                        for ( int i=0; i<currentRow.size(); i++ ) {
+                            values.add("field"+i);
+                            try {
+                                Double.parseDouble(currentRow.get(i));
+                                haveNumber= true;
+                            } catch ( NumberFormatException ex ) {
+                                
+                            }
+                        }
+                        if ( haveNumber==false ) { // https://commons.apache.org/proper/commons-math/userguide/optimization.html uses the first row as headers.
+                            ascii.addHeader( currentRow );
+                        } else {
+                            ascii.addHeader( values );
+                        }
+                    } else {
+                        recordCount++;
+                        ascii.addRecord( currentRow );
+                    }
+                }
+                
+            } else if (t == HTML.Tag.TH) {
+                inField = false;
+                currentRow.add(fieldText);
+                for ( int i=1; i<icolspan; i++ ) {
+                    currentRow.add(fieldText);
+                }
+                fieldText = "";
+            } else if (t == HTML.Tag.TD) {
+                inField = false;
+                currentRow.add(fieldText);
+                fieldText = "";
+            }
+        } 
+        if (t == HTML.Tag.BODY ) {
+            ascii.hasNextRecord= false;
+        }
+    }
+
+    /**
+     * return a list of table names "name: &lt;dims&gt;
+     * @return 
+     */
+    public List<String> getTables() {
+        return new ArrayList<>(tables);
+    }
+
+}
