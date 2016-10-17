9eec32c4cbee0a43f5c857124316a3cea878ee52
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-10-17 11:37:01 +0000

    1686: always enter the editor for the open local action, so that Overplot or Plot Below could be selected.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@19675 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/virbo/datasource/DataSetSelectorSupport.java
+++ DataSource/src/org/virbo/datasource/DataSetSelectorSupport.java
@@ -4,6 +4,8 @@ package org.virbo.datasource;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -273,7 +275,8 @@ public class DataSetSelectorSupport {
 
                 if (result != null ) {
                     ui.setValue(result);
-                    ui.maybePlot(false);
+                    ui.maybePlot( KeyEvent.ALT_MASK );
+                    //ui.maybePlot(false);
                 }
 
             }
