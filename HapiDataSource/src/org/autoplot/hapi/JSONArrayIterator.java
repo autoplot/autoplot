dfb55faf272407c2d72264b29760e726e10b02ce
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-11-09 16:04:49 +0000

    introduce code to iterate through JSONArray.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@19911 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ HapiDataSource/src/org/autoplot/hapi/JSONArrayIterator.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.autoplot.hapi;
+
+import java.util.Iterator;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Allow iteration over JSONArrays.
+ * @author jbf
+ */
+public class JSONArrayIterator implements Iterable<JSONObject>, Iterator<JSONObject> {
+
+    int index;
+    JSONArray array;
+    
+    public JSONArrayIterator( JSONArray array ) {
+        this.array= array;
+        this.index= 0;
+    }
+    
+    @Override
+    public boolean hasNext() {
+        return this.index<this.array.length();
+    }
+
+    @Override
+    public JSONObject next() {
+        try {
+            return this.array.getJSONObject(this.index++);
+        } catch (JSONException ex) {
+            throw new IllegalArgumentException(ex);
+        }
+    }
+
+    @Override
+    public Iterator<JSONObject> iterator() {
+        return this;
+    }
+    
+}
