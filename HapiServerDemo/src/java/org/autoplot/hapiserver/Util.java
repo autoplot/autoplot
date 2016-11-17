1b42f9a9c9324e21a3ad25538f0a37d61f8c31ee
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-11-17 13:19:48 +0000

    1712 locale
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@19977 0c594bb0-8543-0410-a25e-d8ec440261db

--- HapiServerDemo/src/java/org/autoplot/hapiserver/Util.java
+++ HapiServerDemo/src/java/org/autoplot/hapiserver/Util.java
@@ -1,6 +1,8 @@
 
 package org.autoplot.hapiserver;
 
+import java.util.Locale;
+
 /**
  * helpful functions.
  * @author jbf
@@ -16,13 +18,13 @@ public class Util {
         if ( dt<2*1000 ) {
             return dt+" milliseconds";
         } else if ( dt<2*60000 ) {
-            return String.format("%.1f",dt/1000.)+" seconds";
+            return String.format( Locale.US, "%.1f",dt/1000.)+" seconds";
         } else if ( dt<2*3600000 ) {
-            return String.format("%.1f",dt/60000.)+" minutes";
+            return String.format( Locale.US, "%.1f",dt/60000.)+" minutes";
         } else if ( dt<2*86400000 ) {
-            return String.format("%.1f",dt/3600000.)+" hours";
+            return String.format( Locale.US, "%.1f",dt/3600000.)+" hours";
         } else {
-            return String.format("%.1f",dt/86400000.)+" days";
+            return String.format( Locale.US, "%.1f",dt/86400000.)+" days";
         }
     }
 }
