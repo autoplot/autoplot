5ac80d204ca28477e95672e5e197c6c343f96266
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2016-11-18 23:57:07 +0000

    BatchMaster needs to be in the application.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@20007 0c594bb0-8543-0410-a25e-d8ec440261db

--- VirboAutoplot/src/org/autoplot/BatchMaster.java
+++ VirboAutoplot/src/org/autoplot/BatchMaster.java
@@ -1,25 +1,38 @@
 
-package org.virbo.jythonsupport.ui;
+package org.autoplot;
 
+import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.AbstractAction;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import org.das2.components.DasProgressPanel;
+import org.das2.datum.DatumRange;
+import org.das2.datum.DatumRangeUtil;
 import org.das2.util.LoggerManager;
 import org.das2.util.filesystem.FileSystem;
+import org.das2.util.monitor.NullProgressMonitor;
 import org.das2.util.monitor.ProgressMonitor;
 import org.python.util.InteractiveInterpreter;
+import org.virbo.autoplot.JythonUtil;
+import org.virbo.autoplot.dom.Application;
 import org.virbo.datasource.DataSetURI;
 import org.virbo.datasource.URISplit;
-import org.virbo.jythonsupport.JythonUtil;
+import org.virbo.jythonsupport.ui.Util;
 
 /**
  *
@@ -31,9 +44,39 @@ public class BatchMaster extends javax.swing.JPanel {
     
     /**
      * Creates new form MiracleMashMachine
+     * @param dom
      */
-    public BatchMaster() {
+    public BatchMaster( final Application dom ) {
         initComponents();
+        /**
+         * register the browse trigger to the same action, because we always browse.
+         */
+        dataSetSelector1.registerBrowseTrigger( "(.*)\\.jy(\\?.*)?", new AbstractAction( "Review Script" ) {
+            @Override
+            public void actionPerformed( ActionEvent ev ) {
+                org.das2.util.LoggerManager.logGuiEvent(ev);                    
+                String s= dataSetSelector1.getValue();
+                Map<String,String> args;
+                try {
+                    URISplit split= URISplit.parse(s);        //bug 1408--note runScript doesn't account for changes made to the GUI.
+                    args= URISplit.parseParams(split.params);
+                    Map<String,String> env= new HashMap<>();
+                    File scriptFile= DataSetURI.getFile(s,new NullProgressMonitor());
+                    if ( JOptionPane.OK_OPTION==JythonUtil.showScriptDialog( 
+                            BatchMaster.this, 
+                            new HashMap<String, Object>(), 
+                            scriptFile, 
+                            args, 
+                            enabled, 
+                            split.resourceUri ) ) {
+                        // do nothing.
+                        
+                    }
+                } catch ( IOException ex ) { 
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
     }
 
     /**
@@ -49,7 +92,7 @@ public class BatchMaster extends javax.swing.JPanel {
         jList2 = new javax.swing.JList<>();
         param1Name = new javax.swing.JTextField();
         param2Name = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        goButton = new javax.swing.JButton();
         jScrollPane3 = new javax.swing.JScrollPane();
         param1Values = new javax.swing.JTextArea();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -66,10 +109,10 @@ public class BatchMaster extends javax.swing.JPanel {
         });
         jScrollPane2.setViewportView(jList2);
 
-        jButton1.setText("Go!");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        goButton.setText("Go!");
+        goButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                goButtonActionPerformed(evt);
             }
         });
 
@@ -119,7 +162,7 @@ public class BatchMaster extends javax.swing.JPanel {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton3)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(goButton, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -131,24 +174,25 @@ public class BatchMaster extends javax.swing.JPanel {
                     .addComponent(param1Name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(param2Name, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 278, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane3)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 278, Short.MAX_VALUE))
+                .addGap(9, 9, 9)
                 .addComponent(messageLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
+                    .addComponent(goButton)
                     .addComponent(jButton2)
                     .addComponent(jButton3))
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void goButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_goButtonActionPerformed
         Runnable run= new Runnable() {
             public void run() {
                 try {
+                    goButton.setEnabled(false);
                     doIt();
                 } catch (IOException ex) {
                     messageLabel.setText(ex.getMessage());
@@ -156,7 +200,7 @@ public class BatchMaster extends javax.swing.JPanel {
             }
         };
         new Thread(run).start();
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_goButtonActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         param1Name.setText("ie");
@@ -170,9 +214,10 @@ public class BatchMaster extends javax.swing.JPanel {
             param1Name.setText("resourceURI");
             StringBuilder sb= new StringBuilder();
             FileSystem fs= FileSystem.create("file:/home/jbf/ct/autoplot/userlog/20161116/");
-            String[] ss= fs.listDirectoryDeep( "/", "access.autoplot.log.*.gz" ); 
+            String[] ss= fs.listDirectory( "/", "access.autoplot.log.*.gz" ); 
+            Arrays.sort(ss);
             for ( String s: ss ) {
-                sb.append( s+"\n" );
+                sb.append(s).append("\n");
             }
             param1Values.setText(sb.toString());            
         } catch (UnknownHostException ex) {
@@ -184,65 +229,144 @@ public class BatchMaster extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_jButton3ActionPerformed
 
-    public void doIt() throws IOException {
+    /**
+     * TODO: this is not complete!
+     * @param interp
+     * @param paramDescription
+     * @param paramName
+     * @param f1
+     * @throws IOException 
+     */
+    private void setParam( InteractiveInterpreter interp, org.virbo.jythonsupport.JythonUtil.Param paramDescription, 
+            String paramName, String f1 ) throws IOException {
+        if ( paramDescription==null ) {
+            throw new IllegalArgumentException("expected to see parameter description!");
+        }
+        switch (paramDescription.type) {
+            case 'U':
+                URI uri;
+                try {
+                    URISplit split= URISplit.parse(f1);
+                    if ( split.path==null ) {
+                        uri= new URI( pwd + f1 );
+                    } else {
+                        uri= new URI(f1);
+                    }
+                } catch ( URISyntaxException ex ) {
+                    throw new IOException(ex);
+                }   interp.set("_apuri", uri );
+                interp.exec("autoplot.params[\'"+paramName+"\']=_apuri");
+                break;
+            case 'A':
+                interp.exec("autoplot.params[\'"+paramName+"\']=\'"+f1+"\'");
+                break;
+            case 'T':
+                try {
+                    DatumRange timeRange= DatumRangeUtil.parseTimeRange(f1);
+                    interp.set("_apdr", timeRange );
+                    interp.exec("autoplot.params[\'"+paramName+"\']=_apdr");
+                } catch (ParseException ex) {
+                    Logger.getLogger(BatchMaster.class.getName()).log(Level.SEVERE, null, ex);
+                }   break;
+            default:
+                interp.exec("autoplot.params[\'"+paramName+"\']="+f1);
+                break;
+        }
+        
+    }
+    
+    private String pwd;
+    
+    /**
+     * read the script into a string
+     * @param f
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException 
+     */
+    private String readScript( File f ) throws FileNotFoundException, IOException {
+        StringBuilder build= new StringBuilder();
+        BufferedReader r;
+        r = new BufferedReader( new FileReader(f) );
+        try {    
+            String line= r.readLine();
+            while ( line!=null ) {
+                build.append(line).append("\n");
+                line= r.readLine();
+            }
+        } finally {
+            r.close();
+        }        
+        return build.toString();
+    }
+    
+    public synchronized void doIt() throws IOException {
         ProgressMonitor monitor= DasProgressPanel.createFramed("batchMaster");
         try {
-            String script= dataSetSelector1.getValue();
-            if ( !script.endsWith(".jy") ) {
-                JOptionPane.showMessageDialog( this, "script must end in .jy: "+script );
+            String scriptName= dataSetSelector1.getValue();
+            if ( !scriptName.endsWith(".jy") ) {
+                JOptionPane.showMessageDialog( this, "script must end in .jy: "+scriptName );
                 return;
             }
-            String[] ff= param1Values.getText().split("\n");
-            monitor.setTaskSize(ff.length);
-            monitor.started();
-            File scriptFile= DataSetURI.getFile( script, monitor.getSubtaskMonitor("download script") );
 
-            URISplit s= URISplit.parse(script);
-            String pwd= s.path;
+            String[] ff1= param1Values.getText().split("\n");
+
+            monitor.setTaskSize(ff1.length);
+            monitor.started();
+            
+            URISplit split= URISplit.parse(scriptName);
+            pwd= split.path;
+            
+            Map<String,String> params= URISplit.parseParams(split.params);
+            Map<String,Object> env= new HashMap<>();
                     
-            for ( String f : ff ) {
+            File scriptFile= DataSetURI.getFile( split.file, monitor.getSubtaskMonitor("download script") );
+            String script= readScript( scriptFile );
+            
+            Map<String,org.virbo.jythonsupport.JythonUtil.Param> parms= Util.getParams( env, script, URISplit.parseParams(split.params), new NullProgressMonitor() );
+
+            InteractiveInterpreter interp = JythonUtil.createInterpreter( true, false );
+            interp.exec("import autoplot");
+            
+            int i1=0;
+            for ( String f1 : ff1 ) {
                 try {
-                    monitor.setProgressMessage(f);
+                    monitor.setProgressMessage(f1);
                     if ( monitor.isCancelled() ) break;
                     monitor.setTaskProgress(monitor.getTaskProgress()+1);
-                    InteractiveInterpreter interp = JythonUtil.createInterpreter( false );
-                    interp.set( "monitor", monitor.getSubtaskMonitor(f) );
-                    interp.exec("import autoplot");
+                    interp.set( "monitor", monitor.getSubtaskMonitor(f1) );
                     String paramName= param1Name.getText();
                     
-                    if ( paramName.equals("resourceURI" ) ) {
-                        URI uri;
-                        try {
-                            URISplit split= URISplit.parse(f);
-                            //uri= new URI(f);
-                            uri= new URI( pwd + "/" + f );
-                        } catch (Exception ex ) {
-                            try {
-                                uri= new URI( pwd + "/" + f );
-                            } catch ( URISyntaxException ex2 ) {
-                                throw new IOException(ex.toString());
-                            }
-                        }       
-                        interp.set("apuri", uri );
-                        interp.exec("autoplot.params[\'"+paramName+"\']=apuri");
+                    setParam( interp, parms.get(paramName), paramName, f1 );
+                    
+                    if ( param2Name.getText().trim().length()==0 ) {
+                        interp.execfile( new FileInputStream(scriptFile), scriptFile.getName() );
                     } else {
-                        interp.exec("autoplot.params[\'"+paramName+"\']="+f);
+                        String[] ff2= param2Values.getText().split("\n");
+                        int i2=0;
+                        for ( String f2: ff2 ) {
+                            paramName= param2Name.getText();
+                            setParam( interp, parms.get(paramName), paramName, f2 );
+                            interp.execfile( new FileInputStream(scriptFile), scriptFile.getName() );
+                            i2=i2+f2.length()+1;
+                        }
                     }
-                    interp.execfile( new FileInputStream(scriptFile), scriptFile.getName() );
 
                 } catch (IOException ex) {
                     Logger.getLogger(BatchMaster.class.getName()).log(Level.SEVERE, null, ex);
                 }
+                i1=i1+f1.length()+1;
             }
         } finally {
             monitor.finished();
+            goButton.setEnabled(true);
         }
     }
     
     public static void main( String[] args ) {
         JDialog dia= new JDialog();
         dia.setResizable(true);
-        BatchMaster mmm= new BatchMaster();
+        BatchMaster mmm= new BatchMaster(new Application());
         dia.setContentPane( mmm );
         mmm.param1Name.setText("ie");
         mmm.dataSetSelector1.setValue("/home/jbf/ct/autoplot/script/demos/paramTypes.jy");
@@ -253,7 +377,7 @@ public class BatchMaster extends javax.swing.JPanel {
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private org.virbo.datasource.DataSetSelector dataSetSelector1;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton goButton;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JList<String> jList2;
