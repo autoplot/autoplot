a1448b5cf77d017b89d9e6dd752cc208f4c9fb60
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2017-03-23 20:56:31 +0000

    update javadocs lookup to Java8 site.  Java8 uses new anchor tags.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@20595 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonSupport/src/org/das2/jythoncompletion/JavadocLookup.java
+++ JythonSupport/src/org/das2/jythoncompletion/JavadocLookup.java
@@ -20,7 +20,7 @@ public class JavadocLookup {
     public static synchronized JavadocLookup getInstance() {
         if ( instance==null ) {
             instance= new JavadocLookup();
-            // note the order is important, so that org.w3c.dom
+            // note the order is important, so that org.w3c.dom is matched before org.
             instance.setLinkForJavaSignature("javax","http://docs.oracle.com/javase/8/docs/api/");  // Java 7 docs look terrible in the popup, so there's work to be done.
             instance.setLinkForJavaSignature("java","http://docs.oracle.com/javase/8/docs/api/");
             instance.setLinkForJavaSignature("org/w3c/dom","http://docs.oracle.com/javase/8/docs/api/");
@@ -37,6 +37,7 @@ public class JavadocLookup {
      * return a link to the documentation for a java signature.  For standard library
      * things, this goes to Oracle's website.  For other things, this goes
      * to the Autoplot/Das2 javadocs.
+     * Java8 http://docs.oracle.com/javase/8/docs/api/java/io/File.html#createTempFile-java.lang.String-java.lang.String-java.io.File-
      * @param signature signature like javax.swing.JCheckBox#paramString()
      * @return null or the link, like http://docs.oracle.com/javase/6/docs/api/javax/swing/JCheckBox#paramString()
      */
@@ -46,7 +47,12 @@ public class JavadocLookup {
         Set<String> keys= lookups.keySet();
         for ( String key : keys ) {
             if ( lookfor.startsWith(key) ) {
-                return lookups.get(key) + signature.replaceAll(",", ", ");
+                String s= lookups.get(key);
+                if ( s.startsWith("http://docs.oracle.com/javase/8/docs/api/") ) {
+                    return lookups.get(key) + signature.replaceAll("[\\(\\)\\,]", "-");
+                } else {
+                    return lookups.get(key) + signature.replaceAll(",", ", ");
+                }
             }
         }
         return null;
