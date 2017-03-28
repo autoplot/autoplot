6874c62d6d1a84aecf0f02ae5794cead96d928c3
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2017-03-28 14:49:16 +0000

    support for nominal data.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@20610 0c594bb0-8543-0410-a25e-d8ec440261db

--- HapiServerDemo/src/java/org/autoplot/hapiserver/CsvDataFormatter.java
+++ HapiServerDemo/src/java/org/autoplot/hapiserver/CsvDataFormatter.java
@@ -8,6 +8,8 @@ import org.das2.datum.Datum;
 import org.das2.datum.Units;
 import org.das2.datum.UnitsUtil;
 import org.das2.datum.format.DatumFormatter;
+import org.das2.datum.format.DefaultDatumFormatter;
+import org.das2.datum.format.EnumerationDatumFormatter;
 import org.das2.datum.format.TimeDatumFormatter;
 import org.das2.util.NumberFormatUtil;
 import org.json.JSONObject;
@@ -22,17 +24,24 @@ public class CsvDataFormatter implements DataFormatter {
 
     boolean[] unitsFormatter;
     DatumFormatter[] datumFormatter;
+    boolean[] quotes;
     
     @Override
     public void initialize( JSONObject info, OutputStream out, QDataSet record) {
         unitsFormatter= new boolean[record.length()];
         datumFormatter= new DatumFormatter[record.length()];
+        quotes= new boolean[record.length()];
         for ( int i=0; i<record.length(); i++ ) {
             QDataSet field= record.slice(i);
             Units u= (Units)field.property(QDataSet.UNITS);
-            if (  u!=null && ( UnitsUtil.isTimeLocation(u) || UnitsUtil.isNominalMeasurement(u) ) ) {
+            if (  u!=null && UnitsUtil.isTimeLocation(u) ) {
                 unitsFormatter[i]= true;
                 datumFormatter[i]= TimeDatumFormatter.DEFAULT;
+                quotes[i]= false;
+            } else if ( u!=null && UnitsUtil.isNominalMeasurement(u) ) {
+                unitsFormatter[i]= true;
+                datumFormatter[i]= new EnumerationDatumFormatter();
+                quotes[i]= true;
             } else {
                 unitsFormatter[i]= false;
                 final DecimalFormat format= NumberFormatUtil.getDecimalFormat( "0.###E00;-#");
@@ -42,8 +51,17 @@ public class CsvDataFormatter implements DataFormatter {
                         return format.format( datum.doubleValue(datum.getUnits()) );
                     }
                 };
+                quotes[i]= false;
             }
         }
+        if ( false ) {
+            System.err.println("===");
+            for ( int i=0; i<record.length(); i++ ) {
+                System.err.println( String.format( "%4d %s", i,datumFormatter[i] ) );
+            }
+            System.err.println("===");
+        }
+        
     }
 
     @Override
@@ -53,11 +71,13 @@ public class CsvDataFormatter implements DataFormatter {
             QDataSet field= record.slice(i);
             Datum fieldDatum;
             fieldDatum= DataSetUtil.asDatum(field);
+            if ( quotes[i] ) out.write('"');
             if ( fieldDatum.isFill() ) {
                 out.write( String.valueOf( field.value() ).getBytes() );
             } else {
                 out.write( datumFormatter[i].format( fieldDatum ).getBytes() );
             }
+            if ( quotes[i] ) out.write('"');
             if ( i<n-1 ) out.write(',');
         }
         out.write((byte)10);
