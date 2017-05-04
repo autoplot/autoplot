a514c4161b1736d6a8e32fc156cfa77f292a4a3c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2017-05-04 12:20:55 +0000

    rfe571: bundle coming out of ascii parser is causing problems with rank2.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@20752 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/virbo/ascii/AsciiTableDataSource.java
+++ DataSourcePack/src/org/virbo/ascii/AsciiTableDataSource.java
@@ -282,6 +282,12 @@ public class AsciiTableDataSource extends AbstractDataSource {
                 dep0 = ArrayDataSet.copy(DataSetOps.slice1(ds, icol));
             }
             dep0.putProperty(QDataSet.UNITS, parser.getUnits(icol));
+            String tf= params.get("timeFormat");
+            if ( UnitsUtil.isTimeLocation( parser.getUnits(icol) ) && tf!=null && !tf.equals("ISO8601") ) {
+                dep0.putProperty(QDataSet.LABEL,null);
+                dep0.putProperty(QDataSet.NAME,"time");
+            }
+            
             if (DataSetUtil.isMonotonic(dep0)) {
                 dep0.putProperty(QDataSet.MONOTONIC, Boolean.TRUE);
             }
