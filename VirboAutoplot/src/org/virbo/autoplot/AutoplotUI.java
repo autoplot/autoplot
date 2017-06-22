3fd4e3ee8d229bbe904d8d60e7f05ece2cea8152
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2017-06-22 23:42:07 +0000

    add stubs to support old script calls.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@20982 0c594bb0-8543-0410-a25e-d8ec440261db

---
+++ VirboAutoplot/src/org/virbo/autoplot/AutoplotUI.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.virbo.autoplot;
+
+/**
+ *
+ * @author jbf
+ */
+public class AutoplotUI {
+    public static void main( String[] args ) {
+        System.err.println("reference to old org.virbo.autoplot needs fixing. http://autoplot.org/autoplot2017");
+        org.autoplot.AutoplotUI.main(args);
+    }
+}
