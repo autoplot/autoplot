f617361e7e333d5c289fce3058b015bddfa27a8c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2017-06-28 11:04:21 +0000

    add deprecated with link in javadoc
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@21059 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/org/autoplot/pngwalk/PngWalkTool1.java
+++ Autoplot/src/org/autoplot/pngwalk/PngWalkTool1.java
@@ -8,10 +8,9 @@ package org.autoplot.pngwalk;
 import java.awt.Window;
 
 /**
- * Launcher for PngWalkTool1.  The "1" in PngWalkTool1 was to indicate it was provisional.  Now it's official.
- * This is a half-hearted attempt to support Jython scripts that used the PngWalkTool1 name.  Please let us know if this
- * needs attention, at autoplot@groups.google.com
- * 
+ * Launcher for PngWalkTool to support old name in legacy codes.  The "1" in 
+ * PngWalkTool1 was to indicate it was provisional.  Now it's official.
+ * @deprecated see {@link org.autoplot.pngwalk.PngWalkTool}
  * @author jbf
  */
 public class PngWalkTool1 {
