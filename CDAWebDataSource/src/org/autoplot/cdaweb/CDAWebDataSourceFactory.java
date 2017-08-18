7ab099ec4f1d7e9553309ee28e52b72857005a0d
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2017-08-18 21:44:20 +0000

    netbeans suggestions,  findbugs WMI_WRONG_MAP_ITERATOR
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@21272 0c594bb0-8543-0410-a25e-d8ec440261db

--- CDAWebDataSource/src/org/autoplot/cdaweb/CDAWebDataSourceFactory.java
+++ CDAWebDataSource/src/org/autoplot/cdaweb/CDAWebDataSourceFactory.java
@@ -12,6 +12,7 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.logging.Logger;
 import org.autoplot.cdf.CdfDataSource;
 import org.das2.datum.DatumRange;
@@ -46,7 +47,7 @@ public class CDAWebDataSourceFactory implements DataSourceFactory {
     public List<CompletionContext> getCompletions(CompletionContext cc, ProgressMonitor mon) throws Exception {
         CDAWebDB.getInstance().maybeRefresh(mon);
         if ( cc.context==CompletionContext.CONTEXT_PARAMETER_NAME ) {
-            List<CompletionContext> ccresult= new ArrayList<CompletionContext>(10);
+            List<CompletionContext> ccresult= new ArrayList<>(10);
             ccresult.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "ds=" ) );
             ccresult.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "id=" ) );
             ccresult.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, "timerange=" ) );
@@ -54,72 +55,82 @@ public class CDAWebDataSourceFactory implements DataSourceFactory {
             return ccresult;
         } else if ( cc.context==CompletionContext.CONTEXT_PARAMETER_VALUE ) {
             String param= CompletionContext.get( CompletionContext.CONTEXT_PARAMETER_NAME, cc );
-            if ( param.equals("ds") ) {
-                Map<String,String> dss= CDAWebDB.getInstance().getServiceProviderIds();
-                List<CompletionContext> ccresult= new ArrayList<CompletionContext>(dss.size());
-                for ( String ds:dss.keySet() ) {
-                    CompletionContext cc1= new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, ds, this, null, ds, ds, false  );
-                    ccresult.add(cc1);
-                }
-                return ccresult;
-            } else if ( param.equals("timerange") ) {
-                URISplit split= URISplit.parse(cc.surl);
-                Map<String,String> params= URISplit.parseParams(split.params);
-                String ds= params.get("ds");
-                if ( ds!=null && ds.length()>0 ) {
-                    ds= ds.toUpperCase();
-                    CDAWebDB db= CDAWebDB.getInstance();
-
-                    String tr= db.getTimeRange(ds);
-
-                    String tmpl= db.getNaming(ds);
-                    String base= db.getBaseUrl(ds);
-
-                    FileSystem fs= FileSystem.create( new URI( base ) );
-                    FileStorageModel fsm= FileStorageModel.create( fs, tmpl );
-
-                    DatumRange dr= DatumRangeUtil.parseTimeRangeValid(tr);
-                    String[] names= fsm.getNamesFor(dr);  //TODO: this could be slow
-                    String name= names.length>1 ? names[1] : names[0];
-                    DatumRange one= fsm.getRangeFor(name);
-
-                    List<CompletionContext> ccresult= new ArrayList<CompletionContext>();
-                    String key= one.toString().replaceAll(" ","+");
-                    CompletionContext cc1= new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, key, this, null, key, key, true  );
-                    ccresult.add(cc1);
-                    return ccresult;
-                    
-                }
-            } else if ( param.equals("id") ) {
-                URISplit split= URISplit.parse(cc.surl);
-                Map<String,String> params= URISplit.parseParams(split.params);
-                String ds= params.get("ds");
-                if ( ds!=null && ds.length()>0 ) {
-                    ds= ds.toUpperCase();
-                    String master= CDAWebDB.getInstance().getMasterFile( ds.toLowerCase(), mon );
-
-                    File f= FileSystemUtil.doDownload( master, mon );
-
-                    Map<String,String> result;
-
-                    CDFReader cdf;
-                    cdf = CdfDataSource.getCdfFile(f.toString());
-                    result= org.autoplot.cdf.CdfUtil.getPlottable( cdf, true, 4);
-
-                    List<CompletionContext> ccresult= new ArrayList<CompletionContext>();
-                    for ( String key:result.keySet() ) {
-                        CompletionContext cc1= new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, key, this, null, key, result.get(key), true  );
+            switch (param) {
+                case "ds":
+                {
+                    Map<String,String> dss= CDAWebDB.getInstance().getServiceProviderIds();
+                    List<CompletionContext> ccresult= new ArrayList<>(dss.size());
+                    for ( String ds:dss.keySet() ) {
+                        CompletionContext cc1= new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, ds, this, null, ds, ds, false  );
                         ccresult.add(cc1);
                     }
                     return ccresult;
                 }
-
-
-            } else if ( param.equals("ws") ) {
-                List<CompletionContext> ccresult= new ArrayList<CompletionContext>(10);
-                ccresult.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "T", "T", "use webservice" ) );
-                ccresult.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "F", "F", "use files for offline use" ) );
-                return ccresult;
+                case "timerange":
+                    {
+                        URISplit split= URISplit.parse(cc.surl);
+                        Map<String,String> params= URISplit.parseParams(split.params);
+                        String ds= params.get("ds");
+                        if ( ds!=null && ds.length()>0 ) {
+                            ds= ds.toUpperCase();
+                            CDAWebDB db= CDAWebDB.getInstance();
+                            
+                            String tr= db.getTimeRange(ds);
+                            
+                            String tmpl= db.getNaming(ds);
+                            String base= db.getBaseUrl(ds);
+                            
+                            FileSystem fs= FileSystem.create( new URI( base ) );
+                            FileStorageModel fsm= FileStorageModel.create( fs, tmpl );
+                            
+                            DatumRange dr= DatumRangeUtil.parseTimeRangeValid(tr);
+                            String[] names= fsm.getNamesFor(dr);  //TODO: this could be slow
+                            String name= names.length>1 ? names[1] : names[0];
+                            DatumRange one= fsm.getRangeFor(name);
+                            
+                            List<CompletionContext> ccresult= new ArrayList<>();
+                            String key= one.toString().replaceAll(" ","+");
+                            CompletionContext cc1= new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, key, this, null, key, key, true  );
+                            ccresult.add(cc1);
+                            return ccresult;
+                            
+                        }       break;
+                    }
+                case "id":
+                    {
+                        URISplit split= URISplit.parse(cc.surl);
+                        Map<String,String> params= URISplit.parseParams(split.params);
+                        String ds= params.get("ds");
+                        if ( ds!=null && ds.length()>0 ) {
+                            ds= ds.toUpperCase();
+                            String master= CDAWebDB.getInstance().getMasterFile( ds.toLowerCase(), mon );
+                            
+                            File f= FileSystemUtil.doDownload( master, mon );
+                            
+                            Map<String,String> result;
+                            
+                            CDFReader cdf;
+                            cdf = CdfDataSource.getCdfFile(f.toString());
+                            result= org.autoplot.cdf.CdfUtil.getPlottable( cdf, true, 4);
+                            
+                            List<CompletionContext> ccresult= new ArrayList<>();
+                            for ( Entry<String,String> e:result.entrySet() ) {
+                                String key= e.getKey();
+                                CompletionContext cc1= new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, key, this, null, key, e.getValue(), true  );
+                                ccresult.add(cc1);
+                            }
+                            return ccresult;
+                        }       break;
+                    }
+                case "ws":
+                {
+                    List<CompletionContext> ccresult= new ArrayList<>(10);
+                    ccresult.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "T", "T", "use webservice" ) );
+                    ccresult.add( new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, "F", "F", "use files for offline use" ) );
+                    return ccresult;
+                }
+                default:
+                    break;
             }
         }
         return new ArrayList<CompletionContext>() {};
