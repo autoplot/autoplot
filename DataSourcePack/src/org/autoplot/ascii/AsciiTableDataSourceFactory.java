20673ecbba19beecc4108f9d9fa311e00cef43ac
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2017-10-06 21:42:10 +0000

    bug1900: remove magic number "6" (columns in an events dataset) and replace with magic number "7" :).
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@21494 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/autoplot/ascii/AsciiTableDataSourceFactory.java
+++ DataSourcePack/src/org/autoplot/ascii/AsciiTableDataSourceFactory.java
@@ -258,7 +258,7 @@ public class AsciiTableDataSourceFactory implements DataSourceFactory {
                             return false;
                         }
                     }
-                    if ( cc.size()>0 && cc.size()<6 ) {  // kludge where the last completion will be for eventListColumn when this could be done automatically.
+                    if ( cc.size()>0 && cc.size()<7 ) {  // kludge where the last completion will be for eventListColumn when this could be done automatically.
                         CompletionContext lastCC= cc.get(cc.size()-1);
                         if ( lastCC.context==CompletionContext.CONTEXT_PARAMETER_NAME 
                                 && lastCC.completable.equals("eventListColumn") ) {
