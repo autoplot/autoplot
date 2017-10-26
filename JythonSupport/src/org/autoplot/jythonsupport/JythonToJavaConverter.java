e9ce8e640be0646a0a9baae9ea19b21c46695796
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2017-10-26 20:05:24 +0000

    expand jython to java for more complex script.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@21593 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonSupport/src/org/autoplot/jythonsupport/JythonToJavaConverter.java
+++ JythonSupport/src/org/autoplot/jythonsupport/JythonToJavaConverter.java
@@ -5,6 +5,11 @@
  */
 package org.autoplot.jythonsupport;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import org.autoplot.datasource.DataSetURI;
+import org.das2.util.monitor.NullProgressMonitor;
 import org.python.parser.SimpleNode;
 import org.python.parser.ast.*;
 
@@ -20,6 +25,7 @@ public class JythonToJavaConverter {
          boolean visitNameFail= false;
          
          StringBuilder builder;
+         int lineNumber=1;
          
          MyVisitorBase( StringBuilder builder ) {
              this.builder= builder;
@@ -45,19 +51,23 @@ public class JythonToJavaConverter {
          }
          
          public void traverse(String indent, SimpleNode sn) throws Exception {
+             while ( sn.beginLine>lineNumber ) {
+                 this.builder.append("\n");
+                 lineNumber++;
+             }
              if ( sn instanceof org.python.parser.ast.FunctionDef ) {
                  FunctionDef fd = (FunctionDef)sn;
-                 this.builder.append("void ").append(fd.name).append("(");
+                 this.builder.append("private void ").append(fd.name).append("(");
                  for ( int i=0; i<fd.args.args.length; i++ ) {
                      if (i>0 ) this.builder.append(",");
                      this.builder.append("").append(fd.args.args[i].getImage().toString());
                  }
-                 this.builder.append( ") {\n");
+                 this.builder.append( ") {\n"); lineNumber++;
                  for ( int i=0; i<fd.body.length; i++ ) {
                      traverse( "\t", fd.body[i] );
-                     this.builder.append( "\n");
+                     this.builder.append( "\n"); lineNumber++;
                  }
-                 this.builder.append( "}\n");
+                 this.builder.append( "}\n"); lineNumber++;
              } else if ( sn instanceof Expr ) {
                  ((Expr)sn).getImage();
              } else if ( sn instanceof Print ) {
@@ -69,6 +79,11 @@ public class JythonToJavaConverter {
                      traverse( "", pr.values[i] );
                  }
                  this.builder.append(");");
+             } else if ( sn instanceof ImportFrom ) {
+                 ImportFrom ff= ((ImportFrom)sn);
+                 for ( int i=0; i<ff.names.length; i++ ) {
+                     this.builder.append("import ").append(ff.module).append('.').append(ff.names[i].name).append("\n"); lineNumber++;
+                 }
              } else if ( sn instanceof Str ) {
                  Str ss= (Str)sn;
                  this.builder.append("\"");
@@ -79,15 +94,59 @@ public class JythonToJavaConverter {
                  this.builder.append(indent);
                  for ( int i=0; i<as.targets.length; i++ ) {
                      if ( i>0 ) this.builder.append(",");
-                     this.builder.append(as.targets[i]);
+                     traverse("",as.targets[i]);
                  }
                  this.builder.append("=");
                  traverse( "",as.value );
                  
              } else if ( sn instanceof Name ) {
                  this.builder.append(((Name)sn).id);
+             } else if ( sn instanceof Call ) {
+                 Call cc= (Call)sn;
+                 if ( cc.func instanceof Name ) {
+                     this.builder.append(((Name)cc.func).id);                 
+                     this.builder.append("(");
+                     for ( int i=0; i<cc.args.length; i++ ) {
+                         if ( i>0 ) this.builder.append(",");
+                         traverse( "", cc.args[i] );
+                     }
+                     this.builder.append(")\n"); lineNumber++;
+                 } else {
+                     this.builder.append(cc.func.toString()).append("\n");  lineNumber++;
+                 }
+             } else if ( sn instanceof For ) {
+                 For ff= (For)sn;
+                 this.builder.append(indent).append("for ( Object ");
+                 traverse("",ff.target);
+                 this.builder.append(" : ");
+                 traverse("",ff.iter);
+                 this.builder.append(" ) {\n"); lineNumber++;
+                 for ( int i=0; i<ff.body.length; i++ ) {
+                     this.builder.append(indent).append(indent);
+                     traverse( indent+indent, ff.body[i] );
+                 }
+                 this.builder.append(indent).append("}\n");  lineNumber++;
+             } else if ( sn instanceof If ) {
+                 If ff= (If)sn;
+                 this.builder.append(indent).append("if ( ");
+                 traverse("",ff.test );
+                 this.builder.append(" ) {\n"); lineNumber++;
+                 for ( int i=0; i<ff.body.length; i++ ) {
+                     this.builder.append(indent).append(indent);
+                     traverse( indent+indent, ff.body[i] );
+                 }
+                 this.builder.append(indent).append("}\n");  lineNumber++;
+             } else if ( sn instanceof Compare ) {
+                 Compare cp= (Compare)sn;
+                 traverse( "", cp.left );
+                 this.builder.append( "?in?" );
+                 for ( exprType t: cp.comparators) {
+                    traverse( "", t );
+                 }
+             } else if ( sn instanceof Continue ) {
+                 this.builder.append("continue");
              } else {
-                 this.builder.append(sn.toString());
+                 this.builder.append(sn.toString()).append("\n"); lineNumber++;
              }
          }
          
@@ -117,8 +176,18 @@ public class JythonToJavaConverter {
     }
     
     public static void main( String[] args ) throws Exception {
-        String code= "def foo():\n  print 'hello'\nfoo()";
-        System.err.println( convert(code) );
+        String code;
+        //String code= "def foo():\n  print 'hello'\nfoo()";
+        //System.err.println( convert(code) );
+        
+        String furi= "/home/jbf/project/autoplot/script/lookAtUserComments.jy";
+        
+        File src = DataSetURI.getFile(furi, new NullProgressMonitor() );
+
+        try (FileReader reader = new FileReader(src)) {
+            code= JythonUtil.readScript( new BufferedReader( reader ) );
+            System.err.println( convert(code) );
+        }
     }
     
 }
