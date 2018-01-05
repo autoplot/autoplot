7ea33cf16b161dd105fc93600c0b3335345549c1
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2018-01-05 15:29:44 +0000

    pngwalk of small images (<65 pixel width) will use NN interpolation.  Also, paint a background on small gif images.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@21908 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/org/autoplot/pngwalk/WalkUtil.java
+++ Autoplot/src/org/autoplot/pngwalk/WalkUtil.java
@@ -195,10 +195,13 @@ public class WalkUtil {
     public static BufferedImage resizeImage( BufferedImage originalImage, int width, int height ) {
         BufferedImage resizedImage = new BufferedImage( width, height, originalImage.getType() );
         Graphics2D g = resizedImage.createGraphics();
-        g.setComposite(AlphaComposite.Src);
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        g.fillRect(0,0,width,height);
+        if ( originalImage.getWidth()>65 ) {
+            g.setComposite(AlphaComposite.Src);
+            g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+            g.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+        }
         g.drawImage(originalImage, 0, 0, width, height, null);
         g.dispose();
         return resizedImage;
