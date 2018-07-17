317a0bf4479adf29c743c632ecdaf6e36d2305ea
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2018-07-17 19:12:08 +0000

    add javadoc
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@22579 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonSupport/src/org/python/core/PyMethodPeeker.java
+++ JythonSupport/src/org/python/core/PyMethodPeeker.java
@@ -1,12 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.python.core;
 
 /**
- *
+ * Provide access to package-private data.
  * @author jbf
  */
 public class PyMethodPeeker {
--- JythonSupport/src/org/python/core/PyReflectedFunctionPeeker.java
+++ JythonSupport/src/org/python/core/PyReflectedFunctionPeeker.java
@@ -1,13 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.python.core;
 
 import java.lang.reflect.Method;
 
 /**
- *
+ * Provide access to package-private data.
  * @author jbf
  */
 public class PyReflectedFunctionPeeker {
