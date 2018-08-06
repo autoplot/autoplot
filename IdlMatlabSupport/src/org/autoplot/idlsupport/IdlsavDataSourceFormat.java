f0237f82ebb19220605aeb3a9f493afec55d3567
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2018-08-06 21:31:25 +0000

    bugfix: .idlsav cannot format non-qubes.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@22686 0c594bb0-8543-0410-a25e-d8ec440261db

--- IdlMatlabSupport/src/org/autoplot/idlsupport/IdlsavDataSourceFormat.java
+++ IdlMatlabSupport/src/org/autoplot/idlsupport/IdlsavDataSourceFormat.java
@@ -10,6 +10,7 @@ import org.das2.util.monitor.ProgressMonitor;
 import org.das2.qds.QDataSet;
 import org.das2.qds.SemanticOps;
 import org.autoplot.datasource.AbstractDataSourceFormat;
+import org.das2.qds.DataSetUtil;
 import org.das2.qds.ops.Ops;
 
 /**
@@ -193,7 +194,7 @@ public class IdlsavDataSourceFormat extends AbstractDataSourceFormat {
 
     @Override
     public boolean canFormat(QDataSet ds) {
-        return ds.rank()==1 || ds.rank()==2 || ds.rank()==3;
+        return DataSetUtil.isQube(ds) && ( ds.rank()==1 || ds.rank()==2 || ds.rank()==3 );
     }
 
     @Override
