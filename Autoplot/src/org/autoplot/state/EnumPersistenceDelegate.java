2c4470e85d246404962f17715e0db4eae4c15887
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2018-10-22 10:39:32 +0000

    trivial javadoc
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@22930 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/org/autoplot/state/EnumPersistenceDelegate.java
+++ Autoplot/src/org/autoplot/state/EnumPersistenceDelegate.java
@@ -1,54 +1,43 @@
-/*
- * UnitsPersistenceDelegate.java
- *
- * Created on August 8, 2007, 11:04 AM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package org.autoplot.state;
 
-import org.das2.datum.Units;
 import java.beans.DefaultPersistenceDelegate;
 import java.beans.Encoder;
 import java.beans.Expression;
 import java.beans.PropertyEditor;
 import org.das2.beans.BeansUtil;
-import org.das2.components.propertyeditor.Displayable;
 
 /**
- *
+ * PersistenceDelegate for nominal data (e.g. upper, lower)
  * @author jbf
  */
 public class EnumPersistenceDelegate extends DefaultPersistenceDelegate {
     
-    /** Creates a new instance of UnitsPersistenceDelegate */
     public EnumPersistenceDelegate() {
         
     }
     
     public Object findInstance( Class enumClass, String name ) {
-        Displayable d;
         PropertyEditor edit= BeansUtil.findEditor(enumClass);
         edit.setAsText(name);
         return edit.getValue();
     }
     
+    @Override
     protected Expression instantiate(Object oldInstance, Encoder out) {
-        Expression retValue;
-        
         return new Expression( oldInstance, this.getClass(), "findInstance", new Object[] { oldInstance.getClass(), oldInstance.toString() } );
     }
 
+    @Override
     protected void initialize(Class<?> type, Object oldInstance, Object newInstance, Encoder out) {
         super.initialize(type, oldInstance, newInstance, out);
     }
 
+    @Override
     public void writeObject(Object oldInstance, Encoder out) {
         super.writeObject(oldInstance, out);
     }
 
+    @Override
     protected boolean mutatesTo(Object oldInstance, Object newInstance) {
         boolean retValue;
         
