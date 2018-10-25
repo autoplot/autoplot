b3c6691913e9e5abba5abb74ade95122456187a9
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2018-10-25 12:24:40 +0000

    tweak the version string.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@22962 0c594bb0-8543-0410-a25e-d8ec440261db

--- AutoplotServlet/src/java/org/autoplot/servlet/SimpleServlet.java
+++ AutoplotServlet/src/java/org/autoplot/servlet/SimpleServlet.java
@@ -83,7 +83,7 @@ import org.das2.qds.ops.Ops;
 public class SimpleServlet extends HttpServlet {
 
     private static final Logger logger= Logger.getLogger("autoplot.servlet" );
-    public static final String version= "v20170930.1132";
+    public static final String version= "v20181025.0723";
 
     static FileHandler handler;
 
