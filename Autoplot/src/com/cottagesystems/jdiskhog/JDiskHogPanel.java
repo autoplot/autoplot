b2bca908f5590231080aa30da79759f9258e7ebb
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2018-12-20 16:34:19 +0000

    bug 0498: use close not Ok
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@23138 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/com/cottagesystems/jdiskhog/JDiskHogPanel.java
+++ Autoplot/src/com/cottagesystems/jdiskhog/JDiskHogPanel.java
@@ -418,7 +418,7 @@ public final class JDiskHogPanel extends javax.swing.JPanel {
 
         jScrollPane2.setViewportView(jTree1);
 
-        jButton1.setText("Ok");
+        jButton1.setText("Close");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
