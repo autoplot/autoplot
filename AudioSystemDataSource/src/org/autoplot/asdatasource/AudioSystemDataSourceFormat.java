bb96188b98f66e12390dfa7ce5340c4e92767059
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2019-01-07 13:14:00 +0000

    trivial javadoc
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@23185 0c594bb0-8543-0410-a25e-d8ec440261db

--- AudioSystemDataSource/src/org/autoplot/asdatasource/AudioSystemDataSourceFormat.java
+++ AudioSystemDataSource/src/org/autoplot/asdatasource/AudioSystemDataSourceFormat.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.autoplot.asdatasource;
 
 import java.util.Map;
@@ -25,7 +21,7 @@ public class AudioSystemDataSourceFormat implements DataSourceFormat {
         if ( data==null ) {
             throw new IllegalArgumentException( "data is null" );
         }
-            
+        
         Auralizor auralizor= new Auralizor(data);
         
         URISplit split= URISplit.parse(uri);
