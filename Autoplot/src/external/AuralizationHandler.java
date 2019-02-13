7052c7657bec17458cbecc28d713b3a9c58aa8a5
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2019-02-13 17:38:54 +0000

    make AuralizationHandler more appropriate for public use.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@23308 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/external/AuralizationHandler.java
+++ Autoplot/src/external/AuralizationHandler.java
@@ -1,24 +1,33 @@
 
 package external;
 
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.logging.Handler;
-import java.util.logging.Level;
 import java.util.logging.LogRecord;
-import java.util.logging.Logger;
 import java.util.regex.Pattern;
-import org.autoplot.wav.WavDataSource2;
 import org.das2.graph.Auralizor;
 import org.das2.qds.QDataSet;
-import org.das2.util.monitor.NullProgressMonitor;
 
 /**
  * experiment to see if auralization is useful with log messages.  This will make
  * one of two sounds when HEAD or GET is found within the log message.
+ * <pre>
+ * from org.das2.util import LoggerManager
+ * t= LoggerManager.getLogger('das2.url')
+ * from external import AuralizationHandler
+ * handler= AuralizationHandler()
+ * handler.addSound( 'HEAD.*', getDataSet( 'http://jfaden.net/~jbf/autoplot/data/wav/dinkq.wav' ) )
+ * handler.addSound( 'GET.*', getDataSet( 'http://jfaden.net/~jbf/autoplot/data/wav/ding2.wav' ) )
+ * t.addHandler( handler )
+ * from java.util.logging import Level
+ * t.setLevel( Level.FINER )
+ * t.info('GET test:')
+ * sleep(2000)
+ * getDataSet( 'http://jfaden.net/~jbf/autoplot/data/events/colorEvents.txt' )
+ * </pre>
+ * 
  * @author jbf
  */
 public class AuralizationHandler extends Handler {
@@ -32,11 +41,11 @@ public class AuralizationHandler extends Handler {
     
     /**
      * auralize the sound when the pattern is encountered.
-     * @param p the pattern, for example Pattern.compile("HEAD.*")
+     * @param regex the regex, for example 'HEAD.*'
      * @param s the sound, for example getDataSet("http://jfaden.net/~jbf/autoplot/data/wav/dinkq.wav")
      */
-    public void addSound( Pattern p, QDataSet s ) {
-        sounds.put( p, s );
+    public void addSound( String regex, QDataSet s ) {
+        sounds.put( Pattern.compile(regex), s );
     }
     
     
