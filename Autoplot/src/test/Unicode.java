10770c061005d07e8974857f49b8c6f20d81bf4b
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2019-03-22 16:37:50 +0000

    2108: reopen bug and add code which demos problem.  This should be used from the command line to see if the problem is independent of Tomcat/Jenkins, or not.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@23470 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/test/Unicode.java
+++ Autoplot/src/test/Unicode.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package test;
 
 import java.io.File;
@@ -14,10 +10,7 @@ import java.io.UnsupportedEncodingException;
  */
 public class Unicode {
     public static void main(String[] args) throws UnsupportedEncodingException {
-        //System.err.println("/Spaß/");
-        //for ( byte c : "/Spaß/".getBytes("UTF-8") ) {
-        //    System.err.println( c );
-        //}
+        // There is interesting code in the SVN history.
         String[] ff= new File("/home/jbf/public_html/i18n/").list();
         System.err.println("ls /home/jbf/public_html/i18n/");
         for ( String f: ff ) { 
