4e86f4fce3f33fc01052fbbc3e990d119b389952
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2019-03-22 16:48:32 +0000

    2108: dump output to file as well as stdout (and the web).
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@23471 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/test/Unicode.java
+++ Autoplot/src/test/Unicode.java
@@ -2,19 +2,26 @@
 package test;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
 /**
- *
+ * See https://sourceforge.net/p/autoplot/bugs/2108/
  * @author jbf
  */
 public class Unicode {
-    public static void main(String[] args) throws UnsupportedEncodingException {
+    public static void main(String[] args) throws UnsupportedEncodingException, IOException {
         // There is interesting code in the SVN history.
-        String[] ff= new File("/home/jbf/public_html/i18n/").list();
-        System.err.println("ls /home/jbf/public_html/i18n/");
-        for ( String f: ff ) { 
-            System.err.println(f);
+        try ( PrintWriter fw = new PrintWriter( new FileWriter("unicode.output.txt") ) ) {
+            String[] ff= new File("/home/jbf/public_html/i18n/").list();
+            System.err.println("ls /home/jbf/public_html/i18n/");
+            fw.println("ls /home/jbf/public_html/i18n/");
+            for ( String f: ff ) {
+                System.err.println(f);
+                fw.println(f);
+            }
         }
     }
 }
