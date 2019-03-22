f75289c7657818da4f7c0f73dc99ed6ac7ec0c3c
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2019-03-22 17:08:53 +0000

    2108: print Charset.defaultCharset
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@23472 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/test/Unicode.java
+++ Autoplot/src/test/Unicode.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
 
 /**
  * See https://sourceforge.net/p/autoplot/bugs/2108/
@@ -14,7 +15,9 @@ import java.io.UnsupportedEncodingException;
 public class Unicode {
     public static void main(String[] args) throws UnsupportedEncodingException, IOException {
         // There is interesting code in the SVN history.
+        System.err.println("Charset.defaultCharset()="+Charset.defaultCharset() );
         try ( PrintWriter fw = new PrintWriter( new FileWriter("unicode.output.txt") ) ) {
+            
             String[] ff= new File("/home/jbf/public_html/i18n/").list();
             System.err.println("ls /home/jbf/public_html/i18n/");
             fw.println("ls /home/jbf/public_html/i18n/");
