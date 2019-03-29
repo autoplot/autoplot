52576663816b532ad545ec2cef5b192b76b77fe8
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2019-03-29 21:54:44 +0000

    bugfix: vap+orbit: worked strangely because there was no extension on the unused file part.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@23513 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/autoplot/datasource/DataSetSelector.java
+++ DataSource/src/org/autoplot/datasource/DataSetSelector.java
@@ -803,10 +803,7 @@ public class DataSetSelector extends javax.swing.JPanel {
             } else {
                 URISplit split= URISplit.parse(surl);
                 if ( !".vap".equals(split.ext) ) { 
-                    if ( split.ext!=null ) {
-                        //experiment with GUI based on completions.
-                        edit= new CompletionsDataSourceEditor();
-                    }
+                    edit= new CompletionsDataSourceEditor();
                 } else {
                     if ( split.path.startsWith("file:") ) { //TODO: I believe this is now dead code which will not be reached because of .vap browse trigger.
                         String result= DataSetSelectorSupport.browseLocalVap(this, surl);
