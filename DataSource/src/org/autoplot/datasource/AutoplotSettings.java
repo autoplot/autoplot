908af0dd14f39a9c591bbaede48d07867b5541ce
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2019-07-26 16:00:32 +0000

    getPreferences can be static
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@23962 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSource/src/org/autoplot/datasource/AutoplotSettings.java
+++ DataSource/src/org/autoplot/datasource/AutoplotSettings.java
@@ -89,7 +89,7 @@ public final class AutoplotSettings {
      * @param c the class requesting preferences.
      * @return the preferences object this class should use.
      */
-    public Preferences getPreferences( Class c ) {
+    public static Preferences getPreferences( Class c ) {
         String s= c.getPackage().getName();
         Preferences p1= Preferences.userRoot().node("/"+s.replace('.','/'));
         switch (s) {
