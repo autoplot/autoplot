6d06d2c9999b045927baff89248ff8c907314249
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2019-09-20 16:32:42 +0000

    rfe696: add initial support for references, recursing into JSONObjects like bins
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@24131 0c594bb0-8543-0410-a25e-d8ec440261db

--- HapiDataSource/src/org/autoplot/hapi/HapiUtil.java
+++ HapiDataSource/src/org/autoplot/hapi/HapiUtil.java
@@ -16,14 +16,42 @@ public final class HapiUtil {
     public static final String KEY_PARAMETERS= "parameters";
     public static final String KEY_REF= "$ref";
     
+    private static void resolveRefRecursive( JSONObject definitions, JSONObject param ) throws JSONException {
+        Iterator keys= param.keys();
+        while ( keys.hasNext() ) {
+            String k= (String)keys.next();
+            Object o= param.get(k);
+            if ( o instanceof JSONObject ) {
+                JSONObject maybeRef= (JSONObject)o;
+                if ( maybeRef.has(KEY_REF) ) {
+                    String theRef= maybeRef.getString(KEY_REF);
+                    if ( theRef.startsWith("#/definitions/") ) {
+                        String theDefinitionsRef= theRef.substring(14);
+                        if ( definitions.has(theDefinitionsRef) ) {
+                            Object deref= definitions.get(theDefinitionsRef);
+                            param.put( k, deref );
+                        } else {
+                            throw new IllegalArgumentException("reference not found within definitions: "+theRef);
+                        }
+                    } else {
+                        throw new IllegalArgumentException("references may only be to nodes within definitions: "+theRef);
+                    }
+                } else {
+                    resolveRefRecursive( definitions, maybeRef );
+                }
+            }
+        }
+    }
+    
     /**
      * resolve references within the JSON.  These references must be
      * to the definitions node.  
      * TODO: This is not complete, and does not go into bins object.
      * 
-     * @param jo
+     * @param jo the JSONObject returned by the "info" request.
      * @return
      * @throws JSONException 
+     * @see https://sourceforge.net/p/autoplot/feature-requests/696/
      */
     public static JSONObject resolveRefs( JSONObject jo ) throws JSONException {
         if ( !jo.has(KEY_DEFINITIONS) ) {
@@ -34,28 +62,7 @@ public final class HapiUtil {
         
         for ( int i=0; i<ja.length(); i++ ) {
             JSONObject param= ja.getJSONObject(i);
-            Iterator keys= param.keys();
-            while ( keys.hasNext() ) {
-                String k= (String)keys.next();
-                Object o= param.get(k);
-                if ( o instanceof JSONObject ) {
-                    JSONObject maybeRef= (JSONObject)o;
-                    if ( maybeRef.has(KEY_REF) ) {
-                        String theRef= maybeRef.getString(KEY_REF);
-                        if ( theRef.startsWith("#/definitions/") ) {
-                            String theDefinitionsRef= theRef.substring(14);
-                            if ( definitions.has(theDefinitionsRef) ) {
-                                Object deref= definitions.get(theDefinitionsRef);
-                                param.put( k, deref );
-                            } else {
-                                throw new IllegalArgumentException("reference not found within definitions: "+theRef);
-                            }
-                        } else {
-                            throw new IllegalArgumentException("references may only be to nodes within definitions: "+theRef);
-                        }
-                    }
-                }
-            }
+            resolveRefRecursive( definitions, param );
         }
         return jo;
     }
