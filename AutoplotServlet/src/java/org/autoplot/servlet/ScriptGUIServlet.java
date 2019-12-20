b9e3dd2a58963322414bfdf9780da81061ffaede
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2019-12-20 15:27:52 +0000

    add picker for scripts based on SCRIPT_HOME/log/ScriptGUIServlet
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@24412 0c594bb0-8543-0410-a25e-d8ec440261db

--- AutoplotServlet/src/java/org/autoplot/servlet/ScriptGUIServlet.java
+++ AutoplotServlet/src/java/org/autoplot/servlet/ScriptGUIServlet.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.lang.reflect.Array;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -126,155 +127,165 @@ public class ScriptGUIServlet extends HttpServlet {
         script= FileUtil.readFileToString(scriptFile);            
         
         if ( request.getParameter("img")!=null ) {
-            // now run the script
-            
-            File scriptLogArea= new File( ServletUtil.getServletHome(), "log" );
-            if ( !scriptLogArea.exists() ) {
-                if ( !scriptLogArea.mkdirs() ) {
-                    logger.warning("unable to make log area");
-                }
-            }
-            File scriptLogFile= new File( scriptLogArea, "ScriptGUIServlet.log" );
-            if ( scriptLogFile.exists() ) {
-                try ( PrintWriter w= new PrintWriter( new FileWriter( scriptLogFile, true ) ) ) {
-                    Datum n= TimeUtil.now();
-                    TimeParser tp= TimeParser.create( TimeParser.TIMEFORMAT_Z );
-                    String s= tp.format( n ) + "\t" + scriptURI; 
-                    w.println(s);
-                }
-            } else {
-                try ( PrintWriter w= new PrintWriter( new FileWriter( scriptLogFile ) ) ) {
-                    w.println(scriptURI);
-                }
-            }
-            
-            org.autoplot.Util.addFonts();
-                    
-            ApplicationModel model = new ApplicationModel();
-            model.setExceptionHandler( new DumpRteExceptionHandler() );
-            model.addDasPeersToAppAndWait();
-            Application dom= model.getDocumentModel();
-            
-            logger.log(Level.FINE, "dom: {0}", dom);
-            logger.log(Level.FINE, "dom options: {0}", dom.getOptions());
-            
-            dom.getOptions().setAutolayout(false);
-            
-            PythonInterpreter interp = JythonUtil.createInterpreter( true, true );
-            interp.set("java",null);
-            interp.set("org",null);
-            interp.set("getFile",null);
-            interp.set("dom",dom);
-            interp.set("downloadResourceAsTempFile",null);
-
-            LoggingOutputStream los1= new LoggingOutputStream( Logger.getLogger("autoplot.servlet.scriptservlet"), Level.INFO );
-            interp.setOut( los1 );
-            
-            interp.set( "response", response );
-
-            // To support load balancing, insert the actual host that resolved the request
-            response.setHeader( "X-Served-By", java.net.InetAddress.getLocalHost().getCanonicalHostName() );
-            
-            //TODO: this limits to one user!
-            LoggingOutputStream los2= new LoggingOutputStream( Logger.getLogger("autoplot.servlet.scriptservlet"), Level.INFO ); 
-            //ScriptContext._setOutputStream( los2 ); 
-            
-            script= JythonRefactory.fixImports(script);
-            
-            ScriptContext.setApplicationModel(model); // why must I do this???
-            
-            JythonUtil.runScript( dom, 
-                    new ByteArrayInputStream(script.getBytes("UTF-8")), 
-                    name, 
-                    aaparams, 
-                    pwd );
-            
-            try (OutputStream out = response.getOutputStream()) {
-                writeToPng(dom,out);
-                try { los1.close(); } catch ( IOException ex ) {}
-                try { los2.close(); } catch ( IOException ex ) {}
-            }
+            writeOutputImage(scriptURI, response, script, name, aaparams, pwd);
             
         } else {
-            response.setContentType("text/html;charset=UTF-8");
-        
-            Map<String,Object> env= new HashMap<>();
-            env.put( "PWD", pwd );
+            writeParametersForm(response, pwd, script, ssparams, name, request, scriptURI, sparams);
             
-            Map<String,Param> parms= Util.getParams( env, script, ssparams, new NullProgressMonitor() );
+        }
+    }
+
+    private void writeOutputImage(String scriptURI, HttpServletResponse response, String script, String name, String[] aaparams, String pwd) throws IOException, UnknownHostException {
+        // now run the script
         
-            try (PrintWriter out = response.getWriter()) {
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>"+name+"</title>");            
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>Servlet ScriptGUIServlet at " + request.getContextPath() + "</h1>");
-                out.println("Running script <a href="+scriptURI+">"+scriptURI+"</a>");
-                out.println("<table><tr>");
-                out.println("<td valign='top'>");
-                out.println("<form action='ScriptGUIServlet'>");
-                for ( Entry<String,Param> pe: parms.entrySet() ) {
-                    Param p= pe.getValue();
-                    p.doc= p.doc.trim();
-                    Object currentValue= p.value == null ? p.deft : p.value;
-                    String andDoc= p.doc.length()>0 ? ( ", <em>"+ p.doc +"</em>" ) : "";
-                    boolean isCheckBox= p.enums!=null && p.enums.size()==2 && p.enums.contains("T") && p.enums.contains("F");
-                    if ( !isCheckBox ) {
-                        out.println(""+p.name + andDoc +"<br>");
-                    }
-                    if ( p.enums!=null ) {
-                        if ( isCheckBox ) {
-                            if ( "T".equals(currentValue) ) {
-                                out.println("<input type='checkbox' name='"+p.name+"' checked>"+p.name + andDoc );
-                            } else if ( "on".equals(currentValue) ) {
-                                out.println("<input type='checkbox' name='"+p.name+"' checked>"+p.name + andDoc );
-                                sparams= sparams.replace(p.name+"=on", p.name+"=T");
-                            } else {
-                                out.println("<input type='checkbox' name='"+p.name+"'>"+p.name + andDoc );
-                            }
-                        } else {
-                            out.println("<select name='"+p.name+"'>");
-                            for ( Object s: p.enums ) {
-                                if ( s.equals(currentValue) ) {
-                                    out.println("<option value='"+s+"' selected>"+s+"</input>");
-                                } else {
-                                    out.println("<option value='"+s+"'>"+s+"</input>");
-                                }
-                            }
-                            out.println("</select>");
-                        }
-                    } else if ( (p.type=='F') || (p.type=='A') ) {
-                        Object s= (p.value!=null) ? p.value : p.deft;
-                        out.println("<input name='"+p.name+"' value='"+s+"'></input>");
-                    } else if ( p.type=='T' ) {
-                        //TODO: nice timerange GUI
-                        Object s= (p.value!=null) ? p.value : p.deft;
-                        out.println("<input name='"+p.name+"' value='"+s+"'></input>");
-                    } else {
-                        //TODO: GUIs for URIs and other parameters.
-                        Object s= (p.value!=null) ? p.value : p.deft;
-                        out.println("<input name='"+p.name+"' value='"+s+"'></input>");
-                    }
-                    out.println("<br><br>");
-                }
-                if ( parms.isEmpty() ) {
-                    out.println("script has no parameters.");
-                }
-                out.println("<input type='hidden' name='script' value='"+scriptURI+"'>");
-                out.println("<input type='submit' value='Submit'>");
-                out.println("</form>");
-                out.println( "</td>");
-                out.println( "<td valign='top'>");
-                out.println( "<img src='ScriptGUIServlet?img=1"+sparams+"' alt='image'>" );
-                out.println( "</td>");
-                out.println( "</tr>");
-                out.println( "</table>");
-                out.println("</body>");
-                out.close();
-                
+        File scriptLogArea= new File( ServletUtil.getServletHome(), "log" );
+        if ( !scriptLogArea.exists() ) {
+            if ( !scriptLogArea.mkdirs() ) {
+                logger.warning("unable to make log area");
             }
+        }            
+        File scriptLogFile= new File( scriptLogArea, "ScriptGUIServlet.log" );
+        if ( scriptLogFile.exists() ) {
+            try ( PrintWriter w= new PrintWriter( new FileWriter( scriptLogFile, true ) ) ) {
+                Datum n= TimeUtil.now();
+                TimeParser tp= TimeParser.create( TimeParser.TIMEFORMAT_Z );
+                String s= tp.format( n ) + "\t" + scriptURI;
+                w.println(s);
+            }
+        } else {
+            try ( PrintWriter w= new PrintWriter( new FileWriter( scriptLogFile ) ) ) {
+                w.println(scriptURI);
+            }
+        }
+        
+        org.autoplot.Util.addFonts();
+        
+        ApplicationModel model = new ApplicationModel();
+        model.setExceptionHandler( new DumpRteExceptionHandler() );
+        model.addDasPeersToAppAndWait();
+        Application dom= model.getDocumentModel();
+        
+        logger.log(Level.FINE, "dom: {0}", dom);
+        logger.log(Level.FINE, "dom options: {0}", dom.getOptions());
+        
+        dom.getOptions().setAutolayout(false);
+        
+        PythonInterpreter interp = JythonUtil.createInterpreter( true, true );
+        interp.set("java",null);
+        interp.set("org",null);
+        interp.set("getFile",null);
+        interp.set("dom",dom);
+        interp.set("downloadResourceAsTempFile",null);
+        
+        LoggingOutputStream los1= new LoggingOutputStream( Logger.getLogger("autoplot.servlet.scriptservlet"), Level.INFO );
+        interp.setOut( los1 );
+        
+        interp.set( "response", response );
+        
+        // To support load balancing, insert the actual host that resolved the request
+        response.setHeader( "X-Served-By", java.net.InetAddress.getLocalHost().getCanonicalHostName() );
+        
+        //TODO: this limits to one user!
+        LoggingOutputStream los2= new LoggingOutputStream( Logger.getLogger("autoplot.servlet.scriptservlet"), Level.INFO );
+        //ScriptContext._setOutputStream( los2 );
+        
+        script= JythonRefactory.fixImports(script);
+        
+        ScriptContext.setApplicationModel(model); // why must I do this???
+        
+        JythonUtil.runScript( dom,
+                new ByteArrayInputStream(script.getBytes("UTF-8")),
+                name,
+                aaparams,
+                pwd );
+        
+        try (OutputStream out = response.getOutputStream()) {
+            writeToPng(dom,out);
+            try { los1.close(); } catch ( IOException ex ) {}
+            try { los2.close(); } catch ( IOException ex ) {}
+        }
+    }
+
+    private void writeParametersForm(HttpServletResponse response, String pwd, String script, Map<String, String> ssparams, String name, HttpServletRequest request, String scriptURI, String sparams) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        Map<String,Object> env= new HashMap<>();
+        env.put( "PWD", pwd );
+        
+        Map<String,Param> parms= Util.getParams( env, script, ssparams, new NullProgressMonitor() );
+        
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>"+name+"</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ScriptGUIServlet at " + request.getContextPath() + "</h1>");
+            out.println("<table><tr>");
+            out.println("<td valign='top'>");
+            out.println("<form action='ScriptGUIServlet'>");
+            for ( Entry<String,Param> pe: parms.entrySet() ) {
+                Param p= pe.getValue();
+                p.doc= p.doc.trim();
+                Object currentValue= p.value == null ? p.deft : p.value;
+                String andDoc= p.doc.length()>0 ? ( ", <em>"+ p.doc +"</em>" ) : "";
+                boolean isCheckBox= p.enums!=null && p.enums.size()==2 && p.enums.contains("T") && p.enums.contains("F");
+                if ( !isCheckBox ) {
+                    out.println(""+p.name + andDoc +"<br>");
+                }
+                if ( p.enums!=null ) {
+                    if ( isCheckBox ) {
+                        if ( "T".equals(currentValue) ) {
+                            out.println("<input type='checkbox' name='"+p.name+"' checked>"+p.name + andDoc );
+                        } else if ( "on".equals(currentValue) ) {
+                            out.println("<input type='checkbox' name='"+p.name+"' checked>"+p.name + andDoc );
+                            sparams= sparams.replace(p.name+"=on", p.name+"=T");
+                        } else {
+                            out.println("<input type='checkbox' name='"+p.name+"'>"+p.name + andDoc );
+                        }
+                    } else {
+                        out.println("<select name='"+p.name+"'>");
+                        for ( Object s: p.enums ) {
+                            if ( s.equals(currentValue) ) {
+                                out.println("<option value='"+s+"' selected>"+s+"</input>");
+                            } else {
+                                out.println("<option value='"+s+"'>"+s+"</input>");
+                            }
+                        }
+                        out.println("</select>");
+                    }
+                } else if ( (p.type=='F') || (p.type=='A') ) {
+                    Object s= (p.value!=null) ? p.value : p.deft;
+                    out.println("<input name='"+p.name+"' value='"+s+"'></input>");
+                } else if ( p.type=='T' ) {
+                    //TODO: nice timerange GUI
+                    Object s= (p.value!=null) ? p.value : p.deft;
+                    out.println("<input name='"+p.name+"' value='"+s+"'></input>");
+                } else {
+                    //TODO: GUIs for URIs and other parameters.
+                    Object s= (p.value!=null) ? p.value : p.deft;
+                    out.println("<input name='"+p.name+"' value='"+s+"'></input>");
+                }
+                out.println("<br><br>");
+            }
+            if ( parms.isEmpty() ) {
+                out.println("script has no parameters.");
+            }
+            out.println("<input type='hidden' name='script' value='"+scriptURI+"'>");
+            out.println("<input type='submit' value='Submit'>");
+            out.println("</form>");
+            out.println( "</td>");
+            out.println( "<td valign='top'>");
+            out.println( "<img src='ScriptGUIServlet?img=1"+sparams+"' alt='image'>" );
+            out.println( "</td>");
+            out.println( "</tr>");
+            out.println( "</table>");
+            out.println( "<hr>" );
+            out.println("Running script <a href="+scriptURI+">"+scriptURI+"</a>");
+            out.println("Pick <a href='ScriptGUIServletPick'>another</a>\n");
+            out.println("</body>");
+            out.close();
             
         }
     }
