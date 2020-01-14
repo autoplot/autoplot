8fe8e68c8287a57736836b121a6db8c8449720ac
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2020-01-14 22:03:22 +0000

    trivial javadoc and reformat
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@24461 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonSupport/src/org/das2/jythoncompletion/DataSetUrlCompletionTask.java
+++ JythonSupport/src/org/das2/jythoncompletion/DataSetUrlCompletionTask.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.das2.jythoncompletion;
 
@@ -17,41 +13,43 @@ import org.autoplot.datasource.DataSetURI;
 import org.autoplot.datasource.DataSetURI.CompletionResult;
 
 /**
- *
+ * completions of a URI within the editor, which delegate down to the 
+ * data source.
  * @author jbf
  */
 class DataSetUrlCompletionTask implements CompletionTask {
 
-    private static final Logger logger= Logger.getLogger("jython.editor");
-    
+    private static final Logger logger = Logger.getLogger("jython.editor");
+
     JTextComponent editor;
-    
+
     public DataSetUrlCompletionTask(JTextComponent arg1) {
-        this.editor= arg1;
+        this.editor = arg1;
     }
 
+    @Override
     public void cancel() {
 
     }
 
-    public static String popString( JTextComponent editor, int [] pos ) {
+    public static String popString(JTextComponent editor, int[] pos) {
         try {
             int i0 = Utilities.getRowStart(editor, editor.getCaretPosition());
-            int i1 = Utilities.getRowEnd(editor, editor.getCaretPosition())-1; // trim end of line
+            int i1 = Utilities.getRowEnd(editor, editor.getCaretPosition()) - 1; // trim end of line
             String line = editor.getText(i0, i1 - i0);
             int ipos = editor.getCaretPosition() - i0;
-            i0 = line.lastIndexOf('\'', ipos-1);
-            boolean doubleQuotes=false;
+            i0 = line.lastIndexOf('\'', ipos - 1);
+            boolean doubleQuotes = false;
             if (i0 == -1) {
-                i0 = line.lastIndexOf('\"', ipos-1);
-                if ( i0==-1 ) {
+                i0 = line.lastIndexOf('\"', ipos - 1);
+                if (i0 == -1) {
                     throw new IllegalArgumentException("expected single quote");
                 } else {
-                    doubleQuotes= true;
+                    doubleQuotes = true;
                 }
             }
-	    i0+=1;
-            if ( doubleQuotes ) {
+            i0 += 1;
+            if (doubleQuotes) {
                 i1 = line.indexOf('\"', ipos);
             } else {
                 i1 = line.indexOf('\'', ipos);
@@ -59,29 +57,30 @@ class DataSetUrlCompletionTask implements CompletionTask {
             if (i1 == -1) {
                 i1 = line.length();
             }
-            pos[0]= i0;
-            pos[1]= i1;
-            return line.substring(i0,i1);
-        } catch ( BadLocationException ex) {
+            pos[0] = i0;
+            pos[1] = i1;
+            return line.substring(i0, i1);
+        } catch (BadLocationException ex) {
             logger.log(Level.SEVERE, ex.getMessage(), ex);
         } catch (Exception ex) {
             logger.log(Level.SEVERE, ex.getMessage(), ex);
         }
         return null;
     }
-    
-    public void query( CompletionResultSet arg0 ) {
+
+    @Override
+    public void query(CompletionResultSet arg0) {
         try {
             int i0 = Utilities.getRowStart(editor, editor.getCaretPosition());
             int ipos = editor.getCaretPosition() - i0;
-            int[] pos= new int[2];
+            int[] pos = new int[2];
             String surl1 = popString(editor, pos);
             int carotPos = ipos - pos[0];
 
-            List<CompletionResult> rs= DataSetURI.getCompletions( surl1, carotPos, new NullProgressMonitor() );
-            
-            for ( CompletionResult rs1:rs ) {
-                arg0.addItem( new DataSetUrlCompletionItem(rs1) );
+            List<CompletionResult> rs = DataSetURI.getCompletions(surl1, carotPos, new NullProgressMonitor());
+
+            for (CompletionResult rs1 : rs) {
+                arg0.addItem(new DataSetUrlCompletionItem(rs1));
             }
         } catch (BadLocationException ex) {
             logger.log(Level.SEVERE, ex.getMessage(), ex);
@@ -90,7 +89,8 @@ class DataSetUrlCompletionTask implements CompletionTask {
         }
     }
 
-    public void refresh( CompletionResultSet arg0 ) {
+    @Override
+    public void refresh(CompletionResultSet arg0) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
