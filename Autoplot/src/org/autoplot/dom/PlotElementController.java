eb63a4be220a3a5d8933ee3765700bfc22ae2bc8
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2020-01-22 19:31:42 +0000

    2169: wi_h0_mfi_19941123_v04.cdf?ORTH_I showed more precision was needed in the test.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@24492 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/org/autoplot/dom/PlotElementController.java
+++ Autoplot/src/org/autoplot/dom/PlotElementController.java
@@ -1466,7 +1466,8 @@ public class PlotElementController extends DomNodeController {
             //boolean lastDimBundle= isLastDimBundle( fillDs );
             //boolean joinOfBundle= fillDs.property(QDataSet.JOIN_0)!=null && lastDimBundle;
             int ndim= Ops.dimensionCount(fillDs);
-            boolean shouldSlice= ( fillDs.rank()>2 && ndim>3 && plotElement.isAutoComponent() && fillDs.property(QDataSet.DEPEND_0)!=null );
+            boolean isxyz= SemanticOps.isBundle(fillDs) && fillDs.property(QDataSet.DEPEND_0)!=null;
+            boolean shouldSlice= ( fillDs.rank()>2 && ndim>3 && plotElement.isAutoComponent() && !isxyz );
             if ( renderType==RenderType.image && fillDs.rank()==3 ) {
                 shouldSlice= false; //TODO: some how render types should indicate they can handle a slice.
             }
@@ -1500,7 +1501,7 @@ public class PlotElementController extends DomNodeController {
 
             boolean shouldHaveChildren= 
                     fillDs.rank() == 2 
-                    && fillDs.property(QDataSet.DEPEND_0)!=null
+                    && !isxyz
                     && !isWaveform
                     &&  ( renderType == RenderType.hugeScatter 
                     || renderType==RenderType.series 
