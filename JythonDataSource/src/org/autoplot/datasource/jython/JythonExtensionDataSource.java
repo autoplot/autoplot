2638f304f3c91c96000c7c07f0fa904c6f28dbab
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2020-02-24 16:48:49 +0000

    log the name of the script used to implement.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@24616 0c594bb0-8543-0410-a25e-d8ec440261db

--- JythonDataSource/src/org/autoplot/datasource/jython/JythonExtensionDataSource.java
+++ JythonDataSource/src/org/autoplot/datasource/jython/JythonExtensionDataSource.java
@@ -2,9 +2,13 @@
 package org.autoplot.datasource.jython;
 
 import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.autoplot.datasource.AbstractDataSource;
 import org.autoplot.datasource.DataSetURI;
 import org.autoplot.datasource.DataSourceRegistry;
+import org.autoplot.datasource.LogNames;
+import org.das2.datum.LoggerManager;
 import org.das2.qds.QDataSet;
 import org.das2.util.monitor.ProgressMonitor;
 
@@ -15,6 +19,8 @@ import org.das2.util.monitor.ProgressMonitor;
  */
 public class JythonExtensionDataSource extends AbstractDataSource {
 
+    private static final Logger logger= LoggerManager.getLogger( LogNames.APDSS_JYDS );
+    
     public JythonExtensionDataSource(URI uri) {
         super(uri);
     }
@@ -23,6 +29,8 @@ public class JythonExtensionDataSource extends AbstractDataSource {
     public QDataSet getDataSet(ProgressMonitor mon) throws Exception {
     
         String jydsUri= JythonExtensionDataSourceFactory.getJydsUri(uri);
+        logger.log(Level.FINE, "Using script to implement extension: {0}", jydsUri);
+        
         JythonDataSourceFactory jdsf= (JythonDataSourceFactory)DataSourceRegistry.getInstance().getSource(".jyds");
         
         JythonDataSource jyds= (JythonDataSource) jdsf.getDataSource( DataSetURI.getURI(jydsUri) );
