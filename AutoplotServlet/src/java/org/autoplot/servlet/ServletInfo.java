af7951b7c5c6cf12344e991a9565df3527bd2af7
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2020-03-25 14:56:55 +0000

    check whitelist to see if vap file is from a trusted source.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@24705 0c594bb0-8543-0410-a25e-d8ec440261db

--- AutoplotServlet/src/java/org/autoplot/servlet/ServletInfo.java
+++ AutoplotServlet/src/java/org/autoplot/servlet/ServletInfo.java
@@ -22,7 +22,7 @@ import org.autoplot.dom.Application;
 public class ServletInfo extends HttpServlet {
 
     private static final long birthMilli= System.currentTimeMillis();
-    public static final String version = "v20200325.0925";
+    public static final String version = "v20200325.0956";
     
     public static long getAgeMillis() {
         return System.currentTimeMillis() - birthMilli;
