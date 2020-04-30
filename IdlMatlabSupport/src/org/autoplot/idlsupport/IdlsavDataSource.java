2de8b0620b27029bdc1f0944a5ca51f2b507a62f
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2020-04-30 16:02:59 +0000

    allow commas in arg_0, so that you can specify X,Y,Z
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@24813 0c594bb0-8543-0410-a25e-d8ec440261db

--- IdlMatlabSupport/src/org/autoplot/idlsupport/IdlsavDataSource.java
+++ IdlMatlabSupport/src/org/autoplot/idlsupport/IdlsavDataSource.java
@@ -5,7 +5,11 @@
  */
 package org.autoplot.idlsupport;
 
+import com.jmatio.io.MatFileReader;
+import com.jmatio.types.MLArray;
+import com.jmatio.types.MLNumericArray;
 import java.io.File;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.URI;
 import java.nio.ByteBuffer;
@@ -13,7 +17,9 @@ import java.nio.channels.FileChannel;
 import java.util.Map;
 import org.autoplot.datasource.AbstractDataSource;
 import org.das2.qds.ArrayDataSet;
+import org.das2.qds.DataSetUtil;
 import org.das2.qds.QDataSet;
+import org.das2.qds.buffer.BufferDataSet;
 import org.das2.qds.ops.Ops;
 import org.das2.util.monitor.ProgressMonitor;
 
@@ -37,26 +43,7 @@ public class IdlsavDataSource extends AbstractDataSource {
         }
     }
     
-    @Override
-    public QDataSet getDataSet(ProgressMonitor mon) throws Exception {
-        File f= getFile(mon);
-        
-        RandomAccessFile aFile = new RandomAccessFile(f,"r");
-        FileChannel inChannel = aFile.getChannel();
-        long fileSize = inChannel.size();
-        ByteBuffer buffer = ByteBuffer.allocate((int) fileSize);
-        int bytesRead= 0;
-        while ( bytesRead<fileSize ) {
-            bytesRead+= inChannel.read(buffer);
-        }
-       
-        String arg= getParam( "arg_0", "" );
-        if ( arg.length()==0 ) {
-            throw new IllegalArgumentException("specify a variable");
-        }
-
-        ReadIDLSav reader= new ReadIDLSav();
-        
+    private QDataSet getArray( ReadIDLSav reader, ByteBuffer buffer, String arg ) throws IOException {
         Object v;
         
         int i= arg.indexOf('.');
@@ -87,7 +74,76 @@ public class IdlsavDataSource extends AbstractDataSource {
         } else {
             return Ops.dataset(v);
         }
+    }
+    
+    @Override
+    public QDataSet getDataSet(ProgressMonitor mon) throws Exception {
+        File f= getFile( uri, mon );
+        ReadIDLSav reader= new ReadIDLSav();
         
+        RandomAccessFile aFile = new RandomAccessFile(f,"r");
+        FileChannel inChannel = aFile.getChannel();
+        long fileSize = inChannel.size();
+        ByteBuffer buffer = ByteBuffer.allocate((int) fileSize);
+        int bytesRead= 0;
+        while ( bytesRead<fileSize ) {
+            bytesRead+= inChannel.read(buffer);
+        }
+        
+        String name = getParam( "arg_0", "" );
+        if ( name.length()==0 ) {
+            throw new IllegalArgumentException("name must be set");
+        }
+        
+        String[] names= name.split(",");
+        QDataSet[] datas= new QDataSet[names.length];
+        if ( names.length>4 ) {
+            throw new IllegalArgumentException("first argument can only"
+                    + " contain four comma-separated names." );
+        }
+        
+        for ( int i=0; i<names.length; i++ ) {
+            QDataSet array= getArray( reader, buffer, names[i]);
+
+            int[] qube= DataSetUtil.qubeDims(array);
+
+            QDataSet result;
+            switch (qube.length) {
+                case 2:
+                    int t= qube[0];
+                    qube[0]= qube[1];
+                    qube[1]= t;
+
+                    result= array;
+                    if ( result.length(0)==6 && result.length()>0 ) {
+                        double yr= result.value(0,0);
+                        if ( Math.floor(yr)==yr && yr>1900 && yr<2200 ) {
+                            result= Ops.toTimeDataSet( Ops.slice1(result,0),
+                                    Ops.slice1(result,1), 
+                                    Ops.slice1(result,2),
+                                    Ops.slice1(result,3),
+                                    Ops.slice1(result,4),
+                                    Ops.slice1(result,5), null );
+                        }
+                    }
+                    break;
+                default:
+                    result= array;
+                    break;
+            }
+            result= Ops.putProperty( result, QDataSet.NAME, names[i].replaceAll("\\.","_") );
+            result= Ops.putProperty( result, QDataSet.LABEL, names[i] );
+            datas[i]= result;
+            
+        }
+        
+        switch( names.length ) {
+            case 1: return datas[0];
+            case 2: return Ops.link(datas[0],datas[1]);
+            case 3: return Ops.link(datas[0],datas[1],datas[2]);
+            case 4: return Ops.link(datas[0],datas[1],datas[2],datas[3]);
+            default: throw new IllegalArgumentException("not supported");
+        }
     }
     
 }
