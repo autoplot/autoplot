2efd6950281a462a3bc35dd42dc7c46a0fa14e56
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2020-07-19 13:52:34 +0000

    up version number
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@24963 0c594bb0-8543-0410-a25e-d8ec440261db

--- AutoplotServlet/src/java/org/autoplot/servlet/ServletInfo.java
+++ AutoplotServlet/src/java/org/autoplot/servlet/ServletInfo.java
@@ -22,7 +22,7 @@ import org.autoplot.dom.Application;
 public class ServletInfo extends HttpServlet {
 
     private static final long birthMilli= System.currentTimeMillis();
-    public static final String version = "v20200529.0939";
+    public static final String version = "v20200719.0851";
     
     public static long getAgeMillis() {
         return System.currentTimeMillis() - birthMilli;
