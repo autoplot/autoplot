a7d36ffbbdd19388b67dd8b66af67ff8286dfedf
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2020-08-08 13:39:37 +0000

    improve feedback on HAPI when requested time is out of range
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@25019 0c594bb0-8543-0410-a25e-d8ec440261db

--- AutoplotServlet/src/java/org/autoplot/servlet/ServletInfo.java
+++ AutoplotServlet/src/java/org/autoplot/servlet/ServletInfo.java
@@ -22,7 +22,7 @@ import org.autoplot.dom.Application;
 public class ServletInfo extends HttpServlet {
 
     private static final long birthMilli= System.currentTimeMillis();
-    public static final String version = "v20200808.0741";
+    public static final String version = "v20200808.0839";
     
     public static long getAgeMillis() {
         return System.currentTimeMillis() - birthMilli;
