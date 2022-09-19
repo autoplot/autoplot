ee69efa50cdbedb3311d98db78fb91d21b3513e0
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2022-09-19 22:20:28 +0000

    correction where regex was handled improperly
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@26743 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/org/autoplot/pngwalk/WalkUtil.java
+++ Autoplot/src/org/autoplot/pngwalk/WalkUtil.java
@@ -120,9 +120,7 @@ public class WalkUtil {
         FileSystem fs = FileSystem.create( surls );
         String spec= sansArgs.substring(i+1);
 
-        spec= spec.replaceAll("\\*", ".*"); //GRR.  What if I put .* in there knowing it was a regex.
-        spec= spec.replaceAll("\\?", ".");
-        spec= spec.replaceAll("\\$x", ".*");
+        spec= spec.replaceAll("\\?", "*");
         
         FileStorageModel fsm=null;
         if ( TimeParser.isSpec(spec) ) fsm= FileStorageModel.create( fs, spec );
