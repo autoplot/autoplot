8384a3754267f2798684c8800b8ebbef2d2e95b8
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2022-11-20 14:19:02 +0000

    test013 showed NullPointerException when I didn't check for null in dims (non-qube).
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@26798 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/org/autoplot/AutoplotUtil.java
+++ Autoplot/src/org/autoplot/AutoplotUtil.java
@@ -1264,7 +1264,7 @@ public class AutoplotUtil {
                 int[] dims= DataSetUtil.qubeDims(fillds);
                 if ( dep1==null && fillds.rank()==2 && fillds.length()>3 && fillds.length(0)<4 ) { // Vector quantities without labels. [3x3] is a left a matrix.
                     spec = RenderType.series;
-                } else if ( fillds.rank()==2 && dims[0]==2 && dims[1]==2 ) {
+                } else if ( fillds.rank()==2 && dims!=null && dims[0]==2 && dims[1]==2 ) {
                     spec = RenderType.bounds;
                 } else if ( fillds.rank()==3 && dims!=null && dims[1]==2 && dims[2]==2 ) {
                     spec = RenderType.bounds;
