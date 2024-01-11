7a5fb300916440356244d10202258ab935764cbe
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2024-01-11 00:38:29 +0000

    FilePointer now has units on the offset, since the offset can be in bytes
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@27501 0c594bb0-8543-0410-a25e-d8ec440261db

--- PDSDataSource/src/org/autoplot/pds/FilePointer.java
+++ PDSDataSource/src/org/autoplot/pds/FilePointer.java
@@ -3,8 +3,6 @@ package org.autoplot.pds;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,7 +17,7 @@ public class FilePointer {
     
     public FilePointer( URL url, int line ) {
         this.url= url;
-        this.line= line;
+        this.offset= line;
     }
     
     /**
@@ -30,15 +28,20 @@ public class FilePointer {
      */
     public FilePointer( URL labelFile, String f) {
         try {
-            Pattern p= Pattern.compile("([^,]+)(,\\s*(\\d+))?");
+            Pattern p= Pattern.compile("([^,]+)(,\\s*(\\d+)(\\s*\\<BYTES\\>)?)?");
             Matcher m= p.matcher(f);
             if ( !m.matches() ) {
-                throw new IllegalArgumentException("LABEL/POINTER should match ([^,]+)(,\\s*(\\d+))?");
+                throw new IllegalArgumentException("LABEL/POINTER should match ([^,]+)(,\\s*(\\d+)(\\s*\\<BYTES\\>)?)?");
             }
             URL url= new URL(labelFile,m.group(1));
             this.url= url;
             if ( m.group(3)!=null ) {
-                line= Integer.parseInt(m.group(3));
+                if ( m.group(4)!=null && m.group(4).endsWith("<BYTES>") ) {
+                    offsetUnits= Unit.BYTES;
+                } else {
+                    offsetUnits= Unit.LINES;
+                }
+                offset= Integer.parseInt(m.group(3));
             }
         } catch (MalformedURLException ex) {
             throw new IllegalArgumentException(ex);
@@ -46,7 +49,7 @@ public class FilePointer {
     }
     
     public String toString() {
-        return this.url.toString() + ( this.line!=1 ? ", " + this.line : "" ); 
+        return this.url.toString() + ( this.offset!=1 ? ", " + this.offset : "" ); 
     }
     private URL url;
 
@@ -58,17 +61,32 @@ public class FilePointer {
         this.url = url;
     }
 
-    private int line;
+    private int offset;
 
     public static final String PROP_LINE = "line";
 
     public int getOffset() {
-        return line;
+        return offset;
     }
 
     public void setOffset(int offset) {
-        this.line = offset;
+        this.offset = offset;
     }
     
+    public enum Unit {
+        BYTES,
+        LINES
+    }
+    
+    private Unit offsetUnits = Unit.LINES;
+
+    public Unit getOffsetUnits() {
+        return offsetUnits;
+    }
+
+    public void setOffsetUnits(Unit offsetUnits) {
+        this.offsetUnits = offsetUnits;
+    }
+
 
 }
