e83ea53c3a96c72b5d10c35ae3c25bc77f8228db
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2024-05-12 13:23:44 +0000

    Still can't get the return code out to jenkins...
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@27647 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/org/autoplot/AppManager.java
+++ Autoplot/src/org/autoplot/AppManager.java
@@ -143,7 +143,7 @@ public class AppManager {
      */
     public void quit( int status ) {
         if ( this.allowExit ) {
-            System.err.println("about to exit");
+            System.err.println("about to exit with status "+status);
             System.exit(status); //TODO: findbugs DM_EXIT--and I wonder what happens when Autoplot is used on a Tomcat web server?  Otherwise this is appropriate for swing apps.
             System.err.println("should not get here");
         }
