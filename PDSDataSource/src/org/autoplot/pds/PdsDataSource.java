17d61d74d1f91f30157c11e9e56be1df4e48b65b
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2024-09-25 22:00:39 +0000

    use convertDocumentToMap which gets the name=value pairs right
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@27772 0c594bb0-8543-0410-a25e-d8ec440261db

--- PDSDataSource/src/org/autoplot/pds/PdsDataSource.java
+++ PDSDataSource/src/org/autoplot/pds/PdsDataSource.java
@@ -610,13 +610,36 @@ public class PdsDataSource extends AbstractDataSource {
         return result;
     }
     
+    protected static boolean isLeaf( Node node ) {
+        return node.getChildNodes().getLength()==1 && node.getFirstChild().getNodeType()==Node.TEXT_NODE;
+    }
+    
+    protected static Map<String, Object> convertDocumentToMap( Node root ) {
+        Map<String, Object> resultMap = new HashMap<>();
+        NodeList nodeList = root.getChildNodes();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node node = nodeList.item(i);
+            String key = node.getNodeName();
+            if ( isLeaf(node) ) {
+                Object value = node.getTextContent(); // or another method to extract the value
+                resultMap.put(key, value);
+            } else if ( node.getNodeType() == Node.ELEMENT_NODE ) {
+                Map<String,Object> subNode= convertDocumentToMap( node );
+                resultMap.put(key, subNode);
+            }
+        }
+
+        return resultMap;
+    }
+    
     @Override
     public Map<String, Object> getMetadata(ProgressMonitor mon) throws Exception {
         URISplit split= URISplit.parse( getURI() );
             
         File xmlfile = DataSetURI.getFile( split.resourceUri.toURL() , mon );
         Document doc= readXML(xmlfile);
-        return MetadataUtil.toMetaTree(doc);
+        return convertDocumentToMap(doc);
         
     }
     
