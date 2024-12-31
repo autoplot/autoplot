2d9a9a26e06694a64a50ddc1200cb180cec52084
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2024-12-31 15:19:14 +0000

    bugfix: Odl parser (used for .sts files) rejects when parameter is missing.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@28027 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/autoplot/ascii/OdlDataSourceFactory.java
+++ DataSourcePack/src/org/autoplot/ascii/OdlDataSourceFactory.java
@@ -4,15 +4,21 @@ package org.autoplot.ascii;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.autoplot.datasource.AbstractDataSourceFactory;
 import org.autoplot.datasource.CompletionContext;
 import org.autoplot.datasource.DataSetURI;
 import org.autoplot.datasource.DataSource;
+import org.autoplot.datasource.URISplit;
 import org.das2.qds.util.OdlParser;
 import org.das2.util.monitor.ProgressMonitor;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -51,5 +57,45 @@ public class OdlDataSourceFactory  extends AbstractDataSourceFactory {
             return super.getCompletions( cc, mon );
         }
     }
+
+    @Override
+    public boolean reject(String suri, List<String> problems, ProgressMonitor mon) {
+        URISplit split = URISplit.parse(suri);
+
+        Map<String, String> params = URISplit.parseParams(split.params);
+            
+        String arg_0= params.get("arg_0");
+        
+        if ( split.resourceUri==null ) {
+            return true;
+        }
+        
+        try {
+            File ff= DataSetURI.getFile( split.resourceUri, mon );
+            
+            JSONObject record = new JSONObject();
+            BufferedReader reader = new BufferedReader(new FileReader(ff));
+            
+            String ss = OdlParser.readOdl(reader,record);
+        
+            String[] nn= OdlParser.getNames( record, "", true, null);
+            
+            if ( arg_0==null ) {
+                return nn.length>2; // allow X,Y
+            }
+            
+            for ( String s: nn ) {
+                if ( s.equals(arg_0) ) {
+                    return false;
+                }
+            }
+            return true;
+            
+        } catch ( IOException | JSONException ex ) {
+            return true;
+        }
+    }
+    
+    
 
 }
