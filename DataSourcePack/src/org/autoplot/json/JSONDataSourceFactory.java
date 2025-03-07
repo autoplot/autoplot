3bc114ce615913eeb527ee88c98baf695dbfad39
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2025-03-07 23:44:00 +0000

    add depend0= keyword, since often you want to read in the independent variable as well.
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@28139 0c594bb0-8543-0410-a25e-d8ec440261db

--- DataSourcePack/src/org/autoplot/json/JSONDataSourceFactory.java
+++ DataSourcePack/src/org/autoplot/json/JSONDataSourceFactory.java
@@ -25,14 +25,31 @@ import org.json.JSONObject;
 public class JSONDataSourceFactory extends AbstractDataSourceFactory {
 
     private List<CompletionContext> getFieldNames( CompletionContext cc, JSONArray jarray, List<CompletionContext> result ) throws JSONException {
-        
         for ( int i=0; i<jarray.length(); i++ ) {
             String s= "field"+i;
             String val= String.valueOf( jarray.get(i) );
             result.add(new CompletionContext(
                 CompletionContext.CONTEXT_PARAMETER_VALUE,
                 s,
-                s+"("+val+")", null ) );
+                s+" ("+val+")", null ) );
+        }
+        return result;
+    }
+    
+    private List<CompletionContext> getFieldNames( CompletionContext cc, JSONObject jobj, List<CompletionContext> result ) throws JSONException {
+        Iterator<String> keys= jobj.keys();
+        while ( keys.hasNext() ) {
+            String s= keys.next();
+            String val= String.valueOf( jobj.get(s) );
+            CompletionContext cc1;
+            if ( cc.context==CompletionContext.CONTEXT_PARAMETER_NAME ) {
+                cc1=
+                    new CompletionContext( CompletionContext.CONTEXT_PARAMETER_NAME, s, this, "arg_0", s + " ("+val+")", null, true );
+            } else {
+                cc1=
+                    new CompletionContext( CompletionContext.CONTEXT_PARAMETER_VALUE, s, this, null, s + " ("+val+")", null, true );
+            }
+            result.add( cc1 );   
         }
         return result;
     }
@@ -40,30 +57,50 @@ public class JSONDataSourceFactory extends AbstractDataSourceFactory {
     @Override
     public List<CompletionContext> getCompletions(CompletionContext cc, ProgressMonitor mon) throws Exception {
         File f= DataSetURI.getFile( cc.surl, mon );
+        JSONObject jobj=null;
+        JSONArray jarray=null;
+
+        List<CompletionContext> result = new ArrayList<>();
+        
         try ( InputStream ins= new FileInputStream(f) ) {
             JSONJIterator jiter= new JSONJIterator(ins);
             if ( jiter.hasNext() ) {
                 Object obj= jiter.next();
-                List<CompletionContext> result = new ArrayList<CompletionContext>();
                 if ( obj instanceof JSONArray ) {
-                    result = getFieldNames( cc, (JSONArray)obj, result );
-                    return result;
+                    jarray= (JSONArray)obj;
                 } else if ( obj instanceof JSONObject ) {
-                    JSONObject jobj= (JSONObject)obj;
-                    Iterator<String> keys= jobj.keys();
-                    while ( keys.hasNext() ) {
-                        String s= keys.next();
-                        String val= String.valueOf( jobj.get(s) );
-                        result.add(new CompletionContext(
-                            CompletionContext.CONTEXT_PARAMETER_VALUE,
-                            s,
-                            s+"("+val+")", null ) );   
-                    }
-                    return result;
+                    jobj= (JSONObject)obj;
                 }
             }
+        } // this should close after reading on record.
+        
+        if ( cc.context==CompletionContext.CONTEXT_PARAMETER_NAME ) {
+            if ( jarray!=null ) {
+                result = getFieldNames( cc, jarray, result );
+            } else if ( jobj!=null ) {
+                result= getFieldNames( cc, jobj, result );
+            } else {
+                throw new IllegalArgumentException("Expected to see JSONArray or JSONObject in: " + cc.surl );
+            }
+            result.add(new CompletionContext(CompletionContext.CONTEXT_PARAMETER_NAME, "depend0=", "Name of the independent variable."));
+            return result;   
+        } else if ( cc.context==CompletionContext.CONTEXT_PARAMETER_VALUE ) {
+            String paramName = CompletionContext.get(CompletionContext.CONTEXT_PARAMETER_NAME, cc);
+            switch ( paramName ) {
+                case "depend0": {
+                    if ( jarray!=null ) {
+                        result = getFieldNames( cc, jarray, result );
+                    } else if ( jobj!=null ) {
+                        result= getFieldNames( cc, jobj, result );
+                    } else {
+                        throw new IllegalArgumentException("Expected to see JSONArray or JSONObject in: " + cc.surl );
+                    }
+                }
+            }
+            return result;
+        } else {
+            throw new IllegalArgumentException("not implemented l78");
         }
-        throw new IllegalArgumentException("File is empty: "+cc.surl);
     }
 
     @Override
