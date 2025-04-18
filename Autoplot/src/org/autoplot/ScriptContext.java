d9c09d03a0519bc30cffd6311478cb8f6280458d
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2025-04-18 11:46:37 +0000

    see if single quotes fix the documentation rendering bug in addMouseModule rendering
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@28201 0c594bb0-8543-0410-a25e-d8ec440261db

--- Autoplot/src/org/autoplot/ScriptContext.java
+++ Autoplot/src/org/autoplot/ScriptContext.java
@@ -401,7 +401,7 @@ public class ScriptContext extends PyJavaInstance {
      */
     public static void setApplicationModel(ApplicationModel m) {
         model = m;
-        dom= m.getDocumentModel();
+        dom= m.getDom();
     }
     
     private static AutoplotUI view = null;
@@ -938,7 +938,7 @@ public class ScriptContext extends PyJavaInstance {
      * event following the mouse release when a box is dragged out.
      *<blockquote><pre><small>{@code
 def boxLookup( evt ):
-    showMessageDialog( "<html>start: (%s,%s)<br>finish: (%s,%s)" %
+    showMessageDialog( '<html>start: (%s,%s)<br>finish: (%s,%s)' %
         ( evt.getStartX(), evt.getStartY(), 
         evt.getFinishX(), evt.getFinishY() ) )
   
