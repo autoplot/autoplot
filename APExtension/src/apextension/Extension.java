e46d99ec269982c637377db1ae6dcfd4b326a8b8
jbfaden <jbfaden@0c594bb0-8543-0410-a25e-d8ec440261db>
2025-06-03 15:24:35 +0000

    clean up docs for this project
    git-svn-id: https://svn.code.sf.net/p/autoplot/code/autoplot/trunk@28269 0c594bb0-8543-0410-a25e-d8ec440261db

--- APExtension/src/apextension/Extension.java
+++ APExtension/src/apextension/Extension.java
@@ -1,18 +1,17 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package apextension;
 
 /**
- * Demo of extension to Autoplot.  Note an extension will have restricted security, so do not attempt to load and save files.
+ * Demo of extension to Autoplot.  Note an extension will have restricted security, so do not attempt 
+ * to load and save files.
  *
- * create a jar file from this project, and load it like so:
+ * Create a jar file from this project (APExtenson), and load it like so:
+ *<blockquote><pre><small>{@code
  * import sys
  * sys.path.append("/home/jbf/APExtension.jar")
  * from apextension import Extension
  * print Extension.total( 3,4 )
+ *}</small></pre></blockquote>
  * 
  * @author jbf
  */
