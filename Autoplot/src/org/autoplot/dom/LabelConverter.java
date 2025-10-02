466bfc0df28433dbf7c72b4229fb610a4aecbae1
Jeremy Faden <faden@cottagesystems.com>
2025-10-02 12:29:06 -0500

    remove code which describes itself as "crazy kludge" and I can't imagine where it would be used.

--- Autoplot/src/org/autoplot/dom/LabelConverter.java
+++ Autoplot/src/org/autoplot/dom/LabelConverter.java
@@ -141,13 +141,13 @@ public class LabelConverter extends Converter {
                     }
                     QDataSet dataSet= pe.getController().getDataSet();
                     if ( dataSet!=null ) {
-                        if ( plot!=null && plot.getXaxis()==axis ) {  // crazy kludge, sure to cause problems.  This assumes that DEPEND_0 is the dataset causing the variation in X.
-                            logger.finer("getting the CONTEXT property from DEPEND_0");
-                            QDataSet d= (QDataSet) dataSet.property(QDataSet.DEPEND_0);
-                            if ( d!=null && SemanticOps.getUnits(d).isConvertibleTo(axis.getRange().getUnits()) ) {
-                                dataSet= d;
-                            }
-                        }
+//                        if ( plot!=null && plot.getXaxis()==axis ) {  // crazy kludge, sure to cause problems.  This assumes that DEPEND_0 is the dataset causing the variation in X.
+//                            logger.finer("getting the CONTEXT property from DEPEND_0");
+//                            QDataSet d= (QDataSet) dataSet.property(QDataSet.DEPEND_0);
+//                            if ( d!=null && SemanticOps.getUnits(d).isConvertibleTo(axis.getRange().getUnits()) ) {
+//                                dataSet= d;
+//                            }
+//                        }
                         logger.log(Level.FINEST, "bug1814: context substitution success. {0}", Thread.currentThread().getName());
                         title= insertUnformattedData( title, "CONTEXT", dataSet ); 
                     } else {
