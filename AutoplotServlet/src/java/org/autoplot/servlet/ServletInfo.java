0d622d9fdb5de9a881cd8e1af086fd887b4209c2
Jeremy Faden <faden@cottagesystems.com>
2025-10-28 08:23:20 -0500

    tweak version

--- AutoplotServlet/src/java/org/autoplot/servlet/ServletInfo.java
+++ AutoplotServlet/src/java/org/autoplot/servlet/ServletInfo.java
@@ -22,7 +22,7 @@ import org.autoplot.dom.Application;
 public class ServletInfo extends HttpServlet {
 
     private static final long birthMilli= System.currentTimeMillis();
-    public static final String version = "v20250327.1141";
+    public static final String version = "v20251028.0823";
     
     public static long getAgeMillis() {
         return System.currentTimeMillis() - birthMilli;
