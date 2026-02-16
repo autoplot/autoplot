a06a5f3268e31b882671151b7f0f0b3b6a822c3f
Jeremy Faden <faden@cottagesystems.com>
2026-02-16 10:36:20 -0600

    restore org/autoplot/scriptconsole/ScriptPanelSupport.java

--- Autoplot/compile-application.sh
+++ Autoplot/compile-application.sh
@@ -328,6 +328,7 @@ compilef 'external/AuralizationHandler.java'
 compilef 'org/das2/util/filesystem/GitCommand.java'
 compilef 'org/das2/graph/util/GraphicalLogHandler.java'
 compilef 'org/autoplot/util/CsvFileLogHandler.java'
+compilef 'org/autoplot/scriptconsole/ScriptPanelSupport.java' # some git scripts are still using this.
 
 cat ../temp-volatile-classes/META-INF/org.autoplot.datasource.DataSourceFactory.extensions | cut -d' ' -f1
 for i in `cat ../temp-volatile-classes/META-INF/org.autoplot.datasource.DataSourceFactory.extensions | cut -d' ' -f1 | sed 's/\./\//g'`; do
