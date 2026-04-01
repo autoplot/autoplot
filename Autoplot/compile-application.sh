4020cda8ddaa1ae716ca62bc48538cf2ade2bf3f
jbf <jeremy-faden@uiowa.edu>
2026-04-01 14:11:44 -0500

    add NullDragRenderer, a base class for drawing mouse feedback

--- Autoplot/compile-application.sh
+++ Autoplot/compile-application.sh
@@ -330,6 +330,7 @@ compilef 'org/das2/graph/util/GraphicalLogHandler.java'
 compilef 'org/das2/util/CsvFileLogHandler.java'
 compilef 'org/autoplot/scriptconsole/ScriptPanelSupport.java' # some git scripts are still using this.
 compilef 'org/autoplot/batch/RunBatchTool.java'
+compilef 'org/das2/event/NullDragRenderer.java'
 
 cat ../temp-volatile-classes/META-INF/org.autoplot.datasource.DataSourceFactory.extensions | cut -d' ' -f1
 for i in `cat ../temp-volatile-classes/META-INF/org.autoplot.datasource.DataSourceFactory.extensions | cut -d' ' -f1 | sed 's/\./\//g'`; do
